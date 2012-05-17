6ab98f78de7765248ff1e120586e0ed447e8cc6b
brett lockspeiser <blocks@gmail.com>
2012-05-17 15:46:34 -0700

    use html for password reset emails

--- sefaria/forms.py
+++ sefaria/forms.py
@@ -2,6 +2,9 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from emailusernames.forms import EmailUserCreationForm
+from django.contrib.auth.forms import *
+
+
  
 
 class NewUserForm(EmailUserCreationForm):
@@ -24,3 +27,35 @@ class NewUserForm(EmailUserCreationForm):
         if commit:
             user.save()
         return user
+
+
+class HTMLPasswordResetForm(PasswordResetForm):
+    def save(self, domain_override=None, email_template_name='registration/password_reset_email.html', subject_template_name='registration/pass_reset_subject.txt',
+             use_https=False, token_generator=default_token_generator, from_email=None, request=None):
+        """
+        Generates a one-use only link for resetting password and sends to the user
+        """
+        from django.core.mail import EmailMessage
+        for user in self.users_cache:
+            if not domain_override:
+                current_site = get_current_site(request)
+                site_name = current_site.name
+                domain = current_site.domain
+            else:
+                site_name = domain = domain_override
+            c = {
+                'email': user.email,
+                'domain': domain,
+                'site_name': site_name,
+                'uid': int_to_base36(user.id),
+                'user': user,
+                'token': token_generator.make_token(user),
+                'protocol': use_https and 'https' or 'http',
+            }
+            subject = loader.render_to_string(subject_template_name, c)
+            # Email subject *must not* contain newlines
+            subject = ''.join(subject.splitlines())
+            email = loader.render_to_string(email_template_name, c)
+            msg = EmailMessage(subject, email, from_email, [user.email])
+            msg.content_subtype = "html"
+            msg.send()
