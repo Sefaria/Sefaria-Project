ed5423668044cb4cfb02da93c64817c7926f407b
brett lockspeiser <blocks@gmail.com>
2012-05-23 22:59:26 -0700

    util.js

---
+++ static/js/util.js
@@ -0,0 +1,232 @@
+var sjs = sjs || {};
+
+sjs.cache = {
+// Caching of texts
+	get: function(ref) {
+		var pRef = parseRef(ref);
+		var normRef = makeRef(pRef);
+
+		if (normRef in this._cache) {
+			var data = clone(this._cache[normRef]);
+			
+			if (!("remake" in data))
+				return data;
+		
+			// If the ref has more than 1 section listed, try trimming the last section
+			var normRef = normRef.replace(/:/g, ".").slice(0, normRef.lastIndexOf("."));
+
+			var data = clone(this._cache[normRef]);
+			var lastSection = parseInt(pRef.sections[pRef.sections.length -1]);
+			var lastToSection = parseInt(pRef.toSections[pRef.toSections.length -1]);
+			
+			data.sections.push(lastSection);
+			data.toSections.push(lastToSection);
+			data.ref = ref;
+			
+			return data;
+		}
+
+		return false;
+	},
+	
+	save: function(origData) {
+		var data = {};
+		$.extend(data, origData);
+		
+		// Store data for book name alone (eg "Genesis") immediatley
+		// normalizing below will render this "Genesis.1" which we also store
+		if (data.ref.indexOf(".") == -1) {
+			this._cache[data.ref] = data;
+		}
+		
+		// Trim the data to "chapter" level
+		if (data.sections.length == data.sectionNames.length) {
+			data.sections = data.sections.slice(0, data.sections.length - 1);
+		}
+		if (data.toSections.length == data.sectionNames.length) {
+			data.toSections = data.toSections.slice(0, data.toSections.length - 1);
+		}
+		
+		var ref = makeRef(data);
+		this._cache[ref] = data;
+	
+		
+		// Leave links for each lower level (e.g. "verse") request
+		for (var i = 1; i <= Math.max(data.text.length, data.he.length); i++)
+			this._cache[ref+"."+i] = {"remake": 1};	
+	},
+	
+	kill: function(ref) {
+		ref = makeRef(parseRef(ref));
+		if (ref in this._cache) delete this._cache[ref];
+		else if (ref.indexOf(".") != ref.lastIndexOf(".")) {
+			ref = ref.slice(0, ref.lastIndexOf("."));
+			delete this._cache[ref];
+		}
+	},
+	killAll: function() {
+		this._cache = {};
+	},
+	_cache: {}
+}
+
+function prefetch(ref) {
+	// grab a text from the server and put it in the cache
+	if (!ref) return;
+	
+	ref = makeRef(parseRef(ref));
+	if (sjs.cache.get(ref)) return;	
+
+	$.getJSON("/texts/" + ref, function(data) {
+		if (data.error) return;
+		sjs.cache.save(data);
+	})
+}
+
+function parseRef(q) {
+	var response = {book: false, 
+					sections: [],
+					toSections: [],
+					ref: ""};
+					
+	if (!q) return response;
+	
+	var q = q.replace(/[.:]/g, " ").replace(/ +/, " ");
+	var toSplit = q.split("-");
+	var p = toSplit[0].split(" ");
+	
+	for (i = 0; i < p.length; i++) {
+		if (p[i].match(/\d+[ab]?/)) {
+			boundary = i;
+			break;
+		}
+	}
+	
+	words = p.slice(0,i);
+	nums = p.slice(i);
+	
+	response.book = words.join("_");
+	response.sections = nums.slice();
+	response.toSections = nums.slice();
+	response.ref = q;
+	
+	// Parse range end (if any)
+	if (toSplit.length == 2) {
+		var toSections = toSplit[1].replace(/[.:]/g, " ").split(" ");
+		
+		var diff = response.sections.length - toSections.length;
+		
+		for (var i = diff; i < toSections.length + diff; i++) {
+			response.toSections[i] = toSections[i-diff];
+		}
+	}
+	
+	return response;
+}
+
+function makeRef(q) {
+	var ref = q.book.replace(/ /g, "_");
+
+	if (q.sections.length)
+		ref += "." + q.sections.join(".");
+	
+	if (!q.sections.compare(q.toSections)) {
+		
+		for (var i = 0; i < q.toSections.length; i ++)
+			if (q.sections[i] != q.toSections[i]) break;
+		ref += "-" + q.toSections.slice(i).join(".");
+	}
+	
+	return ref;
+}
+
+function wrapRefLinks(text) {
+	
+	text = text || "";
+	
+	var refReStr = "(" + sjs.books.join("|") + ") (\\d+[ab]?)(:(\\d+)([\\-–]\\d+(:\\d+)?)?)?";
+	var refRe = new RegExp(refReStr, "g");
+	try {
+		var refText = text.replace(refRe, '<span class="refLink" data-ref="$1.$2$3">$1 $2$3</span>');
+	} catch (TypeError) {
+		// this catches an error caused by some bad data
+		var refText = "Error: bad data";
+	}
+	return refText;
+	
+}
+
+
+function isTouchDevice() {  
+	return "ontouchstart" in document.documentElement;
+}
+
+function isInt(x) {
+		var y=parseInt(x);
+		if (isNaN(y)) return false;
+		return x==y && x.toString()==y.toString();
+	}
+
+function isArray(a) {
+	return ( Object.prototype.toString.call( a ) === '[object Array]' );
+}
+
+function clone(obj) {
+    // Handle the 3 simple types, and null or undefined
+    if (null == obj || "object" != typeof obj) return obj;
+
+    // Handle Date
+    if (obj instanceof Date) {
+        var copy = new Date();
+        copy.setTime(obj.getTime());
+        return copy;
+    }
+
+    // Handle Array
+    if (obj instanceof Array) {
+        var copy = [];
+        var len = obj.length;
+        for (var i = 0; i < len; ++i) {
+            copy[i] = clone(obj[i]);
+        }
+        return copy;
+    }
+
+    // Handle Object
+    if (obj instanceof Object) {
+        var copy = {};
+        for (var attr in obj) {
+            if (obj.hasOwnProperty(attr)) copy[attr] = clone(obj[attr]);
+        }
+        return copy;
+    }
+
+    throw new Error("Unable to copy obj! Its type isn't supported.");
+}
+
+Array.prototype.compare = function(testArr) {
+    if (this.length != testArr.length) return false;
+    for (var i = 0; i < testArr.length; i++) {
+        if (this[i].compare) { 
+            if (!this[i].compare(testArr[i])) return false;
+        }
+        if (this[i] !== testArr[i]) return false;
+    }
+    return true;
+}
+
+Array.prototype.pad =
+  function(s,v) {
+    var l = Math.abs(s) - this.length;
+    var a = [].concat(this);
+    if (l <= 0)
+      return a;
+    for(var i=0; i<l; i++)
+      s < 0 ? a.unshift(v) : a.push(v);
+    return a;
+};
+
+if(typeof(console) === 'undefined') {
+    var console = {}
+    console.log = function() {};
+}
