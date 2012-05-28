ef0ae9721233fa029d70a69f8d93320f452950d3
brett lockspeiser <blocks@gmail.com>
2012-05-28 19:55:32 -0400

    Begin revamp of sheets add source

--- static/js/util.js
+++ static/js/util.js
@@ -164,6 +164,309 @@ function wrapRefLinks(text) {
 }
 
 
+
+function checkRef($input, $msg, $ok, level, success, commentatorOnly) {
+	
+	/* check the user inputed text ref
+	   give fedback to make it corret to a certain level of specificity 
+	   talk to the server when needed to find section names
+		* level -- how deep the ref should go - (0: to the end, 1: one level above)
+		* success -- a function to call when a valid ref has been found
+		* commentatorOnly --- whether to stop at only a commentatory name
+	*/
+	
+	// sort books by length so longest matches first in regex
+	var sortedBooks = sjs.books.sort(function(a,b){
+		if (a.length == b.length) return 0;
+		return (a.length < b.length ? 1 : -1); 
+	})
+	
+	var booksReStr = "^(" + sortedBooks.join("\\b|") + ")";
+	var booksRe = new RegExp(booksReStr, "i");
+	var baseTests = [{test: /^/,
+					  msg: "Enter a text or commentator name",
+					  action: "pass"},
+					 {test: /^$/,
+					  msg: "Enter a text or commentator name",
+					  action: "reset"},
+					 {test: /^.{3,}/,
+					  msg: "Unkown text. Would you like to add it?",
+					  action: "allow"},
+					 {test: booksRe,
+					  msg: "...",
+					  action: "getBook"}];
+	
+	
+	// An array of objects with properites 'test', 'msg', 'action' which are tested with each change
+	// Test are tried backwards from the last. If 'test' matched, then 'msg' is displayed to the user
+	// and 'action' is carried out (according to the switch in this code).
+	sjs.ref.tests = sjs.ref.tests || baseTests;
+	var tests = sjs.ref.tests;
+	
+	var ref = $input.val();
+	console.log("Seeing: " + ref);
+	console.log("Test Queue:");
+	console.log(sjs.ref.tests)
+	// Walk backwards through all tests, give the message and do the action of first match
+	for (var i = tests.length -1;  i > -1;  i--) {
+		if (ref.match(tests[i].test)) {
+			action = tests[i].action;
+			$msg.html("<i>"+tests[i].msg+"</i>");
+			break;
+		}
+	}
+	$msg.removeClass("he");
+	
+	console.log("Action: " + action);
+	switch(action){
+	
+		// Go back to square 1
+		case("reset"):
+			sjs.ref.tests = baseTests;
+			sjs.ref.index = {};
+			sjs.editing.index = null;
+			$input.val("");
+			$ok.addClass("inactive");
+			$("#addSourceTextControls .btn").addClass("inactive");
+			$("#addSourceCancel").removeClass("inactive");
+			break;
+		
+		// Don't change anything	
+		case("pass"):
+			sjs.editing.index = null;
+			$ok.addClass("inactive");
+			// this reaches in to logic specific to add source
+			$("#addSourceTextControls .btn").addClass("inactive");
+			$("#addSourceCancel").removeClass("inactive")
+			break;
+
+		// The current value of the ref is acceptable, allow is to be accepted
+		// Don't understand this anymore -- is it only to allow New (unknown) Texts?
+		case("allow"):
+			$ok.removeClass("inactive").text("Add Text");
+			// also reaches into specific logic
+			$("#addSourceTextControls .btn").addClass("inactive");
+			$("#addSourceCancel").removeClass("inactive")
+			break;
+
+		// When a commentator's name is entered, insert a text reference, 
+		// e.g., "Rashi" -> "Rashi on Genesis 2:5"
+		case("insertRef"):
+			$input.val($input.val() + " on " + sjs.add.source.ref)
+				.autocomplete("close");
+			checkRef($input, $msg, $ok, level, success, commentatorOnly);
+			break;
+		
+		// Get information about an entered book (e.g., "Genesis", "Rashi", "Brachot") from server
+		// then add appropriate tests and prompts	
+		case("getBook"):
+			match = ref.match(booksRe);
+			if (!match) return;
+			else ref = match[0];
+			// Don't look up info we already have
+			if (sjs.ref.index && sjs.ref.index.title == ref) break;
+			
+			$.getJSON("/index/" + ref, function(data){
+				if ("error" in data) {
+					$msg.html(data.error);
+					$ok.addClass("inactive");
+
+				} else {
+					sjs.ref.index = data;
+					$ok.addClass("inactive");
+
+					if (data.categories[0] == "Commentary") {
+						$input.val(data.title);
+						if (commentatorOnly) {
+							sjs.ref.tests.push(
+								{test: new RegExp("^" + data.title + "$"), 
+								 msg: "", 
+								 action: "insertRef"});
+							sjs.ref.tests.push(
+								{test: new RegExp("^" + data.title + " on " + sjs.add.source.ref + "$"), 
+								 msg: "", 
+								 action: "ok"});
+							
+						} else {
+							$input.val(data.title + " on ");
+							var commentatorRe = new RegExp("^" + data.title)
+							sjs.ref.tests.push(
+								{test: commentatorRe, 
+								 msg: "Enter a <b>Text</b> that " + data.title + " comments on", 
+								 action: "pass"});
+							
+							var commentaryReStr = "^" + data.title + " on (" + sjs.books.join("|") + ")$";
+							var commentaryRe = new RegExp(commentaryReStr, "i");
+							sjs.ref.tests.push(
+								{test: commentaryRe,
+								 msg: "...",
+								 action: "getCommentaryBook"});
+					
+						}
+					} else if (data.categories[0] == "Talmud") {
+						$input.val(data.title)
+							.autocomplete("close");
+						
+						sjs.ref.tests.push(
+							{test: RegExp("^" + data.title),
+							 msg: "Enter a <b>Daf</b> of Tractate " + data.title + " to add, e.g. " +
+							 	data.title + " 4b",
+							 action: "pass"});
+						if (level == 1) {
+							sjs.ref.tests.push(
+								{test:  RegExp("^" + data.title + " \\d+[ab]$", "i"),
+								 msg: "OK. Click <b>add</b> to continue.",
+								 action: "ok"});
+						} else {
+							sjs.ref.tests.push(
+								{test:  RegExp("^" + data.title + " \\d+[ab]", "i"),
+								 msg: "Enter a starting <b>line number</b>, e.g. " + 
+								 	data.title + " 4b:1",
+								 action: "pass"});
+							sjs.ref.tests.push(
+								{test:  RegExp("^" + data.title + " \\d+[ab][ .:]\\d+", "i"),
+								 msg: "Enter an ending <b>line number</b>, e.g. " +
+								 	data.title + " 4b:1-5",
+								 action: "pass"});	
+							sjs.ref.tests.push(
+								{test:  RegExp("^" + data.title + " \\d+[ab][ .:]\\d+-\\d+$", "i"),
+								 msg: "",
+								 action: "ok"});
+						}
+						
+
+					
+					} else {
+						$input.val(data.title + " ")
+							.autocomplete("close");
+						var bookRe = new RegExp("^" + data.title + " ?$");
+						sjs.ref.tests.push(
+									{test: bookRe,
+									 msg: "Enter a <b>" + data.sectionNames[0] + "</b> of " + data.title + 
+									 	" to add, e.g., " + data.title + " 5",
+									 action: "pass"});
+						
+						var reStr = "^" + data.title + " \\d+"
+						for (var i = 0; i < data.sectionNames.length - level - 1; i++) {
+							sjs.ref.tests.push(
+									{test: RegExp(reStr),
+									msg: "Enter a <b>" + data.sectionNames[i+1] + "</b> of " + data.title + 
+										" to add, e.g., " + data.title + " 5:7",
+									action: "pass"});
+							reStr += "[ .:]\\d+";
+						}
+
+						sjs.ref.tests.push(
+							{test: RegExp(reStr + "$"),
+							 msg: "OK. Click <b>add</b> to continue.",
+							 action: "ok"});
+							 
+						sjs.ref.tests.push(
+							{test: RegExp(reStr + "-"),
+							 msg: "Enter an ending <b>" + data.sectionNames[i] + "</b>",
+							 action: "pass"});
+							 
+						sjs.ref.tests.push(
+							{test: RegExp(reStr + "-\\d+$"),
+							 msg: "OK. Click <b>add</b> to continue.",
+							 action: "ok"});
+						
+					}
+					
+					checkRef($input, $msg, $ok, level, success, commentatorOnly);
+				}	
+			});
+			break;
+		
+		// get information about a book entered as the object of a commentator 
+		//(e.g. the "Genesis" in "Rashi on Genesis")		
+		case("getCommentaryBook"):
+		console.log("gcb")
+		console.log("ref: " + ref)
+			
+			// reset stored title to commentator name only
+			sjs.ref.index.title = ref.slice(0, ref.indexOf(" on "));
+			var book = ref.slice((sjs.ref.index.title + " on ").length) 
+			
+			$.getJSON("/index/" + book, function(data){
+				if ("error" in data) {
+					$msg.html(data.error);
+				} else {
+					sjs.ref.index.title += " on " + data.title;
+					data.sectionNames.push("Comment");
+					sjs.ref.index.sectionNames = data.sectionNames;
+					$ok.addClass("inactive");
+
+					if (data.categories[0] == "Commentary") {
+						var commentaryRe = new RegExp(sjs.ref.index.title, "i");
+						sjs.ref.tests.push(
+							{test: commentaryRe,
+							 msg: "No commentary on commentary action.",
+							 action: "pass"});
+					
+					} else if (data.categories[0] == "Talmud") {
+						$input.val(sjs.ref.index.title);
+						var tractateRe = new RegExp("^" + sjs.ref.index.title);
+						sjs.ref.tests.push(
+							{test: tractateRe,
+							 msg: "Enter a Daf of tractate " + data.title + ", e.g, " +
+							 	data.title + " 4b",
+							 action: "pass"});
+						
+						var talmudReStr = "^" + sjs.ref.index.title + " \\d+[ab]$";
+						var talmudRe = new RegExp(talmudReStr, "i");
+						sjs.ref.tests.push(
+							{test: talmudRe,
+							 msg: "OK. Click <b>add</b> to conitnue.",
+							 action: "ok"});
+					
+					} else {
+						$input.val(sjs.ref.index.title);
+						var bookRe = new RegExp("^" + sjs.ref.index.title);
+						sjs.ref.tests.push(
+							{test: bookRe,
+							 msg: "Enter a " + data.sectionNames[0] + " of " + data.title,
+							 action: "pass"});
+						
+						var reStr = "^" + sjs.ref.index.title + " \\d+";
+						
+						console.log("level: " + level);
+						console.log("length: " + data.sectionNames.length);
+
+						// Cycle through sections, add tests and msg for each
+						for (var i = 1; i < data.sectionNames.length - level; i++) {
+							var re = new RegExp(reStr)
+							sjs.ref.tests.push(
+								{test: re,
+								 msg: "Enter a " + data.sectionNames[i] + " of " + data.title,
+								 action: "pass"});
+							reStr += "[ .:]\\d+";
+						}
+						reStr += "$"
+						var re = new RegExp(reStr);
+						sjs.ref.tests.push(
+							{test: re,
+							 msg: "OK. Click <b>add</b> to conitnue.",
+							 action: "ok"});				
+					
+					}
+					
+					checkRef($input, $msg, $ok, level, success, false);
+				}
+			});
+			break;
+			
+		case("ok"):
+			sjs.editing.index = sjs.ref.index;
+			$ok.removeClass("inactive");
+			success();
+			break;
+	}
+		
+}	
+
+
+
 function isTouchDevice() {  
 	return "ontouchstart" in document.documentElement;
 }
