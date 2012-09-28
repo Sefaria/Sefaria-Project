b9c8dc6386924b7f820aebf0bbadd265ac831879
brett lockspeiser <blocks@gmail.com>
2012-09-28 13:39:20 -0700

    playing a little with tests

--- reader/tests.py
+++ reader/tests.py
@@ -1,16 +1,16 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
+from django.test.client import Client
+import selenium
+
+class GetTest(TestCase):
+	def setUp(self):
+		pass
+		
+	def test_get_text(self):
+		response = self.client.get('/api/texts/Genesis.1')
+		self.assertEqual(200, response.status_code)
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+	def test_login(self):
+		response = self.client.post('/login/', {'username': 'john', 'password': 'smith'})
+		self.assertEqual(200, response.status_code)
