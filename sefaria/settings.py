2aed83f4104e0202114b7717c2a4b5a5f9f2529e
brett lockspeiser <blocks@gmail.com>
2013-02-06 15:56:14 -0800

    HTML logo

--- sefaria/settings.py
+++ sefaria/settings.py
@@ -88,7 +88,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'sefaria.profile.ProfileMiddleware',
+    # 'sefaria.profile.ProfileMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
