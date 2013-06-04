a90956de6e777a6c8ee1c02fb7d27b1b181d8644
brett lockspeiser <blocks@gmail.com>
2013-06-04 00:10:14 -0400

    Change home tagline, callout mishnah campaign, show stats

--- reader/views.py
+++ reader/views.py
@@ -446,15 +446,19 @@ def splash(request):
 	#daf_today = daf_yomi(datetime.now())
 	#daf_tomorrow = daf_yomi(datetime.now() + timedelta(1))
 
-	connected_texts = db.texts_by_multiplied_connections.find().sort("count", -1).limit(13)
+	connected_texts = db.texts_by_multiplied_connections.find().sort("count", -1).limit(9)
 	connected_texts = [t["_id"] for t in connected_texts ]
-	active_texts = db.texts_by_activity_30.find().sort("value", -1).limit(13)
+	active_texts = db.texts_by_activity_30.find().sort("value", -1).limit(9)
 	active_texts = [t["_id"] for t in active_texts]
+
+	metrics = db.metrics.find().sort("timestamp", -1).limit(1)[0]
+	pprint(metrics)
 
 	return render_to_response('static/splash.html',
 							 {"titlesJSON": json.dumps(get_text_titles()),
 							  "connected_texts": connected_texts,
 							  "active_texts": active_texts,
+							  "metrics": metrics,
 							 #"daf_today": daf_today,
 							 #"daf_tomorrow": daf_tomorrow,
 							  'toc': get_toc(),},
