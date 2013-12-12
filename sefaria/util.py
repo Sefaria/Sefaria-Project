3100f8be0be4aca51691f1ea7f7f8e5466de7cbb
brett lockspeiser <blocks@gmail.com>
2013-12-12 14:19:43 -0800

    Display empty names as 'anonymous'

--- sefaria/util.py
+++ sefaria/util.py
@@ -30,18 +30,21 @@ def jsonpResponse(data, callback, status=200):
 
 
 def delete_template_cache(fragment_name='', *args):
-    cache.delete('template.cache.%s.%s' % (fragment_name, hashlib.md5(u':'.join([arg for arg in args])).hexdigest()))
+	cache.delete('template.cache.%s.%s' % (fragment_name, hashlib.md5(u':'.join([arg for arg in args])).hexdigest()))
 
 
 def user_link(uid):
 	try:
-		uid = int(uid)
+		uid  = int(uid)
 		user = User.objects.get(id=uid)
 		name = user.first_name + " " + user.last_name
-		url = '/contributors/' + user._username
+		name = "Anonymous" if name == " " else name
+		url  = '/contributors/' + user._username
 	except:
+		# Don't choke on unknown users, just leave a placeholder
+		# (so that testing on history can happen without needing the user DB)
 		name = "Someone"
-		url = "#"
+		url  = "#"
 
 	link = "<a href='" + url + "'>" + name + "</a>"
 	return link
@@ -59,41 +62,41 @@ def subscribe_to_announce(email, first_name=None, last_name=None):
 
 
 class MLStripper(HTMLParser):
-    def __init__(self):
-        self.reset()
-        self.fed = []
-    def handle_data(self, d):
-        self.fed.append(d)
-    def get_data(self):
-        return ''.join(self.fed)
+	def __init__(self):
+		self.reset()
+		self.fed = []
+	def handle_data(self, d):
+		self.fed.append(d)
+	def get_data(self):
+		return ''.join(self.fed)
 
 def strip_tags(html):
-    s = MLStripper()
-    s.feed(html)
-    return s.get_data()
+	s = MLStripper()
+	s.feed(html)
+	return s.get_data()
 
 
 def td_format(td_object):
 	"""
 	Turn a timedelta object into a nicely formatted string.
 	"""
-        seconds = int(td_object.total_seconds())
-        periods = [
-                ('year',        60*60*24*365),
-                ('month',       60*60*24*30),
-                ('day',         60*60*24),
-                ('hour',        60*60),
-                ('minute',      60),
-                ('second',      1)
-                ]
+	seconds = int(td_object.total_seconds())
+	periods = [
+			('year',        60*60*24*365),
+			('month',       60*60*24*30),
+			('day',         60*60*24),
+			('hour',        60*60),
+			('minute',      60),
+			('second',      1)
+			]
 
-        strings=[]
-        for period_name,period_seconds in periods:
-                if seconds > period_seconds:
-                        period_value , seconds = divmod(seconds,period_seconds)
-                        if period_value == 1:
-                                strings.append("%s %s" % (period_value, period_name))
-                        else:
-                                strings.append("%s %ss" % (period_value, period_name))
+	strings=[]
+	for period_name,period_seconds in periods:
+			if seconds > period_seconds:
+					period_value , seconds = divmod(seconds,period_seconds)
+					if period_value == 1:
+							strings.append("%s %s" % (period_value, period_name))
+					else:
+							strings.append("%s %ss" % (period_value, period_name))
 
-        return ", ".join(strings)
+	return ", ".join(strings)
