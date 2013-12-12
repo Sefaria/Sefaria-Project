9ef00a6dfa34eccc221d548b6684c22f4c770b11
brett lockspeiser <blocks@gmail.com>
2013-12-12 12:23:54 -0800

    add calendars file for parasha and daf yomi

---
+++ sefaria/calendars.py
@@ -0,0 +1,27 @@
+from datetime import date, datetime
+
+from texts import db, url_ref
+
+def daf_yomi(date):
+	"""
+	Returns the daf yomi for date
+	"""
+
+	date_str = date.strftime(" %m/ %d/%Y").replace(" 0", "").replace(" ", "")
+	daf = db.dafyomi.find_one({"date": date_str})
+	yom = {
+		"name": daf["daf"],
+		"url": url_ref(daf["daf"] + "a")
+	}
+	return yom
+
+
+def parasha(datetime):
+	"""
+	Returns the upcoming Parasha for date. 
+	"""
+
+	p = db.parshiot.find({"date": {"$gt": datetime}}, limit=1).sort([("date", 1)])
+	p = p.next()
+
+	return p
