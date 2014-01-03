1b245a4b25f014a564e7a83c47d34f993bd10cf5
brett lockspeiser <blocks@gmail.com>
2014-01-03 11:23:04 -0800

    Add Rashi Script

--- static/js/sheets.js
+++ static/js/sheets.js
@@ -174,14 +174,14 @@ $(function() {
 		CKEDITOR.config.extraAllowedContent = 'small';
 		CKEDITOR.config.font_names =
 			'Arial/Arial, Helvetica, sans-serif;' +
-			'Comic Sans MS/Comic Sans MS, cursive;' +
+			'Comic Sans/Comic Sans MS, cursive;' +
 			'Courier New/Courier New, Courier, monospace;' +
 			'Georgia/Georgia, serif;' +
-			'Lucida Sans Unicode/Lucida Sans Unicode, Lucida Grande, sans-serif;' +
+			'Lucida Sans/Lucida Sans Unicode, Lucida Grande, sans-serif;' +
+			'Rashi Script/Rashi Script, serif;' +
 			'Tahoma/Tahoma, Geneva, sans-serif;' +
 			'Times New Roman/Times New Roman, Times, serif;' +
-			'Trebuchet MS/Trebuchet MS, Helvetica, sans-serif;' +
-			'Verdana/Verdana, Geneva, sans-serif';
+			'Verdana/Verdana, Geneva, sans-serif;';
 		CKEDITOR.config.toolbar = [
 			{name: 'removestyle', items: ['RemoveFormat']},
 			{name: 'basicstyles', items: [ 'Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript' ] },
