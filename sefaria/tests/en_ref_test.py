d878f5fd5b9af91e911eb1d2d88c76b1123b260d
Lev Israel <eliezer.israel@gmail.com>
2014-05-11 15:05:39 +0300

    Testing get_titles_in_text and get_refs_in_text

--- sefaria/tests/en_ref_test.py
+++ sefaria/tests/en_ref_test.py
@@ -3,21 +3,47 @@ import pytest
 
 from .. import texts as t
 
+
 def setup_module(module):
 	global texts
 	texts = {}
+	texts['bible_mid'] = u"Here we have Genesis 3:5 may it be blessed"
+	texts['bible_begin'] = u"Genesis 3:5 in the house"
+	texts['bible_end'] = u"Let there be Genesis 3:5"
 	texts['2ref'] = u"This is a test of a Brachot 7b and also of an Isaiah 12:13."
 	texts['barenum'] = u"In this text, there is no reference but there is 1 bare number."  	
 
-class TestGetTitlesInText():
 
-	def test_catches_titles(self):
-		res = t.get_titles_in_text(texts['2ref'])
-		assert set(res) >= set(['Brachot','Isaiah'])
-		
+class Test_get_titles_in_text():
+
+
+	def test_no_bare_number(self):
 		res = t.get_titles_in_text(texts['barenum'])
 		assert set(res) == set()
 
-		
+	def test_positions(self):
+		for a in ['bible_mid','bible_begin', 'bible_end']:
+			assert set(['Genesis']) <= set(t.get_titles_in_text(texts[a]))
+
+	def test_multi_titles(self):
+		res = t.get_titles_in_text(texts['2ref'])
+		assert set(res) >= set(['Brachot','Isaiah'])
+
+class Test_get_refs_in_text():
+
+	def test_bare_digits(self):
+		assert set() == set(t.get_refs_in_text(texts['barenum'])) # Fixed in 5a4b813819ef652def8360da2ac1b7539896c732
+
+	def test_positions(self):
+		for a in ['bible_mid','bible_begin', 'bible_end']:
+			ref = t.get_refs_in_text(texts[a])
+			assert 1 == len(ref)
+			assert ref[0] == "Genesis 3:5"
+
+	def test_multiple(self):
+		ref = t.get_refs_in_text(texts['2ref'])
+		assert 2 == len(ref)
+		assert set(['Brachot 7b','Isaiah 12:13']) == set(t.get_refs_in_text(texts['2ref']))
+
 
 
