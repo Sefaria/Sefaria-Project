e9d1b9afe7195efa79d5bb8a87b8929c520029d9
Lev Israel <eliezer.israel@gmail.com>
2014-05-14 14:53:58 +0300

    script to standardize heTitles of Mishna and to add heTitleVariants to everything that has a Hebrew title

--- data/scripts/hebrew_titles_and_variants.py
+++ data/scripts/hebrew_titles_and_variants.py
@@ -21,22 +21,33 @@ if SEFARIA_DB_USER and SEFARIA_DB_PASSWORD:
 	db.authenticate(SEFARIA_DB_USER, SEFARIA_DB_PASSWORD)
 
 
-remove_refs_with_false()
-
+# For mishna, add a leading u'משנה' to the Hebrew name
 mishnahs = db.index.find({"categories": "Mishna"})
 for m in mishnahs:
-	old = m["title"]
-	new = m["title"].replace("Mishna ", "Mishnah ")
-	update_text_title(old, new)
+	title = m["heTitle"]
+	if not regex.match(u'משנה', m["heTitle"]):
+		m["heTitle"] = u'משנה' + " " + title
+		db.index.save(m)
+# Do we need to do anything like update_text_title()?
 
-rename_category("Mishna", "Mishnah")
+# To all index records with a heTitle, add heTitleVariants and copy the heTitle to heTitleVariants
+idxs = db.index.find({"heTitle": {"$exists": 1}})
+for i in idxs:
+	i['heTitleVariants'] = [i['heTitle']]
+	db.index.save(i)
 
-mishnahs = db.index.find({"categories": "Mishnah"})
-for m in mishnahs:
-	variants = set([v.replace("Mishna ", "Mishnah ") for v in m["titleVariants"]])
-	new_variants = set([v.replace("Mishnah ", "Mishna ") for v in variants])
-	m["titleVariants"] = list(variants.union(new_variants))
-	db.index.save(m)
+# For mishna that does not have a Gemara as well, add the simple name to heTitleVariants
+mlist = [u'פאה', u'דמאי', u'כלאים', u'שביעית' ,u'תרומות', u'מעשרות', u'מעשר שני', u'חלה', u'ערלה', u'ביכורים', u'שקלים', u'עדויות', u'אבות', u'מדות', u'כינים', u'כלים', u'אהלות', u'נגעים', u'פרה', u'טהרות', u'מקואות', u'מכשירים', u'זבים', u'טבול יום', u'ידים', u'עוקצים']
+for m in mlist:
+	search = [m,u'משנה' + " " + m]
+	mrec = db.index.find_one({"categories": "Mishna", "heTitle": {"$in": search}})
+	vars = mrec.get("heTitleVariants")
+	vars.append(m)
+	mrec['heTitleVariants'] = vars
+	print mrec["heTitleVariants"]
+	db.index.save(mrec)
 
+"""
 update_table_of_contents()
 save_toc_to_db()
+"""
