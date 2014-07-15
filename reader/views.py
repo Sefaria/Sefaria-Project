e8945d83014da790790111c00732ce28716fdbae
brett lockspeiser <blocks@gmail.com>
2014-07-15 15:03:37 -0700

    Speed up dashboard DB calls

--- reader/views.py
+++ reader/views.py
@@ -750,7 +750,7 @@ def dashboard(request):
 	Dashboard page -- table view of all content
 	"""
 	counts = db.counts.find({"title": {"$exists": 1}}, 
-		{"availableCounts": 0, "textComplete": 0})
+		{"title": 1, "flags": 1, "linksCount": 1})
 	
 	toc = get_toc()
 	flat_toc = flatten_toc(toc)
