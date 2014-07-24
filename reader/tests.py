9fa6948679a50c4a65c63a15b66dcbefe91775cd
Lev Israel <eliezer.israel@gmail.com>
2014-07-24 16:09:04 +0300

    Basic url tests

--- reader/tests.py
+++ reader/tests.py
@@ -1,16 +1,30 @@
+"""
+Run me with:
+python manage.py test reader
+"""
+
 from django.test import TestCase
 from django.test.client import Client
-import selenium
+#import selenium
+
 
 class GetTest(TestCase):
-	def setUp(self):
-		pass
-		
-	def test_get_text(self):
-		response = self.client.get('/api/texts/Genesis.1')
-		self.assertEqual(200, response.status_code)
+    def setUp(self):
+        global c
+        c = Client()
 
+    def test_root(self):
+        response = c.get('/')
+        self.assertEqual(200, response.status_code)
 
-	def test_login(self):
-		response = self.client.post('/login/', {'username': 'john', 'password': 'smith'})
-		self.assertEqual(200, response.status_code)
+    def test_get_text(self):
+        response = c.get('/Genesis.1')
+        self.assertEqual(200, response.status_code)
+
+    def test_api_get_text(self):
+        response = c.get('/api/texts/Genesis.1')
+        self.assertEqual(200, response.status_code)
+
+    def test_login(self):
+        response = c.post('/login/', {'username': 'john', 'password': 'smith'})
+        self.assertEqual(200, response.status_code)
