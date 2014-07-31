ee2089b088559dfafdb5e3024d4c7b6d4742a8ef
Lev Israel <eliezer.israel@gmail.com>
2014-07-31 21:39:36 +0300

    count model

--- sefaria/model/count.py
+++ sefaria/model/count.py
@@ -1,1 +1,31 @@
-__author__ = 'levisrael'
+"""
+count.py
+Writes to MongoDB Collection: counts
+"""
+import sefaria.model.abstract as abst
+
+
+class Count(abst.AbstractMongoRecord):
+    """
+    A note on a specific place in a text.  May be public or private.
+    """
+    collection = 'counts'
+
+    required_attrs = [
+        "textComplete",
+        "percentAvailable",
+        "availableCounts"
+    ]
+    optional_attrs = [
+        "categories",
+        "availableTexts",
+        "title",
+        "linksCount",
+        "sectionCounts",
+        "estimatedCompleteness",
+        "flags"
+    ]
+
+
+class CountSet(abst.AbstractMongoSet):
+    recordClass = Count
