fc61df28b13526d725a39a23713ac16a144a0526
Lev Israel <eliezer.israel@gmail.com>
2014-08-05 23:40:54 +0300

    IndexQueue model and first implementation

---
+++ sefaria/model/queue.py
@@ -0,0 +1,27 @@
+"""
+queue.py
+Writes to MongoDB Collection: index_queue
+"""
+
+import sefaria.model.abstract as abst
+
+
+class IndexQueue(abst.AbstractMongoRecord):
+    """
+    A note on a specific place in a text.  May be public or private.
+    """
+    collection = 'index_queue'
+
+    required_attrs = [
+        "lang",
+        "type"
+        "version",
+        "ref"
+    ]
+    optional_attrs = [
+
+    ]
+
+
+class IndexQueueSet(abst.AbstractMongoSet):
+    recordClass = IndexQueue
