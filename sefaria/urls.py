298a79307163e486ed64bfa6c3a1ea9f13c9cd1c
brett lockspeiser <blocks@gmail.com>
2014-08-19 15:24:18 -0700

    Splash page for new profile contest, blurb on homepage

--- sefaria/urls.py
+++ sefaria/urls.py
@@ -51,6 +51,7 @@ urlpatterns += patterns('reader.views',
 # Campaigns 
 urlpatterns += patterns('reader.views',
     (r'^translate/(?P<ref>.+)$', 'translation_flow'),
+    (r'^contests/(?P<page>new-profiles-contest)$', 'serve_static'),
     (r'^contests/(?P<slug>.+)$', 'contest_splash'),
     (r'^mishnah-contest-2013/?$', lambda x: HttpResponseRedirect('/contests/mishnah-contest-2013')),
 )
