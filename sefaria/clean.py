2bc826981a753f4780c7847caf54afd71949d92f
brett lockspeiser <blocks@gmail.com>
2014-09-11 12:02:17 -0700

    Fix clean function to remove old counts, add to migration script

--- sefaria/clean.py
+++ sefaria/clean.py
@@ -22,16 +22,23 @@ def remove_old_counts():
     """
     Deletes counts documents which no longer correspond to a text or category.
     """
-    counts = model.CountSet()
+    # counts = model.CountSet()
+    # If there are counts documents save in the DB with invalid titles,
+    # instantiation of the Count will cause a BookNameError.
+    # But in this code instantiation happens in the line 'for count in counts'
+    # How do we catch that? Additionally, we need access to the bad title after
+    # The error has occurred. How would we get that? Reverting to direct DB call for now.
+    counts = db.counts.find()
     for count in counts:
-        if getattr(count, "title", None):
+        if count.get("title", None):
             try:
-                model.Ref(count["title"])
+                model.get_index(count["title"])
             except BookNameError:
-                print u"Old text %s".format(count.title)
-                count.delete()
+                print u"Old count: %s" % count["title"]
+                #count.delete()
+                db.counts.remove({"_id": count["_id"]})
         else:
-            #TODO incomplete
+            #TODO incomplete for Category Counts. 
             continue
             categories = count.categories
             i = model.IndexSet({"$and": [{'categories.0': categories[0]}, {"categories": {"$all": categories}}, {"categories": {"$size": len(categories)}} ]})
