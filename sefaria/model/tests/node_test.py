5e209e879fe65f7b379e1af580ef2940d926c92d
Lev Israel <eliezer.israel@gmail.com>
2014-12-25 15:51:59 -0500

    3 level combinatoric name test

--- sefaria/model/tests/node_test.py
+++ sefaria/model/tests/node_test.py
@@ -119,6 +119,89 @@ class Test_Titles(object):
 
         assert td == target
 
+    def test_grandchild_presentation(self):
+        s = SchemaStructureNode()
+        s.key = "root"
+        s.add_title("root", "en", primary=True)
+        s.add_title("alt root", "en")
+
+        s2 = SchemaStructureNode()
+        s2.key = "l2"
+        s2.add_title("Level 2", "en", primary=True)
+        s2.add_title("Level 2 Alone", "en", presentation="alone")
+        s2.add_title("Level 2 Both", "en", presentation="both")
+        s2.append_to(s)
+
+        j = JaggedArrayNode()
+        j.key = "child1"
+        j.depth = 1
+        j.sectionNames = ["Foo"]
+        j.addressTypes = ["Integer"]
+        j.add_title("Level 3a", "en", primary=True)
+        j.add_title("Level 3a alone", "en", presentation="alone")
+        j.add_title("Level 3a both", "en", presentation="both")
+        j.append_to(s2)
+
+        j2 = JaggedArrayNode()
+        j2.key = "child2"
+        j2.depth = 1
+        j2.sectionNames = ["Foo"]
+        j2.addressTypes = ["Integer"]
+        j2.add_title("Level 3b", "en", primary=True)
+        j2.add_title("Level 3b alone", "en", presentation="alone")
+        j2.add_title("Level 3b both", "en", presentation="both")
+        j2.append_to(s2)
+
+        td = s.title_dict()
+        assert len(td) == 36
+
+        target = {
+            "root": s,
+            "alt root": s,
+            "Level 2 Alone": s2,
+            "Level 3b alone": j2,
+            "Level 3a alone": j,
+            "Level 2 Both": s2,
+            "Level 3a both": j,
+            "Level 3b both": j2,
+
+            "root, Level 2 Both": s2,
+            "root, Level 2": s2,
+            "alt root, Level 2 Both": s2,
+            "alt root, Level 2": s2,
+
+            "root, Level 2 Both, Level 3a": j,
+            "root, Level 2, Level 3a": j,
+            "alt root, Level 2 Both, Level 3a": j,
+            "alt root, Level 2, Level 3a": j,
+            "Level 2 Alone, Level 3a": j,
+            "Level 2 Both, Level 3a": j,
+
+            "root, Level 2 Both, Level 3a both": j,
+            "root, Level 2, Level 3a both": j,
+            "alt root, Level 2 Both, Level 3a both": j,
+            "alt root, Level 2, Level 3a both": j,
+            "Level 2 Alone, Level 3a both": j,
+            "Level 2 Both, Level 3a both": j,
+
+            "root, Level 2 Both, Level 3b": j2,
+            "root, Level 2, Level 3b": j2,
+            "alt root, Level 2 Both, Level 3b": j2,
+            "alt root, Level 2, Level 3b": j2,
+            "Level 2 Alone, Level 3b": j2,
+            "Level 2 Both, Level 3b": j2,
+
+            "root, Level 2 Both, Level 3b both": j2,
+            "root, Level 2, Level 3b both": j2,
+            "alt root, Level 2 Both, Level 3b both": j2,
+            "alt root, Level 2, Level 3b both": j2,
+            "Level 2 Alone, Level 3b both": j2,
+            "Level 2 Both, Level 3b both": j2,
+        }
+
+        assert td == target
+
+
 
 
 
