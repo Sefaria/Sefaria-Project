242d6f3cbe5194aa7b252ebdcfc0a8f721a8e9ef
brett lockspeiser <blocks@gmail.com>
2015-01-08 13:06:53 -0800

    Preview text toggle in Nav Panel

--- templates/js/headers.js
+++ templates/js/headers.js
@@ -64,6 +64,7 @@
 		_path: [],
 		_sections: [],
 		_preview: null,
+		_showPreviews: false,
 		init: function() {
 			$("#navToc").on("click", ".tocCat", this._handleNavClick);
 			// Langugage Toggle
@@ -97,11 +98,21 @@
 			$("#navPanelTexts #lessLink").click(function() {
 				$("#navPanelTexts").removeClass("expanded");
 			});
+			$("#navToc").on("click", "#navTocPreviewToggle", function() {
+				if (sjs.navPanel._showPreviews) {
+					sjs.navPanel._showPreviews = false;
+				} else {
+					sjs.navPanel._showPreviews = true;
+				}
+				sjs.navPanel.setNavContent();
+				sjs.navPanel._saveState();
+			});
 			var prevState = $.cookie("navPanelState")
 			if (prevState) {
 				var state = JSON.parse(prevState);
-				this._path     = state.path;
-				this._sections = state.sections;
+				this._path         = state.path;
+				this._sections     = state.sections;
+				this._showPreviews = state.showPreviews
 				if (state.path.length) {
 					this.setNavContent();
 					$("#navPanelTexts").addClass("expanded");
@@ -128,7 +139,13 @@
 			var dataSections = $(this).attr("data-sections");
 			sjs.navPanel._sections = dataSections ? dataSections.split("/") : [];
 			sjs.navPanel.setNavContent();
-			$.cookie("navPanelState", JSON.stringify({path: sjs.navPanel._path, sections: sjs.navPanel._sections}));
+			sjs.navPanel._saveState();
+		},
+		_saveState: function() {
+			$.cookie("navPanelState", JSON.stringify({path:         sjs.navPanel._path, 
+													  sections:     sjs.navPanel._sections,
+													  showPreviews: sjs.navPanel._showPreviews 
+													}));			
 		},
 		setNavContent: function() {
 			var sections = this._sections;
@@ -153,6 +170,11 @@
 				$("#navPanelTextsMore").hide();
 			//	$("#navPanelLinks, #navPanelFooter, .navLine").hide();
 			}
+			if (this._showPreviews) {
+				$("#navToc").addClass("showPreviews");
+			} else {
+				$("#navToc").removeClass("showPreviews");
+			}
 			$(".navLine").eq(0).show();
 		},
 		makeNavContent: function() {
@@ -163,8 +185,10 @@
 			var backPath     = path.slice(0, -1).join("/").replace(/\'/g, "&apos;");
 			var backSections = sections.slice(0, -1).join("/").replace(/\'/g, "&apos;");
 
-			// Language Toggle
-			var html = "<div id='navTocLangToggleBox'><div id='navTocLangToggle' class='toggle'>" +
+			// Language & Preview Toggles
+			var html = "<div id='navTocLangToggleBox'>" + 
+						"<i id='navTocPreviewToggle' class='fa fa-eye'></i>" +
+						"<div id='navTocLangToggle' class='toggle'>" +
 						"<div class='langToggle toggleOption " + ($("#navToc").hasClass("english") ? "active" : "") + "' data-lang='english'>" +
 							"<img src='/static/img/english.png' /></div>" +
 						"<div class='langToggle toggleOption " + ($("#navToc").hasClass("hebrew") ? "active" : "") + "' data-lang='hebrew'>" + 
@@ -236,6 +260,7 @@
 				}
 				if (previewDepth >= this._preview.sectionNames.length -1 ) {
 					// Section Preview (terminal depth, preview text)
+					if (!this._showPreviews) { html += "<div id='numLinkBox'>"}
 					for (var i=1; i <= previewSection.length; i++) {
 						var num   = isTalmud && !isCommentary ? intToDaf(i-1) : i;
 						var url   = ("/" + sections.join(".") + "." + num).replace(/\'/g, "&apos;");
@@ -243,15 +268,25 @@
 						var en    = previewSection[i-1].en;
 						if (!en && !he) { continue; }
 						var klass = (he ? "" : "enOnly") + " " + (en ? "" : "heOnly");
-						html += "<a class='tocLink previewLink " + klass + "' href='" + url + "'>" +
-									"<i class='fa fa-angle-right'></i>" +
-									"<div class='en'><span class='segmentNumber'>" + num + ".</span>" + en + "</div>" +
-									"<div class='he'><span class='segmentNumber'>" + num + ".</span>" + he + "</div>" +
-								"</a>";
+						
+						if (this._showPreviews) {
+							html += "<a class='tocLink previewLink " + klass + "' href='" + url + "'>" +
+										"<i class='fa fa-angle-right'></i>" +
+										"<div class='en'><span class='segmentNumber'>" + num + ".</span>" + en + "</div>" +
+										"<div class='he'><span class='segmentNumber'>" + num + ".</span>" + he + "</div>" +
+									"</a>";							
+						} else {
+							html += "<a class='tocLink numLink " + klass + "' href='" + url + "'>" +
+										num +
+									"</a>";
+						}
+
 					}
 					if (!previewSection.length) {
 						html += "<br><center><i>No text available.</i></center>";
 					}
+					if (!this._showPreviews) { html += "</div>"}
+
 				} else {
 					// Sections List ("Chapter 1, Chapter 2")
 					for (var i=0; i < previewSection.length; i++) {
