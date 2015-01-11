8928f47db70a3e0547cd7b790eb4ca1555e0a83f
Lev Israel <eliezer.israel@gmail.com>
2015-01-10 21:00:48 -0500

    catch index errors

--- data/scripts/add_all_links.py
+++ data/scripts/add_all_links.py
@@ -26,41 +26,45 @@ texts = db.texts.find()
 text_total = {}
 text_order = []
 for text in texts:
-	if text['title'] not in text_total:
-		text_total[text["title"]] = 0
-		text_order.append(text["title"])
-	print text["title"]
-	index = txt.get_index(text["title"])
-	if not index or not getattr(index, "categories", None):
-		print "No index found for " + text.title
-		continue
-	if "Tanach" in index.categories:
-		continue
-	talmud = True if "Talmud" in index.categories else False
+    if text['title'] not in text_total:
+        text_total[text["title"]] = 0
+        text_order.append(text["title"])
+    print text["title"]
+    try:
+        index = txt.get_index(text["title"])
+    except Exception as e:
+        print "Error loading: {} index : {}".format(text["title"] , e)
+        continue
+    if not index or not getattr(index, "categories", None):
+        print "No index found for " + text.title
+        continue
+    if "Tanach" in index.categories:
+        continue
+    talmud = True if "Talmud" in index.categories else False
 
-	for i in range(len(text['chapter'])):
-		if talmud:
-			if "Bavli" in index.categories and i < 2:
-				continue
-			chap = section_to_daf(i + 1)
-		else:
-			chap = i + 1
-		ref = text['title'] + " " + str(chap)
-		print ref
-		try:
-			result = add_links_from_text(ref, text['chapter'][i], text['_id'], user)
-			if result:
-				text_total[text["title"]] += len(result)
-		except Exception, e:
-			print e
+    for i in range(len(text['chapter'])):
+        if talmud:
+            if "Bavli" in index.categories and i < 2:
+                continue
+            chap = section_to_daf(i + 1)
+        else:
+            chap = i + 1
+        ref = text['title'] + " " + str(chap)
+        print ref
+        try:
+            result = add_links_from_text(ref, text['chapter'][i], text['_id'], user)
+            if result:
+                text_total[text["title"]] += len(result)
+        except Exception, e:
+            print e
 
 total = 0
 for text in text_order:
-	num = text_total[text]
-	index = txt.get_index(text)
-	if getattr(index, "categories", None):
-		print text.replace(",",";") + "," + str(num) + "," + ",".join(index.categories)
-	else:
-		print text.replace(",",";") + "," + str(num)
-	total += num
+    num = text_total[text]
+    index = txt.get_index(text)
+    if getattr(index, "categories", None):
+        print text.replace(",",";") + "," + str(num) + "," + ",".join(index.categories)
+    else:
+        print text.replace(",",";") + "," + str(num)
+    total += num
 print "Total " + str(total)
