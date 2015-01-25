8e1edde859f1ace8f0f0e73d1714a16edd09513d
Lev Israel <eliezer.israel@gmail.com>
2015-01-25 17:16:05 +0200

    Recognize and wrap comma within front-end links.

--- static/js/util.js
+++ static/js/util.js
@@ -1233,7 +1233,7 @@ sjs.makeRefRe = function() {
 	// Construct and store a Regular Expression for matching citations
 	// based on known books.
 	var books = "(" + sjs.books.map(RegExp.escape).join("|")+ ")";
-	var refReStr = books + " (\\d+[ab]?)(:(\\d+)([\\-–]\\d+(:\\d+)?)?)?";
+	var refReStr = books + " (\\d+[ab]?)(?:[:, ]+)?(\\d+)?(?:(?:[\\-–])?(\\d+[ab]?)?(?:[:, ]+)?(\\d+)?)?";
 	sjs.refRe = new RegExp(refReStr, "gi");	
 }
 
@@ -1246,11 +1246,33 @@ function wrapRefLinks(text) {
 	// Reset lastIndex, since we use the same RE object multple times
 	sjs.refRe.lastIndex = 0; 
 
-	var refText = text.replace(sjs.refRe, '<span class="refLink" data-ref="$1.$2$3">$1 $2$3</span>');
-
-	//var refText = text.replace(sjs.refRe, '1: $1, 2: $2, 3: $3, 4: $4, 5: $5');
-	return refText;
-	
+    function replacer(match, p1, p2, p3, p4, p5, offset, string) {
+        // p1: Book
+        // p2: From section
+        // p3: From segment
+        // p4: To section
+        // p5: To segment
+        var uref;
+        var nref;
+        var r;
+        uref = p1 + "." + p2;
+        nref = p1 + " " + p2;
+        if (p3) {
+            uref += "." + p3;
+            nref += ":" + p3;
+        }
+        if (p4) {
+            uref += "-" + p4;
+            nref += "-" + p4;
+        }
+        if (p5) {
+            uref += "." + p5;
+            nref += ":" + p5;
+        }
+        r = '<span class="refLink" data-ref="' + uref + '">' + nref + '</span>';
+        return r;
+    }
+	return text.replace(sjs.refRe, replacer);
 }
 
 
