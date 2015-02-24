f00ca3f777780831f83c40bc7c4477952aff1007
Ephraim Damboritz <ephraim@sefaria.org>
2015-02-24 17:35:23 +0200

    js fix to adding a new commentary text

--- static/js/reader.js
+++ static/js/reader.js
@@ -615,7 +615,8 @@ $(function() {
 			sjs.editing.sectionNames = sjs.editing.index.sectionNames;
 			sjs.editing.textDepth    = sjs.editing.sectionNames.length; 	
 			sjs.editing.text = [""];
-			console.log(sjs.editing);
+			//console.log(sjs.editing);
+			sjs.current.pageRef = sjs.editing.ref;
 			sjs.showNewText();	
 		}
 		$("#newTextCancel").trigger("click");	
@@ -2283,10 +2284,13 @@ sjs.updateUrlParams = function() {
 	else    									   { params["sidebarLang"] = "all" }	
 
 	var base     = sjs.selected ? sjs.selected : sjs.current.pageRef;
-	console.log(sjs.selected);
-	console.log(sjs.current.pageRef);
-	console.log(base);
+	/*if(base){
+		var paramStr = $.param(params) ? "/" + normRef(base) + "?" + $.param(params) : normRef(base);
+	}else{
+		var paramStr = $.param(params) ? "?" + $.param(params) : null
+	}*/
 	var paramStr = $.param(params) ? "/" + normRef(base) + "?" + $.param(params) : normRef(base);
+
 	var state    = History.getState();
 	sjs.flags.localUrlChange = true;
 	History.replaceState(state.data, state.title, paramStr);
