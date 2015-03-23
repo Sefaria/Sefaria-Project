d22dd263dc2d01d0ccb40c46cb00763978974cdd
brett lockspeiser <blocks@gmail.com>
2015-03-23 12:58:00 -0700

    Change language of reader error page

--- static/js/reader.js
+++ static/js/reader.js
@@ -56,7 +56,7 @@ sjs.Init.all = function() {
 
 	if ("error" in sjs.current) {
 		sjs.alert.message(sjs.current.error);
-		sjs._$basetext.html("<center>Open another text with the texts menu above</center>")
+		sjs._$basetext.html("<center>Open another text with the Sefaria menu above</center>");
 		sjs._$aboutBar.hide();
 		return;
 	}
