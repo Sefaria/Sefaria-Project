1d412a79d7c6fb94fce76fea5f95486ecf52f10b
Lev Israel <eliezer.israel@gmail.com>
2015-04-12 17:29:24 +0300

    Convert search to object model.  Prep for filters.

--- static/js/search.js
+++ static/js/search.js
@@ -3,129 +3,140 @@ sjs = sjs || {};
 sjs.pageSize = 100;
 
 $.extend(sjs, {
-	currentPage: "search",
-	currentFacet: null,
-	searchUrl: sjs.searchBaseUrl + "/" + sjs.searchIndex + "/_search?size=" + sjs.pageSize,
-	search: function(query, page) {
+    currentPage: "search",
+    currentFacet: null,
+    searchUrl: sjs.searchBaseUrl + "/" + sjs.searchIndex + "/_search?size=" + sjs.pageSize,
 
-			$header = $("#searchHeader");
-			$results = $("#searchResults");
-			$facets = $("#searchFacets");
-			$header.text("");
+    search: {
+        category_filters: [],
+        query_context: 1,
+        presentation_context: 1,
+        query: "",
+        hits: {},
+        $header: $("#searchHeader"),
+        $results: $("#searchResults"),
+        $facets: $("#searchFacets"),
+        set_presentation_context: function (level) {
+            this.presentation_context = level;
+            this.render()
+        },
+        resultsHtml: function (results) {
+                var html = "";
 
-			var page = page || 0;
-			if (!page) {
-				$results.empty();
-				$(window).scrollTop(0);
-				$header.html("Searching <img src='/static/img/ajax-loader.gif' />");
-			}
+                for (var i = 0; i < results.length; i++) {
+                    if (results[i]._type == "text") {
+                        html += this.textResult(results[i]);
+                    } else if (results[i]._type == "sheet") {
+                        html += this.sheetResult(results[i]);
+                    }
+                }
+                if (results.length == 0) {
+                    html = "<div id='emptySearch' class='well'>" +
+                    "<b>Sefaria Search is still under development.</b><br />" +
+                    "Hebrew words are searched exactly as entered; different forms of the same word may produce different results." +
+                    "</div>";
+                }
+                return html;
+            },
+        textResult: function (result) {
+            var s = result._source;
+            var snippet = result.highlight ? result.highlight.content.join("...") : s.content;
+            snippet = $("<div>" + snippet.replace(/^[ .,;:!-)\]]+/, "") + "</div>").html();
+            html = "<div class='result'>" +
+            '<a href="/' + normRef(s.ref) + '">' + s.ref + "</a>" +
+            "<div class='snippet'>" + snippet + "</div>" +
+            "<div class='version'>" + s.version + "</div>" +
+            "</div>";
+            return html;
+        },
 
-			var post =   {
-							"query" : { 
-								"query_string" : {
-									"query" : query,
-									"default_operator": "AND"
-								} 
-							},
-							"highlight" : {
-								"pre_tags" : ["<b>"],
-        						"post_tags" : ["</b>"],
-						        "fields" : {
-						            "content" : {}
-						        }
-    						}
-						};
+        sheetResult: function (result) {
+            var s = result._source;
+            var snippet = result.highlight ? result.highlight.content.join("...") : s.content;
+            snippet = $("<div>" + snippet.replace(/^[ .,;:!-)\]]+/, "") + "</div>").html();
+            html = "<div class='result'>" +
+            '<a href="/sheets/' + s.sheetId + '">' + s.title + "</a>" +
+            "<div class='snippet'>" + snippet + "</div>" +
+            "<div class='version'>" + s.version + "</div>" +
+            "</div>";
+            return html;
+        },
+        render: function() {
+            this.$header.html(this.hits.total + " results for <b>" + this.query + "</b>");
+            this.$results.find(".moreResults").remove();
+            var results = this.resultsHtml(this.hits.hits);
+            if (this.hits.hits.length == sjs.pageSize) {
+                results += "<div class='moreResults'>More results</div>"
+            }
+            this.$results.append(results);
+            $(".moreResults").click(function () {
+                sjs.search(this.query, page + 1);
+            });
+        },
+        search: function (page) {
+            var page = page || 0;
+            if (!page) {
+                this.$results.empty();
+                $(window).scrollTop(0);
+                this.$header.html("Searching <img src='/static/img/ajax-loader.gif' />");
+            }
 
-			var url = sjs.searchUrl;
- 			if (page) {
- 				url += "&from=" + (page * sjs.pageSize);
- 			}
-			$.ajax({
-				url: url,
-				type: 'POST',
-				data: JSON.stringify(post),
-				crossDomain: true,
-				processData: false,
-				dataType: 'json',
-				success: function(data) {
-					$header.html(data.hits.total + " results for <b>" + query + "</b>");
-					$results.find(".moreResults").remove();
-					var results = resultsHtml(data.hits.hits);
-					if (data.hits.hits.length == sjs.pageSize) {
-						results += "<div class='moreResults'>More results</div>"
-					}
-					$results.append(results);
-					$(".moreResults").click(function(){
-						sjs.search(query, page+1);
-					});
-				},
-				error: function() {
-					html = "<div id='emptySearch' class='well'>" +
-						"<b>Sefaria Search encountered an error.</b><br />" +
-						"This feature is still in development. We're currently working to make our search experience both robust and useful. Please try your search again later." + 
-					"</div>";
-					$results.html(html);
-				}
-			});
+            var post = {
+                "query": {
+                    "query_string": {
+                        "query": this.query,
+                        "default_operator": "AND"
+                    }
+                },
+                "highlight": {
+                    "pre_tags": ["<b>"],
+                    "post_tags": ["</b>"],
+                    "fields": {
+                        "content": {}
+                    }
+                }
+            };
 
-			var resultsHtml = function(results) {
-				var html = "";
+            var url = sjs.searchUrl;
+            if (page) {
+                url += "&from=" + (page * sjs.pageSize);
+            }
+            $.ajax({
+                url: url,
+                type: 'POST',
+                data: JSON.stringify(post),
+                crossDomain: true,
+                processData: false,
+                dataType: 'json',
+                success: function (data) {
+                    sjs.search.hits = data.hits;
+                    sjs.search.render();
+                },
+                error: function () {
+                    html = "<div id='emptySearch' class='well'>" +
+                    "<b>Sefaria Search encountered an error.</b><br />" +
+                    "This feature is still in development. We're currently working to make our search experience both robust and useful. Please try your search again later." +
+                    "</div>";
+                    sjs.search.$results.html(html);
+                }
+            });
 
-				for (var i = 0; i < results.length; i++) {
-					if (results[i]._type == "text") {
-						html += textResult(results[i]);
-					} else if (results[i]._type == "sheet") {
-						html += sheetResult(results[i]);
-					}
-				}
-				if (results.length == 0) {
-					html = "<div id='emptySearch' class='well'>" +
-								"<b>Sefaria Search is still under development.</b><br />" +
-								"Hebrew words are searched exactly as entered; different forms of the same word may produce different results." + 
-							"</div>";
-				}		
-				return html;
-			}
+            var facetsHtml = function (facets) {
+                facets = facets.category.terms;
+                var html = "";
+                for (var i = 0; i < facets.length; i++) {
+                    html += "<div class='facet' data-facet='" + facets[i].term + "'>" +
+                    facets[i].term + " (" + facets[i].count + ")" +
+                    "</div>";
+                }
+                return html;
+            };
 
-			var textResult = function(result) {
-					var s = result._source;
-					var snippet = result.highlight ? result.highlight.content.join("...") : s.content;
-					snippet = $("<div>" + snippet.replace(/^[ .,;:!-)\]]+/, "") + "</div>").html();
-					html = "<div class='result'>" +
-								'<a href="/' + normRef(s.ref)+ '">' + s.ref + "</a>" +
-								"<div class='snippet'>" + snippet + "</div>" +
-								"<div class='version'>" + s.version + "</div>" +
-							"</div>";
-					return html;
-			};
+            $("#goto").blur();
 
-			var sheetResult = function(result) {
-					var s = result._source;
-					var snippet = result.highlight ? result.highlight.content.join("...") : s.content;
-					snippet = $("<div>" + snippet.replace(/^[ .,;:!-)\]]+/, "") + "</div>").html();
-					html = "<div class='result'>" +
-								'<a href="/sheets/' + s.sheetId+ '">' + s.title + "</a>" +
-								"<div class='snippet'>" + snippet + "</div>" +
-								"<div class='version'>" + s.version + "</div>" +
-							"</div>";
-					return html;
-			};
-
-			var facetsHtml = function(facets) {
-				facets = facets.category.terms;
-				var html = "";
-				for (var i = 0; i < facets.length; i++) {
-					html += "<div class='facet' data-facet='" + facets[i].term + "'>" + 
-								facets[i].term + " (" + facets[i].count + ")" +
-							"</div>";
-				}
-				return html;
-			};
-
-			$("#goto").blur();
-
-			sjs.track.search(query);
-		}
+            sjs.track.search(query);
+        }
+    }
 });
 
 $(function() {
@@ -136,13 +147,14 @@ $(function() {
 	if ("q" in vars) {
 		var query = vars["q"].replace(/\+/g, " ")
 		$("#goto").val(query);
-		sjs.search(query);
+        sjs.search.query = query;
+		sjs.search.search();
 	}			
 	$(window).bind("statechange", function(e) {
 		var State = History.getState();
 		if (State && State.data && State.data.q) {
 			page = State.data.page || 0;
-			sjs.search(State.data.q, page);
+			sjs.search.search(State.data.q, page);
 		}
 	})
 });
