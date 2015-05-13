d15127e4b6558c33195a93c98ef38252b5ef00f3
Lev Israel <eliezer.israel@gmail.com>
2015-05-13 12:35:33 +0300

    Skeleton for linking js

---
+++ templates/js/linker.js
@@ -0,0 +1,23 @@
+// Variables in included with {{  name  }}
+
+(function(ns){
+
+    //Test browser support
+    var supports = !!document.querySelector; //&& !!window.addEventListener;
+    if ( !supports ) return;
+
+    //private scoping
+    var base_url = 'http://www.sefaria.org/';
+    function priv(){}
+
+    //public api
+    ns.tag = function(selector) {
+        var elem = document.querySelector(selector)
+
+    };
+
+}(this.sefaria = this.sefaria || {}));
+
+
+
+//called as this.sefaria.tag("#element-id");
