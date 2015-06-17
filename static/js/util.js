66e6b2b737b7e2bbfc6cb2223f31784e3651a0eb
brett lockspeiser <blocks@gmail.com>
2015-06-17 13:55:32 -0700

    Add stopword for propercasing

--- static/js/util.js
+++ static/js/util.js
@@ -2415,7 +2415,7 @@ String.prototype.toProperCase = function() {
 	  // Certain minor words should be left lowercase unless 
 	  // they are the first or last words in the string
 	  lowers = ['A', 'An', 'The', 'And', 'But', 'Or', 'For', 'Nor', 'As', 'At', 
-	  'By', 'For', 'From', 'In', 'Into', 'Near', 'Of', 'On', 'Onto', 'To', 'With'];
+	  'By', 'For', 'From', 'Is', 'In', 'Into', 'Near', 'Of', 'On', 'Onto', 'To', 'With'];
 	  for (i = 0, j = lowers.length; i < j; i++) {
 	    str = str.replace(new RegExp('\\s' + lowers[i] + '\\s', 'g'), 
 	      function(txt) {
