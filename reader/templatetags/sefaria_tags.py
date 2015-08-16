ed79e26a4d8380f59703f28e16df5222290ee1ea
Lev Israel <eliezer.israel@gmail.com>
2015-08-16 15:52:42 +0300

    Template filters for partitioning lists.  Rework Talmud personality page.

--- reader/templatetags/sefaria_tags.py
+++ reader/templatetags/sefaria_tags.py
@@ -6,6 +6,7 @@ import json
 import re
 import dateutil.parser
 import urllib
+import math
 from urlparse import urlparse
 
 from django import template
@@ -372,3 +373,89 @@ def get_private_attribute(model_instance, attrib_name):
 @register.filter(is_safe=True)
 def nice_timestamp(timestamp):
 	return dateutil.parser.parse(timestamp).strftime("%m/%d/%y")
+
+# Derived from https://djangosnippets.org/snippets/6/
+"""
+Template tags for working with lists.
+
+You'll use these in templates thusly::
+
+	{% load listutil %}      # I don't think we need this line.
+	{% for sublist in mylist|parition:"3" %}
+		{% for item in mylist %}
+			do something with {{ item }}
+		{% endfor %}
+	{% endfor %}
+"""
+@register.filter
+def partition_into(thelist, n):
+	"""
+	Break a list into ``n`` pieces. The last list may be larger than the rest if
+	the list doesn't break cleanly. That is::
+
+		>>> l = range(10)
+
+		>>> partition(l, 2)
+		[[0, 1, 2, 3, 4], [5, 6, 7, 8, 9]]
+
+		>>> partition(l, 3)
+		[[0, 1, 2], [3, 4, 5], [6, 7, 8, 9]]
+
+		>>> partition(l, 4)
+		[[0, 1], [2, 3], [4, 5], [6, 7, 8, 9]]
+
+		>>> partition(l, 5)
+		[[0, 1], [2, 3], [4, 5], [6, 7], [8, 9]]
+
+	"""
+	try:
+		n = int(n)
+		thelist = list(thelist)
+	except (ValueError, TypeError):
+		return [thelist]
+	p = len(thelist) / n
+	return [thelist[p*i:p*(i+1)] for i in range(n - 1)] + [thelist[p*(i+1):]]
+
+@register.filter
+def partition_by(thelist, n):
+	"""
+	Break a list into ``n`` sized peices
+	``partition_by(range(10), 3)`` gives::
+
+		[[1, 4, 7],
+		 [2, 5, 8],
+		 [3, 6, 9],
+		 [10]]
+
+	"""
+	try:
+		n = int(n)
+		thelist = list(thelist)
+	except (ValueError, TypeError):
+		return [thelist]
+	rows = int(math.ceil(float(len(thelist)) / n))
+	newlists = [thelist[r * n : (r + 1) * n] for r in range(rows)]
+	return newlists
+
+@register.filter
+def partition_vertical(thelist, n):
+	"""
+	Break a list into ``n`` peices, but "horizontally." That is,
+	``partition_horizontal(range(10), 3)`` gives::
+
+		[[1, 4, 7],
+		 [2, 5, 8],
+		 [3, 6, 9],
+		 [10]]
+
+	Clear as mud?
+	"""
+	try:
+		n = int(n)
+		thelist = list(thelist)
+	except (ValueError, TypeError):
+		return [thelist]
+	newlists = [list() for i in range(n)]
+	for i, val in enumerate(thelist):
+		newlists[i%n].append(val)
+	return newlists
