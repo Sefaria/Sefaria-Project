4440a0060f1101971b8ce3f356db6f4468d8a4b2
Lev Israel <eliezer.israel@gmail.com>
2015-08-26 16:37:32 +0300

    Working skeleton

--- static/js/s2-search.jsx
+++ static/js/s2-search.jsx
@@ -1,3 +1,10 @@
 var sjs = sjs || {};
 var cx  = React.addons.classSet;
 
+var SearchPage = React.createClass({
+    render: function () {
+        return (
+            <h1>Search</h1>
+        )
+    }
+});
