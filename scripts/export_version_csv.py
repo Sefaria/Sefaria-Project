9c8a6a0fe62d59581d83e6f556237c1797de5688
brett lockspeiser <blocks@gmail.com>
2015-08-27 12:22:09 -0700

    Admin URL for exporting versions CSV

--- scripts/export_version_csv.py
+++
@@ -1,9 +0,0 @@
-import csv
-from sefaria.model import *
-
-with open("data/tmp/versions.csv", 'w+') as csvfile:
-    writer = csv.writer(csvfile)
-    writer.writerow(["Text", "Version Title", "Version Source"])
-    vs = VersionSet()
-    for v in vs:
-        writer.writerow([unicode(s).encode("utf-8") for s in [v.title, v.versionTitle, v.versionSource]])
