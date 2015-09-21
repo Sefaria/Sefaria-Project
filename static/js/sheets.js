d775a608f84436efe751bd471a2ae65bb50515e5
Russel Neiss <russel@russelneiss.com>
2015-09-21 16:58:04 -0500

    Add tracking for custom source sheet layout

--- static/js/sheets.js
+++ static/js/sheets.js
@@ -722,7 +722,14 @@ $(function() {
 
 			
 		$("#overlay").show();
+
+		sjs.track.sheets("Open Source Layout Modal");
 	 });
+ 
+		 $("#overrideLayoutModal").on('hide', function () {
+		  alert('closed')
+		})
+
  
 	$("#overrideLayoutModal .ok").click(function(){
 		
@@ -736,12 +743,11 @@ $(function() {
 			$target.removeClass("bilingual english hebrew sideBySide hebLeft hebRight stacked");
 		}
 		
-		
-		
-		
 		$("#overrideLayoutModal, #overlay").hide();
 		autoSave();
 	});
+ 
+ 
  
 
 	// Change Source Layout via modal
@@ -759,6 +765,8 @@ $(function() {
 		} else {
 			$("#biLayoutToggleSource").removeClass("disabled");
 		}
+		autoSave();
+		sjs.track.sheets("Change Source Layout Button");
 	});
 
 
@@ -773,15 +781,14 @@ $(function() {
 			$("#stackedSource").click();
 			$("#biLayoutToggleSource, #sheetLayoutToggleSource").addClass("disabled");
 			$target.removeClass("sideBySide hebLeft hebRight").addClass("stacked");
-
-
-
 		} else {
 			$("#sheetLayoutToggleSource").removeClass("disabled");
 			if ($target.hasClass("sideBySide")) {
 				$("#biLayoutToggleSource").removeClass("disabled");
 			}
 		}
+		autoSave();		
+		sjs.track.sheets("Change Source Language Button");
 	});
 	
 	// Change Language Layout via modal
@@ -791,6 +798,8 @@ $(function() {
 		$(this).addClass("active");
 		$target.removeClass("hebLeft hebRight")
 			.addClass($(this).attr("id").replace("Source",""))
+		autoSave();	
+		sjs.track.sheets("Change Source Language Layout Button");
 	});
 
 	
@@ -802,6 +811,7 @@ $(function() {
 		$target.removeClass("bilingual english hebrew sideBySide hebLeft hebRight stacked");
 		$("#overrideLayoutModal, #overlay").hide();
 		autoSave();
+		sjs.track.sheets("Reset Source Layout to Default");
 	});
 
 
