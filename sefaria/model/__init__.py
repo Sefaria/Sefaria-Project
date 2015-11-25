4a0d479d977d5888c6224c26a32e406baee3cc16
Lev Israel <eliezer.israel@gmail.com>
2015-11-25 13:12:31 +0200

    Try to solve import issue

--- sefaria/model/__init__.py
+++ sefaria/model/__init__.py
@@ -12,7 +12,7 @@ symbols are then accessed directly as, e.g.:
 import abstract
 
 # not sure why we have to do this now - it wasn't previously required
-import history, text, link, note, layer, notification, queue, lock, following, user_profile, version_state, translation_request, lexicon, place, person, garden
+import history, schema, text, link, note, layer, notification, queue, lock, following, user_profile, version_state, translation_request, lexicon, place, person, garden
 
 from history import History, HistorySet, log_add, log_delete, log_update, log_text
 from schema import deserialize_tree, Term, TermSet, TermScheme, TermSchemeSet, TitledTreeNode, SchemaNode, ArrayMapNode, JaggedArrayNode, NumberedTitledTreeNode
