bd2ecef8b0c53f2f78706c9d8c76a33521a164cb
Russel Neiss <russel@russelneiss.com>
2015-12-04 13:06:07 -0600

    change test from css transform to animation

--- templates/js/headers.js
+++ templates/js/headers.js
@@ -854,16 +854,16 @@
 	    $(window).click(sjs.hideOptionsBar);
 
 		// browser check -- 
-		// this attempts to create an element and add css3 2d transformation to it
+		// this attempts to create an element and add css3 animation to it
 		// these are only supported in self-updating firefox, chrome, safari & ie > 9
 		var divForBrowserTest = document.createElement('div');
-		divForBrowserTest.setAttribute('style', 'transition:top 1s ease;');
+		divForBrowserTest.setAttribute('style', 'animationName:"foo";');
 		document.body.appendChild(divForBrowserTest);
-		var sefariaSupportedBrowser = !!(divForBrowserTest.style.transition);
+		var sefariaSupportedBrowser = !!(divForBrowserTest.style.animationName);
 		divForBrowserTest.parentNode.removeChild(divForBrowserTest);
 		divForBrowserTest = null;
 		
-		alert(sefariaSupportedBrowser)
+		alert(sefariaSupportedBrowser);
 
 		if (sefariaSupportedBrowser == false) {
 		$("#alertMessage").html('<strong>Warning:</strong> Your browser is out of date and unsupported by Sefaria<br/>Please use a more up to date browser or download one <a href="http://browsehappy.com/" target="_blank">here</a>.').show();
