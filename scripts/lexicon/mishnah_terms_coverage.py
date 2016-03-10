69f1a3f6c0391f35454f2a9f3ce495f17a5e79fc
Ephraim Damboritz <ephraim@sefaria.org>
2016-03-10 19:52:30 +0200

    add frequency to mishnah HTS coverage script

--- scripts/lexicon/mishnah_terms_coverage.py
+++ scripts/lexicon/mishnah_terms_coverage.py
@@ -23,16 +23,17 @@ with open("/var/tmp/mishnah_term_coverage.csv", 'wb+') as outfile:
                     match = remove_punctuation(match.strip().lower())
                     if len(match):
                         if match in results:
+                            results[match]['frequency'] += 1
                             if results[match]['in_lexicon'] is False:
                                 results[match]['locations'].append(Ref("%s %s.%s" % (mishnah.title, chn, msn)).normal())
                         else:
                             wf = WordForm().load({'form': match})
                             if wf:
-                                results[match] = {'in_lexicon' : True}
+                                results[match] = {'in_lexicon' : True, 'frequency': 1}
                             else:
-                                results[match] = {'in_lexicon' : False, 'locations': [Ref("%s %s.%s" % (mishnah.title, chn, msn)).normal()]}
+                                results[match] = {'in_lexicon' : False, 'locations': [Ref("%s %s.%s" % (mishnah.title, chn, msn)).normal()], 'frequency': 1}
     for result in sorted(results):
-        row = [result.encode('utf-8'), results[result]['in_lexicon'], ";".join(results[result]['locations']) if 'locations' in results[result] else '']
+        row = [result.encode('utf-8'), results[result]['in_lexicon'], results[result]['frequency'],  ";".join(results[result]['locations']) if 'locations' in results[result] else '']
         result_csv.writerow(row)
     print json.dumps(results, sort_keys=True)
 
