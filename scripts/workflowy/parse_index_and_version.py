b3488179cf75f24ce083bf0f359fbcf522a6a3c3
Ephraim Damboritz <ephraim@sefaria.org>
2016-03-20 15:26:37 +0200

    add variant sections and depth ability to Workflowy tool

--- scripts/workflowy/parse_index_and_version.py
+++ scripts/workflowy/parse_index_and_version.py
@@ -69,12 +69,13 @@ class WorkflowyParser(object):
         if self._term_scheme and isinstance(self._term_scheme, basestring):
             self.create_term_scheme()
         # either type of node:
+        ja_sections = self.parse_implied_depth(element)
         titles = self.parse_titles(element)  # an array of titles
         if len(element) == 0:  # length of child nodes
             n = JaggedArrayNode()
-            n.sectionNames = ['Paragraph']
-            n.addressTypes = ['Integer']
-            n.depth = 1
+            n.depth = len(ja_sections['section_names']) if ja_sections else 1
+            n.sectionNames = ja_sections['section_names'] if ja_sections else ['Paragraph']
+            n.addressTypes = ja_sections['address_types'] if ja_sections else ['Integer']
             n.key = titles["enPrim"]
             n = self.add_titles_to_node(n, titles)
         else:  # yes child nodes >> schema node
@@ -92,6 +93,7 @@ class WorkflowyParser(object):
             if text:
                 self.version_info['text'].append({'node': n, 'text': text})
         return n
+
 
     # en & he titles for each element > dict
     def parse_titles(self, element):
@@ -143,6 +145,24 @@ class WorkflowyParser(object):
             self.outline.set('text', re.sub(category_pattern, "", title))
             return categories
         return None
+
+    def parse_implied_depth(self, element):
+        ja_depth_pattern = ur"\[(.*)\]"
+        title_str = element.get('text')
+        sections_match = re.search(ja_depth_pattern, title_str)
+        if sections_match:
+            sections = [s.strip() for s in sections_match.group(1).split(",")]
+            element.set('text', re.sub(ja_depth_pattern, "", title_str))
+            section_names = []
+            address_types = []
+            for s in sections:
+                tpl = s.split(":")
+                section_names.append(tpl[0])
+                address_types.append(tpl[1] if len(tpl) > 1 else 'Integer')
+
+            return {'section_names': section_names, 'address_types' : address_types}
+        else:
+            return None
 
     def extract_version_info(self):
         vinfo_str = self.outline.get("_note")
