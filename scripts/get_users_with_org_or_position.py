b85b915de46a77e9eddd2a695dcb79b20c73725a
Russel Neiss <rneiss@russels-imac.gateway.pace.com>
2016-03-22 19:50:11 -0500

    add script for getting users with org or position listed

---
+++ scripts/get_users_with_org_or_position.py
@@ -0,0 +1,24 @@
+import sys
+import os
+import pymongo
+
+from sefaria.settings import *
+from sefaria.system.database import db
+from sefaria.model.user_profile import UserProfile
+from django.contrib.auth.models import User
+
+
+out = ""
+
+users  = User.objects.all()
+
+for user in users:
+	profile = UserProfile(id=user.id)
+	if profile.organization != "" or profile.position != "":
+		out += "%s,%s,%s,%s,%s\n" % (user.email, user.first_name, user.last_name, profile.organization, profile.position)
+
+print out
+
+f = open('contacts.csv','w')
+f.write(out.encode('utf-8'))
+f.close() 
