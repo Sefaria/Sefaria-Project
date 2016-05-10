e07a613ba0214b5d5c372dc4e36537ee2599c072
brett lockspeiser <blocks@gmail.com>
2016-05-09 18:13:22 -0700

    Working with React Server code copied from GitHub

--- node/main.js
+++ node/main.js
@@ -1,7 +1,68 @@
-var http = require('http');
+// Initially copypasta'd from https://github.com/mhart/react-server-example
+// https://github.com/mhart/react-server-example/blob/master/server.js
 
-var server = http.createServer(function(req, res) {
-  res.writeHead(200);
-  res.end('Hello Http');
-});
-server.listen(4040);
+var http = require('http'),
+    browserify = require('browserify'),
+    literalify = require('literalify'),
+    React = require('react'),
+    ReactDOMServer = require('react-dom/server'),
+    DOM = React.DOM, body = DOM.body, div = DOM.div, script = DOM.script,
+    // This is our React component, shared by server and browser thanks to browserify
+    App = React.createFactory(require('../static/js/s2.js'));
+
+
+// Just create a plain old HTTP server that responds to endpoints correspond to React Class names.
+http.createServer(function(req, res) {
+
+  // If we hit the homepage, then we want to serve up some HTML - including the
+  // server-side rendered React component(s), as well as the script tags
+  // pointing to the client-side code
+  if (req.url == '/ReaderApp') {
+
+    res.setHeader('Content-Type', 'text/html')
+
+    // `props` represents the data to be passed in to the React component for
+    // rendering - just as you would pass data, or expose variables in
+    // templates such as Jade or Handlebars.  We just use some dummy data
+    // here (with some potentially dangerous values for testing), but you could
+    // imagine this would be objects typically fetched async from a DB,
+    // filesystem or API, depending on the logged-in user, etc.
+    var props = {
+      items: [
+        'Item 0',
+        'Item 1',
+        'Item </script>',
+        'Item <!--inject!-->',
+      ]
+    }
+
+    // Here we're using React to render the outer body, so we just use the
+    // simpler renderToStaticMarkup function, but you could use any templating
+    // language (or just a string) for the outer page template
+    var html = ReactDOMServer.renderToStaticMarkup(body(null,
+
+      // The actual server-side rendering of our component occurs here, and we
+      // pass our data in as `props`. This div is the same one that the client
+      // will "render" into on the browser from browser.js
+      div({id: 'content', dangerouslySetInnerHTML: {__html:
+        ReactDOMServer.renderToString(App(props))
+      }}),
+
+    ));
+
+    // Return the page to the browser
+    res.end(html)
+
+  }
+
+// The http server listens on port 4040, TODO read from package.json config
+}).listen(4040, function(err) {
+  if (err) throw err;
+  console.log('Listening on 4040...');
+})
+
+
+// A utility function to safely escape JSON for embedding in a <script> tag
+function safeStringify(obj) {
+  return JSON.stringify(obj).replace(/<\/script/g, '<\\/script').replace(/<!--/g, '<\\!--');
+}
