ef9b934857df927ac07dbbc2b3f308b8171bafde
Palmer Paul <pzpaul2002@yahoo.com>
2016-05-27 08:36:19 -0400

    add client side js for exporting a sheet to Google Drive

--- static/js/sheets.js
+++ static/js/sheets.js
@@ -872,6 +872,9 @@ $(function() {
 	// ---------- Delete Sheet ----------------
 	$("#deleteSheet").click(deleteSheet);
 
+	// ---------- Export Sheet to Google Drive ----------------
+	$("#exportToDrive").click(exportToDrive);
+
 
 	// ------- Sheet Tags --------------
 	sjs.sheetTagger.init(sjs.current.id, sjs.current.tags);
@@ -2898,6 +2901,15 @@ function copySheet() {
 
 }
 
+function exportToDrive() {
+	$.post("/api/sheets/" + sjs.current.id + "/export_to_drive", function(data) {
+		if ("error" in data) {
+			sjs.alert.message(data.error.message);
+		} else {
+			sjs.alert.message("Source Sheet exported to Google Drive.<br><br><a href='" + data.webViewLink + "'></a>");
+		}
+	})
+}
 
 function showEmebed() { 
 	$("#embedSheetModal").show().position({of: window})
