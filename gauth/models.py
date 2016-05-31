edcd96ef87d98c3b3e95e96d458f3eb93fe0383e
Palmer Paul <pzpaul2002@yahoo.com>
2016-05-30 22:11:12 -0400

    add FlowModel to gauth models

--- gauth/models.py
+++ gauth/models.py
@@ -9,6 +9,15 @@ from oauth2client.contrib.django_orm import FlowField
 from oauth2client.contrib.django_orm import CredentialsField
 
 
+class FlowModel(models.Model):
+    id = models.ForeignKey(User, primary_key=True)
+    flow = FlowField()
+
+
+class FlowAdmin(admin.ModelAdmin):
+    pass
+
+
 class CredentialsModel(models.Model):
     id = models.ForeignKey(User, primary_key=True)
     credential = CredentialsField()
@@ -18,4 +27,5 @@ class CredentialsAdmin(admin.ModelAdmin):
     pass
 
 
+admin.site.register(FlowModel, FlowAdmin)
 admin.site.register(CredentialsModel, CredentialsAdmin)
