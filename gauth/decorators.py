f9b585ffc15df13d00985d45c39243d0d74a330e
Palmer Paul <pzpaul2002@yahoo.com>
2016-06-01 15:37:02 -0400

    make gauth_required require the user to be logged in

--- gauth/decorators.py
+++ gauth/decorators.py
@@ -1,12 +1,13 @@
 from functools import wraps
 
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from oauth2client.contrib.django_orm import Storage
 
 from gauth.models import CredentialsModel
 
 
-def gauth_required(func):
+def gauth_required(scope):
     """
     Decorator that requires the user to authenticate
     with Google and authorize Sefaria to act on their behalf.
@@ -15,15 +16,19 @@ def gauth_required(func):
     If not, it will start the OAuth 2.0 flow.
     At the moment, only used for sheets.views.export_to_drive.
     """
-    @wraps(func)
-    def inner(request, *args, **kwargs):
-        storage = Storage(CredentialsModel, 'id', request.user, 'credential')
-        credential = storage.get()
+    def wrapper(func):
+        @login_required
+        @wraps(func)
+        def inner(request, *args, **kwargs):
+            storage = Storage(CredentialsModel, 'id',
+                              request.user, 'credential')
+            credential = storage.get()
 
-        if credential is None or credential.invalid:
-            request.session['next_view'] = request.path
-            return HttpResponseRedirect('/gauth')
+            if credential is None or credential.invalid:
+                request.session['next_view'] = request.path
+                return HttpResponseRedirect('/gauth?scope={}'.format(scope))
 
-        kwargs['credential'] = credential
-        return func(request, *args, **kwargs)
-    return inner
+            kwargs['credential'] = credential
+            return func(request, *args, **kwargs)
+        return inner
+    return wrapper
