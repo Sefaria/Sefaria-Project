66b1f7f8541253bbd2132d5d18716f62c66d4903
brett lockspeiser <blocks@gmail.com>
2016-06-08 19:46:04 -0700

    Restore lost prop

--- static/js/s2.js
+++ static/js/s2.js
@@ -1682,6 +1682,7 @@ var ReaderPanel = React.createClass({
     if (this.state.menuOpen === "home" || this.state.menuOpen == "navigation" || this.state.menuOpen == "compare") {
       var menu = React.createElement(ReaderNavigationMenu, {
         home: this.state.menuOpen === "home",
+        compare: this.state.menuOpen === "compare",
         interfaceLang: this.props.interfaceLang,
         multiPanel: this.props.multiPanel,
         categories: this.state.navigationCategories || [],
