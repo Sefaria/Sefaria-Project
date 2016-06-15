1ab23a07f78653d08f31f3e885e02129cebddc97
Russel Neiss <russel@sefaria.org>
2016-06-15 14:36:51 -0500

    Fix menubar accidentally closing editing panels

--- static/js/sheets.js
+++ static/js/sheets.js
@@ -1310,7 +1310,7 @@ $(function() {
 
 			$("html").on("click", "#content", function (e) {
 				//clicked off of a sheetitem
-				if ($(e.target).closest(".sheetItem").length) {
+				if ($(e.target).closest(".sheetItem").length || $(e.target).closest(".sheetsEditNavTop") ) {
 					return;
 				}
 				if ($(e.target).closest("#addInterface").length) return
