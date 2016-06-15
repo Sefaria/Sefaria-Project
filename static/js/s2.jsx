443f5445aea4bafb4dc545ea4a4c0963b46dbd7c
Lev Israel <eliezer.israel@gmail.com>
2016-06-15 09:22:46 +0300

    Complete enabling of outOfAppLinks on site TOC

--- static/js/s2.jsx
+++ static/js/s2.jsx
@@ -2129,7 +2129,7 @@ var ReaderNavigationMenu = React.createClass({
     return recentlyViewed;
   },
   handleClick: function(event) {
-    if (!$(event.target).hasClass("outOfAppLink")) {
+    if (!$(event.target).hasClass("outOfAppLink") && !$(event.target.parentElement).hasClass("outOfAppLink")) {
       event.preventDefault();
     }
     if ($(event.target).hasClass("refLink") || $(event.target).parent().hasClass("refLink")) {
