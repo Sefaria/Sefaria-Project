bfecf1ad63f84605d0aac483d97b0bc35b9a15ed
Noah Santacruz <noahssantacruz@gmail.com>
2016-07-20 13:13:47 +0300

    cleaned up code

--- manage.py
+++ manage.py
@@ -5,10 +5,6 @@ import sys
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sefaria.settings")
 
-    from sefaria.export import export_texts
-
-    export_texts()
-
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
