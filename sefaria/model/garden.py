85853be16b9467ff1291537b130f4cdea1566ce2
Lev Israel <eliezer.israel@gmail.com>
2016-08-07 16:31:29 +0300

    Allow per-garden override of number of filters in a chart. Clearfix for when filters are longer then results.

--- sefaria/model/garden.py
+++ sefaria/model/garden.py
@@ -38,7 +38,8 @@ class Garden(abst.AbstractMongoRecord):
     default_config = {
         "timeline_scale": "log",  # log / linear
         "timeline_bin_size": None,  # Number of years in a bin.  Defaults to ~20 bins in the extent
-        "filter_order" : [],
+        "filter_order": [],
+        "filter_rows": 9,  # Number of rows in row filters
         "filters": {
             "default": {
                 "en": "Tags",
