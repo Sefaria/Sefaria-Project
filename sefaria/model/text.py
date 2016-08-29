6a7e3319de4a4a726b5243d1a9a6052211a5d50b
brett lockspeiser <blocks@gmail.com>
2016-08-29 14:25:06 -0700

    Index method to return schema with content counts

--- sefaria/model/text.py
+++ sefaria/model/text.py
@@ -120,6 +120,34 @@ class AbstractIndex(object):
     def publication_time_period(self):
         return None
 
+    def contents_with_content_counts(self):
+        """
+        Returns the `contents` dictionary with each node annotated with section lengths info
+        from version_state.
+        """
+        contents = self.contents(v2=True)
+        vstate   = self.versionState()
+
+        def simplify_version_state(vstate_node):
+            return aggregate_available_texts(vstate_node["_all"]["availableTexts"])
+
+        def aggregate_available_texts(available):
+            """Returns a jagged arrary of ints that counts the number of segments in each section,
+            (by throwing out the number of versions of each segment)""" 
+            if len(available) == 0 or type(available[0]) is int:
+                return len(available)
+            else:
+                return [aggregate_available_texts(x) for x in available]
+
+        def annotate_schema(schema, vstate):
+            if "nodes" in schema:
+                for node in schema["nodes"]:
+                    annotate_schema(node, vstate[node["key"]])
+            else:
+                schema["content_counts"] = simplify_version_state(vstate)
+
+        annotate_schema(contents["schema"], vstate.content)
+        return contents
 
 
 class Index(abst.AbstractMongoRecord, AbstractIndex):
@@ -570,6 +598,7 @@ class Index(abst.AbstractMongoRecord, AbstractIndex):
             self.maps[i]["to"] = nref
 
     def toc_contents(self):
+        """Returns to a dictionary used to represent this text in the library wide Table of Contents"""
         firstSection = Ref(self.title).first_available_section_ref()
         toc_contents_dict = {
             "title": self.get_title(),
@@ -592,6 +621,7 @@ class Index(abst.AbstractMongoRecord, AbstractIndex):
                     pass
 
         return toc_contents_dict
+
 
 class IndexSet(abst.AbstractMongoSet):
     """
