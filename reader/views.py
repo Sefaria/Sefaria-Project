03f813f28558df00a1c03d19eaf178c6308b3bee
Ephraim Damboritz <ephraim@sefaria.org>
2016-09-01 10:30:11 +0300

    copy text for bilingual error pages

--- reader/views.py
+++ reader/views.py
@@ -2951,7 +2951,7 @@ def custom_server_error(request, template_name='500.html'):
     500 error handler.
 
     Templates: `500.html`
-    Context: None
+    Context: RequestContext
     """
     t = loader.get_template(template_name) # You need to create a 500.html template.
     return http.HttpResponseServerError(t.render(RequestContext(request, {'request_path': request.path})))
