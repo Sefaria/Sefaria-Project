053dd39d5f91262b6045737d5d1b3519a05db3c4
Lev Israel <eliezer.israel@gmail.com>
2016-10-30 16:34:39 +0200

    Global Announcement Update

--- scripts/init_global_notes.py
+++ scripts/init_global_notes.py
@@ -11,13 +11,13 @@ GlobalNotification()\
     .save()
 
 GlobalNotification()\
-    .set_en("Two more volumes of Teshuvot from <a href='http://www.eretzhemdah.org/'>Eretz Hemdah</a> - B'Mareh HaBazak <a href=\"B'Mareh_HaBazak_Part_Six\">Six</a> and <a href=\"B'Mareh_HaBazak_Part_Eight\">Eight</a>")\
+    .set_en("Two more volumes of Teshuvot from <a href='http://www.eretzhemdah.org/'>Eretz Hemdah</a> - B'Mareh HaBazak <a href=\"B'Mareh_HaBazak_Part_Six\">Six</a> and <a href=\"B'Mareh_HaBazak_Part_Eight\">Eight</a>.")\
     .set_he(u"שני כרכים נוספים של שו״ת <a href='http://www.eretzhemdah.org/'>ארץ חמדה</a>- במראה הבזק חלקים <a href=\"B'Mareh_HaBazak_Part_Six\">ו׳</a> ו-<a href=\"B'Mareh_HaBazak_Part_Eight\">ח׳</a>")\
     .set_date(datetime(2016, 9, 17, 8, 20, 0)) \
     .save()
 
 GlobalNotification()\
-    .set_en("Every comment of the <a href='/Mishnah_Berurah'>Mishnah Berurah</a> is now linked to the <a href='/Shulchan_Arukh,_Orach_Chayim'>Shulchan Aruch</a>")\
+    .set_en("Every comment of the <a href='/Mishnah_Berurah'>Mishnah Berurah</a> is now linked to the <a href='/Shulchan_Arukh,_Orach_Chayim'>Shulchan Aruch</a>.")\
     .set_he(u"כל הערות <a href='/Mishnah_Berurah'>המשנה ברורה</a> מקושרות ל<a href='/Shulchan_Arukh,_Orach_Chayim'>שלחן הערוך</a>")\
     .set_date(datetime(2016,9,21,12,0,0))\
     .save()
@@ -42,11 +42,26 @@ GlobalNotification()\
     .save()
 
 GlobalNotification()\
-    .make_index(library.get_index("Shev_Shmat'ta")) \
+    .make_index(library.get_index("Shev Shmat'ta")) \
     .set_date(datetime(2016, 10, 9, 16, 0, 0)) \
-    .set_en("Shev Shmat'ta: composed of seven intricate halachic discussions, this work is widely popular in Yeshivah curricula") \
+    .set_en("Shev Shmat'ta: composed of seven intricate halachic discussions, this work is widely popular in Yeshivah curricula.") \
     .set_he(u"שב שמעעתא, חיבור העוסק בשבע סוגיות תלמודיות סבוכות. נחשב כפופלרי בעולם הישיבות.") \
     .save()
+
+GlobalNotification()\
+    .make_index(library.get_index("Psalms")) \
+    .set_date(datetime(2016, 10, 30, 14, 30, 0)) \
+    .set_en("At the request of our users, we've added the 30 day cycle of readings to our edition of Psalms.") \
+    .set_he(u"לבקשת הגולשים, הוספנו את הקריאות היומיות (30 יום) לגירסת התהילים שלנו.") \
+    .save()
+
+GlobalNotification()\
+    .make_index(library.get_index("Iggeret haGra")) \
+    .set_date(datetime(2016, 10, 30, 15, 00, 0)) \
+    .set_en("Sent by the Vilna Gaon while traveling to Eretz Yisrael, this letter instructed his family in the ways of Mussar.") \
+    .set_he(u'אגרת מוסר מפורסמת שכתב הגר"א לבני ביתו טרם נסיעתו לארץ ישראל') \
+    .save()
+
 
 #Index: Mei HaShiloah
 #General: Ein Mishpat on ??
