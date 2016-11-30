5d6007ab6ef4fd38158d031d67013e17693602d1
brett lockspeiser <blocks@gmail.com>
2016-11-29 16:59:14 -0800

    Code for past contributor counts

--- scripts/contributor_stats.py
+++ scripts/contributor_stats.py
@@ -7,3 +7,14 @@ all_contributors = set(db.history.distinct("user")+all_sheet_makers)
 print "All Sheet Makers: %d" % len(all_sheet_makers)
 print "Public Sheet Makers: %d" % len(public_sheet_makers)
 print "Public contributors and source sheet makers: %d" % len(all_contributors)
+
+"""
+// Getting counts from times past
+from dateutil.relativedelta import relativedelta
+start = datetime.today() - relativedelta(years=1)
+query = {"dateCreated": {"$lt": start.isoformat()}}
+
+sheet_makers = db.sheets.find(query).distinct("owner")
+contributors = db.history.find({"date": {"$lt": start}}).distinct("user")
+print len(set(sheet_makers+contributors))
+"""
