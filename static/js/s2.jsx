78a3252cb9fc55eba7cb6b22866c2be67b7aae13
Ephraim Damboritz <ephraim@sefaria.org>
2016-12-11 17:55:48 +0200

    Add whiteList to title rendering so certain text titles aren't manipulated for the ToC. [ci skip]

--- static/js/s2.jsx
+++ static/js/s2.jsx
@@ -2778,11 +2778,21 @@ var ReaderNavigationCategoryMenuContents = React.createClass({
     width:      React.PropTypes.number,
     nestLevel:   React.PropTypes.number
   },
+  getRenderedTextTitleString: function(title, heTitle){
+    var whiteList = ['Midrash Mishlei', 'Midrash Tehillim', 'Midrash Tanchuma'];
+    var displayCategory = this.props.category;
+    var displayHeCategory = Sefaria.hebrewCategory(this.props.category);
+    if (whiteList.indexOf(title) == -1){
+      var titleRe = new RegExp(`^(Mishneh Torah,|Shulchan Arukh,|Jerusalem Talmud|${displayCategory})( on )?`);
+      var heTitleRe = new RegExp(`^(משנה תורה, |תלמוד ירושלמי|${displayHeCategory})( על )?`);
+      title   = title == displayCategory ? title : title.replace(titleRe, "");
+      heTitle = heTitle == displayHeCategory ? heTitle : heTitle.replace(heTitleRe, "");
+    }
+    return [title, heTitle];
+  },
   render: function() {
       var content = [];
       var cats = this.props.categories || [];
-      var displayCategory = this.props.category;
-      var displayHeCategory = Sefaria.hebrewCategory(this.props.category);
       for (var i = 0; i < this.props.contents.length; i++) {
         var item = this.props.contents[i];
         if (item.category) {
@@ -2792,10 +2802,7 @@ var ReaderNavigationCategoryMenuContents = React.createClass({
           if (Sefaria.util.inArray(item.category, subcats) > -1 || this.props.nestLevel > 0) {
             if(item.contents.length == 1 && !("category" in item.contents[0])){
                 var chItem = item.contents[0]
-                var titleRe = new RegExp(`^(Mishneh Torah,|Shulchan Arukh,|Jerusalem Talmud|${displayCategory})( on )?`);
-                var title   = chItem.title == displayCategory ? chItem.title : chItem.title.replace(titleRe, "");
-                var heTitleRe = new RegExp(`^(משנה תורה, |תלמוד ירושלמי|${displayHeCategory})( על )?`);
-                var heTitle = chItem.heTitle == displayHeCategory ? chItem.heTitle : chItem.heTitle.replace(heTitleRe, "");
+                var [title, heTitle] = this.getRenderedTextTitleString(chItem.title, chItem.heTitle);
                 var url     = "/" + Sefaria.normRef(chItem.firstSection);
                 content.push((<a href={url}>
                                 <span className={'refLink sparse' + chItem.sparseness} data-ref={chItem.firstSection} key={"text." + this.props.nestLevel + "." + i}>
@@ -2824,10 +2831,7 @@ var ReaderNavigationCategoryMenuContents = React.createClass({
           }
         } else {
           // Add a Text
-          var titleRe = new RegExp(`^(Mishneh Torah,|Shulchan Arukh,|Jerusalem Talmud|${displayCategory})( on )?`);
-          var title   = item.title == displayCategory ? item.title : item.title.replace(titleRe, "");
-          var heTitleRe = new RegExp(`^(משנה תורה, |תלמוד ירושלמי|${displayHeCategory})( על )?`);
-          var heTitle = item.heTitle == displayHeCategory ? item.heTitle : item.heTitle.replace(heTitleRe, "");
+          var [title, heTitle] = this.getRenderedTextTitleString(item.title, item.heTitle);
           var url     = "/" + Sefaria.normRef(item.firstSection);
           content.push((<a href={url}>
                           <span className={'refLink sparse' + item.sparseness} data-ref={item.firstSection} key={"text." + this.props.nestLevel + "." + i}>
