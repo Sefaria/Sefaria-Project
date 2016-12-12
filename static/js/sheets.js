4e264daef27891aaf67baecabf16ff9e15e27d52
Russel Neiss <russel@sefaria.org>
2016-12-12 15:20:00 -0600

    remove erroneous autosave code that was causing comments and other content to not save

--- static/js/sheets.js
+++ static/js/sheets.js
@@ -875,9 +875,7 @@ $(function() {
 			$('.cke_editable').each(function() {
 				sjs.removeCKEditorByElement(this);
 			});
-		  if ($("#lastSaved").text('Changes Pending...') ) {
-				autoSave();
-			}
+
 		});
 	}
 
