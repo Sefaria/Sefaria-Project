6fc4721b2cdaac502970cdcda7254d55568fc583
Lev Israel <eliezer.israel@gmail.com>
2016-12-13 16:54:18 +0200

    remove_zeros_from_rashi_tosafot.py

---
+++ scripts/remove_zeros_from_rashi_tosafot.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from sefaria.model import *
+
+for v in library.get_commentary_versions(["Rashi","Tosafot"]):
+    assert isinstance(v, Version)
+    print u"{} {}".format(v.title, v.versionTitle)
+    index = v.get_index()
+    for sref in index.all_top_section_refs():
+        tc = TextChunk(sref, v.language, v.versionTitle)
+        if [0] in tc.text:
+            tc.text = [s if s != [0] else [] for s in tc.text]
+            tc.save()
+            print "Fixed {}".format(sref.normal())
