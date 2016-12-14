a83e7c7673ac56afc3596e13a54f7f01ec01a372
brett lockspeiser <blocks@gmail.com>
2016-12-14 14:17:40 -0800

    Move cloudflare utils to their own file

--- sefaria/system/cache.py
+++ sefaria/system/cache.py
@@ -6,9 +6,6 @@ try:
     from sefaria.settings import USE_VARNISH
 except ImportError:
     USE_VARNISH = False
-from sefaria.settings import CLOUDFLARE_ZONE, CLOUDFLARE_EMAIL, CLOUDFLARE_TOKEN
-
-
 
 if not hasattr(sys, '_doc_build'):
     from django.core.cache import cache
@@ -115,43 +112,3 @@ def generate_text_toc_cache_key(index_name):
     for zoom in [0,1,2]: #most commonly used values
         keys.append(cache_get_key(*['make_toc_html', index_name], **{'zoom' : zoom}))
     return keys
-
-
-def purge_cloudflare_url(path):
-    """ Calls the Cloudflare API to invalidate cache for the file at current site and `path`"""
-    import requests
-    import json
-    from django.contrib.sites.models import Site
-
-    current_site = Site.objects.get_current()
-    domain = current_site.domain
-    files = ["http://{}{}".format(domain, path)]
-    url = 'https://api.cloudflare.com/client/v4/zones/%s/purge_cache' % CLOUDFLARE_ZONE
-    payload = {"files": files}
-    headers = {
-        "X-Auth-Email": CLOUDFLARE_EMAIL,
-        "X-Auth-Key": CLOUDFLARE_TOKEN,
-        "Content-Type": "application/json",
-    }
-    r = requests.delete(url, data=json.dumps(payload), headers=headers)
-    return r
-
-
-def purge_cloudflare():
-    """Purge the entire Cloudflare cache"""
-    import requests
-    import json
-
-    url = 'https://api.cloudflare.com/client/v4/zones/%s/purge_cache' % CLOUDFLARE_ZONE
-    headers = {
-        "X-Auth-Email": CLOUDFLARE_EMAIL,
-        "X-Auth-Key": CLOUDFLARE_TOKEN,
-        "Content-Type": "application/json",
-    }
-    r = requests.delete(url, data=json.dumps({"purge_everything":True}), headers=headers)
-    return r
-
-
-
-def purge_static_files_from_cloudflare():
-    pass
