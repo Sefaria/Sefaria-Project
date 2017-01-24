2c2c3737b71a502e3e724f12213da27487b84de6
brett lockspeiser <blocks@gmail.com>
2017-01-23 16:06:38 -0800

    Polyfill for Array.fill()

--- static/js/sefaria.js
+++ static/js/sefaria.js
@@ -1914,6 +1914,37 @@ Sefaria.util = {
             return this; // for testing purposes
         };
 
+        if (!Array.prototype.fill) {
+          Object.defineProperty(Array.prototype, 'fill', {
+            value: function(value) {
+
+              if (this == null) {
+                throw new TypeError('this is null or not defined');
+              }
+
+              var O = Object(this);
+              var len = O.length >>> 0;
+
+              var start = arguments[1];
+              var relativeStart = start >> 0;
+              var k = relativeStart < 0 ?
+                Math.max(len + relativeStart, 0) :
+                Math.min(relativeStart, len);
+              var end = arguments[2];
+              var relativeEnd = end === undefined ?
+                len : end >> 0;
+              var final = relativeEnd < 0 ?
+                Math.max(len + relativeEnd, 0) :
+                Math.min(relativeEnd, len);
+              while (k < final) {
+                O[k] = value;
+                k++;
+              }
+              return O;
+            }
+          });
+        }
+
         RegExp.escape = function(s) {
             return s.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
         };
