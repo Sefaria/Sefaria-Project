5b78825e29fcec4ea8f4779df80781cc3bf5fefc
Lev Israel <eliezer.israel@gmail.com>
2017-02-20 17:03:38 +0200

    If a segment is empty, merge it to u"", not 0.

--- sefaria/model/text.py
+++ sefaria/model/text.py
@@ -1110,8 +1110,8 @@ def merge_texts(text, sources):
     if depth > 2:
         results = []
         result_sources = []
-        for x in range(max(map(len, text))):
-            translations = map(None, *text)[x]
+        for x in range(max(map(len, text))):    # Let longest text determine how many times to iterate
+            translations = map(None, *text)[x]  # transpose, and take section x
             remove_nones = lambda x: x or []
             result, source = merge_texts(map(remove_nones, translations), sources)
             results.append(result)
@@ -1124,12 +1124,12 @@ def merge_texts(text, sources):
     if depth == 1:
         text = map(lambda x: [x], text)
 
-    merged = map(None, *text)
+    merged = map(None, *text)  # transpose
     text = []
     text_sources = []
     for verses in merged:
         # Look for the first non empty version (which will be the oldest, or one with highest priority)
-        index, value = 0, 0
+        index, value = 0, u""
         for i, version in enumerate(verses):
             if version:
                 index = i
