2ee2a86d81db4d65b57af86424b3a69415f75f1c
Lev Israel <eliezer.israel@gmail.com>
2017-04-24 12:35:33 +0300

    Quicker cleanup of excess newlines in Tanchuma

--- scripts/rewrite_rashis_in_tanchuma.py
+++ scripts/rewrite_rashis_in_tanchuma.py
@@ -105,9 +105,10 @@ def execute():
         print count
         line = line.decode('utf-8')
         other_ref, vtitle = info[count]
-        modify_text(15399, Ref(other_ref), vtitle, 'en', line.strip())
-
-
+        # modify_text(15399, Ref(other_ref), vtitle, 'en', line.strip())
+        tc = TextChunk(Ref(other_ref), vtitle=vtitle, lang="en")
+        tc.text = tc.text.strip()
+        tc.save()
 
 def write():
     mappings = get_mappings("data/tanchuma_map.csv")
