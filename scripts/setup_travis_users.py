348297ba06333eca1a8969ac74dc3953b0aee27e
Lev Israel <eliezer.israel@gmail.com>
2017-05-29 22:31:54 +0300

    Cowardly back down from testing user login

--- scripts/setup_travis_users.py
+++ scripts/setup_travis_users.py
@@ -2,12 +2,12 @@ import os
 
 from django.contrib.auth.models import User
 
-User.objects.create_user(username='Test User',
-                         email=os.environ["SEFARIA_TEST_USER"],
-                         password=os.environ["SEFARIA_TEST_PASS"])
-
 users = User.objects.all()
 user = users[0]
 if user.email == os.environ["SEFARIA_SUPERUSER"]:
     user.username = "Test Super User"
     user.set_password(os.environ["SEFARIA_SUPERPASS"])
+
+User.objects.create_user(username='Test User',
+                         email=os.environ["SEFARIA_TEST_USER"],
+                         password=os.environ["SEFARIA_TEST_PASS"])
