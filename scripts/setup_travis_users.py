366cbffadb497742b98db39851129fe642ca5442
Lev Israel <eliezer.israel@gmail.com>
2017-06-01 15:50:53 +0300

    Use email as username

--- scripts/setup_travis_users.py
+++ scripts/setup_travis_users.py
@@ -5,9 +5,10 @@ from django.contrib.auth.models import User
 users = User.objects.all()
 user = users[0]
 if user.email == os.environ["SEFARIA_SUPERUSER"]:
-    user.username = "Test Super User"
+    user.username = os.environ["SEFARIA_SUPERUSER"]
     user.set_password(os.environ["SEFARIA_SUPERPASS"])
 
-User.objects.create_user(username='Test User',
+User.objects.create_user(username=os.environ["SEFARIA_TEST_USER"],
                          email=os.environ["SEFARIA_TEST_USER"],
                          password=os.environ["SEFARIA_TEST_PASS"])
+
