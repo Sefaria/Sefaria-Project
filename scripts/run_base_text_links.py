746fb63947e49a3612d76d35f22a65cdf974f149
Ephraim Damboritz <ephraim@sefaria.org>
2017-06-08 15:39:00 +0300

    fix typo

--- scripts/run_base_text_links.py
+++ scripts/run_base_text_links.py
@@ -6,7 +6,7 @@ for idx in idxset:
     if len(getattr(idx, 'base_text_titles', [])) > 1:
         for btitle in idx.base_text_titles:
             flink = LinkSet.get_first_ref_in_linkset(btitle, idx.title)
-            if flink:1
+            if flink:
                 print "{} - {}) [{}] {}".format(idx.title, btitle, flink.refs, flink.type)
                 flink.is_first_comment=True
                 flink.save(override_dependencies=True)
