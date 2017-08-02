6877f205a02a8ba8d74a7ced19b081cd0db27dde
brett lockspeiser <blocks@gmail.com>
2017-08-02 15:54:02 -0700

    Restore S2 header to edit page / clean up unused code on edit page

--- static/js/sefaria/sefariaGa.js
+++ static/js/sefaria/sefariaGa.js
@@ -1,8 +1,9 @@
 var ga;
 if (typeof document !== 'undefined' ) {
-  ga        = DJANGO_VARS.ga;
+  ga = DJANGO_VARS.ga;
 } else {
-  ga    = function() {}; // Fail gracefully if we reach one of these methods server side
+  ga = function() {}; // Fail gracefully if we reach one of these methods server side
+  ga._mock = true;
 }
 
 module.exports = ga;
