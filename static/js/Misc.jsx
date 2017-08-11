c9b9b5fdc36dff68c7871f11ba6fce0abbb550e3
Russel Neiss <russel@sefaria.org>
2017-08-11 10:17:32 -0500

    Sets `outline` attr to `none` on all elements unless user is using keyboard to navigate

--- static/js/Misc.jsx
+++ static/js/Misc.jsx
@@ -129,7 +129,7 @@ class ToggleSet extends Component {
     classes = classNames(classes);
     var value = this.props.name === "layout" ? this.props.currentLayout() : this.props.settings[this.props.name];
     var width = 100.0 - (this.props.separated ? (this.props.options.length - 1) * 3 : 0);
-    var style = {width: (width/this.props.options.length) + "%", outline: "none"};
+    var style = {width: (width/this.props.options.length) + "%"};
     return (
       <div className={classes} role={this.props.role} aria-label={this.props.ariaLabel}>
         {
