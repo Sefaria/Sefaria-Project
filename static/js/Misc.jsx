78522c90e54406cc3a32b75b2f762502681b5355
brett lockspeiser <blocks@gmail.com>
2017-08-17 11:15:11 -0700

    Link component

--- static/js/Misc.jsx
+++ static/js/Misc.jsx
@@ -6,6 +6,25 @@ const classNames = require('classnames');
 const PropTypes  = require('prop-types');
 import Component      from 'react-class';
 
+class Link extends Component {
+  handleClick(e) {
+    e.preventDefault();
+    this.props.onClick();
+  }
+  render() {
+    return <a 
+              className={this.props.className} 
+              href={this.props.href}
+              onClick={this.handleClick}
+              title={this.props.title}>{this.props.children}</a>
+  }
+}
+Link.propTypes = {
+  href:    PropTypes.string.isRequired,
+  onClick: PropTypes.func,
+  title:   PropTypes.string.isRequired,
+}
+
 
 class GlobalWarningMessage extends Component {
   close() {
@@ -633,6 +652,7 @@ var backToS1 = function() {
   window.location = "/";
 };
 
+
 module.exports.backToS1                                  = backToS1;
 module.exports.BlockLink                                 = BlockLink;
 module.exports.CategoryColorLine                         = CategoryColorLine;
@@ -641,6 +661,7 @@ module.exports.Dropdown                                  = Dropdown;
 module.exports.GlobalWarningMessage                      = GlobalWarningMessage;
 module.exports.InterruptingMessage                       = InterruptingMessage;
 module.exports.LanguageToggleButton                      = LanguageToggleButton;
+module.exports.Link                                      = Link;
 module.exports.LoadingMessage                            = LoadingMessage;
 module.exports.LoginPrompt                               = LoginPrompt;
 module.exports.Note                                      = Note;
