1ccb7eb827f659adbb94aad05011453f776aeb18
brett lockspeiser <blocks@gmail.com>
2017-08-28 16:31:37 -0700

    Try making locale/en-us

--- sefaria/settings.py
+++ sefaria/settings.py
@@ -15,11 +15,6 @@ relative_to_abs_path = lambda *x: os.path.join(os.path.dirname(
 # system time zone.
 TIME_ZONE = 'America/Vancouver'
 
-LANGUAGES = [
-  ('en', 'English'),
-  ('he', 'Hebrew'),
-]
-
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
 LANGUAGE_CODE = 'en'
