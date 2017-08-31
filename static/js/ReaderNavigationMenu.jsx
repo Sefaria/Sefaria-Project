66ef4ca119d94e87f660ab165f8ab40e45eeecff
brett lockspeiser <blocks@gmail.com>
2017-08-31 14:38:40 -0700

    Restore topics button on resources list

--- static/js/ReaderNavigationMenu.jsx
+++ static/js/ReaderNavigationMenu.jsx
@@ -203,14 +203,13 @@ class ReaderNavigationMenu extends Component {
                         <span className="int-en">Authors</span>
                         <span className="int-he">רשימת מחברים</span>
                       </a>),
-                    /*
                     (<a className="resourcesLink outOfAppLink" style={sheetsStyle} href="/topics">
                         <img src="/static/img/hashtag-icon.svg" alt="" />
                         <span className="int-en">Topics</span>
                         <span className="int-he">Topics</span>
-                      </a>)*/
+                      </a>)
                       ];
-      resources = (<div className="readerNavCalendar"><TwoOrThreeBox content={resources} width={this.width} /></div>);
+      resources = (<div className="readerNavCalendar"><TwoBox content={resources} width={this.width} /></div>);
 
 
       var topContent = this.props.home ?
