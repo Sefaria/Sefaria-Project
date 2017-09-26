8e993250598c981f707758363414d4af4e791b0d
Lev Israel <eliezer.israel@gmail.com>
2017-09-26 19:49:28 -0400

    Remove superfluous jQuery decorations

--- static/js/sefaria/util.js
+++ static/js/sefaria/util.js
@@ -334,22 +334,6 @@ class Util {
         };
     }
     static setupJQuery() {
-        if (!$.hasOwnProperty("fn")) { return; }
-        $.fn.serializeObject = function() {
-            var o = {};
-            var a = this.serializeArray();
-            $.each(a, function() {
-                if (o[this.name] !== undefined) {
-                    if (!o[this.name].push) {
-                        o[this.name] = [o[this.name]];
-                    }
-                    o[this.name].push(this.value || '');
-                } else {
-                    o[this.name] = this.value || '';
-                }
-            });
-            return o;
-        };
     /*!
          * jQuery Cookie Plugin v1.3
          * https://github.com/carhartl/jquery-cookie
