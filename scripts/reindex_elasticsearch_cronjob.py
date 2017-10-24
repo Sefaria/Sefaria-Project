3f40604391d4118f5dbb6ed8fa60fe560504f827
Noah Santacruz <noahssantacruz@gmail.com>
2017-10-24 16:58:03 +0300

    adding script to reindex elasticsearch

---
+++ scripts/reindex_elasticsearch_cronjob.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+import requests
+from sefaria.model import *
+from sefaria.search import index_all
+from sefaria.local_settings import SEFARIA_BOT_API_KEY
+start_time = datetime.now().replace(second=0, microsecond=0).isoformat()
+index_all(merged=False)
+r = requests.post("https://sefaria.org/admin/index-sheets-by-timestamp", data={"timestamp": start_time, "apikey": SEFARIA_BOT_API_KEY})
+if "error" in r.text:
+    pass
+    # email dev!
