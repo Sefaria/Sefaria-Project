ea552a77cdaf261b2c87a33718ccd1ebcfa35032
brett lockspeiser <blocks@gmail.com>
2017-10-26 20:22:08 -0700

    Fix user agents list

--- sefaria/system/middleware.py
+++ sefaria/system/middleware.py
@@ -41,7 +41,7 @@ class LanguageSettingsMiddleware(object):
         if domain_lang and domain_lang != interface:
             # For crawlers, don't redirect -- just return the pinned language
             no_direct = ("Googlebot", "Bingbot", "Slurp", "DuckDuckBot", "Baiduspider", 
-                            "YandexBot", "Facebot", "facebookexternalhit" "ia_archiver", "Sogou",
+                            "YandexBot", "Facebot", "facebookexternalhit", "ia_archiver", "Sogou",
                             "python-request", "curl", "Wget", "sefaria-node")
             if any([bot in request.META.get('HTTP_USER_AGENT', '') for bot in no_direct]):
                 interface = domain_lang
