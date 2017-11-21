b0b22fd90dbab4c561e2808ab95b1d169bab8c48
Noah Santacruz <noahssantacruz@gmail.com>
2017-11-21 12:04:11 +0200

    make search.py not reliant on pagesheetrank files

--- scripts/reindex_elasticsearch_cronjob.py
+++ scripts/reindex_elasticsearch_cronjob.py
@@ -2,13 +2,15 @@ from datetime import datetime
 import requests
 import os
 from sefaria.model import *
-from sefaria.search import index_all
+from sefaria.search import index_all, init_pagesheetrank_dicts
 from sefaria.local_settings import SEFARIA_BOT_API_KEY
 from sefaria.pagesheetrank import calculate_pagerank, calculate_sheetrank
 
 last_dump = datetime.fromtimestamp(os.path.getmtime("/var/data/sefaria_public/dump/sefaria")).isoformat()
 calculate_pagerank()
 calculate_sheetrank()
+# reinit pagesheetrank after calculation
+init_pagesheetrank_dicts()
 index_all(merged=False)
 r = requests.post("https://www.sefaria.org/admin/index-sheets-by-timestamp", data={"timestamp": last_dump, "apikey": SEFARIA_BOT_API_KEY})
 if "error" in r.text:
