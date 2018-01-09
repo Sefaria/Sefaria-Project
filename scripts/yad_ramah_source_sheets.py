0b2da8c264095c0243938f92adefcf9349f4f2c3
Steven Kaplan <steve@sefaria.org>
2018-01-09 10:03:59 +0200

    fix 9 yad ramah source sheets

---
+++ scripts/yad_ramah_source_sheets.py
@@ -0,0 +1,34 @@
+from sefaria.model import *
+from sefaria.system.database import db
+import csv
+if __name__ == "__main__":
+    f = open("./data/yad_ramah.csv")
+    actual_sources = {}
+    csv_reader = csv.reader(f)
+    for row in csv_reader:
+        id, old, new = row
+        id = int(id)
+        if id not in actual_sources.keys():
+            actual_sources[id] = {}
+        actual_sources[id][old] = new
+    sheets = db.sheets.find()
+
+    for sheet in sheets:
+        if "id" not in sheet.keys():
+            id = -1
+        else:
+            id = sheet["id"]
+        if "id" not in sheet.keys() or id not in actual_sources.keys():
+            continue
+        print id
+        sources = sheet.get("sources", [])
+        for source_n, source in enumerate(sources):
+            if "ref" in source.keys() and source["ref"].startswith("Yad Ramah on Bava Batra"):
+                orig_source_ref = source["ref"]
+                assert actual_sources[id][orig_source_ref]
+                sheet["sources"][source_n]["ref"] = actual_sources[id][orig_source_ref]
+                sheet["sources"][source_n]["heRef"] = Ref(actual_sources[id][orig_source_ref]).he_normal()
+                print "NEW:"
+                print sheet["sources"][source_n]["ref"]
+                print sheet["sources"][source_n]["heRef"]
+                db.sheets.save(sheet)
