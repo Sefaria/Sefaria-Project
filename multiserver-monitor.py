e3e41abd7b7541b48700b7815f9a29d89dddd8d2
Lev Israel <eliezer.israel@gmail.com>
2018-01-11 16:47:54 -0500

    Steps toward a monitor for multiserver arch

---
+++ multiserver-monitor.py
@@ -0,0 +1,22 @@
+import redis
+import json
+import time
+
+from sefaria.local_settings import MULTISERVER_ENABLED, MULTISERVER_REDIS_SERVER, \
+    MULTISERVER_REDIS_PORT, MULTISERVER_REDIS_DB, MULTISERVER_REDIS_EVENT_CHANNEL, \
+    MULTISERVER_REDIS_CONFIRM_CHANNEL
+
+from sefaria.system.multiserver import MultiServerMonitor
+
+import logging
+logger = logging.getLogger(__name__)
+
+
+def log_message():
+    pass
+
+if __name__ == '__main__':
+    monitor = MultiServerMonitor()
+
+    while True:
+        time.sleep(0.2)
