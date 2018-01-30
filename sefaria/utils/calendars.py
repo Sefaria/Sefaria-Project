3aa4acd6364f31622ad03fdae3349cc1048bcd6d
Noah Santacruz <noahssantacruz@gmail.com>
2018-01-30 18:32:29 +0200

    fix the url returned by the parsha calendar api. it is now a correct url

--- sefaria/utils/calendars.py
+++ sefaria/utils/calendars.py
@@ -101,12 +101,13 @@ def this_weeks_parasha(datetime_obj, diaspora=True):
 def parashat_hashavua_and_haftara(datetime_obj, diaspora=True):
     parasha_items = []
     db_parasha = this_weeks_parasha(datetime_obj, diaspora=diaspora)
+    rf = model.Ref(db_parasha["ref"])
     parasha = {
         'title': {'en': 'Parashat Hashavua', 'he': u'פרשת השבוע'},
         'displayValue': {'en': db_parasha["parasha"], 'he': hebrew_parasha_name(db_parasha["parasha"])},
-        'url': db_parasha["ref"],
+        'url': rf.url(),
         'order': 1,
-        'category': model.Ref(db_parasha["ref"]).index.get_primary_category()
+        'category': rf.index.get_primary_category()
     }
     parasha_items.append(parasha)
     for h in db_parasha["haftara"]:
