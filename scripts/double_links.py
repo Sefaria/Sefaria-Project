331c5fafa76032450a97944a0d113e41f5b70352
Steve Kaplan <steve@sefaria.org>
2018-06-06 12:50:30 +0300

    two double_links files: new one that allows user to find/delete double links by Index and old one that finds and deletes any in the Tanakh

--- scripts/double_links.py
+++ scripts/double_links.py
@@ -2,26 +2,17 @@ __author__ = 'stevenkaplan'
 from sefaria.model import *
 from sefaria.system.exceptions import *
 import csv
+import sys
 
 class DoubleLinks:
     def __init__(self):
-        self.specific_first_num = 0
-        self.commentary = 0
         self.double_links = []
-        self.midrash = 0
         self.all_links = {}
         self.both_auto = 0
         self.general_auto_specific_manual = 0
         self.specific_auto_general_manual = 0
         self.both_manual = 0
-        self.tanakh = 0
-        self.tanakh_only = False
-        self.other = 0
         self.duplicate_ids = {}
-
-
-    def set_tanakh(self, bool):
-        self.tanakh_only = bool
 
 
     def get_doubles_for_delete(self, ref1, ref2):
@@ -29,73 +20,47 @@ class DoubleLinks:
             self.all_links[ref1] = [ref2]
         else:
             for each_ref in self.all_links[ref1]:
-                if each_ref.book != ref2.book or each_ref == ref2:
+                if each_ref.book != ref2.book or each_ref == ref2: #first case is this can't be a double link and second case is this is an exact double (see delete_exact_doubles() below)
                     continue
                 general = None
                 specific = None
-                if each_ref.contains(ref2) and each_ref.is_section_level():
+                found_double = False
+                if each_ref.contains(ref2) and each_ref.is_section_level(): #check is_section_level() because each_ref might contain() ref2 because each_ref is a range
                     general = each_ref
                     specific = ref2
                 elif ref2.contains(each_ref) and ref2.is_section_level():
                     general = ref2
                     specific = each_ref
-                
-                if general:
-                    self.create_link_dict_and_gather_data(ref1, general, specific)
+
+                found_double = general is not None
+                if found_double:
+                    general_link = Link().load({"$and": [{"refs": general.normal()}, {"refs": ref1.normal()}]})
+                    specific_link = Link().load({"$and": [{"refs": specific.normal()}, {"refs": ref1.normal()}]})
+                    self.double_links.append(general_link)
+                    self.gather_data(general_link, specific_link)
 
             self.all_links[ref1].append(ref2)
         
     
     
-    def create_link_dict_and_gather_data(self, ref1, general, specific):
-        #FIRST, CHECK THAT SPECIFIC AND GENERAL ARE BOTH TANAKH
-        assert general.primary_category == "Tanakh" and specific.primary_category == "Tanakh", "{} {} {}".format(ref1, general, specific)
-
-        general_link = Link().load({"$and": [{"refs": general.normal()}, {"refs": ref1.normal()}]})
-        self.double_links.append(general_link)
-
+    def gather_data(self, general_link, specific_link):
         #FINALLY, GATHER DATA ON THE TIME OF CREATION OF THE LINK AND WHETHER IT IS MANUAL OR AUTOMATIC,
         #AND WHETHER IT IS FROM MIDRASH OR RASHI
 
         general_auto = general_link.auto
-        general_date = general_link._id.generation_time
+        # general_date = general_link._id.generation_time
 
-        specific_link = Link().load({"$and": [{"refs": specific.normal()}, {"refs": ref1.normal()}]})
         specific_auto = specific_link.auto
-        specific_date = specific_link._id.generation_time
+        # specific_date = specific_link._id.generation_time
 
-        if specific_date >= general_date:
-            self.specific_first_num += 1
-            if specific_auto and general_auto:
-                self.both_auto += 1
-            elif general_auto and not specific_auto:
-                self.general_auto_specific_manual += 1
-            elif specific_auto and not general_auto:
-                self.specific_auto_general_manual += 1
-            else:
-                self.both_manual += 1
-
-        if "Commentary" in ref1.index.categories:
-            assert self.tanakh_only is False, ref1.normal()
-            self.commentary += 1
-        elif "Midrash" in ref1.index.categories:
-            assert self.tanakh_only is False, ref1.normal()
-            self.midrash += 1
-        elif "Tanakh" in ref1.index.categories:
-            assert self.tanakh_only is True, ref1.normal()
-            self.tanakh += 1
+        if specific_auto and general_auto:
+            self.both_auto += 1
+        elif general_auto and not specific_auto:
+            self.general_auto_specific_manual += 1
+        elif specific_auto and not general_auto:
+            self.specific_auto_general_manual += 1
         else:
-            assert self.tanakh_only is False, ref1.normal()
-            self.other += 1
-
-
-    def get_links(self, category):
-        ls = []
-        indexes = library.get_indexes_in_category(category)
-        for index in indexes:
-            ls += LinkSet(Ref(index))
-        return ls
-
+            self.both_manual += 1
 
     
     def delete_links_and_output_results(self):
@@ -111,12 +76,7 @@ class DoubleLinks:
         print "General auto only: {}".format(self.general_auto_specific_manual)
         print "Specific auto only: {}".format(self.specific_auto_general_manual)
 
-        print "Specific Added After/Same Time as General: {}".format(self.specific_first_num)
 
-        print "Commentary links: {}".format(self.commentary)
-        print "Midrash links: {}".format(self.midrash)
-        print "Tanakh-to-Tanakh links: {}".format(self.tanakh)
-        print "Other links: {}".format(self.other)
 
 
 
@@ -185,14 +145,18 @@ class DoubleLinks:
 
 if __name__ == "__main__":
     DL = DoubleLinks()
-    DL.get_exact_doubles()
-    DL.delete_exact_doubles()
+    #DL.get_exact_doubles()
+    #DL.delete_exact_doubles()
 
 
     count = 0
-    category = "Tanakh"
+    assert len(sys.argv) != 1, "Input error: Requires name of text."
+    title = " ".join(sys.argv[1:])
+    print title
+    index = library.get_index(title)
+    category = index.categories[0]
     delete = True
-    for l in DL.get_links(category):
+    for l in LinkSet(Ref(title)):
         count += 1
         if count % 10000 == 0:
             print count
@@ -212,14 +176,11 @@ if __name__ == "__main__":
             continue
 
         if not invalid:
-            if ref1.primary_category != category:
-                DL.set_tanakh(False)
+            if ref1.book != title:
                 DL.get_doubles_for_delete(ref1, ref2)
-            elif ref2.primary_category != category:
-                DL.set_tanakh(False)
+            elif ref2.book != title:
                 DL.get_doubles_for_delete(ref2, ref1)
             else:
-                DL.set_tanakh(True)
                 DL.get_doubles_for_delete(ref1, ref2)
                 DL.get_doubles_for_delete(ref2, ref1)
 
