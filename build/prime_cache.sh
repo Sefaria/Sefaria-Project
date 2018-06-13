4b86f3dc2c6befee5def549e6b5ff95fe0a92829
Lev Israel <eliezer.israel@gmail.com>
2018-06-12 20:32:07 -0400

    Tune cache priming

--- build/prime_cache.sh
+++ build/prime_cache.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
-curl http://localhost/api/texts/Genesis.1 > /dev/null &
-curl http://localhost/ > /dev/null &
-curl http://localhost/api/name/stam > /dev/null &
-curl http://localhost/api/name/stam?ref_only=1 > /dev/null &
-curl http://localhost/api/name/%D7%A1%D7%AA%D7%9D?ref_only=1 > /dev/null &
-curl http://localhost/api/name/%D7%A1%D7%AA%D7%9D > /dev/null &
-curl http://localhost/api/counts/links/Tanach/Bavli > /dev/null &
-curl http://localhost/api/preview/Zohar > /dev/null &
+echo "Started cache priming on $(hostname)"
+curl -s http://localhost/api/texts/Genesis.1 > /dev/null &
+curl -s http://localhost/ > /dev/null &
+curl -s http://localhost/api/name/stam > /dev/null &
+curl -s http://localhost/api/name/stam?ref_only=1 > /dev/null &
+curl -s http://localhost/api/name/%D7%A1%D7%AA%D7%9D?ref_only=1 > /dev/null &
+curl -s http://localhost/api/name/%D7%A1%D7%AA%D7%9D > /dev/null &
+curl -s http://localhost/api/counts/links/Tanach/Bavli > /dev/null &
+curl -s http://localhost/api/preview/Zohar > /dev/null &
+echo "Completed cache priming on $(hostname)"
