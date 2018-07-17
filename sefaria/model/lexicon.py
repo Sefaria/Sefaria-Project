238295f0137de0105f1f79559306115e2ae127d1
Kevin <kevwolf@umich.edu>
2018-07-17 14:51:55 -0400

    Start Jastrow specific as_string

--- sefaria/model/lexicon.py
+++ sefaria/model/lexicon.py
@@ -113,6 +113,65 @@ class DictionaryEntry(LexiconEntry):
 
         next_line = u', '.join([u'<strong>{}</strong>'.format(hw) for hw in [self.headword] + getattr(self, 'alt_headwords', [])])
 
+        if self.parent_lexicon is 'Jastrow Dictionary':
+            for field in ['morphology']:
+                if field in self.content:
+                    next_line += u" " + self.content[field]
+
+            lang = u''
+            if hasattr(self, 'language_code'):
+                lang += u" " + self.language_code
+            if hasattr(self, 'language_reference'):
+                if lang:
+                    lang += u' '
+                lang += self.language_reference
+            if lang:
+                next_line += lang
+
+            for sense in self.content['senses']:
+                if 'senses' in sense:
+                    # Start a new segment for the new form
+                    new_content += next_line
+                    next_line = u'<br/>&nbsp;&nbsp;&nbsp;&nbsp;<strong>{} - {}</strong>'.format(sense['grammar']['verbal_stem'],
+                                                                          sense['grammar']['binyan_form'])
+                    for binyan_sense in sense['senses']:
+                        next_line += u" " + self.get_sense(binyan_sense)
+                else:
+                    next_line += u" " + self.get_sense(sense)
+
+            if next_line:
+                new_content += next_line
+
+            if hasattr(self, 'derivatives'):
+                new_content += u' {}'.format(self.derivatives)
+        return [new_content]
+
+
+class StrongsDictionaryEntry(DictionaryEntry):
+    required_attrs = DictionaryEntry.required_attrs + ["content", "strong_number"]
+
+
+class RashiDictionaryEntry(DictionaryEntry):
+    required_attrs = DictionaryEntry.required_attrs + ["content", "orig_word", "orig_ref", "catane_number"]
+
+
+class JastrowDictionaryEntry(DictionaryEntry):
+    required_attrs = DictionaryEntry.required_attrs + ["rid"]
+    
+    def get_sense(self, sense):
+        text = u''
+        text += sense.get('number', u'')
+        if text:
+            text = u"<b>{}</b> ".format(text)
+        for field in ['definition', 'alternative', 'notes']:
+            text += sense.get(field, u'')
+        return text
+    
+    def as_strings(self):
+        new_content = u""
+
+        next_line = u', '.join([u'<strong>{}</strong>'.format(hw) for hw in [self.headword] + getattr(self, 'alt_headwords', [])])
+
         for field in ['morphology']:
             if field in self.content:
                 next_line += u" " + self.content[field]
@@ -133,6 +192,7 @@ class DictionaryEntry(LexiconEntry):
                 new_content += next_line
                 next_line = u'<br/>&nbsp;&nbsp;&nbsp;&nbsp;<strong>{} - {}</strong>'.format(sense['grammar']['verbal_stem'],
                                                                       sense['grammar']['binyan_form'])
+                
                 for binyan_sense in sense['senses']:
                     next_line += u" " + self.get_sense(binyan_sense)
             else:
@@ -140,9 +200,6 @@ class DictionaryEntry(LexiconEntry):
 
         if next_line:
             new_content += next_line
-
-        if hasattr(self, 'derivatives'):
-            new_content += u' {}'.format(self.derivatives)
         return [new_content]
 
 
