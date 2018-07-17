9cf2d6d7723ecdb472d792e242fc1e7478efab41
Kevin <kevwolf@umich.edu>
2018-07-17 14:56:34 -0400

    Remove after title delimiter s and to

--- sefaria/model/schema.py
+++ sefaria/model/schema.py
@@ -595,7 +595,7 @@ class TitledTreeNode(TreeNode, AbstractTitledOrTermedObject):
     A tree node that has a collection of titles - as contained in a TitleGroup instance.
     In this class, node titles, terms, 'default', and combined titles are handled.
     """
-    after_title_delimiter_re = ur"[,.: \r\nsto]+"  # should be an arg?  \r\n are for html matches
+    after_title_delimiter_re = ur"[,.: \r\n]+"  # should be an arg?  \r\n are for html matches
     title_separators = [u", "]
 
     def __init__(self, serial=None, **kwargs):
