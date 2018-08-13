f3665bc3a8d48703e28dbfa99deeb8d4fa21f321
Brett Lockspeiser <blocks@gmail.com>
2018-08-13 10:08:59 -0700

    Fix privacy policy link

--- static/js/Misc.jsx
+++ static/js/Misc.jsx
@@ -720,11 +720,11 @@ class CookiesNotification extends Component {
       <div className="cookiesNotification">
           
           <div>
-            <span className="int-en">We use cookies to give you the best experience possible on our site. Click OK to continue using Sefaria. <a href="/privacy">Learn More</a>.</span>
+            <span className="int-en">We use cookies to give you the best experience possible on our site. Click OK to continue using Sefaria. <a href="/privacy-policy">Learn More</a>.</span>
             <span className='int-en button small white' onClick={this.setCookie}>OK</span>
           </div>   
           <div>
-            <span className="int-he">אנחנו משתמשים בעוגיות כדי לתת למשתמשים את חווית השימוש הטובה ביותר. לחץ כאן לאישור. <a href="/privacy">קרא עוד בנושא</a>.</span>
+            <span className="int-he">אנחנו משתמשים בעוגיות כדי לתת למשתמשים את חווית השימוש הטובה ביותר. לחץ כאן לאישור. <a href="/privacy-policy">קרא עוד בנושא</a>.</span>
             <span className='int-he button small white'onClick={this.setCookie}>כאן</span>
           </div>
 
