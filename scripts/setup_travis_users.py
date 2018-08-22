1d7418179ca06353762fea62657a468c5fb7e798
Ephraim Damboritz <ephraim@sefaria.org>
2018-08-22 13:38:27 +0300

    fix setup users for local runs

--- scripts/setup_travis_users.py
+++ scripts/setup_travis_users.py
@@ -3,12 +3,14 @@ import django
 django.setup()
 from django.contrib.auth.models import User
 from sefaria.model import UserProfile
+
 users = User.objects.all()
 user = users[0]
-if user.email == os.environ["SEFARIA_SUPERUSER"]:
-    user.username = os.environ["SEFARIA_SUPERUSER"]
-    user.set_password(os.environ["SEFARIA_SUPERPASS"])
-    user.save()
+if 'SEFARIA_SUPERUSER' in os.environ and "SEFARIA_SUPERPASS" in os.environ:
+    if user.email == os.environ["SEFARIA_SUPERUSER"]:
+        user.username = os.environ["SEFARIA_SUPERUSER"]
+        user.set_password(os.environ["SEFARIA_SUPERPASS"])
+        user.save()
 
 u = User.objects.create_user(os.environ["SEFARIA_TEST_USER"],
                              email=os.environ["SEFARIA_TEST_USER"],
