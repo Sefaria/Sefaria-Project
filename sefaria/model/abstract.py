28be495c79fd529dc923cc44aa8c0c004bdf6fdc
Kevin <kevwolf@umich.edu>
2018-08-30 14:19:50 -0400

    Type so that load all method can use a set

--- sefaria/model/abstract.py
+++ sefaria/model/abstract.py
@@ -19,6 +19,13 @@ logging.basicConfig()
 logger = logging.getLogger("abstract")
 logger.setLevel(logging.WARNING)
 
+class hashabledict(dict):
+  def __key(self):
+    return tuple((k,self[k]) for k in sorted(self))
+  def __hash__(self):
+    return hash(self.__key())
+  def __eq__(self, other):
+    return self.__key() == other.__key()
 
 class AbstractMongoRecord(object):
     """
