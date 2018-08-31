05dcca61fb1dcfdec50479f0c75112360973c066
Lev Israel <eliezer.israel@gmail.com>
2018-08-30 23:13:58 -0400

    Re-add check for vowels

--- sefaria/model/lexicon.py
+++ sefaria/model/lexicon.py
@@ -322,13 +322,15 @@ class LexiconLookupAggregator(object):
 
     @classmethod
     def _single_lookup(cls, input_word, lookup_key='form', **kwargs):
-        from sefaria.utils.hebrew import is_hebrew, strip_cantillation
+        from sefaria.utils.hebrew import is_hebrew, strip_cantillation, has_cantillation
         from sefaria.model import Ref
 
         lookup_ref = kwargs.get("lookup_ref", None)
         wform_pkey = lookup_key
         if is_hebrew(input_word):
             input_word = strip_cantillation(input_word)
+            if not has_cantillation(input_word, detect_vowels=True):
+                wform_pkey = 'c_form'
         query_obj = {wform_pkey: input_word}
         if lookup_ref:
             nref = Ref(lookup_ref).normal()
