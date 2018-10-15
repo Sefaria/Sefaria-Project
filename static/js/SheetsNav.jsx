c6ea4b9ed8041290bf4ee03382d55747395ab256
Ephraim Damboritz <ephraim@sefaria.org>
2018-10-15 23:13:25 +0300

    Fix html `class` to React `className`

--- static/js/SheetsNav.jsx
+++ static/js/SheetsNav.jsx
@@ -589,7 +589,7 @@ class PrivateSheetListing extends Component {
               <div className="userSheetTitle">
                 <a className="sheetTitle" href={url}>{title}</a>
                 <span className="sheetAccess"><SheetAccessIcon sheet={sheet} /></span>
-                <a class="button white mini" href={url+"?editor=1"}><span class="int-en">Edit Sheet</span><span class="int-he">ערוך</span></a>
+                <a className="button white mini" href={url+"?editor=1"}><span className="int-en">Edit Sheet</span><span className="int-he">ערוך</span></a>
               </div>
               <div className="userSheetInfo">
                 <span>{sheet.views} {Sefaria._('Views')}</span><span>{sheet.modified}</span><span className="tagString">{tagString}</span>
