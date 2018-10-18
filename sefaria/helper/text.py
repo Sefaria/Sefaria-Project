50dac3c47449d7108f1e05b753631273987078f9
Kevin <kevwolf@umich.edu>
2018-10-19 01:41:00 +0300

    Other ending symbols

--- sefaria/helper/text.py
+++ sefaria/helper/text.py
@@ -292,7 +292,7 @@ def replace_roman_numerals(text, allow_lowercase=False):
     """
     import roman
     flag = re.I if allow_lowercase else 0
-    regex = re.compile(u"((^|[{\[( ])[{\[( ]*)(M{0,4}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3}))($|[., ]+)(\d)?", flag)
+    regex = re.compile(u"((^|[{\[( ])[{\[( ]*)(M{0,4}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3}))($|[.,;\])}: ]+)(\d)?", flag)
 
 
     def replace_roman_numerals_in_match(m):
