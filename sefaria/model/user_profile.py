a0f7f7f32f6e8fcc39b4f78e1f365c6d9a22cd20
Noah Santacruz <noahssantacruz@gmail.com>
2019-01-02 18:59:04 +0200

    use utc please

--- sefaria/model/user_profile.py
+++ sefaria/model/user_profile.py
@@ -95,7 +95,7 @@ class UserHistory(abst.AbstractMongoRecord):
             except KeyError:
                 pass
         if kwargs.get("natural_time", False):
-            d["natural_time"] = concise_natural_time(datetime.fromtimestamp(d["time_stamp"]))
+            d["natural_time"] = concise_natural_time(datetime.utcfromtimestamp(d["time_stamp"]))
         return d
 
 
