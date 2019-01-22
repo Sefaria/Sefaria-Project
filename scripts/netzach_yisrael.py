4fe430093711fe2e2f88a28cebf0db37239974ff
Steve Kaplan <steve@sefaria.org>
2019-01-22 14:12:50 +0200

    remove first segments

--- scripts/netzach_yisrael.py
+++ scripts/netzach_yisrael.py
@@ -32,3 +32,14 @@ for i in range(1, 65):
 migrate_to_complex_structure("Netzach Yisrael", schema, mapping)
 
 
+library.rebuild(include_toc=True)
+
+i = library.get_index("Netzach Yisrael")
+section_refs = i.all_section_refs()
+for ref in section_refs:
+    new_text = ref.text('he').text[1:]
+    tc = TextChunk(ref, vtitle="Netzach Yisrael", lang="he")
+    tc.text = new_text
+    tc.save(force_save=True)
+
+
