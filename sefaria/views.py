b67232439d19cb33ad16b48ef3ff0aec60352eb2
Josh Soref <jsoref@users.noreply.github.com>
2019-01-27 02:51:53 -0500

    spelling: occurred

--- sefaria/views.py
+++ sefaria/views.py
@@ -528,7 +528,7 @@ def cause_error(request):
     try:
         erorr = excepting
     except Exception as e:
-        logger.exception('An Exception has ocurred in the code')
+        logger.exception('An Exception has occurred in the code')
     erorr = error
     return jsonResponse(resp)
 
