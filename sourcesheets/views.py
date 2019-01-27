3faf2837302f33a7a679589555bff4c224276999
Josh Soref <jsoref@users.noreply.github.com>
2019-01-27 02:57:11 -0500

    spelling: people

--- sourcesheets/views.py
+++ sourcesheets/views.py
@@ -864,7 +864,7 @@ def group_tag_list_api(request, group):
 
 def trending_tags_api(request):
 	"""
-	API to retrieve the list of peopke who like sheet_id.
+	API to retrieve the list of people who like sheet_id.
 	"""
 	response = recent_public_tags(days=14, ntags=18)
 	response = jsonResponse(response, callback=request.GET.get("callback", None))
