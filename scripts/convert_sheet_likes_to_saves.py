14a3c4b47bdb242b08085ff4412be579f0ca292a
Noah Santacruz <noahssantacruz@gmail.com>
2019-03-18 14:32:28 +0200

    finalize script

--- scripts/convert_sheet_likes_to_saves.py
+++ scripts/convert_sheet_likes_to_saves.py
@@ -13,19 +13,32 @@ now = epoch_time()
 default_epoch_time = epoch_time(datetime(2017, 12, 1))  # the Sefaria epoch. approx time since we added time stamps to recent items
 for sheet in all_sheets:
     likes = sheet.get("likes", [])
+    if "id" not in sheet:
+        print sheet
+        continue
     tref = "Sheet {}".format(sheet["id"])
-    oref = Ref(tref)
-    for l in likes:
-        d = {
-            "ref": tref,
-            "he_ref": oref.he_normal(),
-            "book": oref.book,
-            "versions": {},
-            "last_place": False,
-            "saved": True,
-            "secondary": False,
-            "sheet_title": sheet["title"],
-            "sheet_owner": sheet["owner"],
-            "time_stamp": default_epoch_time,
-            "server_time_stamp": now
-        }
+    if len(likes) > 0:
+        pass
+        # oref = Ref(tref)
+        # for l in likes:
+        #     d = {
+        #         "uid": l,
+        #         "ref": tref,
+        #         "he_ref": oref.he_normal(),
+        #         "book": oref.book,
+        #         "versions": {},
+        #         "last_place": False,
+        #         "saved": True,
+        #         "secondary": False,
+        #         "sheet_title": sheet["title"],
+        #         "sheet_owner": sheet["owner"],
+        #         "time_stamp": default_epoch_time,
+        #         "server_time_stamp": now
+        #     }
+        #     uh = UserHistory(d)
+        #     uh.save()
+    try:
+        del sheet["likes"]
+        db.sheets.save(sheet)
+    except KeyError:
+        pass
