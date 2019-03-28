19983fa0e44ec9d6c735eed1e70af970c62df9ff
Brett Lockspeiser <blocks@gmail.com>
2019-03-28 16:26:11 -0700

    Adding Google Tag Manager

--- static/js/sefaria/track.js
+++ static/js/sefaria/track.js
@@ -1,6 +1,7 @@
 var ga;
 if (typeof window !== 'undefined' && typeof window.ga === "function" ) {
   ga = window.ga;
+  console.log(ga)
 } else {
   ga = function() {}; // Fail gracefully if we reach one of these methods server side
   ga._mock = true;
