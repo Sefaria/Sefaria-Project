5455a2835de32e7be1de272c2e86430ebd4ef502
Brett Lockspeiser <blocks@gmail.com>
2019-04-17 10:47:41 -0700

    Dedication tweak

--- scripts/set_nechama_group_data.py
+++ scripts/set_nechama_group_data.py
@@ -12,7 +12,7 @@ g.toc = {
 	"categories": ["Tanakh", "Commentary"],
 	"title": "The Nechama Leibowitz Collection",
 	"heTitle": u"גיליונות נחמה",
-	"description": "<span class='dedication'>In loving memory of our mother, grandmother, and great-grandmother Miriam Magda Reichner.<br>The Rubinstein family.<br> Toronto, Canada and Raanana, Israel.</span>",
+	"description": "<span class='dedication'>In loving memory of our mother, grandmother, and great-grandmother Miriam Magda Reichner<br>The Rubinstein family<br> Toronto, Canada and Raanana, Israel</span>",
 	"heDescription": u"<span class='dedication'>לעילוי נשמת אמנו, וסבתנו, סבתא רבתא שלנו מרים רייכנער<br>נדבת משפחת רובינשטיין<br>טורונטו, קנדה ורעננה, ישראל<span class='dedication'>"}
 
 g.save()
