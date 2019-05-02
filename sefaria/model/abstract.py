a7d23d7e19ae6b7937e053f8adb85b144d577a18
Lev Israel <eliezer.israel@gmail.com>
2019-05-02 16:08:43 -0400

    First swing at new count interface

--- sefaria/model/abstract.py
+++ sefaria/model/abstract.py
@@ -275,6 +275,10 @@ class AbstractMongoSet(collections.Iterable):
 
     def __init__(self, query={}, page=0, limit=0, sort=[("_id", 1)], proj=None, hint=None):
         self.raw_records = getattr(db, self.recordClass.collection).find(query, proj).sort(sort).skip(page * limit).limit(limit)
+        self.query = query
+        self.hint = hint
+        self.limit = limit
+        self.skip = page * limit
         if hint:
             self.raw_records.hint(hint)
         #self.has_more = limit != 0 and self.raw_records.count() == limit
@@ -302,7 +306,8 @@ class AbstractMongoSet(collections.Iterable):
         if self.max:
             return self.max
         else:
-            return self.raw_records.count()
+            kwargs = {k: getattr(self, k) for k in ["skip", "limit", "hint"] if getattr(self, k, None)}
+            return getattr(db, self.recordClass.collection).count_documents(self.query, **kwargs)
 
     def array(self):
         self._read_records()
