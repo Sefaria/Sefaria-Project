aa386a7cfc6526b0ffa1af06d394dcca6805bd1f
Lev Israel <eliezer.israel@gmail.com>
2019-06-14 13:48:02 -0400

    Code polish

--- static/js/Editor.jsx
+++ static/js/Editor.jsx
@@ -1,36 +1,36 @@
 import React from 'react';
 import {Editor, EditorState, RichUtils, convertFromHTML, ContentState, CompositeDecorator} from 'draft-js';
 
-      const SEFREF_REGEX = /(Genesis|Gen) [0-9][0-9]?:?[0-9]?[0-9]?/g;
+  const SEFREF_REGEX = /(Genesis|Gen) [0-9][0-9]?:?[0-9]?[0-9]?/g;
 
-      function sefrefStrategy(contentBlock, callback, contentState) {
-        findWithRegex(SEFREF_REGEX, contentBlock, callback);
-      }
-      function findWithRegex(regex, contentBlock, callback) {
-        const text = contentBlock.getText();
-        let matchArr, start;
-        while ((matchArr = regex.exec(text)) !== null) {
-          start = matchArr.index;
-          callback(start, start + matchArr[0].length);
-        }
-      }
-      const SefRefSpan = (props) => {
-	console.log(props)
-        return (
-          <span
-            style={styles.sefref}
-            data-offset-key={props.offsetKey}
-          >
-            {props.children}
-          </span>
-        );
-      };
+  function sefrefStrategy(contentBlock, callback, contentState) {
+    findWithRegex(SEFREF_REGEX, contentBlock, callback);
+  }
+  function findWithRegex(regex, contentBlock, callback) {
+    const text = contentBlock.getText();
+    let matchArr, start;
+    while ((matchArr = regex.exec(text)) !== null) {
+      start = matchArr.index;
+      callback(start, start + matchArr[0].length);
+    }
+  }
+  const SefRefSpan = (props) => {
+    console.log(props);
+    return (
+      <span
+        style={styles.sefref}
+        data-offset-key={props.offsetKey}
+      >
+        {props.children}
+      </span>
+    );
+  };
 
-      const styles = {
-        sefref: {
-          color: 'rgba(98, 177, 254, 1.0)',
-        },
-      };
+  const styles = {
+    sefref: {
+      color: 'rgba(98, 177, 254, 1.0)',
+    },
+  };
 
 
 
@@ -68,10 +68,10 @@ class SefariaEditor extends React.Component {
   }
 
   setToolbarPosition() {
-    var sel = window.getSelection()
+    const sel = window.getSelection();
     if (sel.toString().length > 0) {
-        var range = sel.getRangeAt(0);
-        var boundary = range.getBoundingClientRect();
+        const range = sel.getRangeAt(0);
+        const boundary = range.getBoundingClientRect();
         this.setState(
             {
                 "showToolbar": true,
@@ -91,27 +91,17 @@ class SefariaEditor extends React.Component {
 
   }
 
-
-  onBoldClick() {
-    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'BOLD'));
+  onStyleClick(style_name) {
+    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, style_name));
   }
-
-  onItalicClick() {
-    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'ITALIC'));
-  }
-
-  onUnderlineClick() {
-    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'UNDERLINE'));
-  }
-
 
   render() {
     return (
         <div>
           <EditorToolbar
-            onItalicClick={()=>this.onItalicClick()}
-            onBoldClick={()=>this.onBoldClick()}
-            onUnderlineClick={()=>this.onUnderlineClick()}
+            onItalicClick={()=>this.onStyleClick("ITALIC")}
+            onBoldClick={()=>this.onStyleClick("BOLD")}
+            onUnderlineClick={()=>this.onStyleClick("UNDERLINE")}
             showToolbar={this.state.showToolbar}
             toolbarPosition={this.state.toolbarPosition}
           />
@@ -122,22 +112,17 @@ class SefariaEditor extends React.Component {
 }
 
 
-class EditorToolbar extends React.Component {
 
-  render() {
-    return (
-    <div className="editorToolbar" style={{display: this.props.showToolbar ? "block" : "none", left: this.props.toolbarPosition.x, top: this.props.toolbarPosition.y  } }
+const EditorToolbar = ({showToolbar, toolbarPosition, onBoldClick, onItalicClick, onUnderlineClick}) =>
+    <div className="editorToolbar" style={{display: showToolbar ? "block" : "none", left: toolbarPosition.x, top: toolbarPosition.y  } }
         onMouseDown={(event) => {
         event.preventDefault();
         }}
     >
-        <button onClick={this.props.onBoldClick}><strong>b</strong></button>
-        <button onClick={this.props.onItalicClick}><em>i</em></button>
-        <button onClick={this.props.onUnderlineClick}><span style={{textDecoration: 'underline'}}>u</span></button>
-    </div>
+        <button onClick={onBoldClick}><strong>b</strong></button>
+        <button onClick={onItalicClick}><em>i</em></button>
+        <button onClick={onUnderlineClick}><span style={{textDecoration: 'underline'}}>u</span></button>
+    </div>;
 
-    );
-  }
-}
 
 module.exports = SefariaEditor;
