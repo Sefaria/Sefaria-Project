e7debe5bdf1018f6f1b22e1cffc73691bc00fbc0
Noah Santacruz <noahssantacruz@gmail.com>
2019-06-19 16:10:45 +0300

    merge

--- sefaria/recommendation_engine.py
+++ sefaria/recommendation_engine.py
@@ -5,6 +5,7 @@ from sefaria.model import *
 from sefaria.client.wrapper import get_links
 from sefaria.system.database import db
 from sefaria.system.exceptions import InputError
+from sefaria.model.schema import DictionaryEntryNode
 
 # TODO dont double count source and its commentary (might be costly)
 # TODO do better job of double author
@@ -19,21 +20,23 @@ REF_RANGE_MAX = 30
 
 class Recommendation:
 
-    def __init__(self, oref=None, relevance=0.0, score=None, novelty=None, sources=None):
+    def __init__(self, oref=None, relevance=0.0, score=None, novelty=None, sources=None, anchor_refs=None):
         self.ref = oref
         self.relevance = relevance
         self.novelty = novelty
         self._score = score
         self.sources = sources if sources is not None else []
+        self.anchor_refs = anchor_refs if anchor_refs is not None else []
 
     def __add__(self, other):
         new_ref = self.ref if self.ref is not None else other.ref
-        return Recommendation(new_ref, relevance=self.relevance + other.relevance, novelty=self.novelty, sources=self.sources + other.sources)
+        return Recommendation(new_ref, relevance=self.relevance + other.relevance, novelty=self.novelty, sources=self.sources + other.sources, anchor_refs=self.anchor_refs + other.anchor_refs)
 
     def __iadd__(self, other):
         self.ref = self.ref if self.ref is not None else other.ref
         self.relevance += other.relevance
         self.sources += other.sources
+        self.anchor_refs += other.anchor_refs
         return self
 
     def __unicode__(self):
@@ -42,14 +45,15 @@ class Recommendation:
     def __str__(self):
         return unicode(self).encode('utf-8')
 
-    def __repr__(self):  # Wanted to use orig_tref, but repr can not include Unicode
-        return u"{}({}, score={}, sources={})".format(self.__class__.__name__, self.ref, self.score, self.sources)
+    def __repr__(self):
+        return u"{}({}, score={}, sources={}, anchor_refs={})".format(self.__class__.__name__, self.ref, self.score, self.sources, self.anchor_refs).encode('utf-8')
 
     def to_dict(self):
         return {
             "ref": self.ref.normal(),
             "score": self.score,
-            "sources": self.sources
+            "sources": self.sources,
+            "anchor_refs": [r.normal() for r in self.anchor_refs]
         }
 
     @property
@@ -169,23 +173,24 @@ class RecommendationEngine:
             temp_direct_links = set()
             initial_links = get_links(section_ref.normal(), with_text=False)
             filtered_links = filter(lambda l: len(range_set & {r.normal() for r in Ref(l['anchorRef']).range_list()}) > 0, initial_links)
-            direct_links |= {(l['ref'], l['category'] in ('Commentary', 'Modern Commentary')) for l in filtered_links}
-        for link_tref, is_comment in direct_links:
+            direct_links |= {(l['ref'], l['category'] in ('Commentary', 'Modern Commentary'), Ref(l['anchorRef'])) for l in filtered_links}
+        for link_tref, is_comment, anchor_ref in direct_links:
             # Steinsaltz is hard-coded to have same connections as Talmud which will double count Talmud connections
             if is_comment and not link_tref.startswith("Steinsaltz on "):
                 link_oref = Ref(link_tref)
                 author = getattr(link_oref.index, "collective_title", None)
-                temp_commentary_links, _, _ = RecommendationEngine.normalize_related_refs([x["ref"] for x in get_links(link_tref, with_text=False)], None, COMMENTARY_LINK_SCORE)
+                temp_commentary_links, _, _, _ = RecommendationEngine.normalize_related_refs([x["ref"] for x in get_links(link_tref, with_text=False)], None, COMMENTARY_LINK_SCORE)
                 for commentary_link in temp_commentary_links:
                     if author is not None and (commentary_link, author) in commentary_author_set:
                         # don't add same ref twice from same author
                         continue
                     commentary_author_set.add((commentary_link, author))
-                    commentary_links += [Recommendation(Ref(commentary_link), relevance=COMMENTARY_LINK_SCORE, sources=[link_tref])]
-        is_comment_list = [x[1] for x in direct_links]
-        direct_links, _, is_comment_list = RecommendationEngine.normalize_related_refs([x[0] for x in direct_links], None, DIRECT_LINK_SCORE, other_data=is_comment_list)
+                    commentary_links += [Recommendation(Ref(commentary_link), relevance=COMMENTARY_LINK_SCORE, sources=[link_tref], anchor_refs=[anchor_ref])]
+        other_data = [(x[1], x[2]) for x in direct_links]
+        direct_links, _, other_data, focus_ref_subref = RecommendationEngine.normalize_related_refs([x[0] for x in direct_links], None, DIRECT_LINK_SCORE, other_data=other_data)
         direct_ref_set = set(direct_links)
-        final_rex = [Recommendation(Ref(x), relevance=DIRECT_LINK_SCORE, sources=["direct"]) for x in direct_links] + commentary_links
+        is_comment_list, anchor_ref_list = zip(*other_data)
+        final_rex = [Recommendation(Ref(x), relevance=DIRECT_LINK_SCORE, sources=["direct"], anchor_refs=[anchor_ref]) for x, anchor_ref in zip(direct_links, anchor_ref_list)] + commentary_links
         commentary_ref_set = set(map(lambda x: x[0], filter(lambda x: x[1], zip(direct_links, is_comment_list))))
         return final_rex, commentary_ref_set, direct_ref_set
 
@@ -218,15 +223,15 @@ class RecommendationEngine:
             sheets += [s for s in sheets_cursor if RecommendationEngine.is_interesting_sheet(s)]
         included_ref_dict = {}
         for sheet in sheets:
-            temp_included, focus_range_factor, _ = RecommendationEngine.normalize_related_refs(sheet.get("includedRefs", []), range_set, SHEET_REF_SCORE, check_has_ref=True, count_steinsaltz=True)
+            temp_included, focus_range_factor, _, focus_ref_subref = RecommendationEngine.normalize_related_refs(sheet.get("includedRefs", []), range_set, SHEET_REF_SCORE, check_has_ref=True, count_steinsaltz=True)
             if focus_range_factor == 0:
                 continue
             ref_owner_keys = [(r, sheet["owner"]) for r in temp_included]
             for k in ref_owner_keys:
                 if (k in included_ref_dict and included_ref_dict[k]["score"] < focus_range_factor) or k not in included_ref_dict:
-                    included_ref_dict[k] = {"score": focus_range_factor, "source": "Sheet " + str(sheet["id"])}
+                    included_ref_dict[k] = {"score": focus_range_factor, "source": "Sheet " + str(sheet["id"]), "anchor_ref": focus_ref_subref}
 
-        return [Recommendation(Ref(r), relevance=d["score"], sources=[d["source"]]) for (r, _), d in included_ref_dict.items()]
+        return [Recommendation(Ref(r), relevance=d["score"], sources=[d["source"]], anchor_refs=[[d["anchor_ref"]]]) for (r, _), d in included_ref_dict.items()]
 
     @staticmethod
     def cluster_close_refs(ref_list, data_list, dist_threshold):
@@ -286,7 +291,7 @@ class RecommendationEngine:
         :return:
         '''
         # make sure oref is in includedRefs but don't actually add those to the final includedRefs
-        has_tref = not check_has_ref
+        focus_ref_subset = None
         focus_range_factor = 0.0  # multiplicative factor based on how big a range the focus_ref is in
         final_refs = []
         other_data = [None]*len(related_refs) if other_data is None else other_data
@@ -296,16 +301,21 @@ class RecommendationEngine:
                 temp_oref = Ref(temp_tref)
             except InputError:
                 continue
+
+            if isinstance(temp_oref.index_node, DictionaryEntryNode):
+                # dictionary entries aren't interesting
+                continue
             if RecommendationEngine.includes_section(temp_oref):
                 continue
             if temp_oref.is_range():
                 temp_range_set = {subref.normal() for subref in temp_oref.range_list()}
-                in_common = 0 if focus_ref_set is None else len(temp_range_set & focus_ref_set)
-                if in_common > 0:
-                    temp_focus_range_factor = (in_common * base_score)/len(temp_range_set) if len(temp_range_set) < REF_RANGE_MAX else 0.0
+                in_common_set = set() if focus_ref_set is None else temp_range_set & focus_ref_set
+                if len(in_common_set) > 0:
+                    temp_focus_range_factor = (len(in_common_set) * base_score)/len(temp_range_set) if len(temp_range_set) < REF_RANGE_MAX else 0.0
                     if temp_focus_range_factor > focus_range_factor:
                         focus_range_factor = temp_focus_range_factor
-                    has_tref = True
+                    if focus_ref_subset is None or len(in_common_set) < len(focus_ref_subset):
+                        focus_ref_subset = in_common_set
                     continue
                 final_refs += list(temp_range_set)
                 final_other_data += [other_data_item] * len(temp_range_set)
@@ -313,7 +323,7 @@ class RecommendationEngine:
             else:
                 if focus_ref_set is not None and temp_oref.normal() in focus_ref_set:
                     focus_range_factor = base_score
-                    has_tref = True
+                    focus_ref_subset = {temp_oref.normal()}
                     continue
                 final_refs += [temp_tref]
                 final_other_data += [other_data_item]
@@ -324,7 +334,12 @@ class RecommendationEngine:
             # throw out steinsaltz
             filter_result = filter(lambda x: "Steinsaltz on " not in x[0], zip(final_refs, final_other_data))
             final_refs, final_other_data = reduce(lambda a, b: [a[0]+[b[0]], a[1]+[b[1]]], filter_result, [[], []])
-        if has_tref:
-            return final_refs, focus_range_factor, final_other_data
-        return [], focus_range_factor, final_other_data
+
+        focus_ref_subref = None
+        if focus_ref_subset is not None:
+            focus_ref_subref_list = sorted(map(lambda x: Ref(x), list(focus_ref_subset)), key=lambda x: x.order_id())
+            focus_ref_subref = focus_ref_subref_list[0].to(focus_ref_subref_list[-1])
+        if focus_ref_subset is not None or not check_has_ref:
+            return final_refs, focus_range_factor, final_other_data, focus_ref_subref
+        return [], focus_range_factor, final_other_data, focus_ref_subref
 
