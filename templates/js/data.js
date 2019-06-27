1ec5badae55728a3d75624e1aa6ddec37931f2bb
Ephraim Damboritz <ephraim@sefaria.org>
2019-06-27 11:58:40 +0300

    remove some spaces

--- templates/js/data.js
+++ templates/js/data.js
@@ -1,8 +1,3 @@
 {% autoescape off %}
-//all vars in this file will be available in global js scope
-
 var DJANGO_DATA_VARS = {{ data_props }};
-
-
-console.log(DJANGO_DATA_VARS);
 {% endautoescape %}
