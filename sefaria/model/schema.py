6a67048383a598adb790e26fc6b93a1905f1a31c
Noah Rapoport <nmrapoport@gmail.com>
2019-07-10 14:43:39 +0300

    Added function to remove sharedTerm from an index; fixed bug in parasha validation

--- sefaria/model/schema.py
+++ sefaria/model/schema.py
@@ -201,6 +201,11 @@ class AbstractTitledOrTermedObject(AbstractTitledObject):
         self.sharedTitle = term
         self._process_terms()
 
+    def remove_shared_term(self, term):
+        if self.sharedTitle == term:
+            self.sharedTitle = None
+            self.title_group = self.title_group.copy()
+            return 1
 
 class Term(abst.AbstractMongoRecord, AbstractTitledObject):
     """
@@ -782,10 +787,10 @@ class TitledTreeNode(TreeNode, AbstractTitledOrTermedObject):
             raise IndexSchemaError(u"Schema node {} with sharedTitle can not have explicit titles".format(self))
 
         if not self.sharedTitle:
-            special_book_cases = ["Genesis", "Exodus", "Leviticus", "Numbers", "Deuteronomy"]
+            special_book_cases = ["Genesis", "Exodus", "Leviticus", "Numbers", "Deuteronomy", "Judges"]
             for title in self.title_group.titles:
                 title = title["text"]
-                if title in special_book_cases:
+                if self.get_primary_title() in special_book_cases:
                     break
                 term = Term().load_by_title(title)
                 if term:
