513c21912676eb1fbad77d24768e9449c8cd41d5
Brett Lockspeiser <blocks@gmail.com>
2019-07-29 13:09:34 -0700

    Stats for webpages

--- sefaria/model/webpage.py
+++ sefaria/model/webpage.py
@@ -2,6 +2,7 @@
 import regex
 from urlparse import urlparse
 from datetime import datetime
+from collections import defaultdict
 
 from . import abstract as abst
 from . import text
@@ -83,4 +84,77 @@ def get_webpages_for_ref(tref):
             client_results.append(webpage_contents)
     
     return client_results
+
+
+
+def webpages_stats():
+
+    webpages = WebPageSet()
+    total_pages  = webpages.count()
+    total_links  = 0
+    sites        = defaultdict(int)
+    books        = defaultdict(int)
+    categories   = defaultdict(int)
+    covered_refs = defaultdict(set)
+
+    for webpage in webpages:
+        sites[webpage.domain()] += 1
+        for ref in webpage.refs:
+            total_links += 1
+            oref = text.Ref(ref)
+            books[oref.index.title] += 1
+            category = oref.index.get_primary_category()
+            category = oref.index.categories[0] + " Commentary" if category == "Commentary" else category
+            categories[category] += 1
+            [covered_refs[oref.index.title].add(ref.normal()) for ref in oref.all_segment_refs()]
+
+    # Totals
+    print "{} total pages.\n".format(total_pages)
+    print "{} total connections.\n".format(total_links)
+
+
+    # Count by Site
+    print "\nSITES"
+    sites = sorted(sites.iteritems(), key=lambda x: -x[1])
+    for site in sites:
+        print "{}: {}".format(site[0], site[1])
+
+
+    # Count / Percentage by Category
+    print "\nCATEGORIES"
+    categories = sorted(categories.iteritems(), key=lambda x: -x[1])
+    for category in categories:
+        print "{}: {} ({}%)".format(category[0], category[1], round(category[1]*100.0/total_links, 2))
+
+
+    # Count / Percentage by Book
+    print "\nBOOKS"
+    books = sorted(books.iteritems(), key=lambda x: -x[1])
+    for book in books:
+        print "{}: {} ({}%)".format(book[0], book[1], round(book[1]*100.0/total_links, 2))
+
+
+    # Coverage Percentage / Average pages per ref for Torah, Tanakh, Mishnah, Talmud
+    print "\nCOVERAGE"
+    coverage_cats = ["Torah", "Tanakh", "Bavli", "Mishnah"]
+    for cat in coverage_cats:
+        cat_books = text.library.get_indexes_in_category(cat)
+        covered = 0
+        total   = 0
+        for book in cat_books:
+            covered_in_book = covered_refs[book]
+            try:
+                total_in_book = set([ref.normal() for ref in text.Ref(book).all_segment_refs()])
+            except:
+                continue # Bad data in Mishnah Sukkah
+            
+            # print "{} in covered, not in total:".format(book)
+            # print list(covered_in_book - total_in_book)
+            # Ignore refs that we don't have in the library
+            covered_in_book = covered_in_book.intersection(total_in_book)
+
+            covered += len(covered_in_book)
+            total += len(total_in_book)
+
+        print "{}: {}%".format(cat, round(covered*100.0/total, 2))
 
