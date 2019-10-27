5de06b340cd62e2d289dbdf68b6f5293bd1fa135
Jonathan <jonathan@sefaria.org>
2019-10-27 16:40:32 +0200

    ref removal and protections against duplicate refs

--- sefaria/model/manuscript_image.py
+++ sefaria/model/manuscript_image.py
@@ -1,7 +1,7 @@
 # encoding=utf-8
 
-import re
-from sefaria.system.exceptions import DuplicateRecordError
+from collections import OrderedDict
+from sefaria.system.exceptions import DuplicateRecordError, InputError
 from sefaria.model.abstract import AbstractMongoRecord, AbstractMongoSet
 
 
@@ -30,8 +30,26 @@ class ManuscriptImage(AbstractMongoRecord):
     def add_segment_ref(self, tref):
         self.expanded_refs.append(tref)
 
+    def remove_segment_ref(self, tref):
+        try:
+            self.expanded_refs.remove(tref)
+        except ValueError:
+            raise ValueError(u"Ref {} does not appear on this image".format(tref))
+
     def _normalize(self):
         self.image_id = self.manuscript_title + str(self.page_num)
+        self.expanded_refs = OrderedDict.fromkeys(self.expanded_refs).keys()  # remove duplicates
+
+    def _validate(self):
+        # raise error on duplicates. _normalize should prevent this from happening
+        expanded_refs = set()
+        for seg_ref in expanded_refs:
+            if seg_ref in expanded_refs:
+                raise InputError
+            else:
+                expanded_refs.add(seg_ref)
+
+        return super(ManuscriptImage, self)._validate()
 
     def _pre_save(self):
         """
@@ -44,4 +62,4 @@ class ManuscriptImage(AbstractMongoRecord):
 
 
 class ManuscriptImageSet(AbstractMongoSet):
-    pass
+    recordClass = ManuscriptImage
