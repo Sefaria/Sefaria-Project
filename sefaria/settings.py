64279daf95530eefc90185e4e4827aa6a1ed15cd
lorenzofernando <lorenzo@sefaria.org>
2019-12-17 12:28:08 -0500

    Set STATIC_ROOT

--- sefaria/settings.py
+++ sefaria/settings.py
@@ -49,7 +49,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = '/app/static'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
