2e26e04d2c1cd7a0a476fab5705fd3743b773918
Jonathan <jonathan@sefaria.org>
2019-12-26 14:09:15 +0200

    parser change hotfix

--- sefaria/model/text.py
+++ sefaria/model/text.py
@@ -1031,7 +1031,7 @@ class AbstractTextRecord(object):
     @staticmethod
     def _find_itags(tag):
         if isinstance(tag, Tag):
-            is_footnote = tag.name == "sup" and tag.next_sibling.name == "i" and tag.next_sibling.get('class', '') == 'footnote'
+            is_footnote = tag.name == "sup" and tag.next_sibling.name == "i" and tag.next_sibling.get('class', '')[0] == 'footnote'
             is_inline_commentator = tag.name == "i" and len(tag.get('data-commentator', '')) > 0
             return is_footnote or is_inline_commentator
         return False
