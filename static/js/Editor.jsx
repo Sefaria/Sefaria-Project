de0d67dabde5a6eb877c8e62ed5e4e37e949aa0d
Russel Neiss <russel@sefaria.org>
2020-01-08 11:54:53 -0600

    Add links on paste

--- static/js/Editor.jsx
+++ static/js/Editor.jsx
@@ -749,15 +749,60 @@ const withSheetData = editor => {
     return editor
 }
 
+
 const withLinks = editor => {
-  const { exec, isInline } = editor
+  const { insertData, insertText, isInline } = editor
 
   editor.isInline = element => {
     return element.type === 'link' ? true : isInline(element)
   }
+
+  editor.insertText = text => {
+    if (text && Sefaria.util.isUrl(text)) {
+      wrapLink(editor, text)
+    } else {
+      insertText(text)
+    }
+  }
+
+  editor.insertData = data => {
+    const text = data.getData('text/plain')
+
+    if (text && Sefaria.util.isUrl(text)) {
+      wrapLink(editor, text)
+    } else {
+      insertData(data)
+    }
+  }
+
   return editor
 }
 
+const isLinkActive = editor => {
+  const [link] = Editor.nodes(editor, { match: n => n.type === 'link' })
+  return !!link
+}
+
+const wrapLink = (editor, url) => {
+  if (isLinkActive(editor)) {
+    unwrapLink(editor)
+  }
+
+  const { selection } = editor
+  const isCollapsed = selection && Range.isCollapsed(selection)
+  const link = {
+    type: 'link',
+    url,
+    children: isCollapsed ? [{ text: url }] : [],
+  }
+
+  if (isCollapsed) {
+    Transforms.insertNodes(editor, link)
+  } else {
+    Transforms.wrapNodes(editor, link, { split: true })
+    Transforms.collapse(editor, { edge: 'end' })
+  }
+}
 
 const toggleFormat = (editor, format) => {
   const isActive = isFormatActive(editor, format)
@@ -990,9 +1035,7 @@ const SefariaEditor = (props) => {
     }
 
     function onChange(value) {
-        console.log(value)
         if (currentDocument !== value) {
-        console.log('change')
             setCurrentDocument(value);
         }
 
--- static/js/sefaria/util.js
+++ static/js/sefaria/util.js
@@ -159,7 +159,13 @@ class Util {
       return (typeof window === "undefined" ) ? this._initialPath :
                 window.location.pathname + window.location.search;
     }
-    static parseURL(url) {
+
+    static isUrl(string) {
+      var res = string.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
+      return (res !== null)
+    }
+
+    static parseUrl(url) {
       var a =  document.createElement('a');
       a.href = url;
       return {
