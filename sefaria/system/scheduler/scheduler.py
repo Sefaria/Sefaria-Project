998ca9f7b89a11de5db65f8c563da807df8be05f
lorenzofernando <lorenzo@sefaria.org>
2020-02-18 20:37:09 -0600

    Fix broken import

--- sefaria/system/scheduler/scheduler.py
+++ sefaria/system/scheduler/scheduler.py
@@ -3,7 +3,7 @@ from apscheduler.jobstores.mongodb import MongoDBJobStore
 from apscheduler.schedulers.background import BackgroundScheduler
 from sefaria.system.database import client
 from . import jobs
-from .settings import APSCHEDULER_NAME
+from sefaria.settings import APSCHEDULER_NAME
 
 
 def run_background_scheduler():
