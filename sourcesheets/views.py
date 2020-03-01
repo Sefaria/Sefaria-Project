476329022a9bc12368592e0ff5d2692ba2147b32
Noah Santacruz <noahssantacruz@gmail.com>
2020-03-01 19:17:26 +0200

    redirect when sheets api fails

--- sourcesheets/views.py
+++ sourcesheets/views.py
@@ -588,7 +588,7 @@ def save_sheet_api(request):
 		if not request.user.is_authenticated:
 			key = request.POST.get("apikey")
 			if not key:
-				return jsonResponse({"error": "You must be logged in or use an API key to save."})
+				return jsonResponse({"error": "You must be logged in or use an API key to save.", "errorAction": "loginRedirect"})
 			apikey = db.apikeys.find_one({"key": key})
 			if not apikey:
 				return jsonResponse({"error": "Unrecognized API key."})
