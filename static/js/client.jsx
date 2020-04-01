3450bb934990b3c7c62eb5b63f3c1f10799a2460
Brett Lockspeiser <blocks@gmail.com>
2020-04-01 15:24:28 -0700

    Allow header and specific component to both render on one page

--- static/js/client.jsx
+++ static/js/client.jsx
@@ -21,16 +21,6 @@ $(function() {
     component = React.createElement(SefariaReact.ReaderApp, DJANGO_VARS.props);
     renderFunc(component, container);
   
-  } else if (DJANGO_VARS.containerId && DJANGO_VARS.reactComponentName) {
-    // Rendering just a specifc component to a container
-    container = document.getElementById(DJANGO_VARS.containerId);
-    component = React.createElement(SefariaReact[DJANGO_VARS.reactComponentName], DJANGO_VARS.props);
-    renderFunc(component, container);
-    if (footerContainer){
-      renderFunc(React.createElement(SefariaReact.Footer), footerContainer);
-    }
-
-  
   } else {
     // Rendering the Header & Footer only on top of a static page
     var settings = {
@@ -63,4 +53,12 @@ $(function() {
       renderFunc(React.createElement(SefariaReact.Footer), footerContainer);
     }
   }
+
+  if (DJANGO_VARS.containerId && DJANGO_VARS.reactComponentName) {
+    // Render a specifc component to a container
+    container = document.getElementById(DJANGO_VARS.containerId);
+    component = React.createElement(SefariaReact[DJANGO_VARS.reactComponentName], DJANGO_VARS.props);
+    renderFunc(component, container);
+  }
+
 });
