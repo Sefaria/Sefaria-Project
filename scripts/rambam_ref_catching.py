82c46c9c26c4d34935af92ced32e0219d69de1cb
shaneerosen <shanee@sefaria.org>
2020-04-22 22:46:55 +0300

    tests and a function to create the genTitles for mongo

--- scripts/rambam_ref_catching.py
+++ scripts/rambam_ref_catching.py
@@ -2,10 +2,9 @@ import django
 django.setup()
 from sefaria.model import *
 
-if __name__ == "__main__":
+def test(assertyes=False):
     ref1 = library.get_wrapped_refs_string('(רמב"ם כלאים א, א-ג; רמב"ם בכורות א, יד; שו"ע יו"ד סא, יא)')
     print(ref1)
-    # assert ref1[0] == Ref('Mishneh Torah, Diverse Species 1:1-3')
     ref11 = library.get_wrapped_refs_string('(רמב"ם תרומות ג, יח; שו"ע או"ח שלא, כו)')
     print(ref11)
     ref2 = library.get_refs_in_string('(משנה תורה מעשר שני ב, ה-ו)')
@@ -26,12 +25,86 @@ if __name__ == "__main__":
     print(ref9)
     ref10 = library.get_wrapped_refs_string('פני קצירת השעורים לקרבן העומר. שנאמר (ויקרא כג, ט-יד): ')
     print(ref10)
+    ref11 = library.get_refs_in_string('(רש"י שמות א, א).')
+    print(ref11)
 
 
-    # assert ref1 == [Ref('Mishneh Torah, Diverse Species 1:1-3')]
-    assert ref2 == [Ref('''Mishneh Torah, Second Tithes and Fourth Year's Fruit 2:5-6''')]
-    assert ref3 == [Ref('''Mishneh Torah, Second Tithes and Fourth Year's Fruit 11:5-6''')]
-    assert ref4 == [Ref('Mishneh Torah, Daily Offerings and Additional Offerings 7:21')]
-    assert ref5 == [Ref('Mishneh Torah, Heave Offerings 2:9')]
-    assert ref6 == [Ref('Mishneh Torah, Forbidden Foods 14:3')]
+    if assertyes:
+        assert ref2 == [Ref('''Mishneh Torah, Second Tithes and Fourth Year's Fruit 2:5-6''')]
+        assert ref3 == [Ref('''Mishneh Torah, Second Tithes and Fourth Year's Fruit 11:5-6''')]
+        assert ref4 == [Ref('Mishneh Torah, Daily Offerings and Additional Offerings 7:21')]
+        assert ref5 == [Ref('Mishneh Torah, Heave Offerings 2:9')]
+        assert ref6 == [Ref('Mishneh Torah, Forbidden Foods 14:3')]
 
+def to_add_to_mongo(endParts, title_type = None, comentators_name = []):
+    if title_type == 'rambam':
+        gen_titles = [
+        {
+            "lang": "he",
+            "titleParts": [
+                [
+                    "רמב\"ם",
+                    "משנה תורה"
+                ],
+                [
+                    " הלכות ",
+                    ", ",
+                    " הלכו' ",
+                    " "
+                ]
+            ]
+        }
+        #     ,
+        # {
+        #     "lang": "en",
+        #     "titleParts": [
+        #         [
+        #             "Rambam",
+        #             "Mishneh Torah"
+        #         ],
+        #         [
+        #             " Hilchot ",
+        #             ", "
+        #         ],
+        #         [
+        #             ""
+        #         ]
+        #     ]
+        # }
+    ]
+    else:
+        gen_titles = [
+            {
+                "lang": "he",
+                "titleParts": [
+                    [
+                        ", ",
+                        " "
+                    ]
+                ]
+            }
+                ,
+            {
+                "lang": "en",
+                "titleParts": [
+                    [
+                         ""
+                    ],
+                    [
+                        " ",
+                        ", "
+                    ],
+                    [
+                        ""
+                    ]
+                ]
+            }
+        ]
+        gen_titles[0]["titleParts"].insert(0, comentators_name)
+    gen_titles[0]["titleParts"].append(endParts)
+    return str(gen_titles)
+
+if __name__ == "__main__":
+    test(assertyes=True)
+    print('"genTitles" :' + to_add_to_mongo(['בכורות'], title_type='rambam') + ',')
+    # print('"genTitles" :' + to_add_to_mongo(['שמות'], comentators_name=['רש"י']) + ',')
