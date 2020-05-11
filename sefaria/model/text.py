48847d9970dec2c890419c65a8a75be5274ae6e5
Ron Shapiro <shapiro.rd@gmail.com>
2020-05-11 13:34:19 +0300

    Use Sefaria/Sefaria-Project github instead of blockspeiser/Sefaria-Project

--- sefaria/model/text.py
+++ sefaria/model/text.py
@@ -19,7 +19,7 @@ try:
     import re2 as re
     re.set_fallback_notification(re.FALLBACK_WARNING)
 except ImportError:
-    logging.warning("Failed to load 're2'.  Falling back to 're' for regular expression parsing. See https://github.com/blockspeiser/Sefaria-Project/wiki/Regular-Expression-Engines")
+    logging.warning("Failed to load 're2'.  Falling back to 're' for regular expression parsing. See https://github.com/sefaria/Sefaria-Project/wiki/Regular-Expression-Engines")
     import re
 
 from . import abstract as abst
