b6c224ce0c619d541e29f9d0a52e3d5c18d57d2f
Ephraim <ephraim@sefaria.org>
2020-06-10 13:50:40 +0300

    Remove function that isnt used (?) and is crashing the node server?

--- static/js/sefaria/util.js
+++ static/js/sefaria/util.js
@@ -488,61 +488,7 @@ class Util {
         };
     }
     static setupMisc() {
-        /*
-          classnames
-          Copyright (c) 2015 Jed Watson.
-          Licensed under the MIT License (MIT), see
-          http://jedwatson.github.io/classnames
-        */
-        (function () {
-            'use strict';
 
-            function classNames () {
-
-                var classes = '';
-
-                for (var i = 0; i < arguments.length; i++) {
-                    var arg = arguments[i];
-                    if (!arg) continue;
-
-                    var argType = typeof arg;
-
-                    if ('string' === argType || 'number' === argType) {
-                        classes += ' ' + arg;
-
-                    } else if (Array.isArray(arg)) {
-                        classes += ' ' + classNames.apply(null, arg);
-
-                    } else if ('object' === argType) {
-                        for (var key in arg) {
-                            if (arg.hasOwnProperty(key) && arg[key]) {
-                                classes += ' ' + key;
-                            }
-                        }
-                    }
-                }
-
-                return classes.substr(1);
-            }
-
-            if (typeof module !== 'undefined' && module.exports) {
-                module.exports = classNames;
-            } else if (typeof define === 'function' && typeof define.amd === 'object' && define.amd){
-                // AMD. Register as an anonymous module.
-                define(function () {
-                    return classNames;
-                });
-            } else {
-                window.classNames = classNames;
-            }
-
-        }());
-
-        // Protect against browsers without consoles and forgotten console statements
-        if(typeof(console) === 'undefined') {
-            var console = {};
-            console.log = function() {};
-        }
     }
     static handleUserCookie(loggedIn, uid, partner_group, partner_role) {
         var cookie = INBROWSER ? $.cookie : this.cookie;
