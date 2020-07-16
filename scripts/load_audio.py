da6e69672cb72265751765083b7ee18e90111431
Tamar Yastrab <tyastrab@gmail.com>
2020-07-16 12:57:52 -0400

    fixed audio for db

--- scripts/load_audio.py
+++ scripts/load_audio.py
@@ -1,7 +1,13 @@
+import django
+django.setup()
 import os
+import json
 from sefaria.model import *
 from sefaria.system import exceptions
+from sefaria.system.database import db
 
 directory = '/home/tyastrab/Documents/GitHub/Sefaria-Project/tamars_work/jsons/'
 for filename in os.listdir(directory):
-    db.audio.save(filename) # saves a single record
+    with open(directory + filename) as json_file:
+        data = json.load(json_file)
+        db.audio.save(data) # saves a single record
