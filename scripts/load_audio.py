dbf611145902928478ddcdfe93350f87cbae4378
Russel Neiss <russel.neiss@gmail.com>
2020-07-31 15:53:39 -0500

    Generalize `load_audio.py` data directory.

--- scripts/load_audio.py
+++ scripts/load_audio.py
@@ -6,7 +6,7 @@ from sefaria.model import *
 from sefaria.system import exceptions
 from sefaria.system.database import db
 
-directory = '/home/tyastrab/Documents/GitHub/Sefaria-Project/tamars_work/jsons/'
+directory = 'data/tmp/pockettorah_audio/'
 for filename in os.listdir(directory):
     with open(directory + filename) as json_file:
         data = json.load(json_file)
