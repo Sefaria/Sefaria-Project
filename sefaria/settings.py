14a0a2221e051d487f43aa979284eb1f6ffa5222
Brett Lockspeiser <blocks@gmail.com>
2020-09-16 14:07:00 -0700

    Refresh modals

--- sefaria/settings.py
+++ sefaria/settings.py
@@ -298,7 +298,7 @@ CACHES = {
 GLOBAL_INTERRUPTING_MESSAGE = {
     "name":       "highHolidayDonation-2020",
     "style":      "modal", # "modal" or "banner"
-    "repetition": 1,
+    "repetition": 2,
     "condition":  {
         "returning_only": True,
         "english_only": False,
