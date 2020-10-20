d66b300d083ab511dcbe21ad8087921c07f4e5b7
Noah <noahssantacruz@gmail.com>
2020-10-20 11:15:01 +0300

    log unknown jsondecodeerror and hopefully figure it out through the logs

--- reader/views.py
+++ reader/views.py
@@ -3469,7 +3469,12 @@ def profile_sync_api(request):
         if not no_return:
             # determine return value after new history saved to include new saved and deleted saves
             # send back items after `last_sync`
-            last_sync = json.loads(post.get("last_sync", str(profile.last_sync_web)))
+            from json.decoder import JSONDecodeError
+            try:
+                last_sync = json.loads(post.get("last_sync", str(profile.last_sync_web)))
+            except JSONDecodeError as e:
+                logger.warning(f"JSONDecoderError. post['last_sync']: {post.get('last_sync', None)}. profile.last_sync_web: {profile.last_sync_web}")
+                raise e
             uhs = UserHistorySet({"uid": request.user.id, "server_time_stamp": {"$gt": last_sync}}, hint="uid_1_server_time_stamp_1")
             ret["last_sync"] = now
             ret["user_history"] = [uh.contents(for_api=True) for uh in uhs.array()]
