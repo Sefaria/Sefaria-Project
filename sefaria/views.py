2196eef5570d4defdc5d19218de4ac3b2d687784
Ephraim <ephraim@sefaria.org>
2020-11-03 17:10:00 +0200

    Add maual calls to deleting underlying cached library data, so its properly regenerated during dependency cascade events (and multi-server setups)

--- sefaria/views.py
+++ sefaria/views.py
@@ -441,6 +441,7 @@ def reset_index_cache_for_text(request, title):
 
     index = model.library.get_index(title)
     model.library.refresh_index_record_in_cache(index)
+    model.library.reset_text_titles_cache()
 
     if MULTISERVER_ENABLED:
         server_coordinator.publish_event("library", "refresh_index_record_in_cache", [index.title])
@@ -575,6 +576,7 @@ def reset_ref(request, tref):
     oref = model.Ref(tref)
     if oref.is_book_level():
         model.library.refresh_index_record_in_cache(oref.index)
+        model.library.reset_text_titles_cache()
         vs = model.VersionState(index=oref.index)
         vs.refresh()
         model.library.update_index_in_toc(oref.index)
