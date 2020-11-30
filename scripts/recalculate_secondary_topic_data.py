4cb944c4750718e21cdf02d51bd2f0c51a082d03
Noah <noahssantacruz@gmail.com>
2020-11-30 10:53:14 +0200

    disable slug recalculation for now

--- scripts/recalculate_secondary_topic_data.py
+++ scripts/recalculate_secondary_topic_data.py
@@ -4,4 +4,5 @@ from sefaria.model import *
 from sefaria.helper.topic import recalculate_secondary_topic_data, set_all_slugs_to_primary_title
 
 recalculate_secondary_topic_data()
-set_all_slugs_to_primary_title()
+# skip this for now to not change slugs for rabbi project
+# set_all_slugs_to_primary_title()
