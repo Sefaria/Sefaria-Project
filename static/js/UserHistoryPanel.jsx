de5eb122aac15276d4f861e31d197468e63e9259
Ephraim <ephraim@sefaria.org>
2020-12-13 16:28:20 +0200

    Update language on history panel when disabled

--- static/js/UserHistoryPanel.jsx
+++ static/js/UserHistoryPanel.jsx
@@ -51,8 +51,9 @@ class UserHistoryPanel extends Component {
   render() {
     const content = (this.props.menuOpen === 'history' && !Sefaria.is_history_enabled) ? (
         <div id="history-disabled-msg">
-          <span className="int-en">Reading history is currently disabled. Go <a href="/settings/account">settings</a> to turn it back on</span>
-          <span className="int-he">היסטורית הקריאה שלך כבויה. ניתן להפעילה מחדש במסך <a href="/settings/account">ההגדרות</a></span>
+          <span className="int-en">
+            Reading history is currently disabled. You can re-enable this feature in your <a href="/settings/account">settings</a>account settings</a>."
+          <span className="int-he">היסטורית קריאה כבויה כרגע. ניתן להפעילה מחדש במסך <a href="/settings/account">ההגדרות</a></span>
         </div>
     ) : !!this.state.items ?
       this.state.items.reduce((accum, curr, index) => (  // reduce consecutive history items with the same ref
