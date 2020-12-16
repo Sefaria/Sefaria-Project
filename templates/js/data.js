7afc389c3fa2b27a3c79bd801ee9a235a24903a3
Ephraim <ephraim@sefaria.org>
2020-12-16 15:47:06 +0200

    Remove fields from data.js

--- templates/js/data.js
+++ templates/js/data.js
@@ -11,24 +11,7 @@ var DJANGO_DATA_VARS = {
   books:                {{ titles_json }},
   searchIndexText:     '{{ SEARCH_INDEX_NAME_TEXT }}',
   searchIndexSheet:    '{{ SEARCH_INDEX_NAME_SHEET }}',
-
-  /*calendars:            {{ calendars }}, //not in baseprops
-  notificationCount:    {{ notifications_count|default:'0' }},
-  notificationsHtml:    "{{ notifications_html|escape_quotes }}", //not in baseprops
-  saved:                {{ saved|jsonify|default:'[]'}}, //not in baseprops
-  last_place:           {{ last_place|jsonify|default:'[]'}},
-  _uid:                 {{ request.user.id|default:"null" }},
-  _email:               "{{ request.user.email|default:'' }}",
-  slug:                 "{{ slug|default:'' }}",
-  full_name:            "{{ full_name|escape_quotes|default:'' }}",
-  following:            {{ following|jsonify|default:'[]' }}, //not in baseprops
-  profile_pic_url:      "{{ profile_pic_url|default:'' }}",
-  is_moderator:         {{ is_moderator|yesno:'true, false' }},
-  is_editor:            {{ is_editor|yesno:'true, false' }},
-  is_history_enabled:   {% if request.user.is_authenticated %}{{reading_history|yesno:'true,false'}}{% else %}true{% endif %},
-  interfaceLang:        "{{ request.interfaceLang }}",
-  interruptingMessage:  {{ interrupting_message_json|default:'null' }},
-  _siteSettings:        {{ SITE_SETTINGS|jsonify }},
-  _debug:               {% if DEBUG %}true{% else %}false{% endif %},*/
 };
 {% endautoescape %}
+
+
