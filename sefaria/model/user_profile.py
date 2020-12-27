Sefaria/Sefaria-Project

commit f92922a6313d80cfdf3e37bb3a71d86649742d1e

Allow hebrew letters in profile slug

sefaria/model/user_profile.py
@@ -540,7 +540,7 @@ class UserProfile(object):
         slug = "%s-%s" % (self.first_name, self.last_name)
         slug = slug.lower()
         slug = slug.replace(" ", "-")
-        slug = re.sub(r"[^a-z0-9\-]", "", slug)
+        slug = re.sub(r"[^a-z\u05D0-\u05ea0-9\-]", "", slug)
         self.slug = slug
         dupe_count = 0
         while self.errors():

commit 6cd777e042892067d3c3323247f863a51b1c59bd

revert f92922a6313d80cfdf3e37bb3a71d86649742d1e which caused issues for Hebrew inputs to first and last name on signup

sefaria/model/user_profile.py
@@ -540,7 +540,7 @@ class UserProfile(object):
         slug = "%s-%s" % (self.first_name, self.last_name)
         slug = slug.lower()
         slug = slug.replace(" ", "-")
-        slug = re.sub(r"[^a-z\u05D0-\u05ea0-9\-]", "", slug)
+        slug = re.sub(r"[^a-z0-9\-]", "", slug)
         self.slug = slug
         dupe_count = 0
         while self.errors():