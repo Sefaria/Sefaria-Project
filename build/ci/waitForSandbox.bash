808e9208f832ce108acaad6f6416a1bc47926a3c
sefaria <lorenzo@>
2021-01-04 22:55:57 -0500

    Add sandbox deletion step

--- build/ci/waitForSandbox.bash
+++ build/ci/waitForSandbox.bash
@@ -7,3 +7,5 @@ waitDuration=${WAIT_DURATION:-900}
 echo "Waiting for https://${GITHUB_SHA:0:6}.cauldron.sefaria.org/texts to load for $waitDuration seconds"
 
 timeout $waitDuration bash -c 'while [[ "$(curl -s -k -o /dev/null -w ''%{http_code}'' https://${GITHUB_SHA:0:6}.cauldron.sefaria.org/texts)" != "200" ]]; do sleep 5; done'
+
+echo "Reached server. Proceeding."
