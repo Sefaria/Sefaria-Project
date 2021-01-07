92986af17efb0ae66da943ec9dac26d2c07a8bea
sefaria <lorenzo@>
2021-01-07 18:29:47 -0500

    Update `run_tests_on_github` to accept named test suites

--- reader/browsertest/run_tests_on_github.py
+++ reader/browsertest/run_tests_on_github.py
@@ -2,6 +2,11 @@
 # It takes the build name as its only command line argument
 __package__ = "reader.browsertest"
 
+
+import sys
+from selenium import webdriver
+from optparse import OptionParser
+
 import django
 django.setup()
 
@@ -11,17 +16,27 @@ import sys
 
 build = sys.argv[1]
 
+if __name__ == '__main__':
+    parser = OptionParser()
+    parser.add_option("-t", "--tests", dest="tests", help="Comma separated list of tests to run")
+    parser.add_option("-p", "--parallel", dest="parallel", action='store_true', help="Run multiple platforms in parallel")
 
-t = Trial(platform="githubnew", build=build, parallel=True)
-t.run()
-results = t.results()
+    options, user_args = parser.parse_args()
 
-print(results.report())
-fails = results.number_failed()
-if fails > 0:
-    sys.stderr.write(str(results))
-    sys.stderr.flush()
-sys.exit(fails)
+    tests = None
+    if options.tests:
+        tests = [getattr(basic_tests, t) for t in options.tests.split(",")]
+
+    t = Trial(platform="githubnew", build=build, parallel=options.parallel, tests=tests, verbose=True)
+    t.run()
+    results = t.results()
+
+    print(results.report())
+    fails = results.number_failed()
+    if fails > 0:
+        sys.stderr.write(str(results))
+        sys.stderr.flush()
+    sys.exit(fails)
 
 
 # Updates required to adapt run_tests_on_travis into run_tests_on_github
