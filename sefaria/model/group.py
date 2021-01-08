12f03f9e541535d0ff56988799af7f4d114d2898
Brett Lockspeiser <blocks@gmail.com>
2021-01-08 11:39:41 -0800

    Setting slugs on groups in advance of collections

--- sefaria/model/group.py
+++ sefaria/model/group.py
@@ -3,6 +3,8 @@ group.py
 Writes to MongoDB Collection: groups
 """
 import bleach
+import secrets
+import re
 
 from django.utils.translation import ugettext as _
 
@@ -94,6 +96,32 @@ class Group(abst.AbstractMongoRecord):
             old, new = self.pkeys_orig_values.get(field, None), getattr(self, field, None)
             if old != new:
                 self._handle_image_change(old, new)
+
+    def assign_slug(self):
+        """
+        Assign a slug for the collection. For public collections based on the collection 
+        name, for private collections a random string.
+        """
+        if getattr(self, "listed", False):
+            slug = self.name
+            slug = slug.lower()
+            slug = slug.strip()
+            slug = slug.replace(" ", "-")
+            slug = re.sub(r"[^a-z\u05D0-\u05ea0-9\-]", "", slug)
+            self.slug = slug
+            dupe_count = 0
+            while self.slug_taken():
+                dupe_count += 1
+                self.slug = "%s%d" % (slug, dupe_count)
+        else:
+            while True:
+                self.slug = secrets.token_urlsafe(6)
+                if not self.slug_taken():
+                    break
+
+    def slug_taken(self):
+        existing = Group().load({"slug": self.slug})
+        return bool(existing) and existing._id != getattr(self, "_id", None)
 
     def all_names(self, lang):
         primary_name = self.primary_name(lang)
