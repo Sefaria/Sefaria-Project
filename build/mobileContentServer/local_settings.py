8aff84744a6517cd79d79569e32b8ccebf365bcc
sefaria <lorenzo@>
2021-01-25 12:01:44 -0500

    Intermediate commit

---
+++ build/mobileContentServer/local_settings.py
@@ -0,0 +1,19 @@
+"""
+Local Settings Example file for Sefaria-iOS export script.
+
+Copy this file to `local_settings.py` and import values as needed.
+
+`local_settings.py` is excluded from this Git repo.
+"""
+
+# Where does Sefaria-Project live
+SEFARIA_PROJECT_PATH = "/Sefaria-Project"
+SEFARIA_EXPORT_PATH = "/MobileDownloadServer/static/ios-export"
+SEFARIA_IOS_SOURCES_PATH = "/Users/<USER>/Documents/Sefaria-iOS/ios/sources"
+SEFARIA_ANDROID_SOURCES_PATH = "/Users/<USER>/Documents/Sefaria-iOS/android/app/src/main/assets/sources"
+MINIFY_JSON = False
+USE_CLOUDFLARE = False
+CLOUDFLARE_ZONE = ""
+CLOUDFLARE_EMAIL = ""
+CLOUDFLARE_TOKEN = ""
+CLOUDFLARE_PATH = ""
