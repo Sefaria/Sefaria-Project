695d6004bb650a97027260c28da0fc2a7155857c
sefaria <lorenzo@>
2021-02-09 21:02:50 -0500

    print statement

--- build/ci/waitForCIJob.bash
+++ build/ci/waitForCIJob.bash
@@ -2,6 +2,7 @@
 set -e
 
 echo "Waiting for the test job to become ready"
+echo "GitHub Action $GITHUB_ACTION"
 
 timeout ${WAIT_DURATION:-900} bash -c "while [[ $(kubectl get job -l ci-run=$GITHUB_ACTION,test-name=${TEST_NAME:-pytest} -o json | jq -r '.items[0].status.succeeded') != '1' ]]; do sleep 5; done"
 
