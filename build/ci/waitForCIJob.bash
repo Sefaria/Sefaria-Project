bf8987956c8a0063f0d9942af6d3ee80c20f9e6f
sefaria <lorenzo@>
2021-02-11 18:26:31 -0500

    remove single-quotes from waitForCIJob

--- build/ci/waitForCIJob.bash
+++ build/ci/waitForCIJob.bash
@@ -6,6 +6,6 @@ echo "GitHub Run ID $GITHUB_RUN_ID"
 
 kubectl get jobs -l test-name=pytest -o json
 
-timeout ${WAIT_DURATION:-900} bash -c "while [[ $(kubectl get job -l ci-run=$GITHUB_RUN_ID,test-name=${TEST_NAME:-pytest} -o json | jq -r '.items[0].status.succeeded') != '1' ]]; do sleep 5; done"
+timeout ${WAIT_DURATION:-900} bash -c "while [[ $(kubectl get job -l ci-run=$GITHUB_RUN_ID,test-name=${TEST_NAME:-pytest} -o json | jq -r '.items[0].status.succeeded') != 1 ]]; do sleep 5; done"
 
 echo "Job is complete"
