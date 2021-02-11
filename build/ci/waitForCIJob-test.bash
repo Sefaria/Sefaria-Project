d91fbf324781c828bd089ec401709689f7033355
sefaria <lorenzo@>
2021-02-11 18:49:29 -0500

    change TEST input string to a YAML number

--- build/ci/waitForCIJob-test.bash
+++ build/ci/waitForCIJob-test.bash
@@ -3,8 +3,7 @@ set -e
 
 echo "Waiting for the test job to become ready"
 echo "GitHub Run ID $TEST_INPUT"
-
-kubectl get jobs -l test-name=pytest -o json
+echo $TEST_INPUT
 
 timeout ${WAIT_DURATION:-900} bash -c "while [[ $(kubectl get job -l ci-run=$TEST_INPUT,test-name=${TEST_NAME:-pytest} -o json | jq -r '.items[0].status.succeeded') != 1 ]]; do sleep 5; done"
 
--- build/ci/waitForCIJob.bash
+++ build/ci/waitForCIJob.bash
@@ -4,8 +4,6 @@ set -e
 echo "Waiting for the test job to become ready"
 echo "GitHub Run ID $GITHUB_RUN_ID"
 
-kubectl get jobs -l test-name=pytest -o json
-
 timeout ${WAIT_DURATION:-900} bash -c "while [[ $(kubectl get job -l ci-run=$GITHUB_RUN_ID,test-name=${TEST_NAME:-pytest} -o json | jq -r '.items[0].status.succeeded') != 1 ]]; do sleep 5; done"
 
 echo "Job is complete"
