5e0002f7dcb310fab6bf1f5c0c201793335af38b
Ephraim <ephraim@sefaria.org>
2021-02-15 17:51:49 +0200

    import react and better exprot syntax for future

--- static/js/context.js
+++ static/js/context.js
@@ -1,5 +1,10 @@
-export const ContentLanguageContext = React.createContext({
+import React from 'react';
+const ContentLanguageContext = React.createContext({
   language: "english",
   //changeContextLanguage: (language) => {},
 });
 ContentLanguageContext.displayName = 'ContentLanguageContext'; //This lets us see this name in the devtools
+
+export {
+  ContentLanguageContext,
+};
