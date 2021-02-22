d9586a934d77676c05433ea4b027595cb78ced15
Brett Lockspeiser <blocks@gmail.com>
2021-02-22 13:38:00 -0800

    Decode HTML entitles when stripping HTML / fixes for HTML entitles, line breaks in sheet titles

--- static/js/ReaderPanel.jsx
+++ static/js/ReaderPanel.jsx
@@ -1209,7 +1209,7 @@ class ReaderControls extends Component {
     var oref = Sefaria.ref(this.props.currentRef);
 
     if (this.props.sheet) {
-      title = this.props.sheet.title.stripHtmlKeepLineBreaks().replace(/&amp;/g, '&').replace(/(<br>|\n)+/g,' ');
+      title = this.props.sheet.title.stripHtmlConvertLineBreaks();
       heTitle = title;
       if (title == "") {
         title = "Untitled";
