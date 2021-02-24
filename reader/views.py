70cbac85f1583d136d8cca6515034834bc0e1948
sefaria <lorenzo@>
2021-02-24 15:48:54 -0500

    Update `isNodeJsReachable`

--- reader/views.py
+++ reader/views.py
@@ -4275,7 +4275,7 @@ def rollout_health_api(request):
 
     def isNodeJsReachable():
         url = NODE_HOST + "/healthz"
-        statusCode = request.urlopen(url).status
+        statusCode = urllib.request.urlopen(url).status
         return statusCode == "200"
 
     response = {
