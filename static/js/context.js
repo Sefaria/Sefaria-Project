24576a1ee998fc9ace09fc27f53a09e75e57b182
Ephraim <ephraim@sefaria.org>
2021-03-17 17:54:49 +0200

    remove unused code

--- static/js/context.js
+++ static/js/context.js
@@ -1,7 +1,6 @@
 import React from 'react';
 const ContentLanguageContext = React.createContext({
   language: "english",
-  //changeContextLanguage: (language) => {},
 });
 ContentLanguageContext.displayName = 'ContentLanguageContext'; //This lets us see this name in the devtools
 
