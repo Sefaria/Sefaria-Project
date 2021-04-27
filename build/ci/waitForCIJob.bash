80cbb53f133977675641520e6458fb46f7ce7d43
Lev Israel <eliezer.israel@gmail.com>
2021-04-27 15:11:11 -0400

    Reflect true result of pytests in actions

--- build/ci/waitForCIJob.bash
+++ build/ci/waitForCIJob.bash
@@ -6,9 +6,20 @@ echo "GitHub Run ID $GITHUB_RUN_ID"
 
 #timeout ${WAIT_DURATION:-900} bash -c "while [[ $(kubectl get job -l ci-run=$GITHUB_RUN_ID,test-name=${TEST_NAME:-pytest} -o json | jq -r '.items[0].status.succeeded') != 1 ]]; do sleep 5; done"
 
-while [[ $(kubectl get job -l ci-run=$GITHUB_RUN_ID,test-name=${TEST_NAME:-pytest} -o json | jq -r '.items[0].status.succeeded') != 1 ]]
-do 
-    sleep 5; 
+while true;
+do
+  if [[ $(kubectl get job -l ci-run=$GITHUB_RUN_ID,test-name=${TEST_NAME:-pytest} -o json | jq -r '.items[0].status.succeeded') == 1 ]];
+  then
+    echo "Job is complete"
+    exit 0
+  fi
+
+  if [[ $(kubectl get job -l ci-run=$GITHUB_RUN_ID,test-name=${TEST_NAME:-pytest} -o json | jq -r '.items[0].status.failed') > 1 ]];
+  then
+    echo "Job failed"
+    exit 1
+  fi
+
+  sleep 5;
 done
 
-echo "Job is complete"
