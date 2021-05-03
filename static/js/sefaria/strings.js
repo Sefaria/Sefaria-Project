9420ff61438e443cb39011b7436677edfae4b626
Russel Neiss <russel.neiss@gmail.com>
2021-05-03 15:13:21 -0500

    CSS hebrew fix

--- static/js/sefaria/strings.js
+++ static/js/sefaria/strings.js
@@ -325,8 +325,7 @@ const Strings = {
     "Thank you!": "תודה רבה!",
     "You are currently using the old document editor": "כעת הינך עושה שימוש בגרסה הישנה של עורך המסמכים",
     "Try the new version": "להתנסוּת בגרסה החדשה",
-    "Your feedback is greatly appreciated. You can now edit your sheets again using the old source sheet editor. If you have any questions or additional feedback you can reach us at": "אנחנו מעריכים מאוד את המשוב שלך. כעת באפשרותך לחזור לערוך את הדפים שלך באמצעות עורך המסמכים הישן. לשאלות או למשוב נוסף אפשר לפנות אלינו בדוא"ל:",
-
+    "Your feedback is greatly appreciated. You can now edit your sheets again using the old source sheet editor. If you have any questions or additional feedback you can reach us at": "אנחנו מעריכים מאוד את המשוב שלך. כעת באפשרותך לחזור לערוך את הדפים שלך באמצעות עורך המסמכים הישן. לשאלות או למשוב נוסף אפשר לפנות אלינו בדוא\"ל:",
 
 
     //TextTableofContents
