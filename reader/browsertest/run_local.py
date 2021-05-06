b9ad70d53aa85506e685bf0f925edcf2123a5c04
Lev Israel <eliezer.israel@gmail.com>
2021-05-06 12:35:03 -0400

    Forgive me another trivial commit for CI/CD

--- reader/browsertest/run_local.py
+++ reader/browsertest/run_local.py
@@ -37,6 +37,7 @@ def get_chrome_mobile_webdriver():
     return driver
 
 
+
 if __name__ == '__main__':
     parser = OptionParser()
     parser.add_option("-t", "--tests", dest="tests", help="Comma separated list of tests to run")
