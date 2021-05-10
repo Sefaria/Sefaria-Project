b06f1092b8bcbb580647298df4fa0992c47da338
Noah <noahssantacruz@gmail.com>
2021-05-10 11:29:56 +0300

    dont display indexes if len is 0

--- static/js/TopicPage.jsx
+++ static/js/TopicPage.jsx
@@ -543,7 +543,7 @@ const TopicSideColumn = ({ slug, links, clearAndSetTopic, parashaData, tref, int
     <ReadingsComponent parashaData={parashaData} tref={tref} />
   ) : null;
   const topicMetaData = <TopicMetaData timePeriod={timePeriod} properties={properties} />;
-  const indexesComponent = indexes ? (
+  const indexesComponent = (indexes && indexes.length) ? (
     <TopicSideSection title={{en: "Works", he: "Works"}} hasMore={indexes.length > 10}>
       {indexes.map(({en, he, url}) => <SimpleLinkedBlock key={url} en={en} he={he} url={url} aclasses={'relatedTopic'} />)}
     </TopicSideSection>
