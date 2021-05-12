d1326857386e5ce373c4a108311b5c62131d21d3
Steven Kaplan <steve@sefaria.org>
2021-05-12 13:29:38 +0300

    veritical positioning of logged out menu fixed

--- static/js/Header.jsx
+++ static/js/Header.jsx
@@ -16,15 +16,15 @@ import Component from 'react-class';
 
 const Help = () => (
     //hard-coding /help re-direct, also re-directs exist in sites/sefaria/urls.py
-    <div>
+    <div className="help">
       <span className="int-en">
         <a href="/collections/sefaria-faqs">
-        <img className="help" src="/static/img/help.svg" alt="Help" />
+        <img src="/static/img/help.svg" alt="Help" />
         </a>
       </span>
       <span className="int-he">
         <a href="/collections/%D7%A9%D7%90%D7%9C%D7%95%D7%AA-%D7%A0%D7%A4%D7%95%D7%A6%D7%95%D7%AA-%D7%91%D7%A1%D7%A4%D7%A8%D7%99%D7%90">
-        <img className="help" src="/static/img/help.svg" alt="עזרה" />
+        <img src="/static/img/help.svg" alt="עזרה" />
         </a>
       </span>
     </div>
@@ -82,7 +82,7 @@ const ProfilePicMenu = ({len, url, name}) => {
               <span className="int-he">הגדרות</span>
             </a></div>
             <div className="interfaceLinks-row">
-              <a className="enLink" href={`/interface/english?next=${getCurrentPage()}`}>English</a>  &middot;  <a className="heLink" href={`/interface/hebrew?next=${getCurrentPage()}`}>עברית</a>
+              <a className="enLink" href={`/interface/english?next=${getCurrentPage()}`}>English</a><span className="dot">•</span><a className="heLink" href={`/interface/hebrew?next=${getCurrentPage()}`}>עברית</a>
             </div>
             <div><a className="interfaceLinks-row bottom" href="/collections/sefaria-faqs">
               <span className="int-en">Help</span>
