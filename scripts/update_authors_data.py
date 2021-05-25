b550502bca2f925e2f0c769c0ea8e59a0b23c171
Noah <noahssantacruz@gmail.com>
2021-05-25 14:50:45 +0300

    basic changes to authors import script

--- scripts/update_authors_data.py
+++ scripts/update_authors_data.py
@@ -44,54 +44,54 @@ cr = csv.reader(StringIO(data))
 rows = list(cr)[4:]
 
 
-print("*** Deleting old person records ***")
+print("*** Deleting old authorTopic records ***")
 for foo, symbol in eras.items():
-    people = PersonSet({"era": symbol}).distinct("key")
-    db.person_rel.delete_many({"from_key": {"$in": people}})
-    db.person_rel.delete_many({"to_key": {"$in": people}})
-    db.person.delete_many({"era": symbol})
+    people = AuthorTopicSet({"properties.era.value": symbol}).distinct("slug")
+    db.topic_links.delete_many({"generatedBy": "update_authors_data", "toTopic": {"$in": people}})
+    db.topic_links.delete_many({"generatedBy": "update_authors_data", "fromTopic": {"$in": people}})
+    db.topics.delete_many({"properties.era.value": symbol})
     # Dependencies take too long here.  Getting rid of relationship dependencies above.  Assumption is that we'll import works right after to handle those dependencies.
     #PersonSet({"era": symbol}).delete()
 
 
-def _(p, attr, field):
-    if field:
-        setattr(p, attr, field)
+def _(p: Topic, attr, value):
+    if value:
+        p.set_property(attr, value, "sefaria")
 
-print("\n*** Adding person records ***\n")
+print("\n*** Adding authorTopic records ***\n")
 for l in rows:
-    key = l[0].encode('ascii', errors='ignore').decode()
-    if not key:
+    slug = l[0].encode('ascii', errors='ignore').decode()
+    if not slug:
         continue
-    print(key)
-    p = Person().load({"key": key}) or Person()
-    p.key = key
-    p.name_group.add_title(l[1].strip(), "en", primary=True, replace_primary=True)
-    p.name_group.add_title(l[3].strip(), "he", primary=True, replace_primary=True)
+    print(slug)
+    p = AuthorTopic.init(slug) or AuthorTopic()
+    p.slug = slug
+    p.title_group.add_title(l[1].strip(), "en", primary=True, replace_primary=True)
+    p.title_group.add_title(l[3].strip(), "he", primary=True, replace_primary=True)
     for x in l[2].split(","):
         x = x.strip()
         if len(x):
-            p.name_group.add_title(x, "en")
+            p.title_group.add_title(x, "en")
     for x in l[4].split(","):
         x = x.strip()
         if len(x):
-            p.name_group.add_title(x, "he")
+            p.title_group.add_title(x, "he")
     if len(l[5]) > 0:
         if "c" in l[5]:
-            p.birthYearIsApprox = True
+            _(p, 'birthYearIsApprox', True)
         else:
-            p.birthYearIsApprox = False
+            _(p, 'birthYearIsApprox', False)
         m = re.search(r"\d+", l[5])
         if m:
-            p.birthYear = m.group(0)
+            _(p, 'birthYear', m.group(0))
     if len(l[7]) > 0:
         if "c" in l[7]:
-            p.deathYearIsApprox = True
+            _(p, 'deathYearIsApprox', True)
         else:
-            p.deathYearIsApprox = False
+            _(p, 'deathYearIsApprox', False)
         m = re.search(r"\d+", l[7])
         if m:
-            p.deathYear = m.group(0)
+            _(p, 'deathYear', m.group(0))
     _(p, "birthPlace", l[6])
     _(p, "deathPlace", l[8])
     _(p, "era", eras.get(l[9]))
@@ -105,29 +105,31 @@ for l in rows:
 
 #Second Pass
 rowmap = {
-    16: 'child',
-    17: 'grandchild',
-    18: 'childinlaw',
-    19: 'student',
-    20: 'member',
-    21: 'correspondent',
+    16: 'child-of',
+    17: 'grandchild-of',
+    18: 'child-in-law-of',
+    19: 'taught',
+    20: 'member-of',
+    21: 'corresponded-with',
     22: 'opposed',
-    23: 'cousin',
+    23: 'cousin-of',
 }
-
+flip_link_dir = {'taught'}
 print("\n*** Adding relationships ***\n")
 for l in rows:
-    key = l[0].encode('ascii', errors='ignore').decode()
-    p = Person().load({"key": key})
-    for i, type in list(rowmap.items()):
+    from_slug = l[0].encode('ascii', errors='ignore').decode()
+    p = AuthorTopic.init(from_slug)
+    for i, link_type_slug in rowmap.items():
         if l[i]:
             for pkey in l[i].split(","):
-                pkey = pkey.strip().encode('ascii', errors='ignore').decode()
-                print("{} - {}".format(key, pkey))
-                if Person().load({"key": pkey}):
-                    pr = PersonRelationship({
-                        "type": type,
-                        "from_key": key,
-                        "to_key": pkey
-                    })
-                    pr.save()
+                to_slug = pkey.strip().encode('ascii', errors='ignore').decode()
+                to_slug, from_slug = (from_slug, to_slug) if link_type_slug in flip_link_dir else (to_slug, from_slug)
+                print("{} - {}".format(from_slug, pkey))
+                if AuthorTopic.init(to_slug):
+                    IntraTopicLink({
+                        "toTopic": to_slug,
+                        "fromTopic": from_slug,
+                        "linkType": link_type_slug,
+                        "dataSource": "sefaria",
+                        "generatedBy" : "update_authors_data",
+                    }).save()
