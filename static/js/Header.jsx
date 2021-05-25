f364fc58189cc2111f3c3ad2073d80a17f5006b8
Steven Kaplan <steve@sefaria.org>
2021-05-25 10:18:56 +0300

    alert for debugging

--- static/js/Header.jsx
+++ static/js/Header.jsx
@@ -65,6 +65,8 @@ const ProfilePicMenu = ({len, url, name}) => {
       };
   }, []);
   const getCurrentPage = () => {
+    alert(isOpen);
+    alert(encodeURIComponent(Sefaria.util.currentPath()));
     return isOpen ? (encodeURIComponent(Sefaria.util.currentPath())) : "/";
   }
   return (
