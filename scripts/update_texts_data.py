08e2a4e168b7efbf7d7e769710a80f801941bb3d
Brett Lockspeiser <blocks@gmail.com>
2021-06-16 15:24:38 -0700

    Fix parsing of authors for texts sheet

--- scripts/update_texts_data.py
+++ scripts/update_texts_data.py
@@ -59,7 +59,7 @@ for l in rows:
         current_authors = set(getattr(i, "authors", []) or [])
     except TypeError:
         current_authors = set()
-    sheet_authors = set([a.strip() for a in l[1].split(",") if AuthorTopic().load({"slug": a.strip()})])
+    sheet_authors = set([a.strip() for a in l[1].split(",") if len(a) and AuthorTopic.init(a.strip())])
     needs_save = current_authors != sheet_authors
     sheet_authors = list(sheet_authors)
 
