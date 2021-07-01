7f75bb19250f8b147215e501d5437c14daa3bd35
Lev Israel <eliezer.israel@gmail.com>
2021-07-01 12:38:56 -0400

    Regex for punctuation removal

--- static/js/TextRange.jsx
+++ static/js/TextRange.jsx
@@ -281,8 +281,10 @@ class TextRange extends Component {
 
     const showSegmentNumbers = showNumberLabel && this.props.basetext;
 
-    // const punctuationre = /[\.\!\?\:\,]|( —)|(?<![\u0591-\u05bd\u05bf-\u05c5\u05c7\u200d\u05d0-\u05eA])(\״)|(\״)(?![\u0591-\u05bd\u05bf-\u05c5\u05c7\u200d\u05d0-\u05eA])|(\״)(?=[\u0591-\u05bd\u05bf-\u05c5\u05c7\u200d\u05d0-\u05eA]{2,})|(^\״)|(\״$)/g
-    const punctuationre = /[\.\!\?\:\,]|( —)|(\״)/g
+    // [\.\!\?\:\,\u05F4]+                                                                      # Match (and remove) one or more punctuation or gershayim
+    //    (?![\u0591-\u05bd\u05bf-\u05c5\u05c7\u200d\u05d0-\u05eA](?:[\.\!\?\:\,\u05F4\s]|$))   # So long as it's not immediately followed by one letter (followed by space, punctuation, endline, etc.)
+    // |—\s;                                                                                    # OR match (and remove) an mdash followed by a space
+    const punctuationre = /[\.\!\?\:\,\u05F4]+(?![\u0591-\u05bd\u05bf-\u05c5\u05c7\u200d\u05d0-\u05eA](?:[\.\!\?\:\,\u05F4\s]|$))|—\s/g;
 
     const strip_punctuation_re = (this.props.settings?.language === "hebrew" || this.props.settings?.language === "bilingual") && this.props.settings?.punctuationTalmud === "punctuationOff" && data?.type === "Talmud" ? punctuationre : null;
     const nre = /[\u0591-\u05af\u05bd\u05bf\u05c0\u05c4\u05c5\u200d]/g; // cantillation
