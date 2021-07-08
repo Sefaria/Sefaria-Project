accbf088a8e42d3363555b223579c491958a531c
Steven Kaplan <steve@sefaria.org>
2021-07-08 07:41:27 +0300

    works but dedupe slow

--- scripts/webpages_cronjob.py
+++ scripts/webpages_cronjob.py
@@ -14,9 +14,9 @@ days = 20  # num of days we care about in find_sites_that_may_have_removed_linke
 # clean_webpages(test=False)
 #
 # print("Find sites that no longer have linker...")
-# find_sites_that_may_have_removed_linker(last_linker_activity_day=days)
+find_sites_that_may_have_removed_linker(last_linker_activity_day=days)
 # print("Looking for webpages that have no corresponding website.  If WebPages have been accessed in last 20 days, create a new WebSite for them.  Otherwise, delete them.")
-# find_webpages_without_websites(hit_threshold=50, last_linker_activity_day=days)
+find_webpages_without_websites(hit_threshold=50, last_linker_activity_day=days)
 
 print("Dedupe identical urls...")
 dedupe_identical_urls(test=False)
