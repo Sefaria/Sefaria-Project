db06fbfb10321171c2ab409e7c0c61773a93800b
Nissa Mai-Rose <nwymai@gmail.com>
2021-07-09 09:01:45 -0400

    update gravatar script to access profile properly and use try/catch to handle 404

--- scripts/add_gravatars_to_google_storage.py
+++ scripts/add_gravatars_to_google_storage.py
@@ -1,27 +1,35 @@
+import django
+django.setup()
 from sefaria.utils.util import epoch_time
 from sefaria.helper.file import get_resized_file
-import urllib.parse, urllib.request
+import urllib.parse, urllib.request, urllib.error
 from PIL import Image
 import hashlib
 from sefaria.google_storage_manager import GoogleStorageManager
 from sefaria.system.database import db
+from sefaria.model.user_profile import UserProfile
 
-for profile in db.profiles.find():
-    old_profile_pic_url = recentlyViewed = profile["profile_pic_url"]
+
+for profile_mongo in db.profiles.find():
+    old_profile_pic_url = recentlyViewed = profile_mongo["profile_pic_url"]
 
     if not old_profile_pic_url.startswith(GoogleStorageManager.BASE_URL):
-        email_hash = hashlib.md5(profile['email'].lower().encode('utf-8')).hexdigest()
+        profile = UserProfile(id=profile_mongo['id'])
+        email_hash = hashlib.md5(profile.email.lower().encode('utf-8')).hexdigest()
         gravatar_url = "https://www.gravatar.com/avatar/" + email_hash + "?d=404&s=250"
-        with urllib.request.urlopen(gravatar_url) as r:
-            if r.getcode() != 404:
-                bucket_name = GoogleStorageManager.PROFILES_BUCKET
-                with Image.open(r) as image:
-                    now = epoch_time()
-                    big_pic_url = GoogleStorageManager.upload_file(get_resized_file(image, (250, 250)), "{}-{}.png".format(profile['slug'], now), bucket_name, None)
-                    small_pic_url = GoogleStorageManager.upload_file(get_resized_file(image, (80, 80)), "{}-{}-small.png".format(profile['slug'], now), bucket_name, None)
-                    profile["profile_pic_url"] = big_pic_url
-                    profile["profile_pic_url_small"] = small_pic_url
+        try:
+            r = urllib.request.urlopen(gravatar_url)
+            bucket_name = GoogleStorageManager.PROFILES_BUCKET
+            with Image.open(r) as image:
+                now = epoch_time()
+                big_pic_url = GoogleStorageManager.upload_file(get_resized_file(image, (250, 250)), "{}-{}.png".format(profile['slug'], now), bucket_name, None)
+                small_pic_url = GoogleStorageManager.upload_file(get_resized_file(image, (80, 80)), "{}-{}-small.png".format(profile['slug'], now), bucket_name, None)
+                profile_mongo["profile_pic_url"] = big_pic_url
+                profile_mongo["profile_pic_url_small"] = small_pic_url
+        except urllib.error.HTTPError as e:
+            if e.code == 404:
+                profile_mongo["profile_pic_url"]=""
+                profile_mongo["profile_pic_url_small"]=""
             else:
-                    profile["profile_pic_url"] = ""
-                    profile["profile_pic_url_small"] = ""
-            db.profiles.save(profile)
+                print('unexpected error: {}'.format(e))
+        db.profiles.save(profile_mongo)
