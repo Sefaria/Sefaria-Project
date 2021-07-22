b9202a384ecbacba652a56a408913f967539eb21
Ephraim <ephraim@sefaria.org>
2021-07-22 23:05:19 +0300

    General Polish #4 - Profile page web mobile fixes

--- static/js/UserProfile.jsx
+++ static/js/UserProfile.jsx
@@ -317,7 +317,7 @@ class UserProfile extends Component {
       return (
           <div className="tab">
             <a href={tab.href}>
-              <img src={tab.icon} alt={`${tab.text} icon`}/>
+              <img className="tabIcon" src={tab.icon} alt={`${tab.text} icon`}/>
               <InterfaceText>{tab.text}</InterfaceText>
             </a>
           </div>
@@ -325,7 +325,7 @@ class UserProfile extends Component {
     }
     return (
       <div className="tab">
-        <img src={tab.icon} alt={`${tab.text} icon`} />
+        <img className="tabIcon" src={tab.icon} alt={`${tab.text} icon`} />
         <InterfaceText>{tab.text}</InterfaceText>
       </div>
     );
@@ -622,7 +622,7 @@ const ProfileSummary = ({ profile:p, message, follow, openFollowers, openFollowi
               <span className="int-en">Edit Profile</span>
               <span className="int-he">עריכת פרופיל</span>
             </a>
-            <a href="/settings/account" className="resourcesLink sans-serif">
+            <a href="/settings/account" className="resourcesLink sans-serif profile-settings">
               <img src="/static/icons/settings.svg" alt="Profile Settings" />
               <span className="int-en">Settings</span>
               <span className="int-he">הגדרות</span>
