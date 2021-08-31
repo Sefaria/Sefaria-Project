877f2b8e87310419bbbcf1905ae80ab1848f25e2
Nissa Mai-Rose <nwymai@gmail.com>
2021-08-31 11:18:26 -0400

    use svg instead of png for icons

--- static/js/ConnectionsPanel.jsx
+++ static/js/ConnectionsPanel.jsx
@@ -747,11 +747,11 @@ const SheetToolsList = (props) => {
     }
 
   return (<div><ToolsButton en="About this Sheet" he="תרגומים" image="about-text.svg" onClick={() => props.setConnectionsMode("AboutSheet")} />
-      {isOwner ? <ToolsButton en={isPublished ? "Publish Settings" : "Publish"} he="תרגומים" image="publish.png" onClick={() => props.setConnectionsMode("Publish")} /> : null}
+      {isOwner ? <ToolsButton en={isPublished ? "Publish Settings" : "Publish"} he="תרגומים" imageIcon="publish.svg" onClick={() => props.setConnectionsMode("Publish")} /> : null}
       <ToolsButton en="Copy" he="תרגומים" image="copy.png" onClick={() => props.setConnectionsMode("AboutSheet")} />
-      <ToolsButton en="Add to Collection" he="תרגומים" image="add-to-collection.png" onClick={() => toggleCollectionsModal()} />
-      <ToolsButton en="Print" he="תרגומים" image="print.png" onClick={() => window.print()} />
-      <ToolsButton en="Export to Google Docs" he="תרגומים" image="googledrive.png" onClick={() => window.print()} /> 
+      <ToolsButton en="Add to Collection" he="תרגומים" image="add-to-collection.svg" onClick={() => toggleCollectionsModal()} />
+      <ToolsButton en="Print" he="תרגומים" image="print.svg" onClick={() => window.print()} />
+      <ToolsButton en="Export to Google Docs" he="תרגומים" image="googledrive.svg" onClick={() => window.print()} /> 
       {/* todo: update export to google docs button so it works */}
       {showCollectionsModal ? <CollectionsModal
                       sheetID={sheet.id}
