d2620e3bde686d6267551524193593465edc5721
Nissa Mai-Rose <nwymai@gmail.com>
2021-09-23 12:03:30 -0400

    add 'X' button to clear filter

--- static/js/SearchFilters.jsx
+++ static/js/SearchFilters.jsx
@@ -191,8 +191,12 @@ const SearchFilterGroup = ({name, filters, updateSelected, expandable, paged, se
       setFilters(filters);
     }
   }
+  const clearInput = () => {
+    document.getElementById(`filter${name}`).value = "";
+    updateFilters("");
+  }
   // need hebrew for placeholder/title
-  const search = searchable ? <input class="searchBox" id={`filter${name}`} placeholder={Sefaria._(`Search ${name}`)} title={`Type to Filter ${name} Shown`} onChange={e => updateFilters(e.target.value)}></input> : null;
+  const search = searchable ? <div class="searchBox"><input id={`filter${name}`} placeholder={Sefaria._(`Search ${name}`)} title={`Type to Filter ${name} Shown`} onChange={e => updateFilters(e.target.value)}></input><button aria-label="Clear input" onClick={clearInput}><i class="fa fa-times searchFilterIcon" aria-hidden="true" tabIndex="0"></i></button></div>  : null;
 
   return (
     <div className="searchFilterGroup">
