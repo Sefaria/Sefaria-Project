2452396994dd6b42a2eb53e6e3521c05f568ea55
Lev Israel <eliezer.israel@gmail.com>
2021-09-26 12:48:35 -0400

    Banner for Simchat Torah

--- sefaria/settings.py
+++ sefaria/settings.py
@@ -297,19 +297,19 @@ CACHES = {
 }
 
 
-"""GLOBAL_INTERRUPTING_MESSAGE = {
-    "name":       "yom-kippur-2021-modal",
-    "style":      "modal", # "modal" or "banner"
+GLOBAL_INTERRUPTING_MESSAGE = {
+    "name":       "tishrei-thanks-2021-banner",
+    "style":      "banner", # "modal" or "banner"
     "repetition": 1,
-    "is_fundraising": True,
+    "is_fundraising": False,
     "condition":  {
         "returning_only": False,
         "english_only": False,
         "desktop_only": False,
         "debug": False,
     }
-}"""
-GLOBAL_INTERRUPTING_MESSAGE = None
+}
+# GLOBAL_INTERRUPTING_MESSAGE = None
 
 # Grab environment specific settings from a file which
 # is left out of the repo.
