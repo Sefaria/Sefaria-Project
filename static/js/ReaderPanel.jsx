b70ad926837b9df05fdf8673f41f6448b9e38eaf
Ephraim <ephraim@sefaria.org>
2021-10-19 16:45:42 +0300

    Remove caret from ReaderControls

--- static/js/ReaderPanel.jsx
+++ static/js/ReaderPanel.jsx
@@ -1234,13 +1234,9 @@ class ReaderControls extends Component {
       <div className={"readerTextToc" + (categoryAttribution ? ' attributed' : '')} onClick={this.props.sheetID ? this.openSheetConnectionsPanel : this.openTextConnectionsPanel}>
         <div className={"readerTextTocBox" + (this.props.sheetID ? " sheetBox" : "")} role="heading" aria-level="1" aria-live="polite">
           <div>
-            <a href={url} aria-label={"Show table of contents for " + title} >
-              { title ?
-              <i className="fa fa-angle-down invisible"></i> : null }
-
+            <a href={url} aria-label={"Show Connection Panel contents for " + title} >
               { this.props.sheetID ?
               <img src={"/static/img/sheet.svg"} className="sheetTocIcon" alt="" /> : null}
-
               { this.props.sheetID ?
               <h1 style={{direction: Sefaria.hebrew.isHebrew(title) ? "rtl" : "ltr"}}>
                 {title}
@@ -1253,7 +1249,6 @@ class ReaderControls extends Component {
                 </span>
               </h1>
               }
-              { title ? (<i className="fa fa-angle-down"></i>) : null }
               { showVersion ?
               <span className="readerTextVersion">
                 <span className="en">{versionTitle}</span>
