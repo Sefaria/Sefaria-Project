20431a436bfc02763e5d4b2ed84d9816b0f81380
Nissa Mai-Rose <nwymai@gmail.com>
2021-12-14 12:38:13 -0500

    change class to className

--- static/js/ConnectionsPanel.jsx
+++ static/js/ConnectionsPanel.jsx
@@ -302,7 +302,7 @@ class ConnectionsPanel extends Component {
               setConnectionsMode={this.props.setConnectionsMode}
               masterPanelSheetId={this.props.masterPanelSheetId}
             /> :
-            <div class="topToolsButtons">
+            <div className="topToolsButtons">
               <ToolsButton en="About this Text" he="אודות הטקסט" image="about-text.svg" onClick={() => this.props.setConnectionsMode("About")} />
               <ToolsButton en="Table of Contents" he="תוכן העניינים" image="text-navigation.svg" onClick={() => this.props.setConnectionsMode("Navigation")} />
               <ToolsButton en="Translations" he="תרגומים" image="translation.svg"  onClick={() => this.props.setConnectionsMode("Translations")} />
