eed33ee10e1c19cd3dad44ffef9fe4ea7957847b
Yishai <yishai@sefaria.org>
2022-02-03 10:01:40 +0200

    fix

--- sefaria/model/text.py
+++ sefaria/model/text.py
@@ -2032,7 +2032,7 @@ class VirtualTextChunk(AbstractTextRecord):
         self.is_merged = False
         self.sources = []
 
-        if not self._oref.index_node.parent.supports_language(self.lang):
+        if isinstance(self._oref.index_node, DictionaryEntryNode) and not self._oref.index_node.parent.supports_language(self.lang):
             self.text = []
             self._versions = []
             return
