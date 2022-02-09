af3e317b3562d9d981a09d68b06a134c8b9566b8
nsantacruz <noahssantacruz@gmail.com>
2022-02-09 16:21:14 +0200

    speed up script

--- scripts/add_corpora.py
+++ scripts/add_corpora.py
@@ -4,9 +4,13 @@ from tqdm import tqdm
 from sefaria.model import *
 
 if __name__ == '__main__':
+    from tqdm import tqdm
+    from sefaria.system.database import db
     for category in tqdm(['Tanakh', 'Mishnah', 'Bavli', 'Yerushalmi']):
         for index in library.get_indexes_in_category(category, full_records=True):
             if 'Minor Tractates' in index.categories:
                 continue
             index.corpora = [category]
-            index.save()
+            props = index._saveable_attrs()
+            db.index.replace_one({"_id": index._id}, props, upsert=True)
+            # index.save()
