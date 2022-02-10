fbe7084d5ce9287d8b96eb6c8cebc009862320f9
Nissa Mai-Rose <nwymai@gmail.com>
2022-02-10 16:01:23 -0500

    marginally improve this_hebrew_year

--- sefaria/model/trend.py
+++ sefaria/model/trend.py
@@ -5,7 +5,7 @@ trend.py
 """
 
 import time
-from datetime import datetime
+from datetime import datetime, date
 
 from . import abstract as abst
 from . import user_profile
@@ -48,6 +48,18 @@ def get_session_traits(request, uid=None):
 
 
 class DateRange(object):
+    new_years_dict = {
+        2020: datetime(2020, 9, 18),
+        2021: datetime(2021, 9, 6),
+        2022: datetime(2022, 9, 25),
+        2023: datetime(2023, 9, 15),
+        2024: datetime(2024, 10, 2),
+        2025: datetime(2025, 9, 22),
+        2026: datetime(2026, 9, 11),
+        2027: datetime(2027, 10, 1),
+        2028: datetime(2028, 9, 20)
+    }
+
     def __init__(self, key, start=None, end=None):
         """
         :param start: datetime or None, meaning open ended
@@ -63,12 +75,13 @@ class DateRange(object):
 
     @classmethod
     def this_hebrew_year(cls):
-        #todo: improve me!
-        return cls("this_hebrew_year", datetime(2021, 9, 6), datetime(2022, 9, 25))
-        # return cls("this_hebrew_year", datetime(2019, 9, 30), datetime(2020, 9, 18))
-
-        # last year
-        #return cls("this_hebrew_year", datetime(2018, 9, 10), datetime(2019, 9, 29))
+        #todo: add try catch
+        today = date.today()
+        this_gregorian_year_rh = cls.new_years_dict[today.year]
+        if (this_gregorian_year_rh.date() > today):
+            return cls("this_hebrew_year", cls.new_years_dict[today.year-1], this_gregorian_year_rh)
+        else:
+            return cls("this_hebrew_year", this_gregorian_year_rh, cls.new_years_dict[today.year+1])
 
     def needs_clause(self):
         return self.start or self.end
@@ -263,6 +276,9 @@ def setUserLanguageTraits():
                 "uid":          uid
             }).save()
 
+def setNationBuilderTraits():
+    # nationbuilder_updateTags()
+    pass
 
 def getAllUsersLanguageUsage(daterange):
     '''
@@ -478,6 +494,7 @@ def setAllTrends():
     setUserSheetTraits()
     setUserLanguageTraits()
     setCategoryTraits()
+    setNationBuilderTraits()
 
 class EnglishToleranceFactory(TrendFactory):
     name = "EnglishTolerance"
