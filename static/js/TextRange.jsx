83ec545d52773947d4a42a058f5affbe7030c1ac
Steve Kaplan <steve@sefaria.org>
2022-02-10 13:38:54 +0200

    refLinks

--- static/js/TextRange.jsx
+++ static/js/TextRange.jsx
@@ -495,10 +495,9 @@ class TextSegment extends Component {
       //Click of citation
       event.preventDefault();
       let ref = Sefaria.humanRef(refLink.attr("data-ref"));
-      const vtitle = refLink.attr("vtitle");
-      const vlang = refLink.attr("vlang");
-      let currVersions = {"en": null, "he": null};
-      currVersions[vlang] = vtitle;
+      const ven = refLink.attr("data-ven") ? refLink.attr("data-ven") : null;
+      const vhe = refLink.attr("data-vhe") ? refLink.attr("data-vhe") : null;
+      let currVersions = {"en": ven, "he": vhe};
       this.props.onCitationClick(ref, this.props.sref, true, currVersions);
       event.stopPropagation();
       Sefaria.track.event("Reader", "Citation Link Click", ref);
