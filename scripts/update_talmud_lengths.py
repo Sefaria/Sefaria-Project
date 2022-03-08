e295f211874a861efbae888512dcd73c2f212967
nsantacruz <noahssantacruz@gmail.com>
2022-03-08 18:16:36 +0200

    commandeer old lengths script

--- scripts/update_talmud_lengths.py
+++ scripts/update_talmud_lengths.py
@@ -1,18 +1,24 @@
 # -*- coding: utf-8 -*-
-
+import django
+django.setup()
 from sefaria.model import *
 
-i = IndexSet({"$and": [{"categories": "Talmud"}, {"categories": {"$ne": "Commentary"}}]})
-for ind in i:
-    sn = StateNode(ind.title)
-    ac = sn.get_available_counts("he")
-    try:
-        l = ind.nodes.lengths
-        if ac != l:
-            print("{} {}:{}".format(ind.title, ac, l))
+"""
+Commandeering this old script to update lengths on various texts
+"""
+
+if __name__ == '__main__':
+    i = library.get_indexes_in_category("Yerushalmi", full_records=True)
+    for ind in i:
+        sn = StateNode(ind.title)
+        ac = sn.get_available_counts("he")
+        try:
+            l = ind.nodes.lengths
+            if ac != l:
+                print("{} {}:{}".format(ind.title, ac, l))
+                ind.nodes.lengths = ac[:]
+                ind.save()
+        except Exception as e:
+            print("{} {}".format(ind.title, e))
             ind.nodes.lengths = ac[:]
             ind.save()
-    except Exception as e:
-        print("{} {}".format(ind.title, e))
-        ind.nodes.lengths = ac[:]
-        ind.save()
