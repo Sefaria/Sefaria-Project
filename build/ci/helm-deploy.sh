a84b1b674b50069d409942e78804856227411a4f
Brendan Galloway <brendan@flanksource.com>
2022-03-23 10:17:17 +0200

    ci: rework sandbox tests to use helm-chart

---
+++ build/ci/helm-deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+export WEB_IMAGE="gcr.io/$PROJECT_ID/sefaria-web-$BRANCH"
+export NODE_IMAGE="gcr.io/$PROJECT_ID/sefaria-node-$BRANCH"
+export ASSET_IMAGE="gcr.io/$PROJECT_ID/sefaria-asset-$BRANCH"
+export TAG="sha-$GIT_COMMIT"
+export NAME="sandbox-$GIT_COMMIT"
+
+yq e -i 'web.containerImage.imageRegistry = strenv(WEB_IMAGE)' $1
+yq e -i 'nodejs.containerImage.imageRegistry = strenv(NODE_IMAGE)' $1
+yq e -i 'nginx.containerImage.imageRegistry = strenv(ASSET_IMAGE)' $1
+yq e -i 'monitor.containerImage.imageRegistry = strenv(WEB_IMAGE)' $1
+yq e -i 'web.containerImage.tag = strenv(TAG)' $1
+yq e -i 'nodejs.containerImage.tag = strenv(TAG)' $1
+yq e -i 'nginx.containerImage.tag = strenv(TAG)' $1
+yq e -i 'monitor.containerImage.tag = strenv(TAG)' $1
+yq e -i 'deployEnv = strenv(NAME)' $1
+yq e -i 'localSettings.FRONT_END_URL = "https://"+strenv(NAME)+".cauldron.sefarai.org"' $1
+
+helm upgrade -i $NAME ./helm-chart --namespace $NAMESPACE -f $1 --debug
+
--- build/ci/waitForSandbox.bash
+++ build/ci/waitForSandbox.bash
@@ -4,8 +4,8 @@
 # GITHUB_HASH
 waitDuration=${WAIT_DURATION:-900}
 
-echo "Waiting for https://${GITHUB_SHA:0:6}.cauldron.sefaria.org/health-check to load for $waitDuration seconds"
+echo "Waiting for https://sandbox-${GITHUB_SHA:0:6}.cauldron.sefaria.org/health-check to load for $waitDuration seconds"
 
-timeout $waitDuration bash -c 'while [[ "$(curl -s -k -o /dev/null -w ''%{http_code}'' https://${GITHUB_SHA:0:6}.cauldron.sefaria.org/health-check)" != "200" ]]; do sleep 5; done'
+timeout $waitDuration bash -c 'while [[ "$(curl -s -k -o /dev/null -w ''%{http_code}'' https://sandbox-${GITHUB_SHA:0:6}.cauldron.sefaria.org/health-check)" != "200" ]]; do sleep 5; done'
 
 echo "Reached server. Proceeding."
