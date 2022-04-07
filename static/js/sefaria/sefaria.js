6029c9176642ce13a6b071c0ef9e376a44793738
Ephraim <ephraim@sefaria.org>
2022-04-07 12:22:43 +0300

    remove console logs

--- static/js/sefaria/sefaria.js
+++ static/js/sefaria/sefaria.js
@@ -2777,7 +2777,7 @@ Sefaria.setup = function(data, props = null) {
     Sefaria._cacheFromToc(Sefaria.toc);
     Sefaria._cacheHebrewTerms(Sefaria.terms);
     Sefaria._cacheSiteInterfaceStrings();
-    console.log(`sending user logged in status to GA, uid as bool: ${!!Sefaria._uid} | analytics id: ${Sefaria._analytics_uid}`);
+    //console.log(`sending user logged in status to GA, uid as bool: ${!!Sefaria._uid} | analytics id: ${Sefaria._analytics_uid}`);
     Sefaria.track.setUserData(!!Sefaria._uid, Sefaria._analytics_uid);
     Sefaria.search = new Search(Sefaria.searchIndexText, Sefaria.searchIndexSheet);
 };
