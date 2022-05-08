2c71e10869dfab36c0b61bbcc2e8dbe3dd83da71
Steve Kaplan <steve@sefaria.org>
2022-05-08 14:29:26 +0300

    clean_title clean_desc

--- sefaria/model/webpage.py
+++ sefaria/model/webpage.py
@@ -153,10 +153,12 @@ class WebPage(abst.AbstractMongoRecord):
         data["url"] = WebPage.normalize_url(data["url"])
         webpage = WebPage().load(data["url"])
         data["refs"] = WebPage._normalize_refs(data["refs"])  # remove bad refs so pages with empty refs won't get saved
+        data["title"] = WebPage.clean_title(data["title"], getattr(webpage, "_site_data", {}), getattr(webpage, "site_name", ""))
+        data["description"] = WebPage.clean_description(data.get("description", ""))
 
         if webpage:
             existing = True
-            if data["title"] == webpage.title and data.get("description", "") == getattr(webpage, "description", "") and set(data["refs"]) == set(webpage.refs):
+            if set(data["refs"]) == set(webpage.refs) and data["title"] == webpage.title and data["description"] == getattr(webpage, "description", ""):
                 return "excluded"  # no new data
             if data["title"] == "":
                 data["title"] = webpage.title  # dont save an empty title if title exists
@@ -185,20 +187,21 @@ class WebPage(abst.AbstractMongoRecord):
         return d
 
     def clean_client_contents(self, d):
-        d["title"]       = self.clean_title()
-        d["description"] = self.clean_description()
+        d["title"]       = self.clean_title(d["title"], d["_site_data"], d["site_name"])
+        d["description"] = WebPage.clean_description(d["description"])
         return d
 
-    def clean_title(self):
-        if not self._site_data:
-            return self.title
-        title = str(self.title)
+    @staticmethod
+    def clean_title(title, site_data, site_name):
+        if site_data == {}:
+            return title
+        title = str(title)
         title = title.replace("&amp;", "&")
-        brands = [self.site_name] + self._site_data.get("title_branding", [])
+        brands = [site_name] + site_data.get("title_branding", [])
         separators = [("-", ' '), ("|", ' '), ("—", ' '), ("–", ' '), ("»", ' '), ("•", ' '), (":", ''), ("⋆", ' ')]
         for separator, padding in separators:
             for brand in brands:
-                if self._site_data.get("initial_title_branding", False):
+                if site_data.get("initial_title_branding", False):
                     brand_str = f"{brand}{padding}{separator} "
                     if title.startswith(brand_str):
                         title = title[len(brand_str):]
@@ -207,10 +210,10 @@ class WebPage(abst.AbstractMongoRecord):
                     if title.endswith(brand_str):
                         title = title[:-len(brand_str)]
 
-        return title if len(title) else self._site_data["name"]
+        return title if len(title) else site_data["name"]
 
-    def clean_description(self):
-        description = getattr(self, "description", "")
+    @staticmethod
+    def clean_description(description):
         for uhoh_string in ["*/", "*******"]:
             if description.find(uhoh_string) != -1:
                 return None
