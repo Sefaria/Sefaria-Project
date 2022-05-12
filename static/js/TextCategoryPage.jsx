c14d6d140c4c90b4b0b880982f61d92e08286e3c
Steve Kaplan <steve@sefaria.org>
2022-05-12 11:00:03 +0300

    whiteList for JMC

--- static/js/TextCategoryPage.jsx
+++ static/js/TextCategoryPage.jsx
@@ -342,8 +342,8 @@ const getRenderedTextTitleString = (title, heTitle, categories) => {
     if (title === "Pesach Haggadah") {
         return ["Pesach Haggadah Ashkenaz", "הגדה של פסח אשכנז"]
     }
-    const whiteList = ['Imrei Yosher on Ruth', 'Duties of the Heart (abridged)', 'Midrash Mishlei',
-        'Midrash Tehillim', 'Midrash Tanchuma', 'Midrash Aggadah', 'Pesach Haggadah Edot Hamizrah'];
+    const whiteList = Sefaria._siteSettings.TORAH_SPECIFIC ? ['Imrei Yosher on Ruth', 'Duties of the Heart (abridged)', 'Midrash Mishlei',
+        'Midrash Tehillim', 'Midrash Tanchuma', 'Midrash Aggadah', 'Pesach Haggadah Edot Hamizrah'] : ["Farand's Records of the Constitutional Convention"];
     if (whiteList.indexOf(title) > -1 || categories.slice(-1)[0] === "Siddur") {
         return [title, heTitle];
     }
