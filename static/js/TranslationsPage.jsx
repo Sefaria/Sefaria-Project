e21cacdddfb35a5188dbcf00638eafd41b3dde8a
Nissa Mai-Rose <nwymai@gmail.com>
2022-05-17 10:17:52 -0400

    display items in plaintext on language page

--- static/js/TranslationsPage.jsx
+++ static/js/TranslationsPage.jsx
@@ -17,11 +17,18 @@ const TranslationsPage = ({translationsSlug}) => {
         <div className="content">
           <div className="sidebarLayout">
             <div className="contentInner">
-              <h1 className="sans-serif"><InterfaceText>About Texts in {Sefaria.ISOMap[translationsSlug]["name"]}</InterfaceText></h1>
-              {JSON.stringify(translations)}
-            {translations ? Object.keys(translations).map(x => {
+              <h1 className="sans-serif"><InterfaceText>{"About Texts in " + Sefaria.ISOMap[translationsSlug]["name"]}</InterfaceText></h1>
+              {translations ? Object.keys(translations).map(x => {
                 return (<div><h3>{x}</h3>
-                {Object.keys(translations[x]).map(y => JSON.stringify(translations[x][y]))}
+                {Object.keys(translations[x]).map((y, i0) => {
+                  if("title" in translations[x][y]) {
+                    return translations[x][y]["title"]
+                  } else {
+                    return Object.keys(translations[x][y]).map((z, i) =>
+                      {return (translations[x][y][z]["title"] + ", ")}
+                    )
+                  }
+                })}
                 </div>)
             }) : null}  
                    </div>
