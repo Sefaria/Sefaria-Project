d78c76e8c751569a6faa1b5233743d87fa02aba8
Nissa Mai-Rose <nwymai@gmail.com>
2022-05-24 12:58:49 -0400

    remove history pushstate on connectionsPanel (this will break something on export to gdrive)

--- static/js/ConnectionsPanel.jsx
+++ static/js/ConnectionsPanel.jsx
@@ -845,8 +845,8 @@ const SheetToolsList = ({ toggleSignUpModal, masterPanelSheetId, setConnectionsM
   const [copiedSheetId, setCopiedSheetId] = useState(0);
   const sheet = Sefaria.sheets.loadSheetByID(masterPanelSheetId);
   const [showCollectionsModal, setShowCollectionsModal] = useState(false);
-  history.pushState("", document.title, window.location.pathname
-                                                       + window.location.search); // remove hash once state updated
+  // history.pushState("", document.title, window.location.pathname
+  //                                                     + window.location.search); // remove hash once state updated
   // useEffect(() => {
   //   const sheet = Sefaria.sheets.loadSheetByID(masterPanelSheetId)
   //   setIsOwner(sheet.owner === Sefaria._uid);
