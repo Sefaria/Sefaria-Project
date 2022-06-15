0a1c66ab81a9a3be7028829d7e77f643969e2a4d
Lev Israel <eliezer.israel@gmail.com>
2022-06-15 16:00:19 -0400

    Move about.html, and enable

--- sites/s4d/urls.py
+++ sites/s4d/urls.py
@@ -3,13 +3,14 @@ from django.http import HttpResponseRedirect
 import reader.views as reader_views
 
 
-static_pages = []
+static_pages = ["about"]
 
 site_urlpatterns = [
     url(r'^$', reader_views.home, name="home"),
     url(r'^healthz/?$', reader_views.application_health_api_nonlibrary),
     url(r'^health-check/?$', reader_views.application_health_api_nonlibrary),
     url(r'^healthz-rollout/?$', reader_views.rollout_health_api),
-    # url(r'^(%s)/?$' % "|".join(static_pages), reader_views.serve_static),
+    url(r'^(%s)/?$' % "|".join(static_pages), reader_views.serve_static),
+
 ]
 
