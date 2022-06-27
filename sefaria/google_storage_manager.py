f497e42693c5f5178b05abc70c68d04b6b113f82
nsantacruz <noahssantacruz@gmail.com>
2022-06-27 10:59:00 +0300

    add back GOOGLE_APPLICATION_CREDENTIALS_FILEPATH for now.

--- sefaria/google_storage_manager.py
+++ sefaria/google_storage_manager.py
@@ -1,6 +1,8 @@
+from .settings import GOOGLE_APPLICATION_CREDENTIALS_FILEPATH
 from google.cloud import storage
 import re
 from io import BytesIO
+
 
 class GoogleStorageManager(object):
 
@@ -21,7 +23,7 @@ class GoogleStorageManager(object):
     @classmethod
     def get_bucket(cls, bucket_name):
         if getattr(cls, 'client', None) is None:
-            cls.client = storage.Client()
+            cls.client = storage.Client.from_service_account_json(GOOGLE_APPLICATION_CREDENTIALS_FILEPATH)
         bucket = cls.client.get_bucket(bucket_name)
         return bucket
 
