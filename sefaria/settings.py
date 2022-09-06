b008b42603a30bc5dd894d17aeffcbf7167df87d
Shanee <shanee@sefaria.org>
2022-09-06 14:42:15 -0400

    static(modals): Elul web banner

--- sefaria/settings.py
+++ sefaria/settings.py
@@ -299,8 +299,8 @@ CACHES = {
 
 
 GLOBAL_INTERRUPTING_MESSAGE = {
-    "name": "2022-08-25-Elul-sustainer-modal",
-    "style":      "modal",  # "modal" or "banner"
+    "name": "2022-9-7-elul-banner",
+    "style":      "banner",  # "modal" or "banner"
     "repetition": 1,
     "is_fundraising": True,
     "condition":  {
