7fb0641087b3267d9c7ac95f146e2ab04ebd1873
Steve Kaplan <steve@sefaria.org>
2022-09-16 13:27:13 +0300

    feat(login_page): instead of sefaria, it is now generic

--- sites/s4d/site_settings.py
+++ sites/s4d/site_settings.py
@@ -3,16 +3,16 @@
 SITE_SETTINGS = {
 	"TORAH_SPECIFIC": False,
 	"SITE_NAME": {
-		"en": "ConText - The American Democracy Library",
-		"he": "ConText - The American Democracy Library"
+		"en": "ContextUS",
+		"he": "ContextUS"
 	},
 	"LIBRARY_NAME": {
-		"en": "ConText - The American Democracy Library",
-		"he": "ConText - The American Democracy Library",
+		"en": "ContextUS - The American Democracy Library",
+		"he": "ContextUS - The American Democracy Library",
 	},
-	"LONG_SITE_NAME": "ConText - The American Democracy Library",
+	"LONG_SITE_NAME": "ContextUS - The American Democracy Library",
 	"SUPPORTED_TRANSLATION_LANGUAGES": [],
-	"LIBRARY_MESSAGE": 'ConText is home to the core texts in the American political tradition. We provide free access to texts, translations, and commentaries so that everyone can participate in the ongoing process of studying, interpreting, and creating American political history.',
+	"LIBRARY_MESSAGE": 'ContextUS is home to the core texts in the American political tradition. We provide free access to texts, translations, and commentaries so that everyone can participate in the ongoing process of studying, interpreting, and creating American political history.',
 	"LOGO": "/static/img/jmc-logo.png",
 	"HE_LOGO": "/static/img/jmc-logo.png",
 }
