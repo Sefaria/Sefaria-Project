c1cee0ea98439b9726fc0b666d419b2078c5f73a
Shanee <shanee@sefaria.org>
2022-11-05 23:04:53 -0400

    fix(abtesting): Fixes and tweaks needed for the PR pre deploy.

--- static/js/sefaria/track_ga4.js
+++ static/js/sefaria/track_ga4.js
@@ -7,9 +7,6 @@ class FeatureTrack {
             console.log("in FeatureTrack")
             gtag('event', `feature_seen_${feature_name}`, {
                 'feature_name': feature_name,
-                // 'book': analytics_dict.book,
-                // 'ref': analytics_dict.ref,
-                // 'item_category' : "Tanakh",
                 'logged_in': !!Sefaria._uid,
                 'interface_language': Sefaria.interfaceLang,
             });
@@ -21,8 +18,6 @@ class FeatureTrack {
             console.log("in FeatureTrack clicked")
             gtag('event',  `feature_clicked_${feature_name}`, {
                 'feature_name': feature_name,
-                // 'book': analytics_dict.book,
-                // 'ref': analytics_dict.ref,
                 'logged_in': !!Sefaria._uid,
                 'interface_language': Sefaria.interfaceLang,
                 'feature_data' : feature_data? feature_data: {}}
@@ -32,7 +27,7 @@ class FeatureTrack {
 }
 
 class SideBarFeatureTrack extends FeatureTrack {
-    // inherits Helper functions for tracking use of features (with Google Analytics 4)
+    // Inherits Helper functions for tracking use of features (with Google Analytics 4)
     static visible(feature_name, analytics_dict) {
         super.visible()
         if (typeof window !== 'undefined') {
