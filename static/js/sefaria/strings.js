94b63c21e8c05dcecd939fbafb94a278231b7ee8
saengel <sarah@sefaria.org>
2022-11-08 10:09:20 +0200

    fix(Hebrew Search Issues): Fix the about topics to map to Hebrew. Issue #2 on this card - https://trello.com/c/0HHNe8P0/2629-hebrew-language-issues-in-search

--- static/js/sefaria/strings.js
+++ static/js/sefaria/strings.js
@@ -60,7 +60,7 @@ const Strings = {
     "Explore Jewish Texts by Topic": "עיון בארון הספרים היהודי לפי נושא",
     "Explore by Topic": "לימוד לפי נושאים",
     "About Topics": "אודות \"נושאים\"",
-    "Topic pages bring you straight to selections of texts and user created source sheets about thousands of subjects. Sources that appear are drawn from existing indices of Jewish texts (like Aspaklaria) and from the sources our users include on their public source sheets.": "בדפי הנושא מלוקטים מקורות נבחרים ודפי מקורות של משתמשים על נושא מסוים. המקורות המופיעים בדפי הנושא נאספים ממאגרים קיימים של ספרות יהודית (דוגמת 'אספקלריא') ומתוך דפי מקורות פומביים של משתמשי ספריא.",
+    "Topics bring you straight to selections of texts and user created source sheets about thousands of subjects. Sources that appear are drawn from existing indices of Jewish texts (like Aspaklaria) and from the sources our users include on their public source sheets.": "בדפי הנושא מלוקטים מקורות נבחרים ודפי מקורות של משתמשים על נושא מסוים. המקורות המופיעים בדפי הנושא נאספים ממאגרים קיימים של ספרות יהודית (דוגמת 'אספקלריא') ומתוך דפי מקורות פומביים של משתמשי ספריא.",
     "Trending Topics": "נושאים נפוצים",
     "More": "עוד",
     "Less": "פחות",
