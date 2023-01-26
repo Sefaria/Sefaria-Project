8846ed8de1ceedd4bc01440e83d3cb4a7cdc1607
Brendan Galloway <brendan@flanksource.com>
2023-01-26 12:47:23 +0200

    fix: call relative script location with absolute path

--- build/startup/primeAll.bash
+++ build/startup/primeAll.bash
@@ -10,7 +10,6 @@ export SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null &&
 # Wait for pod come online
 timeout ${TIMEOUT:-900} bash -c 'while [[ "$(curl -s -k -o /dev/null -w ''%{http_code}'' http://$TARGET_HOSTNAME/healthz-rollout)" != "200" ]]; do sleep 10; done'
 
-./$SCRIPT_DIR/primeLocalCache.bash
-
+$SCRIPT_DIR/primeLocalCache.bash
 
 exit 0
