c996ce521fcb4f46669eb8825022c944e3c58b7a
yishai <yishai@sefaria.org>
2023-02-06 13:15:56 +0200

    fix(translations): change getHebrewVersions name to getSourceVersions in all the code

--- static/js/ReaderPanel.jsx
+++ static/js/ReaderPanel.jsx
@@ -1234,7 +1234,7 @@ class ReaderControls extends Component {
      * Preload translation versions to get shortVersionTitle to display
      */
     if (!this.shouldShowVersion()) { return; }
-    Sefaria.getHebrewVersions(this.props.currentRef).then(versionList => {
+    Sefaria.getSourceVersions(this.props.currentRef).then(versionList => {
       const enVTitle = this.props.currVersions.enAPIResult;
       if (!enVTitle) {
         // merged version from API
