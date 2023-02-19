8aabeca10bc6ee4d74d5518114285389f6849c4d
yishai <yishai@sefaria.org>
2023-02-19 11:41:04 +0200

    feat(WebPage): pass website favicon to the client.

--- sefaria/model/webpage.py
+++ sefaria/model/webpage.py
@@ -44,6 +44,7 @@ class WebPage(abst.AbstractMongoRecord):
             self.favicon     = "https://www.google.com/s2/favicons?domain={}".format(self.domain)
             self._site_data  = WebPage.site_data_for_domain(self.domain)
             self.site_name   = self._site_data["name"] if self._site_data else self.domain
+            self.site_favicon = "https://www.google.com/s2/favicons?domain={}".format(self._site_data['domains'][0])
             self.whitelisted = self._site_data["is_whitelisted"] if self._site_data else False
 
     def _init_defaults(self):
@@ -209,6 +210,7 @@ class WebPage(abst.AbstractMongoRecord):
         d["domain"]     = self.domain
         d["siteName"]   = self.site_name
         d["faviconUrl"] = self.favicon
+        d["siteFavicon"] = self.site_favicon
         del d["lastUpdated"]
         d = self.clean_client_contents(d)
         return d
