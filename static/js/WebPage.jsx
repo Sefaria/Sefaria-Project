6c1b247a01ee8e4a2c08b6b9629a2ec2c0d9781f
yishai <yishai@sefaria.org>
2023-03-16 12:01:43 +0200

    refactor(Webpage): function for getting the author/s string.

--- static/js/WebPage.jsx
+++ static/js/WebPage.jsx
@@ -4,9 +4,7 @@ import Sefaria from "./sefaria/sefaria";
 import React from "react";
 
 const WebPage = ({authors, isHebrew, favicon, url, domain, title, description, articleSource, anchorRef}) => {
-  const authorsNum = authors?.length;
-  const author = authorsNum > 1 ? 'Authors' : 'Author';
-  function getAuthorsString(lang, authors) {
+  function getAuthorsAsString(lang, authors) {
     const finalJoiner = (lang === 'he') ? ' ו' : ' and ';
     function arrangeAuthorName(authorName) {
       return authorName.split(', ').reverse().join(' ');
@@ -17,16 +15,23 @@ const WebPage = ({authors, isHebrew, favicon, url, domain, title, description, a
     }
     return authors?.reduce((accumulator, author, i) => addAuthorToString(accumulator, author, i), '');
   }
-  const authorsNames = getAuthorsString(isHebrew ? 'he' : 'en', authors);
+  function getAuthorString(authors) {
+    const authorsNum = authors?.length;
+    const author = authorsNum > 1 ? 'Authors' : 'Author';
+    const authorsNames = getAuthorsAsString(isHebrew ? 'he' : 'en', authors);
+    return (
+       authorsNames ? <div className="authors">
+               <InterfaceText>{author}</InterfaceText>: {authorsNames}
+           </div> : null
+    );
+  }
   return (<div className={"webpage" + (isHebrew ? " hebrew" : "")} key={url}>
     <img className="icon" src={favicon} />
     <a className="title" href={url} target="_blank">{title}</a>
     <div className="domain">{domain}</div>
     {description ? <div className="description">{description}</div> : null}
     <div className="webpageMetadata">
-      {authorsNames ? <div className="authors">
-        <InterfaceText>{author}</InterfaceText>: {authorsNames}
-      </div> : null}
+      {getAuthorString(authors)}
       {articleSource ? <div className="articleSource">
         <InterfaceText>Source</InterfaceText>: {articleSource.title}{articleSource.related_parts ? ` ${articleSource.related_parts}`: ''}
       </div> : null}
