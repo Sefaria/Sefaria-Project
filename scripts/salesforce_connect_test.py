f3f78e57e848c399c55093e50af2dc15a471dba5
Nissa Mai-Rose <nwymai@gmail.com>
2023-03-22 16:39:58 -0400

    feat(crm): implement salesforce app user post request

--- scripts/salesforce_connect_test.py
+++ scripts/salesforce_connect_test.py
@@ -9,6 +9,8 @@ cm = SalesforceConnectionManager()
 # session = cm.get_connection()
 headers = {'Content-type': 'application/json', 'Accept': 'application/json'}
 
-res = cm.session.get(cm.base_url + '/services/data/v56.0/sobjects/', headers=headers)
+res = cm.session.get(cm.base_url + '/services/data/v56.0/sobjects/Sefaria_App_User__c', headers=headers)
+res_data = res.json()
+res = cm.add_user_to_crm([], "test@testabcnissa.com", "NissaSefariaTest_First", "NissaSefariaTest_Last") ## figure out permissions error
 res_data = res.json()
 print(res_data)
--- sefaria/helper/crm/salesforce.py
+++ sefaria/helper/crm/salesforce.py
@@ -1,12 +1,34 @@
 import base64
 import requests
-from sefaria.helper.crm.crm_connection_manager import CrmConnectionManager
+import time
 
+from sefaria.helper.crm.crm_connection_manager import CrmConnectionManager
 from sefaria import settings as sls
+
 
 class SalesforceConnectionManager(CrmConnectionManager):
     def __init__(self):
         CrmConnectionManager.__init__(self, sls.SALESFORCE_BASE_URL)
+        self.version = "56.0"
+        self.resource_prefix = f"/services/data/{self.version}/sobjects/"
+
+    def create_endpoint(self, sobject_name):
+        return f"{sls.SALESFORCE_BASE_URL}/{self.resource_prefix}{sobject_name}"
+
+    def make_request(self, request, **kwargs):
+        for attempt in range(0,3):
+            try:
+                return request(**kwargs).json()
+            except Exception as e:
+                print(e)
+                time.sleep(1)
+
+    def get(self, endpoint):
+        headers = {'Content-type': 'application/json', 'Accept': 'application/json'}
+        return self.session.get(endpoint, headers)
+
+    def post(self, endpoint, **kwargs):
+        return self.session.post(endpoint, **kwargs)
 
     def _get_connection(self):
         access_token_url = "%s/services/oauth2/token?grant_type=client_credentials" % self.base_url
@@ -29,6 +51,15 @@ class SalesforceConnectionManager(CrmConnectionManager):
         Adds a new user to the CRM and subscribes them to the specified lists.
         Saves CRM Access info to profile
         """
+        return self.post(self.create_endpoint("Sefaria_App_User__c"),
+                  data={
+                      "First_Name__c": first_name,
+                      "Last_Name__c": last_name,
+                      "Sefaria_App_Email__c": email,
+                  })
+
+
+    def test_get(self):
         pass
 
 
