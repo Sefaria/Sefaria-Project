bea122d3de223290cdece463fb240e715ae11971
Nissa Mai-Rose <nwymai@gmail.com>
2023-05-09 15:10:58 -0400

    chore(crm): remove unused code

--- sefaria/helper/crm/nationbuilder.py
+++ sefaria/helper/crm/nationbuilder.py
@@ -83,38 +83,6 @@ class NationbuilderConnectionManager(CrmConnectionManager):
             yield {
                 "email": nationbuilder_sustainer["email"]
             }
-    #
-    # def sync_sustainers(self):
-    #     sustainers = {profile["id"]: profile for profile in db.profiles.find({"is_sustainer": True})}
-    #     added_count = 0
-    #     removed_count = 0
-    #     no_profile_count = 0
-    #     already_synced_count = 0
-    #     for nationbuilder_sustainer in self.nationbuilder_get_all(self.get_by_tag, ['sustainer_current_engineering']):
-    #
-    #         nationbuilder_sustainer_profile = UserProfile(email=nationbuilder_sustainer['email'])
-    #
-    #         if nationbuilder_sustainer_profile.id is not None:  # has user profile
-    #             existing_sustainer = sustainers.get(nationbuilder_sustainer_profile.id) is not None
-    #
-    #             if existing_sustainer:  # remove sustainer from dictionary; already synced
-    #                 del sustainers[nationbuilder_sustainer_profile.id]
-    #                 already_synced_count += 1
-    #             else:  # add new sustainer to db
-    #                 self.update_user_flags(nationbuilder_sustainer_profile, "is_sustainer", True)
-    #                 added_count += 1
-    #         else:
-    #             no_profile_count += 1
-    #
-    #     for sustainer_to_remove in sustainers:
-    #         profile = UserProfile(sustainer_to_remove)
-    #         self.update_user_flags(profile, "is_sustainer", False)
-    #         removed_count += 1
-    #
-    #     print("added: {}".format(added_count))
-    #     print("removed: {}".format(removed_count))
-    #     print("no_profile: {}".format(no_profile_count))
-    #     print("already synced: {}".format(already_synced_count))
 
     def mark_as_spam_in_crm(self, nationbuilder_id):
         """
