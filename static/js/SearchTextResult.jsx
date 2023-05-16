0605ab1b33ffbf4388c901ce8e7f33fa4016de6e
yonadavGit <92536571+yonadavGit@users.noreply.github.com>
2023-05-16 15:27:53 +0300

    remove debug prints in SearchTextResult.jsx

--- static/js/SearchTextResult.jsx
+++ static/js/SearchTextResult.jsx
@@ -113,10 +113,6 @@ class SearchTextResult extends Component {
                         }.bind(this))}
             </div>) : null;
 
-        const snippetMarkup = this.get_snippet_markup(data);
-        console.log(snippetMarkup.markup)
-        console.log(snippetMarkup)
-        console.log(data)
         const snippetClasses = classNames({snippet: 1, en: snippetMarkup.lang == "en", he: snippetMarkup.lang == "he"});
         return (
             <div className="result textResult">
