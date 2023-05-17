54ecf304d2a6a5481fb3c33e648d99203434f468
Shanee <shanee@sefaria.org>
2023-05-16 21:32:31 -0400

    fix(modals): fix typo

--- sefaria/settings.py
+++ sefaria/settings.py
@@ -299,7 +299,7 @@ CACHES = {
 
 
 GLOBAL_INTERRUPTING_MESSAGE = {
-    "name": "2023-05-16-shavout-donate-banner",
+    "name": "2023-05-16-shavuot-donate-banner",
     "style":      "banner",  # "modal" or "banner"
     "repetition": 1,
     "is_fundraising": True,
