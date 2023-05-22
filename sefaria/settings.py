a26fe07c597f5b94f0b7381e70b66f2a7bd3f001
Shanee <shanee@sefaria.org>
2023-05-22 16:27:18 -0400

    static(modals): shavuot modal

--- sefaria/settings.py
+++ sefaria/settings.py
@@ -299,8 +299,8 @@ CACHES = {
 
 
 GLOBAL_INTERRUPTING_MESSAGE = {
-    "name": "2023-05-16-shavuot-donate-banner",
-    "style":      "banner",  # "modal" or "banner"
+    "name": "2023-05-22-shavuot3",
+    "style":      "modal",  # "modal" or "banner"
     "repetition": 1,
     "is_fundraising": True,
     "condition":  {
