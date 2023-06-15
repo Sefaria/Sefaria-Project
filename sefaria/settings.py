6d95c92064e0b0ba8af4aef4a0994dd55c9d2b9c
Russel Neiss <russel.neiss@gmail.com>
2023-06-15 13:29:58 -0500

    static: sets up banner for help center promo [sc-6006]

--- sefaria/settings.py
+++ sefaria/settings.py
@@ -298,21 +298,21 @@ CACHES = {
 
 
 
-# GLOBAL_INTERRUPTING_MESSAGE = {
-#     "name": "2023-05-22-shavuot3",
-#     "style":      "modal",  # "modal" or "banner"
-#     "repetition": 1,
-#     "is_fundraising": True,
-#     "condition":  {
-#         "returning_only": False,
-#         "english_only": False,
-#         "desktop_only": False,
-#         "debug": False,
-#     }
-# }
+GLOBAL_INTERRUPTING_MESSAGE = {
+    "name": "2023-05-22-shavuot3",
+    "style":      "banner",  # "modal" or "banner"
+    "repetition": 1,
+    "is_fundraising": False,
+    "condition":  {
+        "returning_only": False,
+        "english_only": False,
+        "desktop_only": False,
+        "debug": False,
+    }
+}
 
 
-GLOBAL_INTERRUPTING_MESSAGE = None
+# GLOBAL_INTERRUPTING_MESSAGE = None
 
 
 
