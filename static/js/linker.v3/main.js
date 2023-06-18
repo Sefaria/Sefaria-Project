2c2c1534555c21089854093fff7492433a34b98d
nsantacruz <noahssantacruz@gmail.com>
2023-06-18 11:05:39 +0300

    refactor(linker): refactor getFindRefsUrl say query string is built in stages

--- static/js/linker.v3/main.js
+++ static/js/linker.v3/main.js
@@ -325,7 +325,15 @@ import {LinkExcluder} from "./excluder";
     }
 
     function getFindRefsUrl() {
-        return `${ns.sefariaUrl}/api/find-refs?with_text=1&debug=${0+ns.debug}&max_segments=${ns.maxParagraphs}`;
+        const params = {
+            with_text: 1,
+            debug: 0 + ns.debug,
+            max_segments: ns.maxParagraphs,
+        }
+        const queryString = Object.entries(params)
+            .map(([key, value]) => `${key}=${value}`)
+            .join('&')
+        return `${ns.sefariaUrl}/api/find-refs?${queryString}`;
     }
 
     function getWebsiteApiUrl() {
@@ -408,6 +416,7 @@ import {LinkExcluder} from "./excluder";
             dynamic: false,
             hidePopupsOnMobile: true,
             debug: false,
+            versionTitlePreference: null,
 
             // Deprecated options
             selector: null,           // CSS Selector
@@ -427,6 +436,7 @@ import {LinkExcluder} from "./excluder";
         ns.excludeFromLinking = options.excludeFromLinking;
         ns.dynamic = options.dynamic;
         ns.debug = options.debug;
+        ns.versionTitlePreference = options.versionTitlePreference;
         ns.maxParagraphs = 20;
         // useful to remove sefaria links for now but I think when released we only want this to run in debug mode
         if (options.debug || true) { removeExistingSefariaLinks(); }
