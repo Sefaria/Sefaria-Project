5b2a018ba2e1e3a13c890299faab9ee7057d1fb6
yishai <yishai@sefaria.org>
2023-06-21 11:20:50 +0300

    docs(api texts): docs for split_at_pipe_with_default.

--- api/helper.py
+++ api/helper.py
@@ -1,7 +1,13 @@
 from typing import List
 
 def split_at_pipe_with_default(string: str, list_length: int, defaults: List[str]) -> List[str]:
-    #length of defaults should be one less than list_length
+    """
+    split a string of query params into list of params by pipe. filling the list with defaults when there are not enough
+    :param string:
+    :param list_length: the required length of a parameters list
+    :param defaults: a list of default strings for potentially missing parameters
+    :return: list of parematers
+    """
     substrings = string.split('|', list_length-1)
     if len(substrings) < list_length:
         substrings += defaults[len(substrings)-list_length:]
