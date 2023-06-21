1b655c1925c6ba2a7ade63802cd324b9479a0e38
yishai <yishai@sefaria.org>
2023-06-21 11:17:37 +0300

    fix(api texts): split_at_pipe_with_default for more than 1 pipe.

--- api/helper.py
+++ api/helper.py
@@ -3,6 +3,6 @@ from typing import List
 def split_at_pipe_with_default(string: str, list_length: int, defaults: List[str]) -> List[str]:
     #length of defaults should be one less than list_length
     substrings = string.split('|', list_length-1)
-    while len(substrings) < list_length:
-        substrings.append(defaults.pop())
+    if len(substrings) < list_length:
+        substrings += defaults[len(substrings)-list_length:]
     return substrings
