594b6554f1547a84f792ca69ee5e40c3e612a16c
Akiva Berger <akiva10b@gmail.com>
2023-06-22 11:21:07 +0300

    feat(error tracking): sentry integration

--- sefaria/settings.py
+++ sefaria/settings.py
@@ -316,7 +316,7 @@ GLOBAL_INTERRUPTING_MESSAGE = {
 # GLOBAL_INTERRUPTING_MESSAGE = None
 
 
-
+SENTRY_DSN = None
 
 # Grab environment specific settings from a file which
 # is left out of the repo.
@@ -327,6 +327,40 @@ try:
         from sefaria.local_settings import *
 except ImportError:
     from sefaria.local_settings_example import *
+
+
+if SENTRY_DSN:
+
+    from http.client import HTTPException
+    import sentry_sdk
+    from sentry_sdk.integrations.django import DjangoIntegration
+
+    def before_send(event, hint):
+        # Check if the event has an exception
+        if 'exc_info' in hint:
+            exc_type, exc_value, tb = hint['exc_info']
+            # If it is not an HTTP 500 error, drop the event
+            if isinstance(exc_value, HTTPException) and exc_value.code != 500:
+                return None
+
+        # If there is no exception, drop the event
+        if 'exception' not in event:
+            return None
+
+        return event
+
+    sentry_sdk.init(
+        dsn=SENTRY_DSN,
+        environment=os.getenv("SENTRY_ENVIRONMENT,", "unknown"),
+        integrations=[DjangoIntegration()],
+        traces_sample_rate=1.0,
+        send_default_pii=False,
+        before_send=before_send,
+        max_breadcrumbs=30,
+        release=os.getenv("SENTRY_CODE_VERSION", "unknown"),
+        
+    )
+
 
 # Listed after local settings are imported so CACHE can depend on DEBUG
 WEBPACK_LOADER = {
--- sefaria/system/context_processors.py
+++ sefaria/system/context_processors.py
@@ -74,6 +74,7 @@ def global_settings(request):
         "DEBUG":                  DEBUG,
         "OFFLINE":                OFFLINE,
         "SITE_SETTINGS":          SITE_SETTINGS,
+        "SENTRY_DSN":             SENTRY_DSN,
     }
 
 
