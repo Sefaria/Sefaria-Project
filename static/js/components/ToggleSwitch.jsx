09886aa2c6720ee76287327a0e2831bb40af5bbb
yishai <yishai@sefaria.org>
2023-06-27 09:18:23 +0300

    feat: toggle switch component.

---
+++ static/js/components/ToggleSwitch.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function ToggleSwitch(props) {
+    const name = props.name;
+    return (
+        <div className="toggle-switch-container">
+          <div className="toggle-switch">
+            <input
+                type="checkbox"
+                className="toggle-switch-checkbox"
+                name={name}
+                id={name}
+                disabled={!props.disabled}
+            />
+            <label className="toggle-switch-label" htmlFor={name}>
+              <span className="toggle-switch-inner" />
+              <span className="toggle-switch-switch" />
+            </label>
+          </div>
+        </div>
+    );
+}
+ToggleSwitch.proptypes = {
+    name: PropTypes.string,
+    disabled: PropTypes.bool,
+}
+export default ToggleSwitchButton;
