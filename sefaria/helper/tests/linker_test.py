c0d79d1ebd96df68462660ddd99e32e8f25bafef
nsantacruz <noahssantacruz@gmail.com>
2023-06-28 09:53:33 +0300

    test(linker): add tests for load spacy model

--- sefaria/helper/tests/linker_test.py
+++ sefaria/helper/tests/linker_test.py
@@ -1,17 +1,47 @@
-from pytest import fixture
-from sefaria.helper.linker import _FindRefsText, _create_find_refs_text
+from sefaria.helper.linker import _FindRefsText, _create_find_refs_text, _FindRefsTextOptions, load_spacy_model
+import pytest
+from unittest.mock import patch
+from sefaria.google_storage_manager import GoogleStorageManager
+import spacy
+import tarfile
+import io
 
 
-@fixture
-def post_body():
-    return {"text": {"title": "TITLE", "body": "BODY"}}
+class TestLoadSpacyModel:
 
+    @staticmethod
+    @pytest.fixture
+    def spacy_model():
+        return spacy.blank('en')
 
-@fixture
-def expected_find_refs_text(post_body):
-    return _FindRefsText(post_body['text']['title'], post_body['text']['body'])
+    @staticmethod
+    @pytest.fixture
+    def tarfile_buffer():
+        tar_buffer = io.BytesIO()
+        with tarfile.open(fileobj=tar_buffer, mode='w:gz') as tar:
+            tar.addfile(tarfile.TarInfo('test'))
+        tar_buffer.seek(0)
+        return tar_buffer
 
+    @staticmethod
+    @patch('spacy.load')
+    def test_load_spacy_model_local(spacy_load_mock, spacy_model):
+        spacy_load_mock.return_value = spacy_model
+        assert load_spacy_model('local/path') == spacy_model
 
-def test_create_find_refs_text(post_body: dict, expected_find_refs_text: _FindRefsText):
-    actual_find_refs_text = _create_find_refs_text(post_body)
-    assert expected_find_refs_text == actual_find_refs_text
+    @staticmethod
+    @patch('spacy.load')
+    @patch.object(GoogleStorageManager, 'get_filename')
+    def test_load_spacy_model_cloud(get_filename_mock, spacy_load_mock, spacy_model, tarfile_buffer):
+        get_filename_mock.return_value = tarfile_buffer
+        spacy_load_mock.return_value = spacy_model
+        assert load_spacy_model('gs://bucket_name/blob_name') == spacy_model
+
+    @staticmethod
+    @patch('spacy.load')
+    @patch.object(GoogleStorageManager, 'get_filename')
+    def test_load_spacy_model_cloud_invalid_path(get_filename_mock, spacy_load_mock, spacy_model, tarfile_buffer):
+        get_filename_mock.return_value = tarfile_buffer
+        spacy_load_mock.side_effect = OSError
+        with pytest.raises(OSError):
+            load_spacy_model('invalid_path')
