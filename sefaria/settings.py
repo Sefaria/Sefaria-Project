6bad1dd716f03dba08ae8340e29e18a3d94bbfcf
Akiva Berger <akiva10b@gmail.com>
2023-06-29 13:56:01 +0300

    feat(error catching): move function to utils

--- sefaria/settings.py
+++ sefaria/settings.py
@@ -3,6 +3,8 @@
 import os.path
 from django.utils.translation import ugettext_lazy as _
 
+from sefaria.settings_utils import init_sentry
+
 relative_to_abs_path = lambda *x: os.path.join(os.path.dirname(
                                os.path.realpath(__file__)), *x)
 # Local time zone for this installation. Choices can be found here:
@@ -328,36 +330,7 @@ except ImportError:
 
 
 if SENTRY_DSN:
-
-    from http.client import HTTPException
-    import sentry_sdk
-    from sentry_sdk.integrations.django import DjangoIntegration
-
-    def before_send(event, hint):
-        # Check if the event has an exception
-        if 'exc_info' in hint:
-            exc_type, exc_value, tb = hint['exc_info']
-            # If it is not an HTTP 500 error, drop the event
-            if isinstance(exc_value, HTTPException) and exc_value.code != 500:
-                return None
-
-        # If there is no exception, drop the event
-        if 'exception' not in event:
-            return None
-
-        return event
-
-    sentry_sdk.init(
-        dsn=SENTRY_DSN,
-        environment=os.getenv("SENTRY_ENVIRONMENT,", "unknown"),
-        integrations=[DjangoIntegration()],
-        traces_sample_rate=1.0,
-        send_default_pii=False,
-        before_send=before_send,
-        max_breadcrumbs=30,
-        release=os.getenv("SENTRY_CODE_VERSION", "unknown"),
-        
-    )
+    init_sentry(SENTRY_DSN)
 
 
 # Listed after local settings are imported so CACHE can depend on DEBUG
