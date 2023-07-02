d51b898d8b70d5f1afe91985345b6ff65fcdf47a
yishai <yishai@sefaria.org>
2023-07-02 11:09:39 +0300

    feat(translation menu): reusable popover menu.

---
+++ static/js/components/PopoverMenu.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import PropTypes from "prop-types";
+
+const PopoverMenu = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+  return (
+    <div className="popover-menu">
+      <button onClick={() => setIsOpen(!isOpen)}>{props.button}</button>
+      {isOpen && props.menu}
+    </div>
+  );
+};
+PopoverMenu.prototypes = {
+    button: PropTypes.elementType.isRequired,
+    menu: PropTypes.elementType.isRequired,
+};
+export default PopoverMenu;
