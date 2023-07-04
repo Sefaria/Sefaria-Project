65efe5f0dd6564a8442033c6b85e16529d05b151
Akiva Berger <akiva10b@gmail.com>
2023-07-04 12:58:28 +0300

    feat(testing): add topic page tests

--- e2e-tests/tests/reader.spec.ts
+++ e2e-tests/tests/reader.spec.ts
@@ -1,10 +1,16 @@
 import { test, expect } from '@playwright/test';
+import {goToPageWithLang} from '../utils';
 
-test('has title', async ({ page }) => {
-  await page.goto('/Berakhot.28b.4?vhe=Wikisource_Talmud_Bavli&lang=bi&with=all&lang2=he');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/ברכות/);
+test('Navigate to bereshit', async ({ context }) => {
+  const page = await goToPageWithLang(context, '/texts');
+  await page.getByRole('link', { name: 'Tanakh' }).click();
+  await page.getByRole('link', { name: 'Genesis' }).click();
+  await page.waitForSelector('text=Loading...', { state: 'detached' });
+  await page.locator('.sectionLink').first().click();
+  await expect(page).toHaveTitle(/Genesis 1/);
+  // wait until Bereshit is visible
+  await page.waitForSelector('text=Bereshit', { state: 'visible' });
+  await page.getByText('בְּרֵאשִׁ֖ית בָּרָ֣א אֱלֹהִ֑ים אֵ֥ת הַשָּׁמַ֖יִם וְאֵ֥ת הָאָֽרֶץ׃').isVisible();
 });
 
 test('verify translations', async ({ page }) => {
--- e2e-tests/tests/topics.spec.ts
+++ e2e-tests/tests/topics.spec.ts
@@ -1,6 +1,5 @@
-import { test, expect } from '@playwright/test';
+import { test } from '@playwright/test';
 import {goToPageWithLang} from '../utils';
-
 
 
 test('go to topic page', async ({ context }) => {
@@ -9,3 +8,18 @@ test('go to topic page', async ({ context }) => {
   await page.getByRole('link', { name: 'Rosh Hashanah' }).isVisible();
 });
 
+
+test('filter topics', async ({ context }) => {
+  const page = await goToPageWithLang(context, '/topics/all/a');
+  // wait for getByText('Loading...') to disappear
+  await page.waitForSelector('text=Loading...', { state: 'detached' });
+  await page.getByPlaceholder('Search Topics').fill('Rosh Hashanah');
+  await page.locator('div').filter({ hasText: /^Rosh HashanahRosh Hashanah and Yom Kippur Prayers$/ }).getByRole('link', { name: 'Rosh Hashanah', exact: true }).isVisible();
+  await page.getByPlaceholder('Search Topics').fill('Shabbat');
+  await page.locator('div').filter({ hasText: /^Rosh HashanahRosh Hashanah and Yom Kippur Prayers$/ }).getByRole('link', { name: 'Rosh Hashanah', exact: true }).isHidden();
+  await page.locator('div').filter({ hasText: /^ShabbatThe Prohibitions of Shabbat$/ }).getByRole('link', { name: 'Shabbat', exact: true }).click();
+  // wait for getByText('Loading...') to disappear
+  await page.waitForSelector('text=Loading...', { state: 'detached' });
+  await page.getByRole('link', { name: 'Kiddush', exact: true }).isVisible();
+});
+ 
