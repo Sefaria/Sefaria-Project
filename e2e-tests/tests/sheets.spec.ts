4e45daedbdd92d63eedc3e28f48db8893b5d839d
stevekaplan123 <stevek004@gmail.com>
2023-07-28 14:12:09 +0300

    feat(Sheet Tests): create sheet from sidebar, add source in sheets in old/new editors

--- e2e-tests/tests/sheets.spec.ts
+++ e2e-tests/tests/sheets.spec.ts
@@ -18,9 +18,48 @@ const addSource = async ({ context }) => {
   url = await page.locator('a').filter({ hasText: 'Test' }).first().getAttribute('href');
 };
 
+// test('Add source to sheet', async ({ context }) => {
+//   await addSource({ context });
+// });
+
+
+test('Add comment to sheet', async ({ context }) => {
+  // fill in test
+});
+
+
+test('Create sheet from sidebar', async ({ context }) => {
+  const page = await goToPageWithUser(context, '/Genesis_1.2?lang=en');
+
+  await page.getByText('the earth').first().click();
+  await page.locator('a').filter({ hasText: 'Add to Sheet' }).click();
+
+  await page.locator('.addToSourceSheetBox > .dropdown').first().click();
+  await page.getByPlaceholder('Name New Sheet').click();
+  await page.getByPlaceholder('Name New Sheet').fill('Test 2');
+  await page.getByText('Createיצירה').click();
+  await page.getByText('Add to Sheet').click();
+  await page.getByText('Genesis 1:2 has been added to Test 2.').click();  
+});
+
 test('Add source to sheet', async ({ context }) => {
   await addSource({ context });
 });
+
+// test depends on previous test
+test('Add image to sheet', async ({ context }) => {
+  const page = await goToPageWithUser(context, '/Mishnat_Eretz_Yisrael_on_Pirkei_Avot,_1.1.40?lang=en');
+
+  await page.locator('#panel-1').getByText('Loading...').waitFor({ state: 'detached' });
+  await page.locator('.image-in-text > img').first().click();
+  await page.locator('a').filter({ hasText: 'Add to Sheet' }).click();
+  await page.getByText('Add to Sheetהוספה לדף המקורות').click();
+  await page.locator('#panel-1').getByText('Loading...').waitFor({ state: 'detached' });
+
+  page.on('dialog', dialog => dialog.accept());
+  expect(await page.getByText(' has been added to ').count()).toEqual(0);
+});
+
 
 // test depends on previous test
 test('Delete source from sheet', async ({ context }) => {
@@ -44,49 +83,24 @@ test('Delete source from sheet', async ({ context }) => {
   expect(sheetItems.length - remainingSheetItems).toBe(1);
 });
 
-
-test('Add comment to sheet', async ({ context }) => {
-  // fill in test
+test('Add source with new editor', async ({ context }) => {
+  const page = await goToPageWithUser(context,  '/enable_new_editor');
+  await page.getByRole('link', { name: 'Create a New Sheet' }).click();
+  await page.locator('.spacerSelected').click();
+  await page.locator('#panel-0').getByRole('textbox').fill('Genesis 1:2');
+  await page.locator('#panel-0').getByRole('textbox').press("Enter");
+  await page.locator('#panel-0').getByText('Loading...').waitFor({ state: 'detached' });
+  await page.locator('.SheetSource').click();
 });
 
-
-test('Create sheet from sidebar', async ({ context }) => {
-  const page = await goToPageWithUser(context, '/Mishnat_Eretz_Yisrael_on_Pirkei_Avot,_Introduction?lang=en');
-
-  await page.getByText('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor i').first().click();
-  await page.locator('a').filter({ hasText: 'Add to Sheet' }).click();
-
-  await page.locator('.addToSourceSheetBox > .dropdown').first().click();
-  await page.getByPlaceholder('Name New Sheet').click();
-  await page.getByPlaceholder('Name New Sheet').fill('New Sheet From Sidebar');
-  await page.getByText('Createיצירה').click();
+test('Add source with old editor', async ({ context }) => {
+  const page = await goToPageWithUser(context,  '/disable_new_editor');
+  await page.getByRole('link', { name: 'Source sheet icon Create a New Sheet' }).click();
+  await page.getByPlaceholder('Search for a Text or Commentator').click();
+  await page.getByPlaceholder('Search for a Text or Commentator').fill('Genesis 1:2');
+  await page.getByPlaceholder('Search for a Text or Commentator').press('Enter');
+  await page.getByRole('button', { name: 'Add Source', exact: true }).click();
+  await page.locator('#panel-0').getByText('Loading...').waitFor({ state: 'detached' });
+  await page.locator('.source.sheetItem').click();
 });
 
-// test depends on previous test
-test('Add image to sheet', async ({ context }) => {
-  const page = await goToPageWithUser(context, '/Mishnat_Eretz_Yisrael_on_Pirkei_Avot,_Introduction?lang=en');
-
-  await page.getByText('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor i').first().click();
-  await page.locator('a').filter({ hasText: 'Add to Sheet' }).click();
-  await page.locator('.addToSourceSheetBox > .dropdown').first().click();
-  await page.getByPlaceholder('New Sheet From Sidebar').click();
-
-  await page.getByText('Add to Sheetהוספה לדף המקורות').click();
-
-  await page.getByText('Mishnat Eretz Yisrael on Pirkei Avot, Introduction 1 has been added to New Sheet From Sidebar.').click();
-  
-  await page.locator('#panel-1').getByText('Loading...').waitFor({ state: 'detached' });
-
-  await page.locator('.image-in-text > img').first().click();
-  await page.locator('a').filter({ hasText: 'Add to Sheet' }).click();
-  await page.getByText('Add to Sheetהוספה לדף המקורות').click();
- 
-  // page.on('dialog', dialog => dialog.accept());
-  // await page.getByRole('button').click();
-  page.on('dialog', dialog => {
-    expect(dialog.type()).toContain('alert');   
-    dialog.accept()
-    console.log(`Dialog message: ${dialog.message()}`);
-    dialog.dismiss().catch(() => {});
-  });
-});
