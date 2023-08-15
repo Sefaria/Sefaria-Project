018b221bb64383263907d4fbf4cfc844afa9a6c4
Akiva Berger <akiva10b@gmail.com>
2023-08-15 14:26:56 +0300

    fix(collections): add rout to history on click

--- static/js/Misc.jsx
+++ static/js/Misc.jsx
@@ -1604,6 +1604,9 @@ const SheetListing = ({
       handleSheetClick(e, sheet, null, connectedRefs);
       e.preventDefault();
     }
+    if (e.currentTarget.target === "_self") {
+      history.pushState(null, null, sheet.sheetUrl);
+    }
   };
 
   const handleSheetOwnerClick = (e) => {
