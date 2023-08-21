76ff1a0cb35bda54e5496451ac9565dfa8f1c3e5
yishai <yishai@sefaria.org>
2023-08-21 11:34:25 +0300

    refactor(api errors): rename A|PIError to APIDataError.

--- api/api_errors.py
+++ api/api_errors.py
@@ -4,7 +4,7 @@ from sefaria.model import *
 from typing import List
 
 
-class APIError():
+class APIDataError():
 
     def __init__(self):
         pass
@@ -14,21 +14,21 @@ class APIError():
                 'message': self.message}
 
 
-class APINoVersion(APIError):
+class APINoVersion(APIDataError):
 
     def __init__(self, oref: Ref, vtitle: str, lang: str):
         self.error_code = 101
         self.message = f'We do not have version named {vtitle} with language code {lang} for {oref}'
 
 
-class APINoLanguageVersion(APIError):
+class APINoLanguageVersion(APIDataError):
 
     def __init__(self, oref: Ref, langs: List[str]):
         self.error_code = 102
         self.message = f'We do not have the code language you asked for {oref}. Available codes are {langs}'
 
 
-class APINoSourceText(APIError):
+class APINoSourceText(APIDataError):
 
     def __init__(self, oref: Ref):
         self.error_code = 103
