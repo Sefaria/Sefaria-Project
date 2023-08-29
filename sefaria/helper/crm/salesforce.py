86c2763f143329a06b5df7c524b666283cd91003
Nissa Mai-Rose <nwymai@gmail.com>
2023-08-29 13:14:26 -0400

    feat(salesforce): Add functions to get spam user report, get sustainer report and convert salesforce 15char id to 18char id [sc-20303]

--- sefaria/helper/crm/salesforce.py
+++ sefaria/helper/crm/salesforce.py
@@ -145,3 +145,43 @@ class SalesforceConnectionManager(CrmConnectionManager):
         except:
             return False
         return res
+
+    def get_spam_users(self):
+        endpoint = f"{sls.SALESFORCE_BASE_URL}/services/data/v{self.version}/analytics/reports/{sls.SALESFORCE_SPAM_REPORT}"
+        res = self.post(endpoint)
+        return res
+
+    def get_sustainers(self):
+        pass
+
+    def sf15to18(self, id):
+        # from https://github.com/mslabina/sf15to18/blob/master/sf15to18.py
+        if not id:
+            raise ValueError('No id given.')
+        if not isinstance(id, str):
+            raise TypeError('The given id isn\'t a string')
+        if len(id) == 18:
+            return id
+        if len(id) != 15:
+            raise ValueError('The given id isn\'t 15 characters long.')
+
+        # Generate three last digits of the id
+        for i in range(0, 3):
+            f = 0
+
+            # For every 5-digit block of the given id
+            for j in range(0, 5):
+                # Assign the j-th chracter of the i-th 5-digit block to c
+                c = id[i * 5 + j]
+
+                # Check if c is an uppercase letter
+                if c >= 'A' and c <= 'Z':
+                    # Set a 1 at the character's position in the reversed segment
+                    f += 1 << j
+
+            # Add the calculated character for the current block to the id
+            id += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ012345'[f]
+
+        return id
+
+
