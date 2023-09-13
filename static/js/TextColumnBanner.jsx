97e3aac79c5fc92da7eb20f16e177e6f2050c2a4
nsantacruz <noahssantacruz@gmail.com>
2023-09-13 15:05:21 +0300

    refactor: use `children` for TextColumnBanner

--- static/js/TextColumnBanner.jsx
+++ static/js/TextColumnBanner.jsx
@@ -34,14 +34,13 @@ export const TextColumnBannerChooser = ({ setTranslationLanguagePreference, open
 
 const TransLangPrefAcceptedBanner = () => {
     const lang = Sefaria.translateISOLanguageCode(translation_language_preference_suggestion);
-    const textElement = (
-        <InterfaceText>
-            <EnglishText> Thanks! We'll show you {lang} translations first when we have them. </EnglishText>
-            <HebrewText>תודה! כשנוכל, נציג לכם תרגומים בשפה ה<span className="bold">{Sefaria._(lang)}</span> כאשר אלו יהיו זמינים. </HebrewText>
-        </InterfaceText>
-    );
     return (
-        <TextColumnBanner textElement={textElement} />
+        <TextColumnBanner>
+            <InterfaceText>
+                <EnglishText> Thanks! We'll show you {lang} translations first when we have them. </EnglishText>
+                <HebrewText>תודה! כשנוכל, נציג לכם תרגומים בשפה ה<span className="bold">{Sefaria._(lang)}</span> כאשר אלו יהיו זמינים. </HebrewText>
+            </InterfaceText>
+        </TextColumnBanner>
     );
 }
 
@@ -56,38 +55,32 @@ const TransLangPrefBanner = ({ setAccepted, setTranslationLanguagePreference }) 
         setTranslationLanguagePreference(translation_language_preference_suggestion);
     }
     const lang = Sefaria.translateISOLanguageCode(translation_language_preference_suggestion);
-    const textElement = (
-        <InterfaceText>
-            <EnglishText> Prefer to see <span className="bold"> {lang} </span> translations when available? </EnglishText>
-            <HebrewText>האם תעדיפו לראות תרגומים בשפה ה<span className="bold">{Sefaria._(lang)}</span> כאשר הם זמינים?</HebrewText>
-        </InterfaceText>
-    );
     const buttons = [{text: "Yes", onClick: accept}, {text: "No", onClick: reject, sideEffect: "close" }];
 
     return (
-        <TextColumnBanner textElement={textElement} buttons={buttons} onClose={reject}/>
+        <TextColumnBanner buttons={buttons} onClose={reject}>
+            <InterfaceText>
+                <EnglishText> Prefer to see <span className="bold"> {lang} </span> translations when available? </EnglishText>
+                <HebrewText>האם תעדיפו לראות תרגומים בשפה ה<span className="bold">{Sefaria._(lang)}</span> כאשר הם זמינים?</HebrewText>
+            </InterfaceText>
+        </TextColumnBanner>
     );
 }
 
 
 const TransCallToActionBanner = ({ openTranslations }) => {
-    const textElement = (
-        <InterfaceText>
-            <EnglishText> Want to <span className="bold">change</span> the translation?</EnglishText>
-            <HebrewText> Want to <span className="bold">change</span> the translation?</HebrewText>
-        </InterfaceText>
-    );
-    const buttons = [
-        {
-            text: "Go to translations",
-            onClick: () => {
-                openTranslations();
-                // setConnectionsMode("Translations");
-            }
+    const buttons = [{
+        text: "Go to translations",
+        onClick: () => { openTranslations(); }
     }];
     const reject = () => {};
     return (
-        <TextColumnBanner textElement={textElement} buttons={buttons} onClose={reject} />
+        <TextColumnBanner buttons={buttons} onClose={reject}>
+            <InterfaceText>
+                <EnglishText> Want to <span className="bold">change</span> the translation?</EnglishText>
+                <HebrewText> Want to <span className="bold">change</span> the translation?</HebrewText>
+            </InterfaceText>
+        </TextColumnBanner>
     );
 };
 
@@ -95,13 +88,13 @@ const TransCallToActionBanner = ({ openTranslations }) => {
 
 /**
  * Banner which appears right above text column and informs a user of an action they can take
- * @param textElement: React element to display the call-to-action text.
+ * @param children: React element to display the call-to-action text.
  * @param buttons: List of objects. Each object should have keys "text" and "onClick". Can optionally have key "sideEffect" whose value can be "close" if the button should close the banner.
  * @param onClose: Optional callback that gets called when the banner is closed.
  * @returns {JSX.Element|null}
  * @constructor
  */
-const TextColumnBanner = ({ textElement, buttons, onClose }) => {
+const TextColumnBanner = ({ children, buttons, onClose }) => {
     const [closed, setClosed] = useState(false);
     const closeBanner = () => {
         setClosed(true);
@@ -112,7 +105,7 @@ const TextColumnBanner = ({ textElement, buttons, onClose }) => {
         <div className="readerControls transLangPrefSuggBann">
             <div className="readerControlsInner transLangPrefSuggBannInner sans-serif">
                 <div className="transLangPrefCentered">
-                    { textElement }
+                    { children }
                     <div className="yesNoGroup">
                         { buttons?.map(button => <TextColumnBannerButton key={button.text} button={button} setBannerClosed={setClosed}/>) }
                     </div>
