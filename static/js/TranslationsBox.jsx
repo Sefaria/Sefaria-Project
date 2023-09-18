62437b423399ef748133d8616358a9220fa9c0a5
nsantacruz <noahssantacruz@gmail.com>
2023-09-18 15:47:58 +0300

    refactor: refactor learn more link to match structure of learn more link on home page.

--- static/js/TranslationsBox.jsx
+++ static/js/TranslationsBox.jsx
@@ -119,9 +119,15 @@ const TranslationsHeader = () => (
     </h3>
     <div className="translationsDesc sans-serif">
       <InterfaceText>
-        <EnglishText>Sefaria acquires translations to enrich your learning experience. Preview or choose a different translation below. <a href="https://www.sefaria.org/sheets/511573" target="_blank">Learn more ›</a></EnglishText>
-        <HebrewText>ספריא עושה מאמצים להוסיף תרגומים שונים לספרים כדי להעשיר את חווית הלמידה שלכם. כאן ניתן להחליף לתרגום אחר או לראות תצוגה מקדימה שלו לצד הטקסט הנוכחי. <a href="https://www.sefaria.org/sheets/511573" target="_blank">למידע נוסף ›</a></HebrewText>
+        <EnglishText>Sefaria acquires translations to enrich your learning experience. Preview or choose a different translation below.</EnglishText>
+        <HebrewText>ספריא עושה מאמצים להוסיף תרגומים שונים לספרים כדי להעשיר את חווית הלמידה שלכם. כאן ניתן להחליף לתרגום אחר או לראות תצוגה מקדימה שלו לצד הטקסט הנוכחי.</HebrewText>
       </InterfaceText>
+      <a href="/sheets/511573" target="_blank" className="inTextLink">
+        <InterfaceText>
+          <EnglishText>Learn more ›</EnglishText>
+          <HebrewText>למידע נוסף ›</HebrewText>
+        </InterfaceText>
+      </a>
     </div>
   </div>
 );
