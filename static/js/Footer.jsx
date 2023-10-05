8917192ba123639d7ffc7c7df2995a5de156c393
lungsang-bod <lungsang@esukhia.org>
2023-10-05 19:18:07 +0530

    teach_with_dharma_text_update

--- static/js/Footer.jsx
+++ static/js/Footer.jsx
@@ -52,7 +52,7 @@ class Footer extends Component {
             </Section>
 
             <Section en="Tools" he="כלים">
-                <Link href="/educators" en="Teach with Sefaria" he="מלמדים עם ספריא" />
+                <Link href="/educators" en="Teach with Dharma" he="מלמדים עם ספריא" />
                 <Link href="/calendars" en="Learning Schedules" he="לוח לימוד יומי" />
                 <Link href="/sheets" en="Source Sheets" he="דפי מקורות" />
                 <Link href="/visualizations" en="Visualizations" he="תרשימים גרפיים" />
