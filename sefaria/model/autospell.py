dd2a7410072d9f662497663cefcac0312ec3a59a
YishaiGlasner <ishaigla@gmail.com>
2023-10-26 11:05:52 +0300

    fix(autospell): remove merkha and merkhaot from letter_scope, so they will be removed by unidecode.

--- sefaria/model/autospell.py
+++ sefaria/model/autospell.py
@@ -29,14 +29,13 @@ letter_scope = "\u05b0\u05b1\u05b2\u05b3\u05b4\u05b5\u05b6\u05b7\u05b8\u05b9\u05
             + "\u05c1\u05c2" \
             + "\u05d0\u05d1\u05d2\u05d3\u05d4\u05d5\u05d6\u05d7\u05d8\u05d9\u05da\u05db\u05dc\u05dd\u05de\u05df" \
             + "\u05e0\u05e1\u05e2\u05e3\u05e4\u05e5\u05e6\u05e7\u05e8\u05e9\u05ea" \
-            + "\u05f3\u05f4" \
             + "\u200e\u200f\u2013\u201c\u201d\ufeff" \
             + " Iabcdefghijklmnopqrstuvwxyz1234567890[]`:;.-,*$()'&?/\""
 
 
 def normalizer(lang):
     if lang == "he":
-        return lambda x: "".join([c if c in letter_scope else unidecode(c) for c in hebrew.normalize_final_letters_in_str(x)]).replace('"', '״')
+        return lambda x: "".join([c if c in letter_scope else unidecode(c) for c in hebrew.normalize_final_letters_in_str(x)])
     return lambda x: "".join([c if c in letter_scope else unidecode(c) for c in str.lower(x)])
 
 
