a567d4e89291792756b6ae51d15c68b6d51ccfcc
nsantacruz <noahssantacruz@gmail.com>
2023-10-26 10:20:34 +0300

    fix: remove special casing for normalizer.
    It seems that the start indices from the mapping are already off-by-one, so end doesn't need to be -1.

--- sefaria/helper/normalization.py
+++ sefaria/helper/normalization.py
@@ -126,9 +126,7 @@ class AbstractNormalizer:
         sign = -1 if reverse else 1
         for start, end in normalized_indices:
             unnorm_start_index = bisect_right(removal_keys, start) - 1
-            # special case if range is zero-length. treat end as literal and not off-by-one.
-            bisect_end_index = end if end == start else (end - 1)
-            unnorm_end_index = bisect_right(removal_keys, bisect_end_index) - 1
+            unnorm_end_index = bisect_right(removal_keys, end) - 1
 
             unnorm_start = start if unnorm_start_index < 0 else start + (sign * removal_map[removal_keys[unnorm_start_index]])
             unnorm_end = end if unnorm_end_index < 0 else end + (sign * removal_map[removal_keys[unnorm_end_index]])
