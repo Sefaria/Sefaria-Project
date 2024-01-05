81c4c0bd13e781aa7d182623bea3c8bf42e84d42
stevekaplan123 <stevek004@gmail.com>
2024-01-05 10:25:31 +0200

    chore: use fa-chevron-right for mobile about screen

--- static/js/MobileAbout.jsx
+++ static/js/MobileAbout.jsx
@@ -3,14 +3,14 @@ import React from "react";
 
 const MobileAbout = () => {
   return <div className="mobileAbout"><h1>About Sefaria</h1>
-    <a id="about" href="/about"><InterfaceText>What is Sefaria?</InterfaceText><i className="fa-chevron-right"/></a>
-    <a id="team" href="/team"><InterfaceText>Team</InterfaceText></a>
-    <a id="jobs" href="/jobs"><InterfaceText>Jobs at Sefaria</InterfaceText></a>
-    <a id="supporters" href="/supporters"><InterfaceText>Our Supporters</InterfaceText></a>
-    <a id="metrics" href="/metrics"><InterfaceText>Metrics</InterfaceText></a>
-    <a id="annualreport" href="/link-to-annual-report"><InterfaceText>Annual Report</InterfaceText></a>
-    <a id="terms" href="/terms"><InterfaceText>Terms of Use</InterfaceText></a>
-    <a id="privacy" href="/privacy-policy"><InterfaceText>Privacy Policy</InterfaceText></a>
+    <a id="about" href="/about"><InterfaceText>What is Sefaria?</InterfaceText><i className="fa fa-chevron-right"/></a>
+    <a id="team" href="/team"><InterfaceText>Team</InterfaceText><i className="fa fa-chevron-right"/></a>
+    <a id="jobs" href="/jobs"><InterfaceText>Jobs at Sefaria</InterfaceText><i className="fa fa-chevron-right"/></a>
+    <a id="supporters" href="/supporters"><InterfaceText>Our Supporters</InterfaceText><i className="fa fa-chevron-right"/></a>
+    <a id="metrics" href="/metrics"><InterfaceText>Metrics</InterfaceText><i className="fa fa-chevron-right"/></a>
+    <a id="annualreport" href="/link-to-annual-report"><InterfaceText>Annual Report</InterfaceText><i className="fa fa-chevron-right"/></a>
+    <a id="terms" href="/terms"><InterfaceText>Terms of Use</InterfaceText><i className="fa fa-chevron-right"/></a>
+    <a id="privacy" href="/privacy-policy"><InterfaceText>Privacy Policy</InterfaceText><i className="fa fa-chevron-right"/></a>
         </div>;
 }
 export {MobileAbout};
