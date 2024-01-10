a8aeaeeb006806b7786f48ab4aa9879a5535794e
nsantacruz <noahssantacruz@gmail.com>
2024-01-10 21:54:55 +0200

    fix(legacy ref): clean tref of underscores before replacing alt title so alt title will match exactly

--- sefaria/model/text.py
+++ sefaria/model/text.py
@@ -4873,9 +4873,9 @@ class Ref(object, metaclass=RefCacheType):
         except PartialRefInputError as e:
             matched_ref = Ref(e.matched_part)
             try:
+                tref = Ref.__clean_tref(tref, matched_ref._lang)
                 # replace input title with normalized title in case input was an alt title
                 tref = tref.replace(e.matched_part, matched_ref.normal())
-                tref = Ref.__clean_tref(tref, matched_ref._lang)
                 legacy_ref_parser = legacy_ref_parser_handler[matched_ref.index.title]
                 return legacy_ref_parser.parse(tref)
             except LegacyRefParserError:
