a226db7f462ebbd8a349569cfec28b5050fd40d3
lungsangg <lungsang@esukhia.org>
2024-01-19 12:30:59 +0530

    pecha_icon_&_description_loading_update

--- reader/views.py
+++ reader/views.py
@@ -1029,8 +1029,8 @@ def _get_user_calendar_params(request):
 
 
 def texts_list(request):
-    title = _("Sefaria: a Living Library of Jewish Texts Online")
-    desc  = _("The largest free library of Jewish texts available to read online in Hebrew and English including Torah, Tanakh, Talmud, Mishnah, Midrash, commentaries and more.")
+    title = _("Pecha - Buddhist texts in your own words")
+    desc  = _("The largest free library of Buddhist texts available to read online in Tibetan, English and Chinese")
     return menu_page(request, page="navigation", title=title, desc=desc)
 
 
