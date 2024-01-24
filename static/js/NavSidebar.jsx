e18d82756e647e6cb076b2c13b205c9cba2eef89
lungsangg <lungsang@esukhia.org>
2024-01-24 16:14:45 +0530

    support_sefaria_topic_page_disabled

--- static/js/NavSidebar.jsx
+++ static/js/NavSidebar.jsx
@@ -200,14 +200,14 @@ const TheJewishLibrary = ({hideTitle}) => (
 
 
 const SupportSefaria = ({blue}) => (
-  <Module blue={blue}>
+  <Module blue={blue}>{/*
     <ModuleTitle>Support Sefaria</ModuleTitle>
     <InterfaceText>Sefaria is an open source, nonprofit project. Support us by making a tax-deductible donation.</InterfaceText>
     <br />
     <DonateLink classes={"button small" + (blue ? " white" : "")} source={"NavSidebar-SupportSefaria"}>
       <img src="/static/img/heart.png" alt="donation icon" />
       <InterfaceText>Make a Donation</InterfaceText>
-    </DonateLink>
+    </DonateLink>*/}
   </Module>
 );
 
@@ -587,7 +587,7 @@ const JoinTheCommunity = ({wide}) => {
 
 
 const GetTheApp = () => (
-  <Module>
+  <Module>{/*
     <ModuleTitle>Get the Mobile App</ModuleTitle>
     <InterfaceText>Access the Jewish library anywhere and anytime with the</InterfaceText> <a href="/mobile" className="inTextLink"><InterfaceText>Sefaria mobile app.</InterfaceText></a>
     <br />
@@ -600,7 +600,7 @@ const GetTheApp = () => (
         href="https://play.google.com/store/apps/details?id=org.sefaria.sefaria"
         platform='android'
         altText={Sefaria._("Sefaria app on Android")}
-    />
+    />*/}
   </Module>
 );
 
