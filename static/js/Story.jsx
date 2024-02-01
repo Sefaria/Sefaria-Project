6e776122db2bfd4cb2edc5de58389d7b8556eec7
yonadavGit <yonadav.le@gmail.com>
2024-02-01 15:56:11 +0200

    chore(topic source editor): remove unused function

--- static/js/Story.jsx
+++ static/js/Story.jsx
@@ -127,16 +127,7 @@ StorySheetList.propTypes = {
     toggleSignUpModal: PropTypes.func
 };
 
-function capitalizeWords(str) {
-  const words = str.split(' ');
 
-  const capitalizedWords = words.map(word => {
-    return word.charAt(0).toUpperCase() + word.slice(1);
-  });
-  const result = capitalizedWords.join(' ');
-
-  return result;
-}
 const reviewStateToClassNameMap = {
     "reviewed": "reviewed",
     "not reviewed": "notReviewed",
