6ba6100f25580d82808db13ab74e846d2365aeae
nsantacruz <noahssantacruz@gmail.com>
2024-02-06 23:45:48 +0200

    feat(llm): single function to request and receive topic prompts from LLM server

--- sefaria/helper/llm/tasks.py
+++ sefaria/helper/llm/tasks.py
@@ -2,7 +2,7 @@
 Celery tasks for the LLM server
 """
 from typing import List
-from celery import shared_task
+from celery import shared_task, signature
 from sefaria.model.text import Ref
 from sefaria.model.topic import Topic
 from sefaria.helper.llm.topic_prompt import make_topic_prompt_input, save_topic_prompt_output
@@ -10,11 +10,14 @@ from sefaria.helper.llm.llm_interface import TopicPromptGenerationOutput
 
 
 @shared_task
-def generate_topic_prompts(lang: str, sefaria_topic: Topic, orefs: List[Ref], contexts: List[str]):
-    return make_topic_prompt_input(lang, sefaria_topic, orefs, contexts)
-
-
-@shared_task
 def save_topic_prompts(raw_output: TopicPromptGenerationOutput):
     output = TopicPromptGenerationOutput.create(raw_output)
     save_topic_prompt_output(output)
+
+
+def generate_and_save_topic_prompts(lang: str, sefaria_topic: Topic, orefs: List[Ref], contexts: List[str]):
+    topic_prompt_input = make_topic_prompt_input(lang, sefaria_topic, orefs, contexts)
+    # TODO rename task in LLM repo!!
+    generate_signature = signature('llm.generate_topic_prompts', args=(topic_prompt_input,))
+    chain = generate_signature | save_topic_prompts.s()
+    return chain()
