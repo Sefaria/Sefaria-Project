0710c17439198b5b53a330e7fbb2a8f078a3e3c0
nsantacruz <noahssantacruz@gmail.com>
2024-02-11 14:13:14 +0200

    chore(llm): add type hint for return value

--- sefaria/helper/llm/tasks.py
+++ sefaria/helper/llm/tasks.py
@@ -3,6 +3,7 @@ Celery tasks for the LLM server
 """
 from typing import List
 from celery import shared_task, signature
+from celery.result import AsyncResult
 from sefaria.model.text import Ref
 from sefaria.model.topic import Topic
 from sefaria.helper.llm.topic_prompt import make_topic_prompt_input, save_topic_prompt_output
@@ -15,9 +16,8 @@ def save_topic_prompts(raw_output: TopicPromptGenerationOutput):
     save_topic_prompt_output(output)
 
 
-def generate_and_save_topic_prompts(lang: str, sefaria_topic: Topic, orefs: List[Ref], contexts: List[str]):
+def generate_and_save_topic_prompts(lang: str, sefaria_topic: Topic, orefs: List[Ref], contexts: List[str]) -> AsyncResult:
     topic_prompt_input = make_topic_prompt_input(lang, sefaria_topic, orefs, contexts)
-    # TODO rename task in LLM repo!!
     generate_signature = signature('llm.generate_topic_prompts', args=(topic_prompt_input,))
     chain = generate_signature | save_topic_prompts.s()
     return chain()
