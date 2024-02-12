05f84988f81743ded8c5377543fbaf4c4a5898a1
nsantacruz <noahssantacruz@gmail.com>
2024-02-12 11:39:27 +0200

    fix(llm): use .set() to set the queue on the save_topic_prompts task

--- sefaria/helper/llm/tasks.py
+++ sefaria/helper/llm/tasks.py
@@ -9,7 +9,7 @@ from sefaria.helper.llm.topic_prompt import save_topic_prompt_output, make_topic
 from sefaria.helper.llm.llm_interface import TopicPromptGenerationOutput
 
 
-@shared_task
+@shared_task(name="web.save_topic_prompts")
 def save_topic_prompts(raw_output: TopicPromptGenerationOutput):
     output = TopicPromptGenerationOutput.create(raw_output)
     save_topic_prompt_output(output)
@@ -18,5 +18,6 @@ def save_topic_prompts(raw_output: TopicPromptGenerationOutput):
 def generate_and_save_topic_prompts(lang: str, sefaria_topic: Topic, orefs: List[Ref], contexts: List[str]):
     topic_prompt_input = make_topic_prompt_input(lang, sefaria_topic, orefs, contexts)
     generate_signature = signature('llm.generate_topic_prompts', args=(topic_prompt_input,), queue='llm')
-    chain = generate_signature | save_topic_prompts.s(queue='django')
+    save_signature = save_topic_prompts.s().set(queue='web')
+    chain = generate_signature | save_signature
     return chain()
