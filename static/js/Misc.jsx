653dbf4e5b1dd51a2a2bd670b4cab0eb003ce53f
nsantacruz <noahssantacruz@gmail.com>
2024-02-22 15:10:35 +0200

    fix(llm): add default value to actionButtons

--- static/js/Misc.jsx
+++ static/js/Misc.jsx
@@ -1112,7 +1112,7 @@ const AllAdminButtons = ({ buttonOptions, buttonIDs, adminClasses }) => {
     </span>
   );
 };
-const CategoryHeader =  ({children, type, data = [], toggleButtonIDs = ["subcategory", "edit"], actionButtons}) => {
+const CategoryHeader =  ({children, type, data = [], toggleButtonIDs = ["subcategory", "edit"], actionButtons = {}}) => {
   /*
   Provides an interface for using admin tools.
   `type` is 'sources', 'cats', 'books' or 'topics'
