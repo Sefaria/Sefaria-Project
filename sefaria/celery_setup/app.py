1faa580f2803c044c29f5bb1d6fbc5b7b8b85714
nsantacruz <noahssantacruz@gmail.com>
2024-03-06 12:16:27 +0200

    docs(llm): remove incorrect comment

--- sefaria/celery_setup/app.py
+++ sefaria/celery_setup/app.py
@@ -2,6 +2,4 @@ from celery import Celery
 
 app = Celery('sefaria')
 app.config_from_object('sefaria.celery_setup.config')
-
-# Load task modules from all registered Django apps.
 app.autodiscover_tasks(packages=['sefaria.helper.llm'])
