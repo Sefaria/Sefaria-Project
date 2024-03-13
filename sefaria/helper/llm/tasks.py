484177be384bbb2183546f0bc1d6e2d1c7e14033
nsantacruz <noahssantacruz@gmail.com>
2024-03-13 12:07:33 +0200

    fix: change queue key to 'tasks'

--- sefaria/helper/llm/tasks.py
+++ sefaria/helper/llm/tasks.py
@@ -20,7 +20,7 @@ def save_topic_prompts(raw_output: dict):
 
 def generate_and_save_topic_prompts(lang: str, sefaria_topic: Topic, orefs: List[Ref], contexts: List[str]):
     topic_prompt_input = make_topic_prompt_input(lang, sefaria_topic, orefs, contexts)
-    generate_signature = signature('llm.generate_topic_prompts', args=(asdict(topic_prompt_input),), queue=CELERY_QUEUES.get('llm', 'llm'))
-    save_signature = save_topic_prompts.s().set(queue=CELERY_QUEUES.get('web', 'web'))
+    generate_signature = signature('llm.generate_topic_prompts', args=(asdict(topic_prompt_input),), queue=CELERY_QUEUES['llm'])
+    save_signature = save_topic_prompts.s().set(queue=CELERY_QUEUES['tasks'])
     chain = generate_signature | save_signature
     return chain()
