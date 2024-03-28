fbf7b91c5e3f12fb637e1f4fa9e53310b8049783
Noah <noahssantacruz@gmail.com>
2024-03-28 11:28:15 +0200

    fix(llm): use redis port as sentinel port since only one port is required to be specified

--- sefaria/celery_setup/config.py
+++ sefaria/celery_setup/config.py
@@ -7,7 +7,7 @@ from sefaria.celery_setup.generate_config import generate_config, SentinelConfig
 def generate_config_from_env():
     return generate_config(
         RedisConfig(REDIS_URL, REDIS_PASSWORD, REDIS_PORT, CELERY_REDIS_BROKER_DB_NUM, CELERY_REDIS_RESULT_BACKEND_DB_NUM),
-        SentinelConfig(SENTINEL_HEADLESS_URL, SENTINEL_PASSWORD, SENTINEL_TRANSPORT_OPTS)
+        SentinelConfig(SENTINEL_HEADLESS_URL, SENTINEL_PASSWORD, REDIS_PORT, SENTINEL_TRANSPORT_OPTS)
     )
 
 
