2d96b23222ad5d68ad043fd281078134c29737ff
lungsangg <lungsang@esukhia.org>
2024-04-12 11:42:25 +0530

    new_service_account_linked_for_image_upload

--- sefaria/local_settings.py
+++ sefaria/local_settings.py
@@ -275,7 +275,7 @@ GOOGLE_APPLICATION_CREDENTIALS = {
   "auth_uri": "https://accounts.google.com/o/oauth2/auth",
   "token_uri": "https://oauth2.googleapis.com/token",
   "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/pecha-738%40pecha-418412.iam.gserviceaccount.com",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/pecha-15%40pecha-418412.iam.gserviceaccount.com",
   "universe_domain": "googleapis.com"
 }
 
