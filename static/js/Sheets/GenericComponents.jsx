b5a9b0c65350f061f29f043bb608b05b0fb864e6
stevekaplan123 <stevek004@gmail.com>
2024-06-04 15:50:37 +0300

    chore: added comments to button component

--- static/js/Sheets/GenericComponents.jsx
+++ static/js/Sheets/GenericComponents.jsx
@@ -1,6 +1,13 @@
 import {InterfaceText} from "../Misc";
 import classNames from "classnames";
 const Button = ({img, href, children, classes={}}) => {
+    /*
+        Generic button component that links to an href, and displays text and optionally an image
+     @param {component} img -- expected HTML img component
+     @param {string} href -- Where should the button link to?
+     @param {children} -- Text of the button
+     @param {obj} -- JS object of properties to be passed to classNames such as {small: 1}
+     */
   classes = {button: 1, ...classes};
   return <a className={classNames(classes)} href={href}>
           {img}
