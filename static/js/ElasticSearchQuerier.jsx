3550aec54f08f39edac827c0036e90aba1cde333
stevekaplan123 <stevek004@gmail.com>
2024-07-16 13:46:39 -0400

    chore: in componentWillReceiveProps for ElasticSearchQuerier reset pagesLoaded

--- static/js/ElasticSearchQuerier.jsx
+++ static/js/ElasticSearchQuerier.jsx
@@ -55,22 +55,17 @@ class ElasticSearchQuerier extends Component {
         this._abortRunningQuery();  // todo: make this work w/ promises
     }
     componentWillReceiveProps(newProps) {
-      if(this.props.query !== newProps.query) {
-        this.setState({
-          totals: new SearchTotal(),
-          hits: [],
-          moreToLoad: true,
-        });
-        this._executeAllQueries(newProps);
+      let state = {
+              hits: [],
+              pagesLoaded: 0,
+              moreToLoad: true
+          };
+      if (this.props.query !== newProps.query) {
+        this.setState(state, () => this._executeAllQueries(newProps));
       } else if (this._shouldUpdateQuery(this.props, newProps, this.props.type)) {
-              let state = {
-                  hits: [],
-                  pagesLoaded: 0,
-                  moreToLoad: true
-              };
-              this.setState(state, () => {
-                  this._executeQuery(newProps, this.props.type);
-              })
+          this.setState(state, () => {
+              this._executeQuery(newProps, this.props.type);
+          })
       }
     }
     async addRefTopic(topic) {
