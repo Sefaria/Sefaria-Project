afa44eee85dc09a4e5b4af6ede53b0a66ff337af
yonadavGit <yonadav.le@gmail.com>
2024-08-19 10:36:54 +0300

    chore(topic page): format comments

--- sefaria/sheetsUtils.js
+++ sefaria/sheetsUtils.js
@@ -1,7 +1,10 @@
 import Sefaria from "../static/js/sefaria/sefaria";
 
-//Given an array of ref-strings (could also be ranged refs), turn each ref to segment object and return an array of all segments
 export async function getSegmentObjs(refs) {
+    /*
+    Given an array of ref-strings (could also be ranged refs),
+    turn each ref to segment object and return an array of all segments
+    */
     const segments = [];
 
     for (const ref of refs) {
@@ -11,13 +14,17 @@ export async function getSegmentObjs(refs) {
     }
     return segments;
 }
-//Given a ref-string, get he and en normal ref-string
 export async function getNormalRef(ref) {
+    /*
+    Given a ref-string, get he and en normal ref-string
+    */
     const refObj = await Sefaria.getRef(ref);
     return {en: refObj.ref, he: refObj.heRef}
 }
-//Map each segment object to a formatted text string
 function placedSegmentMapper(lang, segmented, includeNumbers, s) {
+    /*
+    Map each segment object to a formatted text string
+    */
     if (!s[lang]) {return ""}
 
     let numStr = "";
@@ -32,16 +39,20 @@ function placedSegmentMapper(lang, segmented, includeNumbers, s) {
     str = str.replace(/(<br\/>)+/g, ' ')
     return str;
 }
-//Turn array of segment objects to one chunk of formatted text
 export const segmentsToSourceText = (segments, lan) => {
+    /*
+    Turn array of segment objects into one chunk of formatted text
+    */
     const segmented = shouldBeSegmented(segments[0].ref);
     const includeNumbers = shouldIncludeSegmentNums(segments[0].ref);
     return(segments.map(placedSegmentMapper.bind(this, lan, segmented, includeNumbers))
     .filter(Boolean)
     .join(""));
 }
-//Decide if segment of this ref should have segment numbers when turned into text chunk
 function shouldIncludeSegmentNums(ref){
+    /*
+    Decide if segment of this ref should have segment numbers when turned into text chunk
+    */
     const indexTitle = Sefaria.refIndexTitle(ref);
     const categories =  Sefaria.refCategories(ref);
     if (categories.includes("Talmud")) {return false}
@@ -49,8 +60,10 @@ function shouldIncludeSegmentNums(ref){
     if (categories === 1) {return false}
     return true;
 }
-//Decide if segment of this ref should be followed by new line when turned into text chunk
 function shouldBeSegmented(ref){
+    /*
+    Decide if segment of this ref should be followed by new line when turned into text chunk
+    */
     const categories =  Sefaria.refCategories(ref);
     return !(categories[0] in {"Tanakh": 1, "Talmud": 1});
 }
