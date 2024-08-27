b7d7b12475dcae8338c9dba08df605b59e135f52
nsantacruz <noahssantacruz@gmail.com>
2024-08-27 21:57:48 +0300

    feat(topics): add analytics data to topics page

--- static/js/TopicPage.jsx
+++ static/js/TopicPage.jsx
@@ -642,7 +642,7 @@ const TopicPage = ({
             }
         } else {
            sidebar = (
-               <div className="sideColumn">
+               <div className="sideColumn" data-anl-panel_type="topics" data-anl-panel_number={0}>
                     <TopicSideColumn
                         key={topic}
                         slug={topic}
@@ -684,7 +684,7 @@ const TopicPage = ({
 
     const currentLang = getCurrentLang()
 
-    return <div className={classStr} data-anl-topicSlug={topic}>
+    return <div className={classStr} data-anl-project="topics">
         <div className="content noOverflowX" ref={scrollableElement}>
             <div className="columnLayout">
                <div className="mainColumn storyFeedInner">
@@ -804,14 +804,12 @@ const TopicPageTab = ({
 
 
 const TopicLink = ({topic, topicTitle, onClick, isTransliteration, isCategory}) => (
-    <div data-anl-event={"click"} data-anl-topicLink={topic} data-anl-batch={JSON.stringify({yo: 1, sup: 2})}>
-        <Link className="relatedTopic" href={`/topics/${isCategory ? 'category/' : ''}${topic}`}
-              onClick={onClick.bind(null, topic, topicTitle)} key={topic}
-              title={topicTitle.en}
-        >
-            <InterfaceText text={{en:topicTitle.en, he:topicTitle.he}}/>
-        </Link>
-    </div>
+    <Link className="relatedTopic" href={`/topics/${isCategory ? 'category/' : ''}${topic}`}
+          onClick={onClick.bind(null, topic, topicTitle)} key={topic}
+          title={topicTitle.en}
+    >
+        <InterfaceText text={{en:topicTitle.en, he:topicTitle.he}}/>
+    </Link>
 );
 TopicLink.propTypes = {
   topic: PropTypes.string.isRequired,
