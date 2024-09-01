37f211d04842a6a16847b84ea987231ce8d9130c
stevekaplan123 <stevek004@gmail.com>
2024-09-01 13:31:02 +0300

    chore: num of followers for sheet sidebar

--- static/js/SheetSidebar.jsx
+++ static/js/SheetSidebar.jsx
@@ -1,18 +1,28 @@
 import {CollectionStatement, FollowButton, InterfaceText, ProfilePic} from "./Misc";
 import Sefaria from "./sefaria/sefaria";
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 const SheetSidebar = ({authorID, authorImage, authorStatement, authorUrl,
                       collectionImage, collectionSlug, collectionName}) => {
+    const [loading, setLoading] = useState(true);
+    const [followers, setFollowers] = useState(0);
+    useEffect(() => {
+        Sefaria.followAPI(authorUrl.replace("/profile/", ""), "followers").then(data => {
+            setFollowers(data.length);
+            setLoading(false);
+        })
+    });
     return <div className="sheetSidebar">
                 <ProfilePic
                     url={authorImage}
                     len={100}
                     name={authorStatement}
+                    outerStyle={{display: "block"}}
                 />
                 <a href={authorUrl} className="sheetAuthorName">
                     <InterfaceText>{authorStatement}</InterfaceText>
                 </a>
+                {!loading && <div className="sheetFollowers">{followers} followers</div>}
                 <FollowButton
                     large={true}
                     uid={authorID}
