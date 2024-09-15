8a2741b7ceec0c6e61575b1411c0784e14031b55
nsantacruz <noahssantacruz@gmail.com>
2024-09-15 13:24:43 +0300

    chore(topics): make categories into pipe separated list

--- static/js/TopicPage.jsx
+++ static/js/TopicPage.jsx
@@ -168,7 +168,7 @@ const _extractAnalyticsDataFromRef = ref => {
     return {
         item_id: ref,
         content_id: title,
-        content_type: Sefaria.index(title).categories,
+        content_type: Sefaria.index(title).categories.join('|'),
         version: "",
     };
 };
