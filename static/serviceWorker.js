64f4b82ace4896c1ef0cbbf6d76065f685cf92ce
lungsangg <lungsang@esukhia.org>
2024-10-09 11:55:21 +0530

    service_worker_update

--- static/serviceWorker.js
+++ static/serviceWorker.js
@@ -1,53 +1,15 @@
 
-const CACHE_NAME = 'pecha_cache';
-
-// Define the files to be cached
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/styles.css',
-  '/script.js',
-  '/logo.png'
-];
-
 // Install event: cache the files
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
+  console.log('Service worker installing...');
 });
 
 // Fetch event: respond with cached files if available
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        // If the file is in the cache, return it
-        if (response) {
-          return response;
-        }
-        // Otherwise, fetch it from the network
-        return fetch(event.request);
-      })
-  );
+  console.log('Fetch event for ', event.request.url);
 });
 
 // Activate event: clean up old caches
 self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
+  console.log('Service worker activating...');
 });
