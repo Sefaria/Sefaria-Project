5d6851a0e949003a59210850e12bb9e8953ecf8f
YishaiGlasner <ishaigla@gmail.com>
2024-10-15 13:10:08 +0300

    refactor(DropdownMenu): option for using without context from outside, but rather with state.

--- static/js/common/DropdownMenu.jsx
+++ static/js/common/DropdownMenu.jsx
@@ -1,9 +1,23 @@
-import React, {useContext, useRef} from 'react';
+import React, {createContext, useContext, useRef, useState} from 'react';
 import PropTypes from "prop-types";
 import {useOutsideClick} from "../Hooks";
 
-const DropdownMenu = ({buttonContent, menu, context}) => {
-  const {isMenuOpen, setIsMenuOpen} = useContext(context);
+const FallbackContext = createContext(null);
+
+const useIsMenuOpen = (context) => {
+    //React must have the same hooks any time, so we should have context and state any way.
+    //For that we also define FallbackContext. It's defined outside for not be created in any use but only once.
+    const contextValue = useContext(context || FallbackContext);
+    const [stateIsMenuOpen, stateSetIsMenuOpen] = useState(false);
+    return contextValue ? [contextValue.isMenuOpen, contextValue.setIsMenuOpen] : [stateIsMenuOpen, stateSetIsMenuOpen];
+}
+
+const DropdownMenu = ({buttonContent, menu, context=null}) => {
+  /**
+  context - if provided, would be used for menu opening (which also be available outside.
+            if not, will use state (and the opening won't be availabe in the parent component)
+  */
+  const [isMenuOpen, setIsMenuOpen] = useIsMenuOpen(context);
   const wrapperRef = useRef(null);
   useOutsideClick(wrapperRef, () => setIsMenuOpen(false), isMenuOpen);
   const onClick = () => setIsMenuOpen(!isMenuOpen)
