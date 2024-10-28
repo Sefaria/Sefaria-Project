1eea87b8bc6148b85c264dd099ebaa73b41c8f89
YishaiGlasner <ishaigla@gmail.com>
2024-10-28 10:41:47 +0200

    chore: remove spaces

--- sefaria/model/text.py
+++ sefaria/model/text.py
@@ -259,7 +259,7 @@ class Index(abst.AbstractMongoRecord, AbstractIndex):
         Returns the `schema` dictionary with each node annotated with section lengths info
         from version_state.
         """
-        vstate   = self.versionState()
+        vstate = self.versionState()
 
         def simplify_version_state(vstate_node):
             return aggregate_available_texts(vstate_node["_all"]["availableTexts"])
