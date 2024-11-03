f28532aa794f2da28db54648a11536d98fe92e44
yonadavGit <yonadav.le@gmail.com>
2024-11-03 13:46:01 +0200

    test(topic page): test simple topic page elements

--- e2e-tests/tests/topics.spec.ts
+++ e2e-tests/tests/topics.spec.ts
@@ -1,5 +1,6 @@
 import { test } from '@playwright/test';
 import {goToPageWithLang} from '../utils';
+import {LANGUAGES} from "../globals";
 
 
 test('Go to topic page', async ({ context }) => {
@@ -8,6 +9,32 @@ test('Go to topic page', async ({ context }) => {
   await page.getByRole('link', { name: 'Rosh Hashanah' }).first().isVisible();
 });
 
+test('Check source', async ({ context }) => {
+  const page = await goToPageWithLang(context, '/topics');
+  await page.getByRole('link', { name: 'Jewish Calendar' }).click();
+  await page.getByRole('link', { name: 'Shabbat' }).first().click();
+  await page.getByRole('link', { name: 'Notable Sources' }).first().isVisible();
+  await page.getByRole('link', { name: 'All Sources' }).first().isVisible();
+});
+
+test('Check sources he interface', async ({ context }) => {
+  const page = await goToPageWithLang(context, '/topics', LANGUAGES.HE);
+  await page.getByRole('link', { name: 'מועדי השנה' }).click();
+  await page.getByRole('link', { name: 'שבת' }).first().click();
+  await page.getByRole('link', { name: 'מקורות מרכזיים' }).first().isVisible();
+  await page.getByRole('link', { name: 'כל המקורות' }).first().isVisible();
+});
+
+test('Check author page', async ({ context }) => {
+  const page = await goToPageWithLang(context, '/topics/jonathan-sacks');
+  await page.getByRole('link', { name: 'Works on Sefaria' }).first().isVisible();
+});
+
+test('Check redirection for sourcesless topic', async ({ context }) => {
+  const page = await goToPageWithLang(context, '/topics/Monkey');
+  await page.waitForURL('https://example.com/target', { timeout: 5000 });
+  await page.getByRole('link', { name: 'Works on Sefaria' }).first().isVisible();
+});
 
 test('Filter topics', async ({ context }) => {
   const page = await goToPageWithLang(context, '/topics/all/a');
