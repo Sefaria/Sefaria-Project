bbfac1ea1e6093548b61bf0b4f2a35a5416c5ca1
YishaiGlasner <ishaigla@gmail.com>
2024-11-07 15:27:44 +0200

    fix(currVersions): check for versionTitle before adding it to edit url.

--- static/js/ConnectionsPanel.jsx
+++ static/js/ConnectionsPanel.jsx
@@ -1271,8 +1271,7 @@ const AdvancedToolsList = ({srefs, canEditText, currVersions, setConnectionsMode
       let refString = srefs[0];
       let currentPath = Sefaria.util.currentPath();
       let currentLangParam;
-      const langCode = masterPanelLanguage.slice(0, 2);
-      if (currVersions[langCode]) {
+      if (currVersions[langCode]?.versionTitle) {
         refString += "/" + encodeURIComponent(langCode) + "/" + encodeURIComponent(currVersions[langCode].versionTitle);
       }
       let path = "/edit/" + refString;
