967d1ebb844bece236f7c9ad6bad4b4f49ad2763
nsantacruz <noahssantacruz@gmail.com>
2024-11-20 14:38:46 +0200

    chore(topics): add logs

--- reader/management/commands/runserver.py
+++ reader/management/commands/runserver.py
@@ -1,10 +1,13 @@
 from django.contrib.staticfiles.management.commands.runserver import Command as RunserverCommand
 from reader.startup import init_library_cache
+import structlog
+logger = structlog.get_logger(__name__)
 
 
 class Command(RunserverCommand):
 
     def get_handler(self, *args, **options):
         handler = super(Command, self).get_handler(*args, **options)
+        logger.info("Starting reader application")
         init_library_cache()
         return handler
