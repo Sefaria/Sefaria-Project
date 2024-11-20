eef1df256edd07a63c1acec4f2c977330b653da1
nsantacruz <noahssantacruz@gmail.com>
2024-11-20 09:47:14 +0200

    chore(topics): add logs to understand why web pod is failing

--- reader/apps.py
+++ reader/apps.py
@@ -2,11 +2,16 @@ import os
 import sys
 from django.apps import AppConfig
 
+import structlog
+logger = structlog.get_logger(__name__)
+
 
 class ReaderAppConfig(AppConfig):
     name = 'reader'
 
     def ready(self):
         from .startup import init_library_cache
+        logger.info(f'Starting reader app: {os.environ.get("RUN_MAIN")} -- {", ".join(sys.argv)}')
+
         if not os.environ.get('RUN_MAIN', None) and len(sys.argv) > 1 and sys.argv[1] == 'runserver':
             init_library_cache()
