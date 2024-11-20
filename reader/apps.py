5b7eff66de1372883bea449df01c97e3170ce0e8
nsantacruz <noahssantacruz@gmail.com>
2024-11-20 12:55:33 +0200

    chore(topics): add log when starting reader

--- reader/apps.py
+++ reader/apps.py
@@ -1,9 +1,13 @@
 from django.apps import AppConfig
 
+import structlog
+logger = structlog.get_logger(__name__)
+
 
 class ReaderAppConfig(AppConfig):
     name = 'reader'
 
     def ready(self):
         from .startup import init_library_cache
+        logger.info('Starting reader')
         init_library_cache()
