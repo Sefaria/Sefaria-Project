6e0cdeeb54271b7cf6a31369c52f766f8dd5af77
nsantacruz <noahssantacruz@gmail.com>
2024-11-20 12:54:47 +0200

    fix(topics): remove if statements

--- reader/apps.py
+++ reader/apps.py
@@ -1,9 +1,4 @@
-import os
-import sys
 from django.apps import AppConfig
-
-import structlog
-logger = structlog.get_logger(__name__)
 
 
 class ReaderAppConfig(AppConfig):
@@ -11,7 +6,4 @@ class ReaderAppConfig(AppConfig):
 
     def ready(self):
         from .startup import init_library_cache
-        logger.info(f'Starting reader app: {os.environ.get("RUN_MAIN")} -- {", ".join(sys.argv)}')
-
-        if not os.environ.get('RUN_MAIN', None) and len(sys.argv) > 1 and sys.argv[1] == 'runserver':
-            init_library_cache()
+        init_library_cache()
