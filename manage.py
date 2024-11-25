0eba7d79743df92155b6f3c10ade29be326a762b
Akiva Berger <akiva10b@gmail.com>
2024-11-25 18:07:06 +0200

    fix(pach): DONT MERGEgit add .git add . skip migrations command

--- manage.py
+++ manage.py
@@ -6,6 +6,12 @@ if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sefaria.settings")
 
     from django.core.management import execute_from_command_line
+    
+    # skip migrate command
+    if "migrate" in sys.argv:
+        sys.argv.remove("migrate")
+        if "&&" in sys.argv:
+            sys.argv.remove("&&")
 
     execute_from_command_line(sys.argv)
 
