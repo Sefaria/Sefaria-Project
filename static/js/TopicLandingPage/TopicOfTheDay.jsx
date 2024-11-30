d7f8846f8bda22b9a29467eaf7f03b1d85a611dc
nsantacruz <noahssantacruz@gmail.com>
2024-11-30 21:01:57 +0200

    feat(topics): topic of the day frontend WIP

--- static/js/TopicLandingPage/TopicOfTheDay.jsx
+++ static/js/TopicLandingPage/TopicOfTheDay.jsx
@@ -1,11 +1,22 @@
 import React, {useState} from 'react';
 import Sefaria from "../sefaria/sefaria";
+import {ImageWithAltText, InterfaceText} from "../Misc";
 
 export const TopicOfTheDay = () => {
     let [topic, setTopic] = useState(null);
     Sefaria.getTopicOfTheDay().then(result => setTopic(result.topic));
-    return (<>
-        <div>Topic of the day</div>
-        <div>{topic?.slug}</div>
-    </>);
+    console.log("topic", topic)
+    if (!topic) { return null; }
+    return (
+        <div className="topicOfTheDay">
+            <h1><InterfaceText>Topic of the Day</InterfaceText></h1>
+            <div className="topicOfTheDayContent">
+                <ImageWithAltText photoLink={topic.image.image_uri} altText={topic.image.image_caption}/>
+                <div className="topicOfTheDayText">
+                    <h3><InterfaceText text={topic.primaryTitle} /></h3>
+                    <InterfaceText text={topic.description} />
+                </div>
+            </div>
+        </div>
+    );
 };
--- static/js/TopicLandingPage/TopicsLandingPage.jsx
+++ static/js/TopicLandingPage/TopicsLandingPage.jsx
@@ -5,7 +5,7 @@ import {TopicOfTheDay} from "./TopicOfTheDay";
 
 export const TopicsLandingPage = ({openTopic}) => {
     return (
-        <div className='topic-landing-page-wrapper'>
+        <div className='readerNavMenu topic-landing-page-wrapper'>
             <div>Hello, would you like a serving of topics salad?</div>
             <TopicLandingSearch openTopic={openTopic} numOfTopics={5000}/>
             <TopicOfTheDay />
