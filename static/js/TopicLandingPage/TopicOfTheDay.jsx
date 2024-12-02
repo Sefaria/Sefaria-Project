12f022310fdff59b0b9ad001e5e61b79bac4ad06
nsantacruz <noahssantacruz@gmail.com>
2024-12-02 14:01:42 +0200

    feat(topics): polish topic of the day (to be known henceforth as featured topic)

--- static/js/TopicLandingPage/TopicOfTheDay.jsx
+++ static/js/TopicLandingPage/TopicOfTheDay.jsx
@@ -1,20 +1,24 @@
 import React, {useState} from 'react';
 import Sefaria from "../sefaria/sefaria";
-import {ImageWithAltText, InterfaceText} from "../Misc";
+import {ImageWithAltText, InterfaceText, SimpleLinkedBlock} from "../Misc";
 
 export const TopicOfTheDay = () => {
     let [topic, setTopic] = useState(null);
     Sefaria.getTopicOfTheDay().then(result => setTopic(result.topic));
-    console.log("topic", topic)
     if (!topic) { return null; }
     return (
         <div className="topicOfTheDay">
-            <h1><InterfaceText>Topic of the Day</InterfaceText></h1>
+            <h1><InterfaceText>Featured Topic</InterfaceText></h1>
             <div className="topicOfTheDayContent">
                 <ImageWithAltText photoLink={topic.image.image_uri} altText={topic.image.image_caption}/>
                 <div className="topicOfTheDayText">
                     <h3><InterfaceText text={topic.primaryTitle} /></h3>
-                    <InterfaceText text={topic.description} />
+                    <div className="topicDescription systemText">
+                        <InterfaceText markdown={topic.description}/>
+                        <div className="topicOfTheDayGoToLink">
+                            <SimpleLinkedBlock url={`/topics/${topic.slug}`}  en="Go to topic >" he={Sefaria._("Go to topic >")} />
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
