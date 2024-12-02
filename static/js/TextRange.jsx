c4a0fffc55373bfafafae76b0af701ccd5918cd4
YishaiGlasner <ishaigla@gmail.com>
2024-12-02 11:16:38 +0200

    fix(TextRange): return a piece of code for normalizing ref. this solves a bug when clicking on a search result in side bar - it should go to the specific segment and not to the beginning of the section. this is not how this issue should be handle, but this is what happening on prod.

--- static/js/TextRange.jsx
+++ static/js/TextRange.jsx
@@ -114,6 +114,13 @@ class TextRange extends Component {
       return;
     }
 
+    if (this.props.basetext && this.props.sref !== data.ref) {console.log(0)
+      // Replace ReaderPanel contents ref with the normalized form of the ref, if they differ.
+      // Pass parameter to showBaseText to replaceHistory - normalization should't add a step to history
+      this.props.showBaseText(data.ref, true, this.props.currVersions, [], false);
+      return;
+    }
+
     if (this.props.basetext && data.isSpanning) {
       // Replace ReaderPanel contents with split refs if ref is spanning
       // Pass parameter to showBaseText to replaceHistory - normalization should't add a step to history
