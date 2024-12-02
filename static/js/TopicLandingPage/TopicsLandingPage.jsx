6a6d9b7fc52fff65dca503243a897b9b885d5f6c
yonadavGit <yonadav.le@gmail.com>
2024-12-02 13:48:21 +0200

    feat(Topic Landing Page): turn content container into flex box and center child elements

--- static/js/TopicLandingPage/TopicsLandingPage.jsx
+++ static/js/TopicLandingPage/TopicsLandingPage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {TopicLandingSearch} from "./TopicLandingSearch";
 import {NavSidebar} from "../NavSidebar";
 import Footer from "../Footer";
+import {TopicSalad} from "./TopicSalad";
 
 
 export const TopicsLandingPage = ({openTopic}) => {
@@ -12,8 +13,9 @@ export const TopicsLandingPage = ({openTopic}) => {
         <div className="readerNavMenu" key="0">
             <div className="content">
                 <div className="sidebarLayout">
-                    <div className="contentInner mainColumn">
+                    <div className="contentInner mainColumn topic-lading-page-content">
                         <TopicLandingSearch openTopic={openTopic} numOfTopics={5000}/>
+                        <TopicSalad/>
                     </div>
                     <NavSidebar sidebarModules={sidebarModules} />
                 </div>
