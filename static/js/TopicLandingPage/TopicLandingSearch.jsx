3cf7558f70558567ad2350028ff369d058cc0d7a
yonadavGit <yonadav.le@gmail.com>
2024-12-03 11:18:04 +0200

    feat(Topic Search): fix dropdown css

--- static/js/TopicLandingPage/TopicLandingSearch.jsx
+++ static/js/TopicLandingPage/TopicLandingSearch.jsx
@@ -109,7 +109,10 @@ export const TopicLandingSearch = ({openTopic, numOfTopics}) => {
                 renderItems={renderItems.bind(null, openTopic)}
                 containerClassString="topic-landing-search-container"
                 dropdownMenuClassString="topic-landing-search-dropdown"
-                renderInput={renderInput.bind(null, openTopic, numOfTopics)}/>
+                renderInput={renderInput.bind(null, openTopic, numOfTopics)}
+                //to debug styling:
+                //shouldDisplaySuggestions={()=>true}
+            />
         </div>
     );
 };
