734b8d4dde9e14e629eb1013d333656489156bf3
lobsam <sanduplobzang@gmail.com>
2024-12-05 16:33:42 +0530

    small change

--- static/js/ReaderPanel.jsx
+++ static/js/ReaderPanel.jsx
@@ -581,7 +581,7 @@ class ReaderPanel extends Component {
       return this.state.width > 500 ? this.state.settings.biLayout : "stacked";
     }
     // dont allow continuous mode in sidebar since it's currently not possible to control layout from sidebar
-    if (this.state.mode === "Connections") {return "segmented"}
+    if (this.state.mode === "Connections") {return "continuous"}
     const category = this.currentCategory();
     const option = (category && (category === "Tanakh" || category === "Talmud")) ? "layout" + category : "layoutDefault";
     return this.state.settings[option];
