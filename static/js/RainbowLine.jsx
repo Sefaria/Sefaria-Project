97935381f57f0af27b0b66aa437c4ac3666e84b5
yonadavGit <yonadav.le@gmail.com>
2024-12-05 14:23:50 +0200

    refactor(Rainbow of Truth): css based implementation

--- static/js/RainbowLine.jsx
+++ static/js/RainbowLine.jsx
@@ -1,28 +1,7 @@
-import React, {useState} from "react";
-import { useCombobox } from 'downshift';
-
-
-
+import React from "react";
 
 export const RainbowLine = ({rainbowClassname}) => {
-    const colors = [
-        "tankah-teal",
-        "commentary-blue",
-        "musar-purple",
-        "mishna-blue",
-        "talmud-gold",
-        "modrash-green",
-        "halakha-red",
-        "philosophy-purple",
-        "tanaitic-green",
-        "chasidut-green",
-    ];
-
     return (
-        <div className={`rainbow-line ${rainbowClassname}`}>
-            {colors.map((color, index) => (
-                <span key={index} className={`rainbow-segment ${color}`} />
-            ))}
-        </div>
+        <div className={`categoryColorLineRainbow ${rainbowClassname}`}/>
     );
 };
