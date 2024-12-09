c7f03e123ee9d9b47931f8ed7dd70cf8d98c0210
yonadavGit <92536571+yonadavGit@users.noreply.github.com>
2024-12-09 13:35:47 +0200

    feat(Topic Landing Temporal): children of TopicLandingParasha

--- static/js/TopicLandingPage/TopicLandingParsha.jsx
+++ static/js/TopicLandingPage/TopicLandingParsha.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import {InterfaceText} from "../Misc";
-import {Card} from "../common/Card";
 import {TopicLandingCalendar} from "./TopicLandingCalendar";
 import {useState, useEffect} from "react";
+import {ParashahLink} from "../NavSidebar";
+import {InterfaceText} from "../Misc";
+import Sefaria from "../sefaria/sefaria";
 
 
 export const TopicLandingParasha = ({ handleClick }) => {
@@ -14,12 +15,33 @@ export const TopicLandingParasha = ({ handleClick }) => {
 
     const parashahTitle = parashah.displayValue;
     const parashahDesc = parashah.description;
-    console.log(parashahTitle);
+    const parashahTopicLink = `topics/${parashah?.topic}`;
+    const parashahRefLink = `/${parashah?.url}`;
+    const learnMorePrompt = (
+      <a href={parashahTopicLink}>
+        {`Learn More on Parashat ${parashahTitle?.en}>`}
+      </a>
+    );
+    const readPortionButton = (
+        <a href={parashahRefLink} className="button small blue">
+            <InterfaceText text={{en: 'Read the Portion'}}/>
+        </a>
+    )
 
     return (
-            <TopicLandingCalendar
-                header={{en: "This Week’s Torah Portion"}}
-                title={parashahTitle}
-                description={parashahDesc}/>
+        <TopicLandingCalendar
+            header={{en: "This Week’s Torah Portion"}}
+            title={parashahTitle}
+            description={parashahDesc}
+            link={parashahTopicLink}
+        >
+            <div className="learn-more-prompt">
+                {learnMorePrompt}
+            </div>
+            <ParashahLink />
+            <div className={"read-portion-button"}>
+                {readPortionButton}
+            </div>
+        </TopicLandingCalendar>
     );
 };
