d9f25b30e1f24b943dd6f70a7ac0c0968e9f7e2b
yonadavGit <92536571+yonadavGit@users.noreply.github.com>
2024-12-17 11:48:07 +0200

    chore(Topic Landing Temporal): remove redundant variables

--- static/js/TopicLandingPage/TopicLandingParasha.jsx
+++ static/js/TopicLandingPage/TopicLandingParasha.jsx
@@ -13,39 +13,34 @@ export const TopicLandingParasha = () => {
         Sefaria.getUpcomingDay('parasha').then(setParashah);
     }, []);
 
-    const parashahTitle = parashah.displayValue;
-    const parashahDesc = parashah.description;
-    const parashahTopicLink = `topics/${parashah?.topic}`;
-    const parashahRefLink = `/${parashah?.url}`;
-    const learnMorePrompt = {en: `Learn More about ${parashahTitle?.en} ›`,
-        he:`${Sefaria._("Learn More about")} ${parashahTitle?.he} ›`}
-
 
     return (
         <div className="topic-landing-parasha">
-        <TopicLandingCalendar
-            header={<InterfaceText>This Week’s Torah Portion</InterfaceText>}
-            title={parashahTitle}
-            description={parashahDesc}
-            link={parashahTopicLink}
-        >
-            <div className="learn-more-prompt">
-                <a href={parashahTopicLink}>
-                    <InterfaceText text={learnMorePrompt}/>
-                </a>
-            </div>
-            <div className="parashah-link">
-            <ParashahLink />
-            </div>
-            <div className="read-portion-button">
-                <a href={parashahRefLink} className="button small blue">
-                    <InterfaceText>Read the Portion</InterfaceText>
-                </a>
-            </div>
-            <div className="browse-all-parashot-prompt">
-                <a href='/topics/category/torah-portions'><InterfaceText>Browse all Parshayot</InterfaceText></a>
-            </div>
-        </TopicLandingCalendar>
+            <TopicLandingCalendar
+                header={<InterfaceText>This Week’s Torah Portion</InterfaceText>}
+                title={parashah.displayValue}
+                description={parashah.description}
+                link={`topics/${parashah?.topic}`}
+            >
+                <div className="learn-more-prompt">
+                    <a href={`topics/${parashah?.topic}`}>
+                        <InterfaceText text={{en: `Learn More about ${parashah.displayValue?.en} ›`,
+                            he:`${Sefaria._("Learn More about")} ${parashah.displayValue?.he} ›`}}
+                        />
+                    </a>
+                </div>
+                <div className="parashah-link">
+                <ParashahLink />
+                </div>
+                <div className="read-portion-button">
+                    <a href={`/${parashah?.url}`} className="button small blue">
+                        <InterfaceText>Read the Portion</InterfaceText>
+                    </a>
+                </div>
+                <div className="browse-all-parashot-prompt">
+                    <a href='/topics/category/torah-portions'><InterfaceText>Browse all Parshayot</InterfaceText></a>
+                </div>
+            </TopicLandingCalendar>
         </div>
     );
 };
