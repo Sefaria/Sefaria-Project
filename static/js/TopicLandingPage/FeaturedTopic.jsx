74357c2e1418be0d907d759b3e9ed06613988b9b
nsantacruz <noahssantacruz@gmail.com>
2024-12-17 14:48:36 +0200

    fix(topics): use secondary image

--- static/js/TopicLandingPage/FeaturedTopic.jsx
+++ static/js/TopicLandingPage/FeaturedTopic.jsx
@@ -14,7 +14,7 @@ export const FeaturedTopic = () => {
         <div className="featuredTopic">
             <h1><InterfaceText>Featured Topic</InterfaceText></h1>
             <div className="featuredTopicContent">
-                <ImageWithAltText photoLink={topic.image.image_uri} altText={topic.image.image_caption}/>
+                <ImageWithAltText photoLink={topic.image.secondary_image_uri} altText={topic.image.image_caption}/>
                 <div className="featuredTopicText">
                     <h3><InterfaceText text={topic.primaryTitle} /></h3>
                     <div className="topicDescription systemText">
