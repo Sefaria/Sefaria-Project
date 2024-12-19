00e561cfa7ea25397f0c2304a9c839f0c104252e
nsantacruz <noahssantacruz@gmail.com>
2024-12-19 14:58:11 +0200

    chore(topic): add margin between topic landing sections

--- static/js/TopicLandingPage/TopicsLandingPage.jsx
+++ static/js/TopicLandingPage/TopicsLandingPage.jsx
@@ -3,11 +3,9 @@ import {TopicLandingSearch} from "./TopicLandingSearch";
 import {NavSidebar} from "../NavSidebar";
 import Footer from "../Footer";
 import {TopicSalad} from "./TopicSalad";
-import {RainbowLine} from "../RainbowLine";
-import {TopicLandingCalendar} from "./TopicLandingCalendar";
 import {TopicLandingParasha} from "./TopicLandingParasha";
-import Search from "../sefaria/search";
 import {TopicLandingSeasonal} from "./TopicLandingSeasonal";
+import {InterfaceText} from "../Misc";
 
 
 export const TopicsLandingPage = ({openTopic}) => {
@@ -19,12 +17,19 @@ export const TopicsLandingPage = ({openTopic}) => {
             <div className="content">
                 <div className="sidebarLayout">
                     <div className="contentInner mainColumn topic-landing-page-content">
-                        <TopicLandingSearch openTopic={openTopic} numOfTopics={5000}/>
-                        <TopicSalad/>
-                    <div className="topic-landing-temporal">
-                        <TopicLandingParasha/>
-                        <TopicLandingSeasonal/>
-                    </div>
+                        <h1 className="topic-landing-header">
+                            <InterfaceText>Let Your Curiosity Lead The Way</InterfaceText>
+                        </h1>
+                        <div className="topic-landing-section">
+                            <TopicLandingSearch openTopic={openTopic} numOfTopics={5000}/>
+                        </div>
+                        <div className="topic-landing-section">
+                            <TopicSalad/>
+                        </div>
+                        <div className="topic-landing-section topic-landing-temporal">
+                            <TopicLandingParasha/>
+                            <TopicLandingSeasonal/>
+                        </div>
                     </div>
                     <NavSidebar sidebarModules={sidebarModules} />
                 </div>
