1fe0859ffc40aa4ec89273d8c4090e5e6ba2b214
stevekaplan123 <stevek004@gmail.com>
2024-12-31 10:59:36 +0200

    chore: small accessibility fix for go to TextColumnBannerButton button

--- static/js/TextColumnBanner.jsx
+++ static/js/TextColumnBanner.jsx
@@ -145,7 +145,7 @@ const TextColumnBannerButton = ({ button, closeBanner }) => {
         if (button.sideEffect === "close") { closeBanner(true); }
     }
     return (
-        <a className="yesNoButton" onClick={onClick}>
+        <a href="#" role="button" tabIndex="0" className="yesNoButton" aria-label={button.text} onClick={onClick}>
             <InterfaceText>{button.text}</InterfaceText>
         </a>
     );
