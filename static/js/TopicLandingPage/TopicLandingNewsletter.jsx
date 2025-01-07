765a789c00ca8dd7a27a432ed5c1fe02d495383d
yonadavGit <92536571+yonadavGit@users.noreply.github.com>
2025-01-07 15:37:35 +0200

    chore(topic landing newsletter): format error message

--- static/js/TopicLandingPage/TopicLandingNewsletter.jsx
+++ static/js/TopicLandingPage/TopicLandingNewsletter.jsx
@@ -7,6 +7,7 @@ export const TopicLandingNewsletter = () => {
     const lastNameRef = useRef();
     const emailRef = useRef();
     const [subscribeMessage, setSubscribeMessage] = useState(null);
+    const [subscribeErrorMessage, setSubscribeErrorMessage] = useState(null);
 
     function handleSubscribeKeyUp(e) {
         if (e.keyCode === 13) {
@@ -32,43 +33,51 @@ export const TopicLandingNewsletter = () => {
         Sefaria.subscribeSefariaNewsletter(firstNameRef.current?.value, lastNameRef.current?.value, emailRef.current?.value, false, []).then(res => {
             setSubscribeMessage("Subscribed! Welcome to our list.");
         }).catch(error => {
-            setSubscribeMessage(error?.message || "Sorry, there was an error.");
+            setSubscribeErrorMessage("There has been an error. Please re-enter your information.");
+            setSubscribeMessage(null);
         });
     }
     return (
-        <div className="topic-landing-newsletter">
-            <h3 className="topic-landing-newsletter-text">
-                <InterfaceText>Stay curious. Sign up for our free Topic of the Week Newsletter.</InterfaceText>
-            </h3>
-            <div>
-                <div className="topic-landing-newsletter-input-row">
-                    <input
-                        type="text"
-                        placeholder={Sefaria._("First Name")}
-                        ref={firstNameRef}
-                        onKeyUp={handleSubscribeKeyUp}
-                    />
-                    <input
-                        type="text"
-                        placeholder={Sefaria._("Last Name")}
-                        ref={lastNameRef}
-                        onKeyUp={handleSubscribeKeyUp}
-                    />
+        <div className="topic-landing-newsletter-wrapper">
+            <div className="topic-landing-newsletter">
+                <h3 className="topic-landing-newsletter-text">
+                    <InterfaceText>Stay curious. Sign up for our free Topic of the Week Newsletter.</InterfaceText>
+                </h3>
+                <div>
+                    <div className="topic-landing-newsletter-input-row">
+                        <input
+                            type="text"
+                            placeholder={Sefaria._("First Name")}
+                            ref={firstNameRef}
+                            onKeyUp={handleSubscribeKeyUp}
+                        />
+                        <input
+                            type="text"
+                            placeholder={Sefaria._("Last Name")}
+                            ref={lastNameRef}
+                            onKeyUp={handleSubscribeKeyUp}
+                        />
+                    </div>
+                    <div className="topic-landing-newsletter-input-row">
+                        <input
+                            type="text"
+                            placeholder={Sefaria._("Email Address")}
+                            ref={emailRef}
+                            onKeyUp={handleSubscribeKeyUp}
+                        />
+                        <button type="submit" onKeyUp={handleSubscribeKeyUp} onClick={handleSubscribe}>Sign Up</button>
+                    </div>
+                    <div className="topic-landing-newsletter-input-row">
+                        {subscribeMessage ?
+                            <div className="subscribeMessage">{Sefaria._(subscribeMessage)}</div>
+                            : null}
+                    </div>
                 </div>
-                <div className="topic-landing-newsletter-input-row">
-                    <input
-                        type="text"
-                        placeholder={Sefaria._("Email Address")}
-                        ref={emailRef}
-                        onKeyUp={handleSubscribeKeyUp}
-                    />
-                    <button type="submit" onKeyUp={handleSubscribeKeyUp} onClick={handleSubscribe}>Sign Up</button>
-                </div>
-                <div className="topic-landing-newsletter-input-row">
-                    {subscribeMessage ?
-                        <div className="subscribeMessage">{Sefaria._(subscribeMessage)}</div>
-                        : null}
-                </div>
+            </div>
+            <div className="">
+                {subscribeErrorMessage ?
+                    <div className="subscribeErrorMessage">*{Sefaria._(subscribeErrorMessage)}</div>
+                    : null}
             </div>
         </div>
     );
