2160b8e034f662c1036fa2691b975798d12df847
YishaiGlasner <ishaigla@gmail.com>
2025-01-07 11:25:53 +0200

    fix(text properties menu accessibility): add role button, aria-labelle to font size buttons. set redundant alt of img tags to an empty strings.

--- static/js/FontSizeButton.jsx
+++ static/js/FontSizeButton.jsx
@@ -6,13 +6,23 @@ function FontSizeButtons() {
     const {setOption} = useContext(ReaderPanelContext);
     return (
         <div className="font-size-line">
-            <button onClick={()=>setOption('fontSize', 'smaller')} className="font-size-button preventClosing">
-                <img src="/static/icons/reduce_font.svg" alt="Reduce font size" />
-            </button>
+            <div
+                onClick={()=>setOption('fontSize', 'smaller')}
+                className="font-size-button preventClosing"
+                role="button"
+                aria-label="Decrease font size"
+            >
+                <img src="/static/icons/reduce_font.svg" alt=""/>
+            </div>
             <InterfaceText>Font Size</InterfaceText>
-            <button onClick={()=>setOption('fontSize', 'larger')} className="font-size-button preventClosing">
-                <img src="/static/icons/enlarge_font.svg" alt="Enlarge font size" />
-            </button>
+            <div
+                onClick={()=>setOption('fontSize', 'larger')}
+                className="font-size-button preventClosing"
+                role="button"
+                aria-label="Decrease font size"
+            >
+                <img src="/static/icons/enlarge_font.svg" alt=""/>
+            </div>
         </div>
     );
 }
