030fa8bff20e77f3b40810d55c0c9d585ef1aa0b
stevekaplan123 <stevek004@gmail.com>
2025-01-08 15:26:45 +0200

    chore: remove white spaces

--- static/js/sheets/SheetContentSegments.jsx
+++ static/js/sheets/SheetContentSegments.jsx
@@ -5,12 +5,10 @@ import React from "react";
 
 class SheetSource extends Component {
   render() {
-
     const sectionClasses= classNames(
       "SheetSource",
       this.props.source.options ? this.props.source.options.indented : null,
     );
-
     const containerClasses = classNames(
       "sheetItem",
       "segment",
@@ -19,7 +17,6 @@ class SheetSource extends Component {
       (this.props.source.text && this.props.source.text.he && this.props.source.text.he.stripHtml() === "...") || (this.props.source.text && (!this.props.source.text.he || !this.props.source.text.he.stripHtml())) ? "enOnly" : null,
       this.props.source.options && this.props.source.options.refDisplayPosition ? "ref-display-"+ this.props.source.options.refDisplayPosition : null
     );
-
     return (
       <section className={sectionClasses} style={{"borderInlineStart": `4px solid ${Sefaria.palette.refColor(this.props.source.ref)}`}}>
         <div className={containerClasses}
@@ -34,7 +31,6 @@ class SheetSource extends Component {
           <div className="customSourceTitle" role="heading" aria-level="3">
             <div className="titleBox">{this.props.source.title.stripHtml()}</div>
           </div> : null}
-
           {this.props.source.text && this.props.source.text.he && this.props.source.text.he !== "" ?
           <div className="he">
             {this.props.source.options && this.props.source.options.sourcePrefix && this.props.source.options.sourcePrefix != "" ? <sup className="sourcePrefix">{this.props.source.options.sourcePrefix}</sup> : null }
@@ -44,7 +40,6 @@ class SheetSource extends Component {
             </div>
             <div className="sourceContentText" dangerouslySetInnerHTML={ {__html: (Sefaria.util.cleanHTML(this.props.source.text.he))} }></div>
           </div> : null }
-
           {this.props.source.text && this.props.source.text.en && this.props.source.text.en !== "" ?
           <div className="en">
             {this.props.source.options && this.props.source.options.sourcePrefix && this.props.source.options.sourcePrefix != "" ? <sup className="sourcePrefix">{this.props.source.options.sourcePrefix}</sup> : null }
@@ -54,23 +49,18 @@ class SheetSource extends Component {
             </div>
             <div className="sourceContentText" dangerouslySetInnerHTML={ {__html: (Sefaria.util.cleanHTML(this.props.source.text.en))} }></div>
           </div> : null }
-
           <div className="clearFix"></div>
-
           {this.props.source.addedBy ?
           <div className="addedBy">
             <small><em>{Sefaria._("Added by")}: <span dangerouslySetInnerHTML={ {__html: Sefaria.util.cleanHTML(this.props.source.userLink)} }></span></em></small>
           </div>
           : null }
-
         </div>
         {this.props.addToSheetButton}
       </section>
     );
   }
 }
-
-
 class SheetComment extends Component {
   render() {
     const lang = Sefaria.hebrew.isHebrew(this.props.source.comment.stripHtml().replace(/\s+/g, ' ')) ? "he" : "en";
@@ -81,17 +71,14 @@ class SheetComment extends Component {
       this.props.highlight && "highlight",
       this.props.source.options ? this.props.source.options.indented : null
     );
-
     return (
       <section className="SheetComment">
         <div className={containerClasses} data-node={this.props.source.node} onClick={this.props.sheetSourceClick} aria-label={"Click to see " + this.props.linkCount +  " connections to this source"} tabIndex="0" onKeyPress={function(e) {e.charCode == 13 ? this.props.sheetSourceClick(e):null}.bind(this)} >
-
           <div className={lang}>
               <div
                 className="sourceContentText"
                 dangerouslySetInnerHTML={{__html: Sefaria.util.cleanHTML(this.props.source.comment)}}></div>
           </div>
-
           <div className="clearFix"></div>
           {this.props.source.addedBy ?
           <div className="addedBy">
@@ -100,14 +87,12 @@ class SheetComment extends Component {
             </em></small>
           </div>
           : null }
-
         </div>
         {this.props.addToSheetButton}
       </section>
     );
   }
 }
-
 class SheetHeader extends Component {
   render() {
     const lang = Sefaria.hebrew.isHebrew(this.props.source.outsideText.stripHtml().replace(/\s+/g, ' ')) ? "he" : "en";
@@ -117,7 +102,6 @@ class SheetHeader extends Component {
         this.props.highlight && "highlight",
         this.props.source.options ? this.props.source.options.indented : null
     );
-
     return (
         <div className={containerClasses} data-node={this.props.source.node} onClick={this.props.sheetSourceClick} aria-label={"Click to see " + this.props.linkCount +  " connections to this source"} tabIndex="0" onKeyPress={function(e) {e.charCode == 13 ? this.props.sheetSourceClick(e):null}.bind(this)} >
           <div className={lang}>
@@ -126,24 +110,18 @@ class SheetHeader extends Component {
           {this.props.addToSheetButton}
         </div>
     )
-
-
   }
-
 }
-
-
 class SheetOutsideText extends Component {
   shouldPassClick(e) {
     const target = e.target.closest('a')
     if (target) {
-      return
+      return;
     }
     else{
       this.props.sheetSourceClick(this.props.source)
     }
   }
-
   render() {
     const lang = Sefaria.hebrew.isHebrew(this.props.source.outsideText.stripHtml().replace(/\s+/g, ' ')) ? "he" : "en";
     const containerClasses = classNames("sheetItem",
@@ -152,31 +130,24 @@ class SheetOutsideText extends Component {
         this.props.highlight && "highlight",
         this.props.source.options ? this.props.source.options.indented : null
     );
-
     return (
       <section className="SheetOutsideText">
         <div className={containerClasses} data-node={this.props.source.node} onClick={(e) => this.shouldPassClick(e)} aria-label={"Click to see " + this.props.linkCount +  " connections to this source"} tabIndex="0" onKeyPress={function(e) {e.charCode == 13 ? this.props.sheetSourceClick(e):null}.bind(this)} >
-
           <div className={lang}>{this.props.source.options && this.props.source.options.sourcePrefix && this.props.source.options.sourcePrefix != "" ? <sup className="sourcePrefix">{this.props.source.options.sourcePrefix}</sup> : null }
               <div className="sourceContentText" dangerouslySetInnerHTML={ {__html: Sefaria.util.cleanHTML(this.props.source.outsideText)} }></div>
           </div>
-
           <div className="clearFix"></div>
-
           {this.props.source.addedBy ?
           <div className="addedBy">
             <small><em>{Sefaria._("Added by")}: <span dangerouslySetInnerHTML={ {__html: Sefaria.util.cleanHTML(this.props.source.userLink)} }></span></em></small>
           </div>
           : null }
-
         </div>
         {this.props.addToSheetButton}
       </section>
     );
   }
 }
-
-
 class SheetOutsideBiText extends Component {
   render() {
     const containerClasses = classNames(
@@ -186,15 +157,12 @@ class SheetOutsideBiText extends Component {
       (this.props.source.outsideBiText.he && this.props.source.outsideBiText.he.stripHtml() === "...") || (!this.props.source.outsideBiText.he.stripHtml()) ? "enOnly" : null,
       this.props.highlight && "highlight",
     );
-
     const sectionClasses= classNames("SheetOutsideBiText",
       this.props.source.options ? this.props.source.options.indented : null,
     );
-
     return (
       <section className={sectionClasses}>
         <div className={containerClasses} data-node={this.props.source.node} onClick={this.props.sheetSourceClick} aria-label={"Click to see " + this.props.linkCount +  " connections to this source"} tabIndex="0" onKeyPress={function(e) {e.charCode === 13 ? this.props.sheetSourceClick(e):null}.bind(this)} >
-
           <div className="he">
             {this.props.source.options && this.props.source.options.sourcePrefix && this.props.source.options.sourcePrefix !== "" ? <sup className="sourcePrefix">{this.props.source.options.sourcePrefix}</sup> : null }
             <div className="sourceContentText outsideBiText" dangerouslySetInnerHTML={ {__html: Sefaria.util.cleanHTML(this.props.source.outsideBiText.he)} }></div>
@@ -203,23 +171,18 @@ class SheetOutsideBiText extends Component {
             {this.props.source.options && this.props.source.options.sourcePrefix && this.props.source.options.sourcePrefix !== "" ? <sup className="sourcePrefix">{this.props.source.options.sourcePrefix}</sup> : null }
             <div className="sourceContentText outsideBiText" dangerouslySetInnerHTML={ {__html: Sefaria.util.cleanHTML(this.props.source.outsideBiText.en)} }></div>
           </div>
-
           <div className="clearFix"></div>
-
           {this.props.source.addedBy ?
           <div className="addedBy">
             <small><em>{Sefaria._("Added by")}: <span dangerouslySetInnerHTML={ {__html: Sefaria.util.cleanHTML(this.props.source.userLink)} }></span></em></small>
           </div>
           : null }
-
         </div>
         {this.props.addToSheetButton}
       </section>
     );
   }
 }
-
-
 class SheetMedia extends Component {
   makeMediaEmbedContent() {
     var mediaLink;
@@ -234,19 +197,15 @@ class SheetMedia extends Component {
     else if (mediaURL.match(/https?:\/\/www\.youtube\.com\/embed\/.+?rel=0(&amp;|&)showinfo=0$/i) != null) {
       mediaLink = '<div class="youTubeContainer"><iframe width="100%" height="100%" src=' + mediaURL + ' frameborder="0" allowfullscreen></iframe></div>';
     }
-
     else if (mediaURL.toLowerCase().match(/https?:\/\/player\.vimeo\.com\/.*/i) != null) {
       mediaLink = '<div class="youTubeContainer"><iframe width="100%" height="100%" src=' + mediaURL + ' frameborder="0"  allow="autoplay; fullscreen" allowfullscreen></iframe></div>';
     }
-
     else if (mediaURL.match(/https?:\/\/w\.soundcloud\.com\/player\/\?url=.*/i) != null) {
       mediaLink = '<iframe width="100%" height="166" scrolling="no" frameborder="no" src="' + mediaURL + '"></iframe>';
     }
-
     else if (mediaURL.match(/\.(mp3)$/i) != null) {
       mediaLink = '<audio src="' + mediaURL + '" type="audio/mpeg" controls>Your browser does not support the audio element.</audio>';
     }
-
     else {
       mediaLink = 'Error loading media...';
     }
@@ -258,7 +217,6 @@ class SheetMedia extends Component {
                 "<div class='he'>" + (caption.he || "") + "</div>" +
                  "</div></div>";
     }
-
     return "<div class='" + mediaClass + "'>" + mediaLink + mediaCaption + "</div>";
   }
   isImage() {
@@ -274,7 +232,6 @@ class SheetMedia extends Component {
     return (
       <section className="SheetMedia">
         <div className={containerClasses} data-node={this.props.source.node} onClick={this.props.sheetSourceClick} aria-label={"Click to  " + this.props.linkCount +  " connections to this source"} tabIndex="0" onKeyPress={function(e) {e.charCode == 13 ? this.props.sheetSourceClick(e):null}.bind(this)} >
-
           <div className="sourceContentText centeredSheetContent" dangerouslySetInnerHTML={ {__html: this.makeMediaEmbedContent()} }></div>
           <div className="clearFix"></div>
           {this.props.source.addedBy ?
@@ -286,7 +243,6 @@ class SheetMedia extends Component {
     );
   }
 }
-
 export {
    SheetMedia,
    SheetSource,
