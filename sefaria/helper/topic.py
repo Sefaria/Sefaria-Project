47824029c67398e5ab539c3eb57c181339c466f7
yonadavGit <92536571+yonadavGit@users.noreply.github.com>
2025-01-09 11:15:56 +0200

    chore(trending topics): restore cache decorator

--- sefaria/helper/topic.py
+++ sefaria/helper/topic.py
@@ -371,21 +371,14 @@ def get_topics_for_ref(tref, lang="english", annotate=False):
     serialized.sort(key=cmp_to_key(partial(sort_refs_by_relevance, lang=lang)))
     return serialized
 
-# @django_cache(timeout=24 * 60 * 60)
+@django_cache(timeout=24 * 60 * 60)
 def get_trending_topics(num_topics=10):
     from google.analytics.data_v1beta import BetaAnalyticsDataClient
-    from google.analytics.data_v1beta.types import (
-        DateRange,
-        Metric,
-        Dimension,
-        RunReportRequest,
-        FilterExpression,
-        Filter,
-        OrderBy,
-    )
+    from google.analytics.data_v1beta.types import DateRange, Metric, Dimension, RunReportRequest, FilterExpression, Filter, OrderBy
     from sefaria.settings import GOOGLE_APPLICATION_CREDENTIALS_FILEPATH
     import urllib.parse
-    PROPERTY_ID = 204095655
+    sefaria_analytics_mobile_and_web_property_id = 204095655
+    PROPERTY_ID = sefaria_analytics_mobile_and_web_property_id
     client = BetaAnalyticsDataClient.from_service_account_file(GOOGLE_APPLICATION_CREDENTIALS_FILEPATH)
     request = RunReportRequest(
         property=f"properties/{PROPERTY_ID}",
@@ -406,8 +399,8 @@ def get_trending_topics(num_topics=10):
     )
     response = client.run_report(request)
     slugs = [urllib.parse.unquote(row.dimension_values[0].value.removeprefix("/topics/")) for row in response.rows]
-    slugs = [slug for slug in slugs if not slug.startswith("category/")]
-    return slugs
+    filtered_slugs = [slug for slug in slugs if not slug.startswith("category/")]
+    return filtered_slugs
 
 @django_cache(timeout=24 * 60 * 60, cache_prefix="get_topics_for_book")
 def get_topics_for_book(title: str, annotate=False, n=18) -> list:
