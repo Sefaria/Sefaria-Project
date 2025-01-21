dc82c28e0937ec6cea487e4b913b5b91cdbab2c4
yonadavGit <92536571+yonadavGit@users.noreply.github.com>
2025-01-21 10:18:11 +0200

    fix(topic landing): fix docs

--- static/js/RowedWordSalad.jsx
+++ static/js/RowedWordSalad.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
 
 
-export const RowedWordSalad = ({ numLines, salad, renderItem }) => {
+export const RowedWordSalad = ({salad, renderItem }) => {
 /**
- * This component renders a collection of items, styled to fit within a specified
- * number of lines. Each item can be rendered using a custom `renderItem` function, and
- * spaces within the items are replaced with non-breaking spaces to ensure consistent
- * line breaking behavior in CSS.
+ * This component renders a collection of items, and styles them into one (scrollable) lines.
+ * Each item can be rendered using a custom `renderItem` function, and
  *
- * @param {number} props.numLines - The number of lines the container should display.
  * @param {Array} props.salad - An array of objects representing the items to display.
  * @param {Function} props.renderItem - A function that receives an item from the `salad` array
  *                                      and returns a React element to render it.
