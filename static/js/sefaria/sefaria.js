fe4ed33e48a220ed8940289356bd07fd79ea2e37
Ephraim <ephraim@sefaria.org>
2025-02-02 20:46:30 +0200

    chore(modularization): Wire up active module variable into React via base_props and unpackDataFromProps

--- static/js/sefaria/sefaria.js
+++ static/js/sefaria/sefaria.js
@@ -3479,6 +3479,7 @@ Sefaria.unpackBaseProps = function(props){
           return;
       }
       const dataPassedAsProps = [
+      "activeModule",
       "_uid",
       "_email",
       "_uses_new_editor",
