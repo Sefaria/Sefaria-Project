970bb399d978ff3ffd79423209b0b679ab095534
lobsam <sanduplobzang@gmail.com>
2025-02-04 10:54:50 +0530

    fixed complex commentary list

--- static/js/TextList.jsx
+++ static/js/TextList.jsx
@@ -95,6 +95,7 @@ class TextList extends Component {
     var commentator       = filter[0];
     var basetext          = this.getSectionRef();
     var commentarySection = Sefaria.commentarySectionRef(commentator, basetext);
+    console.log("commentary text,, ", commentator, basetext, commentarySection);
     if (!commentarySection) {
       this.setState({waitForText: false});
       return;
@@ -198,6 +199,7 @@ class TextList extends Component {
     var filter             = this.props.filter; // Remove filterSuffix for display
     var displayFilter      = filter.map(filter => filter.split("|")[0]);  // Remove filterSuffix for display
     var links              = this.getLinks();
+    console.log("links : ", links, this.state.waitForText,this.state.textLoaded)
 
     
     var enText = filter.length ? displayFilter.join(", "): ""
@@ -207,7 +209,7 @@ class TextList extends Component {
     var noResultsMessage = <LoadingMessage message={en} heMessage={he} />;
     var message = !this.state.linksLoaded ? (<LoadingMessage />) : (links.length === 0 ? noResultsMessage : null);
     var content = links.length === 0 ? message :
-                  this.state.waitForText && !this.state.textLoaded ?
+                  this.state.waitForText ?
                     (<LoadingMessage />) :
                     links.map(function(link, i) {
                         if (link.isSheet) {
