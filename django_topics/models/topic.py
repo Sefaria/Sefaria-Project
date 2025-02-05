0a380615010a10c0fb2495a407450fd0c854b9f1
Akiva Berger <akiva10b@gmail.com>
2025-02-05 17:50:28 +0200

    chore(topic): update pd df to work off of TopicsManager

--- django_topics/models/topic.py
+++ django_topics/models/topic.py
@@ -5,7 +5,7 @@ from django_topics.models.pool import TopicPool
 
 
 class TopicManager(models.Manager):
-    slug_pools_dataframe: pd.DataFrame = None
+    slug_pools_dataframe: pd.DataFrame = pd.DataFrame
 
     def sample_topic_slugs(self, order, pool: str = None, limit=10) -> list[str]:
         if pool:
@@ -17,12 +17,83 @@ class TopicManager(models.Manager):
         else:
             raise Exception("Invalid order: '{}'".format(order))
 
-    def get_pools_by_topic_slug(self, topic_slug: str) -> list:
-        return self.slug_pools_dataframe[self.slug_pools_dataframe['slug'] == topic_slug]['pool'].tolist()
+    def get_pools_by_topic_slug(self, slugs):
+        """
+        Given one or multiple slugs, return the set of pools they belong to.
+          - If `slugs` is a single string, return a list of pools for that slug.
+          - If `slugs` is a list, return a dict mapping each slug -> list of pools.
+        """
+        df = self.slug_pools_dataframe
+        if isinstance(slugs, str):
+            slugs = [slugs]
 
-    def get_topic_slugs_by_pool(self, pool: str) -> list:
-        return self.slug_pools_dataframe[self.slug_pools_dataframe['pool'] == pool]['slug'].tolist()
+        # Filter rows whose slug is in `slugs`
+        subset = df[df[self.SLUG_COLUMN].isin(slugs)]
 
+        if len(slugs) == 1:
+            # Return unique list of pools for the single slug
+            return subset[self.POOL_COLUMN].unique().tolist()
+
+        # If multiple slugs, build a dict: slug -> [pools...]
+        results = {}
+        for slug in slugs:
+            slug_df = subset[subset[self.SLUG_COLUMN] == slug]
+            results[slug] = slug_df[self.POOL_COLUMN].unique().tolist()
+        return results
+
+    def get_topic_slugs_by_pool(self, pools):
+        """
+        Given one or multiple pool names, return the set of slugs (topics) that appear in those pools.
+          - If `pools` is a single string, return a list of slugs in that pool.
+          - If `pools` is a list, return a dict mapping each pool -> list of slugs.
+        """
+        df = self.slug_pools_dataframe
+        if isinstance(pools, str):
+            pools = [pools]
+
+        # Filter rows whose pool is in `pools`
+        subset = df[df[self.POOL_COLUMN].isin(pools)]
+
+        if len(pools) == 1:
+            # Return unique list of slugs for the single pool
+            return subset[self.SLUG_COLUMN].unique().tolist()
+
+        # If multiple pools, build a dict: pool -> [slugs...]
+        results = {}
+        for pool in pools:
+            pool_df = subset[subset[self.POOL_COLUMN] == pool]
+            results[pool] = pool_df[self.SLUG_COLUMN].unique().tolist()
+        return results
+
+    def get_topics_slugs_by_pools_with_description(self, pools, require_en=True, require_he=False):
+        """
+        Similar to get_topics_by_pools, but also filters by description requirements.
+          - require_en=True means only topics that have an EN description.
+          - require_he=True means only topics that have a HE description.
+        """
+        df = self.slug_pools_dataframe
+        if isinstance(pools, str):
+            pools = [pools]
+
+        # First, filter by the desired pools
+        subset = df[df[self.POOL_COLUMN].isin(pools)]
+
+        # Next, filter by required description flags
+        if require_en:
+            subset = subset[subset[self.EN_DESCRIPTION] == True]
+        if require_he:
+            subset = subset[subset[self.HE_DESCRIPTION] == True]
+
+        if len(pools) == 1:
+            # Unique slugs for that single pool
+            return subset[self.SLUG_COLUMN].unique().tolist()
+
+        # If multiple pools, produce a dict: pool -> [slug...]
+        results = {}
+        for pool in pools:
+            pool_df = subset[subset[self.POOL_COLUMN] == pool]
+            results[pool] = pool_df[self.SLUG_COLUMN].unique().tolist()
+        return results
 
 class Topic(models.Model):
     slug = models.CharField(max_length=255, primary_key=True)
--- django_topics/models/topic_cache.py
+++ django_topics/models/topic_cache.py
@@ -9,17 +9,13 @@ class TopicPoolsCacheManager:
     EN_DESCRIPTION    = 'en_description'
     HE_DESCRIPTION    = 'he_description'
 
-    # A simple "long" DataFrame with one row per (slug, pool)
-    # plus booleans for descriptions
-    slug_pools_dataframe: pd.DataFrame = pd.DataFrame()
-
     def build_slug_to_pools_cache(self, rebuild=False):
         """
         Build or rebuild the internal DataFrame that holds one row per (slug, pool).
         We also attach boolean flags for English & Hebrew descriptions for each slug.
         """
         # If we already have data and don't need to rebuild, just skip
-        if not rebuild and not self.slug_pools_dataframe.empty:
+        if not rebuild and not TopicManager.slug_pools_dataframe.empty:
             return
 
         # 1) Get (slug, pool_name) from relational DB (Django)
@@ -57,83 +53,4 @@ class TopicPoolsCacheManager:
         df_sql[self.HE_DESCRIPTION] = df_sql[self.SLUG_COLUMN].apply(get_he_desc)
 
         # 4) Final DataFrame is now one row per (slug, pool), plus two description flags
-        self.slug_pools_dataframe = df_sql
-
-
-    def get_pools_by_topics(self, slugs):
-        """
-        Given one or multiple slugs, return the set of pools they belong to.
-          - If `slugs` is a single string, return a list of pools for that slug.
-          - If `slugs` is a list, return a dict mapping each slug -> list of pools.
-        """
-        df = self.slug_pools_dataframe
-        if isinstance(slugs, str):
-            slugs = [slugs]
-
-        # Filter rows whose slug is in `slugs`
-        subset = df[df[self.SLUG_COLUMN].isin(slugs)]
-
-        if len(slugs) == 1:
-            # Return unique list of pools for the single slug
-            return subset[self.POOL_COLUMN].unique().tolist()
-
-        # If multiple slugs, build a dict: slug -> [pools...]
-        results = {}
-        for slug in slugs:
-            slug_df = subset[subset[self.SLUG_COLUMN] == slug]
-            results[slug] = slug_df[self.POOL_COLUMN].unique().tolist()
-        return results
-
-    def get_topics_by_pools(self, pools):
-        """
-        Given one or multiple pool names, return the set of slugs (topics) that appear in those pools.
-          - If `pools` is a single string, return a list of slugs in that pool.
-          - If `pools` is a list, return a dict mapping each pool -> list of slugs.
-        """
-        df = self.slug_pools_dataframe
-        if isinstance(pools, str):
-            pools = [pools]
-
-        # Filter rows whose pool is in `pools`
-        subset = df[df[self.POOL_COLUMN].isin(pools)]
-
-        if len(pools) == 1:
-            # Return unique list of slugs for the single pool
-            return subset[self.SLUG_COLUMN].unique().tolist()
-
-        # If multiple pools, build a dict: pool -> [slugs...]
-        results = {}
-        for pool in pools:
-            pool_df = subset[subset[self.POOL_COLUMN] == pool]
-            results[pool] = pool_df[self.SLUG_COLUMN].unique().tolist()
-        return results
-
-    def get_topics_by_pools_with_description(self, pools, require_en=True, require_he=False):
-        """
-        Similar to get_topics_by_pools, but also filters by description requirements.
-          - require_en=True means only topics that have an EN description.
-          - require_he=True means only topics that have a HE description.
-        """
-        df = self.slug_pools_dataframe
-        if isinstance(pools, str):
-            pools = [pools]
-
-        # First, filter by the desired pools
-        subset = df[df[self.POOL_COLUMN].isin(pools)]
-
-        # Next, filter by required description flags
-        if require_en:
-            subset = subset[subset[self.EN_DESCRIPTION] == True]
-        if require_he:
-            subset = subset[subset[self.HE_DESCRIPTION] == True]
-
-        if len(pools) == 1:
-            # Unique slugs for that single pool
-            return subset[self.SLUG_COLUMN].unique().tolist()
-
-        # If multiple pools, produce a dict: pool -> [slug...]
-        results = {}
-        for pool in pools:
-            pool_df = subset[subset[self.POOL_COLUMN] == pool]
-            results[pool] = pool_df[self.SLUG_COLUMN].unique().tolist()
-        return results
+        TopicManager.slug_pools_dataframe = df_sql
