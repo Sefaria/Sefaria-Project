37f2fd5f84d61e0ca017cc43035dd3a2ed00a13b
Lev Israel <eliezer.israel@gmail.com>
2025-02-06 13:35:52 +0200

    fix: toward fixing CSS overflow

--- static/js/common/Card.jsx
+++ static/js/common/Card.jsx
@@ -2,11 +2,13 @@ import {InterfaceText} from "../Misc";
 import React from "react";
 const Card = ({cardTitle, cardTitleHref, oncardTitleClick, cardText, bottomLinkText, bottomLinkUrl}) => {
     return <div className="card">
-                <a href={cardTitleHref} className="cardTitle" onClick={oncardTitleClick}>
-                    <InterfaceText text={cardTitle}/>
-                </a>
-                <div className="cardDescription">
-                    <InterfaceText markdown={cardText}/>
+                <div className="cardTop">
+                    <a href={cardTitleHref} className="cardTitle" onClick={oncardTitleClick}>
+                        <InterfaceText text={cardTitle}/>
+                    </a>
+                    <div className="cardDescription">
+                        <InterfaceText markdown={cardText}/>
+                    </div>
                 </div>
                 {bottomLinkText &&
                     <div className="bottomCardLink">
