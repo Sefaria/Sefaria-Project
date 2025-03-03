1ed55005c819cd16c9798bcf27a48e13050ee050
Akiva Berger <akiva10b@gmail.com>
2025-03-03 17:47:19 +0200

    chore(coolify): add connection for pg

--- sefaria/local_settings_coolify.py
+++ sefaria/local_settings_coolify.py
@@ -5,8 +5,17 @@ ALLOWED_HOSTS = ['*']
 
 MONGO_HOST = os.getenv("MONGO_HOST", "localhost")
 MONGO_PORT = int(os.getenv("MONGO_PORT", 27017))
-# Name of the MongoDB database to use.
 SEFARIA_DB = os.getenv('MONGO_DB_NAME')
-# Leave user and password blank if not using Mongo Auth
 SEFARIA_DB_USER = os.getenv('MONGO_DB_USER', '')
 SEFARIA_DB_PASSWORD = os.getenv('MONGO_DB_PASSWORD', '')
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB', 'sefaria_auth'),
+        'USER': os.getenv('POSTGRES_USER', 'admin'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'admin'),
+        'HOST': os.getenv('POSTGRES_HOST', 'localhost'),
+        'PORT': os.getenv('POSTGRES_PORT', '5432'),
+    }
+}
