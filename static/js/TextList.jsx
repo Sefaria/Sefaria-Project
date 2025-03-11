2b5adf2bb2647ff1231bcdf03ef75f1b56abddbe
stevekaplan123 <steve@sefaria.org>
2025-03-11 16:04:50 +0200

    chore: cleanup

--- static/js/TextList.jsx
+++ static/js/TextList.jsx
@@ -75,13 +75,13 @@ class TextList extends Component {
     // Terms like "Rashi" or "Ibn Ezra" are currently in the Sefaria._index cache.
     // categories[0] === "Commentary" only for these terms, whereas a real index like "Rashi on Genesis" has categories[0] === "Tanakh".
     // We don't want to include "Rashi on Genesis" case because that's a "Quoting Commentary" case.
-    const isCommentary = filter.length === 1 &&
-                          Sefaria.index(filter[0]) &&
-                          (Sefaria.index(filter[0]).categories[0] === "Commentary");
-    if (isCommentary) {
-      const sourceRefs = [...new Set(this.state.links.map(link => Sefaria.humanRef(Sefaria.zoomOutRef(link.sourceRef))))];
+    const isOneText = filter.length === 1 && Sefaria.index(filter[0]);
+    const isCommentary = Sefaria.index(filter[0]).primary_category === "Commentary" || Sefaria.index(filter[0]).categories[0] === "Commentary";
+    if (isCommentary && isOneText) {
       this.setState({waitForText: true});
-      sourceRefs.map(ref => {
+      // Get the refs of the links and zoom out one level.  In most cases, the refs will be the same, so it's helpful to use a Set.
+      const refs = [...new Set(this.state.links.map(link => Sefaria.humanRef(Sefaria.zoomOutRef(link.sourceRef))))];
+      refs.map(ref => {
         Sefaria.getTextFromCurrVersions(ref, {}, this.props.translationLanguagePreference, false).then(data =>{
           this.setState({textLoaded: true, waitForText: false});
         });
