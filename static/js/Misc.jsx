0b879f020355be273e4bca9bf041c2d669c0a39c
stevekaplan123 <steve@sefaria.org>
2025-04-10 10:17:48 +0300

    fix: nechama sheets should display

--- static/js/Misc.jsx
+++ static/js/Misc.jsx
@@ -1601,7 +1601,20 @@ const SheetListing = ({
       </a>
     );
   });
-
+  const topics = sheet.topics.map((topic, i) => {
+    const separator = i !== sheet.topics.length -1 && <span className="separator">,</span>;
+    return (
+      <a href={`/topics/${topic.slug}`}
+        target={openInNewTab ? "_blank" : "_self"}
+        className="sheetTag"
+        key={i}
+        onClick={handleTopicClick.bind(null, topic.slug)}
+      >
+        <InterfaceText text={topic} />
+        {separator}
+      </a>
+    );
+  });
   const created = Sefaria.util.localeDate(sheet.created);
   const underInfo = infoUnderneath ? [
       sheet.status !== 'public' ? (<span className="unlisted"><img src="/static/img/eye-slash.svg"/><span>{Sefaria._("Not Published")}</span></span>) : undefined,
