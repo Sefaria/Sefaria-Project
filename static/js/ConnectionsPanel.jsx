cbd5390df03512874de458d27b9d0adfd914d16f
lungsangg <lungsang@esukhia.org>
2025-04-17 13:38:03 +0530

    twitter_share_update

--- static/js/ConnectionsPanel.jsx
+++ static/js/ConnectionsPanel.jsx
@@ -1462,7 +1462,9 @@ class ShareBox extends Component {
       Sefaria.util.openInNewTab("https://www.facebook.com/sharer/sharer.php?u=" + encodeURIComponent(url));
     };
     const shareTwitter = function () {
-      Sefaria.util.openInNewTab("https://twitter.com/share?url=" + encodeURIComponent(url));
+      const lang = Sefaria.interfaceLang;
+      const shareUrl = url + (url.includes('?') ? '&' : '?') + 'lang=' + lang;
+      Sefaria.util.openInNewTab("https://twitter.com/share?url=" + encodeURIComponent(shareUrl));
     };
     const shareEmail = function () {
       Sefaria.util.openInNewTab("mailto:?&subject=Text on Sefaria&body=" + url);
