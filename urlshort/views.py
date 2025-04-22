53a380e99105630007a8d6e160e343322f7ce8f5
lobsam <sanduplobzang@gmail.com>
2025-04-22 16:23:02 +0530

    encode

--- urlshort/views.py
+++ urlshort/views.py
@@ -28,7 +28,7 @@ def shorturl_api(request):
 
     try:
         # Parse JSON body
-        json_data = json.loads(request.body)
+        json_data = json.loads(request.body).encode('utf-8')
         original_url = json_data.get("original_url")
 
         if not original_url:
