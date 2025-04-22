88265d60e011c31b192db6070937cac862dc63b6
stevekaplan123 <steve@sefaria.org>
2025-04-22 15:23:14 +0300

    chore: images not required

--- sourcesheets/models.py
+++ sourcesheets/models.py
@@ -6,13 +6,15 @@ class InfoCard(Sortable):
     body_en = models.TextField(verbose_name="Body (EN)")
     image_en = models.URLField(
         verbose_name="Image URL (EN)",
-        help_text="Upload the image via Google Cloud and provide the URL here."
+        help_text="Upload the image via Google Cloud and provide the URL here.",
+        blank=True,
     )
     title_he = models.CharField(max_length=255, verbose_name="Title (HE)")
     body_he = models.TextField(verbose_name="Body (HE)")
     image_he = models.URLField(
         verbose_name="Image URL (HE)",
-        help_text="Upload the image via Google Cloud and provide the URL here."
+        help_text="Upload the image via Google Cloud and provide the URL here.",
+        blank=True,
     )
 
     def __str__(self):
