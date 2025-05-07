9cb85987f900fddecb4e22ed06e8df4a581b26b7
YishaiGlasner <yishai@sefaria.org>
2025-05-07 16:52:03 +0300

    feat(service-worker): rewrite to remove 'sheets' from domain and if needed add it to path.

--- static/js/service-worker.js
+++ static/js/service-worker.js
@@ -1,13 +1,44 @@
 self.addEventListener('fetch', (event) => {
-  const navigationEntries = performance.getEntriesByType('navigation');
-  const isReload = navigationEntries.length && navigationEntries[0].type === 'reload';
+  const {request} = event;
+  const documentRequest = request.mode === 'navigate' && request.destination === 'document';
   const url = new URL(event.request.url);
+  const sheetsDomain = url.hostname === 'sheets.localsefaria.org';
+  const sheetsPath = url.pathname.startsWith('/sheets');
 
-  if (!isReload && url.hostname === 'sheets.localsefaria.org') {  //this should be generalized for org.il, cauldrons, local host etc.
+  if (sheetsDomain) {
     url.hostname = url.hostname.replace(/^sheets\./, '');
-    const newRequest = new Request(url.toString(), event.request);
+    if (!sheetsPath && documentRequest) {
+      url.pathname = `/sheets${url.pathname}`
+    }
+    const newRequest = makeNewRequest(request, url)
     event.respondWith(fetch(newRequest));
   } else {
-    event.respondWith(fetch(event.request));
+    event.respondWith(fetch(request));
   }
 });
+
+const makeNewRequest = (request, url) => {
+  const { method, headers, body, redirect, credentials, cache, referrer, referrerPolicy, integrity, keepalive, mode, signal } = request;
+  const requestInit = {
+    method,
+    headers,
+    body: method !== 'GET' && method !== 'HEAD' ? body : undefined,
+    redirect,
+    credentials,
+    cache,
+    referrer,
+    referrerPolicy,
+    integrity,
+    keepalive,
+    signal,
+    mode: mode !== 'navigate' ? mode : undefined,
+  };
+  // If the body is a ReadableStream, set duplex
+  const isStreamBody =
+    body instanceof ReadableStream ||
+    Object.prototype.toString.call(body) === '[object ReadableStream]';
+  if (isStreamBody) {
+    requestInit.duplex = 'half';
+  }
+  return new Request(url.toString(), requestInit);
+}
