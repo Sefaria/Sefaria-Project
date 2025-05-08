4cc7e5dbfdfc4ebb1d8d6cd56f97bc37d8b9e3f3
YishaiGlasner <yishai@sefaria.org>
2025-05-08 21:28:57 +0300

    feat(base): generalize logics of moving from /sheets to sheets.site not only to local.

--- static/js/service-worker.js
+++ static/js/service-worker.js
@@ -10,29 +10,30 @@ self.addEventListener('fetch', (event) => {
   const {request} = event;
   const documentRequest = request.mode === 'navigate' && request.destination === 'document';
   const url = new URL(event.request.url);
-  const sheetsDomain = url.hostname === 'sheets.localsefaria.org';
-  const sheetsPath = url.pathname.startsWith('/sheets');
-
-  if (sheetsDomain) {
-    url.hostname = url.hostname.replace(/^sheets\./, '');
-    if (!sheetsPath && documentRequest) {
-      url.pathname = `/sheets${url.pathname}`;
+  const {hostname, pathname} = url;
+  const isSheetsDomain = hostname === 'sheets.localsefaria.org';
+  const isSheetsPath = /^\/sheets($|\/)/.test(pathname); //bot not '/sheets-something'
+  if (isSheetsDomain) {
+    url.hostname = hostname.replace(/^sheets\./, '');
+    if (!isSheetsPath && documentRequest) {
+      url.pathname = `/sheets${pathname}`;
     }
     const newRequest = makeNewRequest(request, url);
-    event.respondWith(fetch(newRequest));
+    event.respondWith(fetch(newRequest).catch(error => {
+    }));
   } else {
     event.respondWith(fetch(request));
   }
 });
 
 const makeNewRequest = (request, url) => {
-  const { method, headers, body, redirect, credentials, cache, referrer, referrerPolicy, integrity, keepalive, mode, signal } = request;
+  const { method, body, redirect, credentials, cache, referrer, referrerPolicy, integrity, keepalive, mode, signal } = request;
   const requestInit = {
     method,
-    headers,
+    headers: new Headers(request.headers),
     body: method !== 'GET' && method !== 'HEAD' ? body : undefined,
     redirect,
-    credentials,
+    credentials: 'include',
     cache,
     referrer,
     referrerPolicy,
