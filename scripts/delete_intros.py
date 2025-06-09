04e236b50c21889d4256e41dcfe1eb6d1254fd40
stevekaplan123 <steve@sefaria.org>
2025-06-09 09:25:18 +0300

    chore: fix spelling

--- scripts/delete_intros.py
+++ scripts/delete_intros.py
@@ -3,7 +3,7 @@ django.setup()
 from sefaria.model import *
 from sefaria.helper.schema import *
 
-books = {"Sefer HaIkkarim": ["Editor's Introduction"], "Midrash Tanchuma": ["Foreword", "Introduction"], "Or Neerav": ["Introduction"], "Sefer HaYashar": ["Translator's Foreword", "Addendum I", "Addendum II"], "Ibn Ezra on Isaiah": ["Translator's Foreword"]}
+books = {"Sefer HaIkkarim": ["Editor's Introduction"], "Midrash Tanchuma": ["Foreword", "Introduction"], "Or Neerav": ["Introduction"], "Sefer HaYashar": ["Translator's Foreword", "Addendum I", "Addendum II"], "Ibn Ezra on Isaiah": ["Translators Foreword"]}
 for book in books:
 	for node in library.get_index(book).nodes.children:
 		if node.get_primary_title('en') in books[book]:
