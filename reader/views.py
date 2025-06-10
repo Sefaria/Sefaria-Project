aeb079be21a2ceae26f4f675dc13d3f9f365da91
stevekaplan123 <steve@sefaria.org>
2025-06-10 16:38:06 +0300

    chore: get_pools_by_topic_slug

--- reader/views.py
+++ reader/views.py
@@ -3108,7 +3108,7 @@ def topic_page(request, slug, test_version=None):
     """
     slug = SluggedAbstractMongoRecord.normalize_slug(slug)
     topic_obj = Topic.init(slug)
-    if topic_obj is None or request.active_module not in DjangoTopic.objects.slug_to_pools.get(slug, []):
+    if topic_obj is None or request.active_module not in DjangoTopic.objects.get_pools_by_topic_slug(slug):
         raise Http404
 
     short_lang = get_short_lang(request.interfaceLang)
