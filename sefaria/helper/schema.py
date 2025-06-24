be90f07335f0e0ed0bd9af177a3baa582cd4be3c
YishaiGlasner <yishai@sefaria.org>
2025-06-24 09:40:02 +0300

    refactor(change_lexicon_headword): replace assert by if-raise.

--- sefaria/helper/schema.py
+++ sefaria/helper/schema.py
@@ -1038,8 +1038,8 @@ def change_lexicon_headword(parent_lexicon, old_headword, new_headword):
             dictionary_node.headwordMap = hw_map
         return dictionary_node
 
-    assert LexiconEntry().load({'parent_lexicon': parent_lexicon, 'headword': new_headword}) is None, \
-        f'Entry of {parent_lexicon} with headword {new_headword} already exists'
+    if LexiconEntry().load({'parent_lexicon': parent_lexicon, 'headword': new_headword}):
+        raise ValueError(f'Entry of {parent_lexicon} with headword {new_headword} already exists')
 
     # change entry itself
     print('Updating entry')
