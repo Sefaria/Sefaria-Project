79429bf14b3dd0ed5ba7365e29f4d93198f03d44
schreibersefaria <danielschreiber@sefaria.org>
2025-07-09 12:41:31 +0300

    style(playwrightconfig): Cleaned out old comment about dotenv

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,10 +1,5 @@
 import { defineConfig, devices } from '@playwright/test';
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// require('dotenv').config();
 
 /**
  * See https://playwright.dev/docs/test-configuration.
