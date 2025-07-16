b914f9c0bc98da51fed54b0c8b93426cae6887ca
nsantacruz <noahssantacruz@gmail.com>
2025-07-16 15:13:48 +0300

    fix(linker): disable linker initialization in startup

--- reader/startup.py
+++ reader/startup.py
@@ -40,7 +40,7 @@ def init_library_cache():
 
     if settings.ENABLE_LINKER:
         logger.info("Initializing Linker")
-        library.build_linker('he')
+        # library.build_linker('he')
 
     if server_coordinator:
         server_coordinator.connect()
