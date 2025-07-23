31b68a1440de0c91dd8846277728b6dafc028ae9
yonadavGit <92536571+yonadavGit@users.noreply.github.com>
2025-07-23 15:20:33 +0300

    feat(linker): add link_paragraph task to process paragraph linking

--- sefaria/helper/linker/tasks.py
+++ sefaria/helper/linker/tasks.py
@@ -4,12 +4,25 @@ Celery tasks for the LLM server
 from typing import List
 from dataclasses import asdict
 from celery import signature
+
+from sefaria.model import library
 from sefaria.settings import CELERY_QUEUES
 from sefaria.celery_setup.app import app
 
-from sefaria_llm_interface.topic_prompt import TopicPromptGenerationOutput
+
 
 
 @app.task(name="linker.hello_world")
 def hello_world(words: str):
     print(words)
+
+
+
+@app.task(name="linker.link_paragraph")
+def link_paragraph(raw_output: dict):
+    linker = library.get_linker('en')
+    output = linker.link_by_paragraph(raw_output['text'], raw_output['ref'])
+    print(output)
+    return str(output)
+
+
