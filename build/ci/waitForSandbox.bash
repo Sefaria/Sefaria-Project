4ccfd99a7a5b905e9a1d7a8dce4b727b1864cb4e
Yitzhak Clark <yitzhak@sefaria.org>
2025-09-02 13:47:25 +0300

    fix: support redirects for subdomains so healthcheck happens faster

--- build/ci/waitForSandbox.bash
+++ build/ci/waitForSandbox.bash
@@ -6,6 +6,6 @@ waitDuration=${WAIT_DURATION:-900}
 
 echo "Waiting for https://sandbox-${GIT_COMMIT}.cauldron.sefaria.org/health-check to load for $waitDuration seconds"
 
-timeout $waitDuration bash -c 'while [[ "$(curl -s -k -o /dev/null -w ''%{http_code}'' https://sandbox-${GIT_COMMIT}.cauldron.sefaria.org/health-check)" != "200" ]]; do sleep 5; done'
+timeout $waitDuration bash -c 'while [[ "$(curl -s -k -o /dev/null -w ''%{http_code}'' -L https://sandbox-${GIT_COMMIT}.cauldron.sefaria.org/health-check)" != "200" ]]; do sleep 5; done'
 
 echo "Reached server. Proceeding."
