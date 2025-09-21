b52a7de56ed9f205a81f3e40332652b188d9dfb7
Akiva Berger <akiva10b@gmail.com>
2025-09-21 13:33:49 +0300

    fix: bring back primeLocalCache

--- build/startup/primeAll.bash
+++ build/startup/primeAll.bash
@@ -7,7 +7,7 @@ export TARGET_HOSTNAME=${TARGET_HOSTNAME:-localhost} # export makes the variable
 timeout ${TIMEOUT:-900} bash -c 'while [[ "$(curl -s -k -o /dev/null -w ''%{http_code}'' http://$TARGET_HOSTNAME/healthz-rollout)" != "200" ]]; do sleep 10; done'
 
 
-# ./primeLocalCache.bash
+./primeLocalCache.bash
 
 
 exit 0
