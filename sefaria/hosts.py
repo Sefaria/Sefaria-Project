92aee6eace3a1ad0f2a8491dfee15805d7ae83a7
stevekaplan123 <steve@sefaria.org>
2025-10-16 09:21:17 +0300

    chore: only add he domains if there

--- sefaria/hosts.py
+++ sefaria/hosts.py
@@ -7,8 +7,16 @@ import re
 def get_domain_reg(url):
     return re.escape(urlparse(url).netloc)
 
-library_domain = f'{get_domain_reg(settings.DOMAIN_MODULES["en"][LIBRARY_MODULE])}|{get_domain_reg(settings.DOMAIN_MODULES["he"][LIBRARY_MODULE])}'
-sheets_domain = f'{get_domain_reg(settings.DOMAIN_MODULES["en"][VOICES_MODULE])}|{get_domain_reg(settings.DOMAIN_MODULES["he"][VOICES_MODULE])}'
+# Build domain patterns, including Hebrew domains only if they're configured
+library_domains = [get_domain_reg(settings.DOMAIN_MODULES["en"][LIBRARY_MODULE])]
+sheets_domains = [get_domain_reg(settings.DOMAIN_MODULES["en"][VOICES_MODULE])]
+
+if "he" in settings.DOMAIN_MODULES:
+    library_domains.append(get_domain_reg(settings.DOMAIN_MODULES["he"][LIBRARY_MODULE]))
+    sheets_domains.append(get_domain_reg(settings.DOMAIN_MODULES["he"][VOICES_MODULE]))
+
+library_domain = '|'.join(library_domains)
+sheets_domain = '|'.join(sheets_domains)
 host_patterns = patterns('',
     host(library_domain, 'sefaria.urls_library', name='library'),
     host(sheets_domain, 'sefaria.urls_sheets', name='sheets'),
