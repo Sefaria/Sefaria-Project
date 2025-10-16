8546030ec53fb3e5778b416f79c7c79da94ed69f
stevekaplan123 <steve@sefaria.org>
2025-10-16 14:48:47 +0300

    chore: iterate over domain modules

--- sefaria/hosts.py
+++ sefaria/hosts.py
@@ -7,13 +7,13 @@ import re
 def get_domain_reg(url):
     return re.escape(urlparse(url).netloc)
 
-# Build domain patterns, including Hebrew domains only if they're configured
-library_domains = [get_domain_reg(settings.DOMAIN_MODULES["en"][LIBRARY_MODULE])]
-sheets_domains = [get_domain_reg(settings.DOMAIN_MODULES["en"][VOICES_MODULE])]
+# Build domain patterns for all configured languages
+library_domains = []
+sheets_domains = []
 
-if "he" in settings.DOMAIN_MODULES:
-    library_domains.append(get_domain_reg(settings.DOMAIN_MODULES["he"][LIBRARY_MODULE]))
-    sheets_domains.append(get_domain_reg(settings.DOMAIN_MODULES["he"][VOICES_MODULE]))
+for lang in settings.DOMAIN_MODULES.keys():
+    library_domains.append(get_domain_reg(settings.DOMAIN_MODULES[lang][LIBRARY_MODULE]))
+    sheets_domains.append(get_domain_reg(settings.DOMAIN_MODULES[lang][VOICES_MODULE]))
 
 library_domain = '|'.join(library_domains)
 sheets_domain = '|'.join(sheets_domains)
