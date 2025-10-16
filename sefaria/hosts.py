1aff89a1f1bd8b93752e348af27ef861765c9f5a
stevekaplan123 <steve@sefaria.org>
2025-10-16 17:22:47 +0300

    Hosts.py Iteration over Domain Modules (#2726)
    * chore: only add he domains if there
    
    * chore: remove unwanted /sheets
    
    * chore: iterate over domain modules
    
    * chore: from django.conf import settings

--- sefaria/hosts.py
+++ sefaria/hosts.py
@@ -1,5 +1,5 @@
 from django_hosts import patterns, host
-from sefaria import settings    
+from django.conf import settings
 from sefaria.constants.model import LIBRARY_MODULE, VOICES_MODULE
 from urllib.parse import urlparse
 import re
@@ -7,8 +7,16 @@ import re
 def get_domain_reg(url):
     return re.escape(urlparse(url).netloc)
 
-library_domain = f'{get_domain_reg(settings.DOMAIN_MODULES["en"][LIBRARY_MODULE])}|{get_domain_reg(settings.DOMAIN_MODULES["he"][LIBRARY_MODULE])}'
-sheets_domain = f'{get_domain_reg(settings.DOMAIN_MODULES["en"][VOICES_MODULE])}|{get_domain_reg(settings.DOMAIN_MODULES["he"][VOICES_MODULE])}'
+# Build domain patterns for all configured languages
+library_domains = []
+sheets_domains = []
+
+for lang in settings.DOMAIN_MODULES.keys():
+    library_domains.append(get_domain_reg(settings.DOMAIN_MODULES[lang][LIBRARY_MODULE]))
+    sheets_domains.append(get_domain_reg(settings.DOMAIN_MODULES[lang][VOICES_MODULE]))
+
+library_domain = '|'.join(library_domains)
+sheets_domain = '|'.join(sheets_domains)
 host_patterns = patterns('',
     host(library_domain, 'sefaria.urls_library', name='library'),
     host(sheets_domain, 'sefaria.urls_sheets', name='sheets'),
