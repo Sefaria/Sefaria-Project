62eb76b1c5f95e2612e827f0e365e87dc0c2ee4b
YishaiGlasner <yishai@sefaria.org>
2025-10-21 12:22:32 +0300

    feat(calendar api): redirect to our seasonal-topic api rather than use hebcal.

--- reader/views.py
+++ reader/views.py
@@ -1671,24 +1671,8 @@ def find_holiday_in_hebcal_results(response):
 
 @catch_error_as_json
 def next_holiday(request):
-    from datetime import datetime
-    from dateutil.relativedelta import relativedelta
-    import requests
-    current_date = datetime.now().date()
-    date_in_three_months = current_date + relativedelta(months=+3)
+    return redirect(f'/_api/topics/seasonal-topic?lang={request.interfaceLang[:2]}')
 
-    # Format the date as YYYY-MM-DD
-    current_date = current_date.strftime('%Y-%m-%d')
-    date_in_three_months = date_in_three_months.strftime("%Y-%m-%d")
-    response = requests.get(f"https://www.hebcal.com/hebcal?v=1&cfg=json&maj=on&start={current_date}&end={date_in_three_months}")
-    if response.status_code == 200:
-        topic = find_holiday_in_hebcal_results(response)
-        if topic:
-            return jsonResponse(topic)
-        else:
-            return jsonResponse({"error": "Couldn't find any topics corresponding to HebCal results"})
-    else:
-        return jsonResponse({"error": "Couldn't establish connection with HebCal API"})
 @catch_error_as_json
 def table_of_contents_api(request):
     return jsonResponse(library.get_toc(), callback=request.GET.get("callback", None))
