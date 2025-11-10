6c16cf4cd866863a0ddc7530fe28999a128e1c03
nsantacruz <noahssantacruz@gmail.com>
2025-11-10 14:53:45 +0200

    test(linker): add tests to validate LinkerResolutionsDebug

--- sefaria/model/linker/tests/linker_test.py
+++ sefaria/model/linker/tests/linker_test.py
@@ -1,9 +1,11 @@
 from sefaria.model.linker.ref_part import RangedRawRefParts, SectionContext
 from sefaria.model.linker.referenceable_book_node import DiburHamatchilNodeSet, NumberedReferenceableBookNode
-from sefaria.model.linker.ref_resolver import ResolvedRef, ResolutionThoroughness, RefResolver, IbidHistory
+from sefaria.model.linker.ref_resolver import ResolvedRef, RefResolver, IbidHistory
+from sefaria.model.linker.linker import LinkedDoc
 from .linker_test_utils import *
 from sefaria.model import schema
 from sefaria.settings import ENABLE_LINKER
+from sefaria.model.marked_up_text_chunk import LinkerResolutionsDebug
 
 if not ENABLE_LINKER:
     pytest.skip("Linker not enabled", allow_module_level=True)
@@ -321,19 +323,7 @@ def test_multiple_ambiguities():
     [crrd(['@Rashi on Genesis', '#1', '#1', '#1'], lang='en'), ["Rashi on Genesis 1:1:1"]],
 ])
 def test_resolve_raw_ref(resolver_data, expected_trefs):
-    raw_ref, context_ref, lang, prev_trefs = resolver_data
-    linker = library.get_linker(lang)
-    ref_resolver = linker._ref_resolver
-    ref_resolver.reset_ibid_history()  # reset from previous test runs
-    if prev_trefs:
-        for prev_tref in prev_trefs:
-            if prev_tref is None:
-                ref_resolver.reset_ibid_history()
-            else:
-                ref_resolver._ibid_history.last_refs = Ref(prev_tref)
-    print_spans(raw_ref)
-    ref_resolver.set_thoroughness(ResolutionThoroughness.HIGH)
-    matches = ref_resolver.resolve_raw_ref(context_ref, raw_ref)
+    matches = get_matches_from_resolver_data(resolver_data)
     matched_orefs = sorted(reduce(lambda a, b: a + b, [[match.ref] if not match.is_ambiguous else [inner_match.ref for inner_match in match.resolved_raw_refs] for match in matches], []), key=lambda x: x.normal())
     if len(expected_trefs) != len(matched_orefs):
         print(f"Found {len(matched_orefs)} refs instead of {len(expected_trefs)}")
@@ -342,10 +332,7 @@ def test_resolve_raw_ref(resolver_data, expected_trefs):
     assert len(matched_orefs) == len(expected_trefs)
     for expected_tref, matched_oref in zip(sorted(expected_trefs, key=lambda x: x), matched_orefs):
         assert matched_oref == Ref(expected_tref)
-class TestResolveRawRef:
-
-    pass
-
+        
 
 @pytest.mark.parametrize(('context_tref', 'input_str', 'lang', 'expected_trefs', 'expected_pretty_texts', 'expected_part_strs_list'), [
     ["Berakhot 2a", 'It says in the Talmud, "Don\'t steal" which implies it\'s bad to steal.', 'en', tuple(), tuple(), tuple()],  # Don't match Talmud using Berakhot 2a as ibid context
@@ -512,3 +499,21 @@ def test_map_new_indices(crrd_params):
     assert norm_raw_ref.text == raw_ref.text
     for norm_part, part in zip(norm_raw_ref.raw_ref_parts, raw_ref.raw_ref_parts):
         assert norm_part.text == part.text
+    
+
+@pytest.mark.parametrize(('resolver_data',), [
+    [crrd(['@שמות', '#א', '#ב'])],  # not ambiguous
+    [crrd(["@ירושלמי", "@ברכות", "#יג ע״א"])],  # ambiguous
+])
+def test_linker_resolutions_debug(resolver_data):
+    from sefaria.helper.linker.tasks import _extract_debug_spans
+    from cerberus import Validator
+    
+    matches = get_matches_from_resolver_data(resolver_data)
+    doc = LinkedDoc("", matches, [], [])
+    cerberus_span_schema = {'spans': LinkerResolutionsDebug.attr_schemas['spans']}
+    v = Validator(cerberus_span_schema)
+    spans = _extract_debug_spans(doc)
+    if not v.validate({"spans": spans}):
+        raise Exception(v.errors)
+        
