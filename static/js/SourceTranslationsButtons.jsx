a8afbc215cb4ea83a4cbf9fdcf5eabc95e3d1b8a
schreibersefaria <danielschreiber@sefaria.org>
2025-11-16 16:14:17 +0200

    fix(SourceTranslationsButtons): returned react import

--- static/js/SourceTranslationsButtons.jsx
+++ static/js/SourceTranslationsButtons.jsx
@@ -1,4 +1,4 @@
-import {useContext} from "react";
+import React, {useContext} from "react";
 import PropTypes from "prop-types";
 import {ReaderPanelContext} from "./context";
 import RadioButton from "./common/RadioButton";
