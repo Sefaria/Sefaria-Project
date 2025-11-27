1336fdc867d669b898a16b5780826b23d567791a
Akiva Berger <akiva10b@gmail.com>
2025-11-27 09:27:58 +0200

    chore: change backdoor to run after gevent

--- sefaria/wsgi.py
+++ sefaria/wsgi.py
@@ -24,10 +24,14 @@ from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
 
 if os.getenv("DEBUG_BACKDOOR") == "1" or os.getenv("DEBUG") == "true":
-    from gevent.backdoor import BackdoorServer
-    port = int(os.getenv("DEBUG_BACKDOOR_PORT", "3001"))
-    BackdoorServer(("0.0.0.0", port)).start()
+    import threading
 
+    def start_backdoor():
+        from gevent.backdoor import BackdoorServer
+        port = int(os.getenv("DEBUG_BACKDOOR_PORT", "3001"))
+        BackdoorServer(("0.0.0.0", port)).start()
+
+    threading.Thread(target=start_backdoor, daemon=True).start()
 
 # Apply WSGI middleware here.
 # from helloworld.wsgi import HelloWorldApplication
