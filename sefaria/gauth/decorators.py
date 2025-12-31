ff171b7464edb6be8cc21584f0a127175b760299
stevekaplan123 <steve@sefaria.org>
2025-12-31 12:12:44 +0200

    chore: restore space

--- sefaria/gauth/decorators.py
+++ sefaria/gauth/decorators.py
@@ -28,6 +28,7 @@ def gauth_required(scope, ajax=False):
             # Try grabbing credential from storage
             profile = UserProfile(user_obj=request.user)
             credentials_dict = profile.gauth_token
+            
             if credentials_dict is None or not set(scope).issubset(set(credentials_dict['scopes'])):
                 # If no credentials or scopes don't match, redirect to authentication
                 request.session['next_view'] = request.path
