76486769bb1eaa33e3323f230ce992283affad28
stevekaplan123 <steve@sefaria.org>
2025-12-31 12:51:54 +0200

    chore: add getNoteURL

--- static/js/NoteListing.jsx
+++ static/js/NoteListing.jsx
@@ -32,17 +32,32 @@ class NoteListing extends Component {
     const resolve = this.props.onDeleteNote || (()=>{});
     Sefaria.deleteNote(this.props.data._id).then(resolve);
   }
+  getNoteURL(ref) {
+    // This helper function returns the URL of the note for the given ref.  When the URL is to a ref in the library, 
+    // this is very simple, but when the ref is to a sheet, we need to modify the ref and change the link so that it sends the user to the voices module.
+    // The ref will be something like "Sheet 3.4" but it needs to link to "/sheets/3.4" in the voices module.
+    const isSheet = Sefaria.isSheetRef(ref);
+    if (isSheet) {
+      moduleURL = Sefaria.getModuleURL(Sefaria.VOICES_MODULE);
+      const parsedRef = Sefaria.parseRef(ref);
+      path = `/sheets/${parsedRef.sections.join('/')}`;
+    }
+    else {
+      module = Sefaria.getModuleURL(Sefaria.LIBRARY_MODULE);
+      path = `/${ref}`;
+    }
+    const noteURL = new URL(path, moduleURL);
+    noteURL.searchParams.set('with', 'Notes');  
+    return noteURL.toString();
+  }
   render() {
+    const noteURL = this.getNoteURL(this.props.data.ref);
     var data = this.props.data;
-    const module = Sefaria.isSheetRef(data.ref) ? Sefaria.VOICES_MODULE : Sefaria.LIBRARY_MODULE;
-    const path = "/" + Sefaria.normRef(data.ref) + "?with=Notes";
-    const url = Sefaria.getModuleURL(module).origin + path;
-
     return (<div className="noteListing">
               <div className="actionButtons">
                 <img src="/static/icons/circled-x.svg" onClick={this.deleteNote} alt={Sefaria._("Delete note")} />
               </div>
-              <a className="noteRefTitle" href={url}>
+              <a className="noteRefTitle" href={noteURL}>
                 <span>{data.ref}</span>
               </a>
               <span className="noteText"><Note text={data.text}/></span>
