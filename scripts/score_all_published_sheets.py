715bcbf331dc5387ede277d71bf0155d4ac62de1
YishaiGlasner <yishai@sefaria.org>
2026-01-07 10:32:28 +0200

    feat(sheet scoring): script for ranking all sheets.

---
+++ scripts/score_all_published_sheets.py
@@ -0,0 +1,19 @@
+import django
+django.setup()
+from tqdm import tqdm
+from sefaria.helper.llm.tasks.sheet_scoring import generate_and_save_sheet_scoring
+from sefaria.system.database import db
+
+
+def rank_all_sheets(rerank=False):
+    query = {'status': 'public'}
+    if not rerank:
+        query['llm_scoring'] = {'$exists': False}
+    cursor = db.sheets.find(query)
+    total = cursor.count()
+    for sheet in tqdm(cursor, total=total, desc="Scoring sheets"):
+        generate_and_save_sheet_scoring(sheet)
+
+
+if __name__ == '__main__':
+    rank_all_sheets()
