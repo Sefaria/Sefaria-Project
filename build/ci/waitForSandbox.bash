5252037b2d13b57a7552a057faf6cf725d627217
stevekaplan123 <steve@sefaria.org>
2026-01-20 14:26:08 +0200

    chore: handle time out

--- build/ci/waitForSandbox.bash
+++ build/ci/waitForSandbox.bash
@@ -6,6 +6,9 @@ HEALTH_URL="https://sandbox-${GIT_COMMIT}.cauldron.sefaria.org/health-check"
 
 echo "Waiting for ${HEALTH_URL} to load for $waitDuration seconds"
 
-timeout $waitDuration bash -c 'while [[ "$(curl -s -k -o /dev/null -w ''%{http_code}'' -L '"${HEALTH_URL}"')" != "200" ]]; do sleep 5; done'
-
-echo "Reached server. Proceeding."
+if timeout $waitDuration bash -c 'while [[ "$(curl -s -k -o /dev/null -w ''%{http_code}'' -L '"${HEALTH_URL}"')" != "200" ]]; do sleep 5; done'; then
+  echo "Reached server. Proceeding."
+else
+  echo "Timed out waiting for ${HEALTH_URL}."
+  exit 1
+fi
