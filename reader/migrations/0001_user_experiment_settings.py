3cce1592cd8282e4d410ba134298314a5de2ed35
Akiva Berger <akiva10b@gmail.com>
2026-02-01 13:14:05 +0200

    chore: fix migrations

--- reader/migrations/0001_user_experiment_settings.py
+++ reader/migrations/0001_user_experiment_settings.py
@@ -1,5 +1,9 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
 from django.conf import settings
 from django.db import migrations, models
+import django.db.models.deletion
 
 
 class Migration(migrations.Migration):
@@ -15,7 +19,7 @@ class Migration(migrations.Migration):
             fields=[
                 ("id", models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name="ID")),
                 ("experiments", models.BooleanField(default=False)),
-                ("user", models.OneToOneField(on_delete=models.deletion.CASCADE, related_name="experiment_settings", to=settings.AUTH_USER_MODEL)),
+                ("user", models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, related_name="experiment_settings", to=settings.AUTH_USER_MODEL)),
             ],
         ),
     ]
