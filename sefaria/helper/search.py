ddea81f9e4bd36f49b967de957f0c271d11b4494
nsantacruz <noahssantacruz@gmail.com>
2026-06-03 16:45:45 +0300

    fix: fix search filters with agg_key is None which is supposed to default to "path"

--- sefaria/helper/search.py
+++ sefaria/helper/search.py
@@ -138,7 +138,7 @@ def get_filter_obj(type, filters, filter_fields):
 
 
 def make_filter(type, agg_type, agg_key):
-    if type == "text" and agg_type == "path":
+    if type == "text" and agg_type in (None, "path"):
         # filters with '/' might be leading to books. also, very unlikely they'll match an false positives
         agg_key = agg_key.rstrip('/')
         agg_key = re.escape(agg_key)
