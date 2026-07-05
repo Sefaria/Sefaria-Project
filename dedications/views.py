aae58d9e3aac8a2fa0e009fbefbace9fb4729821
stevekaplan123 <steve@sefaria.org>
2026-07-05 10:54:06 +0300

    chore: add renderstatic

--- dedications/views.py
+++ dedications/views.py
@@ -18,5 +18,5 @@ def dedication(request, slug):
         request,
         "static/dedication/dedication.html",
         {"headerMode": True},
-        {"dedication": dedication},
+        {"renderStatic": True, "dedication": dedication},
     )
