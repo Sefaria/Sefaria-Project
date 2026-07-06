2e19a48e1e7173c49b638bbaeaa92c5c139b8b4d
saengel <sarah@sefaria.org>
2026-07-06 11:55:12 +0300

    fix: add back django.contrib.postgres

--- sefaria/settings.py
+++ sefaria/settings.py
@@ -176,6 +176,7 @@ INSTALLED_APPS = (
     'django_hosts',
     'pgvector.django',
     'semantic_search',
+    'django.contrib.postgres'
 )
 
 DATABASE_ROUTERS = ['semantic_search.router.SemanticSearchRouter']
