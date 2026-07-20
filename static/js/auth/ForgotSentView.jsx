664b069864a62affd5b0661d206222e21811e358
YishaiGlasner <yishai@sefaria.org>
2026-07-20 11:31:32 +0300

    fix(sso client): remove button from forgot sent view.

--- static/js/auth/ForgotSentView.jsx
+++ static/js/auth/ForgotSentView.jsx
@@ -7,11 +7,6 @@ import Button from '../common/Button.jsx';
 const ForgotSentView = ({ onSignIn }) => (
   <AuthCard heading={<InterfaceText>Reset Link Sent</InterfaceText>}
     sub={<InterfaceText>Check your email and follow the instructions to reset your password.</InterfaceText>}>
-    <div className="sefaria-auth-stack">
-      <Button variant="sefaria-common-button auth-primary" size="fullwidth" onClick={onSignIn}>
-        <InterfaceText>Log In</InterfaceText>
-      </Button>
-    </div>
   </AuthCard>
 );
 
