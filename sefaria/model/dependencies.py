27e1fcf22756f516bfb3db078c299d5c0e4b1128
yonadavGit <92536571+yonadavGit@users.noreply.github.com>
2026-09-08 17:00:02 +0300

    Move linker chunk cascades onto models

--- sefaria/model/dependencies.py
+++ sefaria/model/dependencies.py
@@ -170,8 +170,8 @@ def process_version_delete_in_global_notifications(ver, **kwargs):
 
 # Version Title Change
 subscribe(history.process_version_title_change_in_history,                         text.Version, "attributeChange", "versionTitle")
-subscribe(marked_up_text_chunk.process_version_title_change_in_marked_up_text_chunks, text.Version, "attributeChange", "versionTitle")
-subscribe(marked_up_text_chunk.process_version_title_change_in_linker_output,       text.Version, "attributeChange", "versionTitle")
+subscribe(marked_up_text_chunk.MarkedUpTextChunk.process_version_title_change,     text.Version, "attributeChange", "versionTitle")
+subscribe(marked_up_text_chunk.LinkerOutput.process_version_title_change,          text.Version, "attributeChange", "versionTitle")
 subscribe(process_version_title_change_in_search,                                  text.Version, "attributeChange", "versionTitle")
 subscribe(process_version_title_change_in_global_notifications,                    text.Version, "attributeChange", "versionTitle")
 
